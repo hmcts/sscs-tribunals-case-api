8f5eb60536237bdf0ef3e057c47d84eec9fa7cea
naveen manga <naveen.manga@transformuk.com>
2017-09-19 11:28:08 +0100

    SSCS-1830 Check style changes

--- src/main/java/uk/gov/hmcts/sscs/service/AuthClient.java
+++ src/main/java/uk/gov/hmcts/sscs/service/AuthClient.java
@@ -1,5 +1,9 @@
 package uk.gov.hmcts.sscs.service;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,38 +14,42 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
-import static org.slf4j.LoggerFactory.getLogger;
-
 @Service
 public class AuthClient {
-	private static final Logger LOG = getLogger(AuthClient.class);
+    private static final Logger LOG = getLogger(AuthClient.class);
 
-	private String vaultSecret;
-	private OtpGenerator otpGenerator;
-	private String authApiUrl;
-	private RestTemplate restTemplate;
+    private String vaultSecret;
+    private OtpGenerator otpGenerator;
+    private String authApiUrl;
+    private RestTemplate restTemplate;
 
-	@Autowired
-	AuthClient(@Value("${auth.micro.service.key}") String vaultSecret, @Value("${auth.provider.service.api}") String authApiUrl,
-			   OtpGenerator otpGenerator, RestTemplate restTemplate) {
-		this.vaultSecret = vaultSecret;
-		this.otpGenerator = otpGenerator;
-		this.authApiUrl = authApiUrl;
-		this.restTemplate = restTemplate;
-	}
+    @Autowired
+    AuthClient(@Value("${auth.micro.service.key}") String vaultSecret,
+               @Value("${auth.provider.service.api}") String authApiUrl,
+               OtpGenerator otpGenerator, RestTemplate restTemplate) {
+        this.vaultSecret = vaultSecret;
+        this.otpGenerator = otpGenerator;
+        this.authApiUrl = authApiUrl;
+        this.restTemplate = restTemplate;
+    }
 
-	public String sendRequest(String path, HttpMethod httpMethod, String requestBody) {
-		ResponseEntity<String> responseEntity = new ResponseEntity<>(HttpStatus.OK);
-		try {
-			String otp = otpGenerator.issueOneTimePassword(vaultSecret);
-			String authUrl = authApiUrl + path + "?microservice=sscs&oneTimePassword=" + otp;
-			responseEntity = restTemplate.exchange(authUrl, httpMethod, new HttpEntity(requestBody), String.class);
-		} catch (InvalidKeyException | NoSuchAlgorithmException e) {
-			LOG.error("Error in auth client: ", e);
-		}
-		return responseEntity.getBody();
-	}
+    /**
+     * Sends the request to auth api.
+     * @param path path of the api
+     * @param httpMethod http method to be invoked on auth api
+     * @param requestBody request body
+     * @return Response string
+     */
+    public String sendRequest(String path, HttpMethod httpMethod, String requestBody) {
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(HttpStatus.OK);
+        try {
+            String otp = otpGenerator.issueOneTimePassword(vaultSecret);
+            String authUrl = authApiUrl + path + "?microservice=sscs&oneTimePassword=" + otp;
+            HttpEntity requestEntity = new HttpEntity(requestBody);
+            responseEntity = restTemplate.exchange(authUrl,httpMethod,requestEntity,String.class);
+        } catch (InvalidKeyException | NoSuchAlgorithmException e) {
+            LOG.error("Error in auth client: ", e);
+        }
+        return responseEntity.getBody();
+    }
 }
--- src/main/java/uk/gov/hmcts/sscs/service/CoreCaseDataClient.java
+++ src/main/java/uk/gov/hmcts/sscs/service/CoreCaseDataClient.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.sscs.service;
 
-import org.apache.catalina.connector.Response;
+import static org.slf4j.LoggerFactory.getLogger;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,33 +13,47 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
 public class CoreCaseDataClient {
-	private static final Logger LOG = getLogger(CoreCaseDataClient.class);
+    private static final Logger LOG = getLogger(CoreCaseDataClient.class);
 
-	private String ccdApiUrl;
-	private RestTemplate restTemplate;
+    private String ccdApiUrl;
+    private RestTemplate restTemplate;
 
-	@Autowired
-	CoreCaseDataClient(@Value("${ccd.service.api}") String ccdApiUrl, RestTemplate restTemplate) {
-		this.ccdApiUrl = ccdApiUrl;
-		this.restTemplate = restTemplate;
-	}
+    @Autowired
+    CoreCaseDataClient(@Value("${ccd.service.api}") String ccdApiUrl, RestTemplate restTemplate) {
+        this.ccdApiUrl = ccdApiUrl;
+        this.restTemplate = restTemplate;
+    }
 
-	public ResponseEntity<String> sendRequest(String userToken, String serviceToken, String path, HttpMethod httpMethod, String requestBody) {
-		try {
-			MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-			headers.add("Authorization", "Bearer " + userToken);
-			headers.add("ServiceAuthorization", serviceToken);
-			headers.add("Content-Type", "application/json");
-			HttpEntity requestEntity = new HttpEntity(requestBody, headers);
-			ResponseEntity<String> responseEntity = restTemplate.exchange(ccdApiUrl + path, httpMethod, requestEntity, String.class);
-			return responseEntity;
-		} catch (Exception ex) {
-			LOG.error("Error while sending request to CCD api: ", ex);
-			return null;
-		}
-	}
+    /**
+     * Sends request to core case data api.
+     * @param userToken User token to send in request headers to core case data api
+     * @param serviceToken Service token to send in request header to core case data api
+     * @param path request path
+     * @param httpMethod request method
+     * @param requestBody request body
+     * @return Response string
+     */
+    public ResponseEntity<String> sendRequest(String userToken,
+                                      String serviceToken,
+                                      String path,
+                                      HttpMethod httpMethod,
+                                      String requestBody) {
+        try {
+            MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+            headers.add("Authorization", "Bearer " + userToken);
+            headers.add("ServiceAuthorization", serviceToken);
+            headers.add("Content-Type", "application/json");
+            HttpEntity requestEntity = new HttpEntity(requestBody, headers);
+            String url = ccdApiUrl + path;
+            ResponseEntity<String> responseEntity =
+                restTemplate.exchange(url,httpMethod,requestEntity,String.class);
+            return responseEntity;
+        } catch (Exception ex) {
+            LOG.error("Error while sending request to CCD api: ", ex);
+            return null;
+        }
+    }
 }
