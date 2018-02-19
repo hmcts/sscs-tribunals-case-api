4f46fc5192b5c3789d95b62aca573b520ac22821
Santhosh Avirneni <savirneni@Santhoshs-MBP.connect>
2018-02-19 16:43:14 +0000

    SSCS-2589:appeal number check against CCD

--- src/main/java/uk/gov/hmcts/sscs/service/AppealNumberGenerator.java
+++ src/main/java/uk/gov/hmcts/sscs/service/AppealNumberGenerator.java
@@ -3,7 +3,11 @@ package uk.gov.hmcts.sscs.service;
 import java.security.SecureRandom;
 import org.apache.commons.text.CharacterPredicates;
 import org.apache.commons.text.RandomStringGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import uk.gov.hmcts.sscs.domain.corecase.CcdCase;
+import uk.gov.hmcts.sscs.exception.CcdException;
 
 @Component
 public class AppealNumberGenerator {
@@ -12,14 +16,35 @@ public class AppealNumberGenerator {
     public static final char MINIMUM_CODE_POINT = '0';
     public static final char MAXIMUM_CODE_POINT = 'z';
 
-    public String generate() {
+    private CcdService ccdService;
+
+    @Autowired
+    public AppealNumberGenerator(CcdService ccdService) {
+        this.ccdService = ccdService;
+    }
+
+    public String generate() throws CcdException {
+
+        String appealNumber = generateAppealNumber();
+
+        CcdCase ccdCase = ccdService.findCcdCaseByAppealNumber(appealNumber);
+        boolean isDuplicateInCcd = (ccdCase != null && ccdCase.getAppeal() != null) ? true : false;
+        if (!isDuplicateInCcd) {
+            return appealNumber;
+        } else {
+            throw new CcdException("AppealNumberGenerator has generated duplicate appeal number against CCD");
+        }
+    }
+
+    protected String generateAppealNumber() {
+
         SecureRandom random = new SecureRandom();
         RandomStringGenerator generator = new RandomStringGenerator.Builder()
                 .withinRange(MINIMUM_CODE_POINT, MAXIMUM_CODE_POINT)
-                .filteredBy(CharacterPredicates.DIGITS,CharacterPredicates.LETTERS)
-                .usingRandom(random::nextInt)
+                .filteredBy(CharacterPredicates.DIGITS, CharacterPredicates.LETTERS).usingRandom(random::nextInt)
                 .build();
-        return generator.generate(LENGTH);
+        String appealNumber = generator.generate(LENGTH);
+        return appealNumber;
     }
 
 }
---
+++ src/test/java/uk/gov/hmcts/sscs/service/AppealNumberGeneratorTest.java
@@ -0,0 +1,55 @@
+package uk.gov.hmcts.sscs.service;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import uk.gov.hmcts.sscs.domain.corecase.Appeal;
+import uk.gov.hmcts.sscs.domain.corecase.CcdCase;
+import uk.gov.hmcts.sscs.exception.CcdException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AppealNumberGeneratorTest {
+
+    @Mock
+    private CcdService ccdService;
+
+    private AppealNumberGenerator appealNumberGenerator;
+
+    @Before
+    public void setup() {
+        appealNumberGenerator = new AppealNumberGenerator(ccdService);
+    }
+
+    @Test
+    public void shouldGenerateAppealNumber() throws CcdException {
+
+        given(ccdService.findCcdCaseByAppealNumber(anyString())).willReturn(null);
+
+        String appealNumber = appealNumberGenerator.generate();
+
+        verify(ccdService).findCcdCaseByAppealNumber(anyString());
+
+        assertNotNull(appealNumber);
+    }
+
+    @Test(expected = CcdException.class)
+    public void shouldHandleException() throws Exception {
+
+        Appeal appeal = new Appeal();
+        appeal.setAppealNumber("abcd1efgh2");
+        CcdCase ccdCase = new CcdCase();
+        ccdCase.setAppeal(appeal);
+
+        given(ccdService.findCcdCaseByAppealNumber(anyString())).willReturn(ccdCase);
+
+        appealNumberGenerator.generate();
+    }
+}
