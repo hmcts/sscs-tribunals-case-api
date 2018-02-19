502be5b670b5c1c0fb57388e20d2f594a2ec8156
Bart Labno <bart.labno@hmcts.net>
2018-02-19 11:04:51 +0000

    Variables per env, vault sensitive data

--- infrastructure/aat.tfvars
+++ infrastructure/aat.tfvars
@@ -1,2 +1,1 @@
 infrastructure_env = "test"
-authprovider_service_api_url = 
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,10 +1,4 @@
 provider "vault" {
-  //  # It is strongly recommended to configure this provider through the
-  //  # environment variables described above, so that each user can have
-  //  # separate credentials set in the environment.
-  //  #
-  //  # This will default to using $VAULT_ADDR
-  //  # But can be set explicitly
   address = "https://vault.reform.hmcts.net:6200"
 }
 
@@ -16,12 +10,52 @@ data "vault_generic_secret" "email_mac_secret" {
   path = "secret/${var.infrastructure_env}/sscs/sscs_email_mac_secret_text"
 }
 
-data "vault_generic_secret" "sscs_rpc_email_id" {
-  path = "secret/${var.infrastructure_env}/sscs/secret/${var.infrastructure_env}/sscs/sscs_rpc_email_id"
-}
-
 data "vault_generic_secret" "sscs_tribunals_case_secret" {
   path = "secret/${var.infrastructure_env}/ccidam/service-auth-provider/api/microservice-keys/sscs-tribunals-case"
+}
+
+data "vault_generic_secret" "idam_api" {
+  path = "secret/${var.infrastructure_env}/sscs/idam_api"
+}
+
+data "vault_generic_secret" "idam_s2s_api" {
+  path = "secret/${var.infrastructure_env}/sscs/idam_s2s_api"
+}
+
+data "vault_generic_secret" "idam_uid" {
+  path = "secret/${var.infrastructure_env}/sscs/idam_uid"
+}
+
+data "vault_generic_secret" "idam_key_sscs" {
+  path = "secret/${var.infrastructure_env}/sscs/idam_key_sscs"
+}
+
+data "vault_generic_secret" "idam_role" {
+  path = "secret/${var.infrastructure_env}/sscs/idam_role"
+}
+
+data "vault_generic_secret" "ccd_api" {
+  path = "secret/${var.infrastructure_env}/sscs/ccd_api"
+}
+
+data "vault_generic_secret" "ccd_case_worker_id" {
+  path = "secret/${var.infrastructure_env}/sscs/ccd_case_worker_id"
+}
+
+data "vault_generic_secret" "appeal_email_from" {
+  path = "secret/${var.infrastructure_env}/sscs/appeal_email_from"
+}
+
+data "vault_generic_secret" "appeal_email_to" {
+  path = "secret/${var.infrastructure_env}/sscs/appeal_email_to"
+}
+
+data "vault_generic_secret" "smtp_host" {
+  path = "secret/${var.infrastructure_env}/sscs/smtp_host"
+}
+
+data "vault_generic_secret" "appeal_email_host_port" {
+  path = "secret/${var.infrastructure_env}/sscs/smtp_port"
 }
 
 
@@ -39,27 +73,27 @@ module "tribunals-case-api" {
 
   app_settings = {
     AUTH_PROVIDER_SERVICE_CLIENT_KEY="${data.vault_generic_secret.sscs_tribunals_case_secret.data["value"]}"
-    AUTH_PROVIDER_SERVICE_API_URL="${var.authprovider_service_api_url}"
+    AUTH_PROVIDER_SERVICE_API_URL="${data.vault_generic_secret.idam_s2s_api.data["value"]}"
 
-    IDAM_API_URL="${var.idam_api_url}"
-    IDAM_USER_ID="${var.idam_user_id}"
-    IDAM_ROLE="${var.idam_role}"
+    IDAM_API_URL="${data.vault_generic_secret.idam_api.data["value"]}"
+    IDAM_USER_ID="${data.vault_generic_secret.idam_uid.data["value"]}"
+    IDAM_ROLE="${data.vault_generic_secret.idam_role.data["value"]}"
 
-    CCD_CASE_WORKER_ID="${var.ccd_case_worker_id}"
+    CCD_CASE_WORKER_ID="${data.vault_generic_secret.ccd_case_worker_id.data["value"]}"
     CCD_SERVICE_API_URL="http://ccd-data-store-api-${var.env}.service.${local.aseName}.internal"
 
-    EMAIL_FROM="${var.appeal_email_from}"
-    EMAIL_TO="${data.vault_generic_secret.sscs_rpc_email_id.data["value"]}"
+    EMAIL_FROM="${data.vault_generic_secret.appeal_email_from.data["value"]}"
+    EMAIL_TO="${data.vault_generic_secret.appeal_email_to.data["value"]}"
     EMAIL_SUBJECT="${var.appeal_email_subject}"
     EMAIL_MESSAGE="${var.appeal_email_message}"
-    EMAIL_SERVER_HOST="${var.appeal_email_host}"
-    EMAIL_SERVER_PORT="${var.appeal_email_port}"
+    EMAIL_SERVER_HOST="${data.vault_generic_secret.smtp_host.data["value"]}"
+    EMAIL_SERVER_PORT="${data.vault_generic_secret.smtp_port.data["value"]}"
     EMAIL_SMTP_TLS_ENABLED="${var.appeal_email_smtp_tls_enabled}"
     EMAIL_SMTP_SSL_TRUST="${var.appeal_email_smtp_ssl_trust}"
 
     IDAM_S2S_AUTH_TOTP_SECRET="${data.vault_generic_secret.cmc_s2s_secret.data["value"]}"
     IDAM_S2S_AUTH_MICROSERVICE="${var.idam_s2s_auth_microservice}"
-    IDAM_S2S_AUTH_URL="${var.idam_s2s_auth_url}"
+    IDAM_S2S_AUTH_URL="${data.vault_generic_secret.idam_s2s_api.data["value"]}"
 
     PDF_API_URL="http://cmc-pdf-service-${var.env}.service.${local.aseName}.internal"
 
