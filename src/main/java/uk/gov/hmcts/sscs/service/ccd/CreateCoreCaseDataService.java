e3b3835fbd5c5a19f439aafe9d8b01f46fcc578b
Santhosh Avirneni <savirneni@Santhoshs-MacBook-Pro.local>
2018-03-13 11:03:47 +0000

    SSCS-2585[SA]: subtask 2854 added functionality for create case

---
+++ src/main/java/uk/gov/hmcts/sscs/service/ccd/CreateCoreCaseDataService.java
@@ -0,0 +1,62 @@
+package uk.gov.hmcts.sscs.service.ccd;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.EventRequestData;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.sscs.model.ccd.CaseData;
+
+@Service
+@Slf4j
+public class CreateCoreCaseDataService {
+
+    private final CoreCaseDataService coreCaseDataService;
+
+    @Autowired
+    public CreateCoreCaseDataService(CoreCaseDataService coreCaseDataService) {
+        this.coreCaseDataService = coreCaseDataService;
+    }
+
+    public CaseDetails createCcdCase(CaseData caseData) {
+        EventRequestData eventRequestData = coreCaseDataService.getEventRequestData("appealCreated");
+        log.info("*** tribunals-service *** eventRequestData: {}", eventRequestData);
+        String serviceAuthorization = coreCaseDataService.generateServiceAuthorization();
+        log.info("*** tribunals-service *** s2s token: {}", serviceAuthorization);
+        StartEventResponse startEventResponse = start(eventRequestData, serviceAuthorization);
+        log.info("*** tribunals-service *** startEventResponse: {}", startEventResponse);
+        return create(eventRequestData, serviceAuthorization, coreCaseDataService.getCaseDataContent(caseData,
+            startEventResponse, "SSCS - appeal created event", "Created SSCS"));
+    }
+
+    private StartEventResponse start(EventRequestData eventRequestData, String serviceAuthorization) {
+        log.info("*** tribunals-service *** Calling CCD (url: {}) endpoint to start Case For Caseworker...",
+            coreCaseDataService.getCcdUrl());
+        return coreCaseDataService.getCoreCaseDataApi().startForCaseworker(
+            eventRequestData.getUserToken(),
+            serviceAuthorization,
+            eventRequestData.getUserId(),
+            eventRequestData.getJurisdictionId(),
+            eventRequestData.getCaseTypeId(),
+            eventRequestData.getEventId());
+    }
+
+    private CaseDetails create(EventRequestData eventRequestData, String serviceAuthorization,
+                               CaseDataContent caseDataContent) {
+        log.info("*** tribunals-service *** Calling CCD endpoint to save CaseDetails For CaseWorker...");
+        return coreCaseDataService.getCoreCaseDataApi().submitForCaseworker(
+            eventRequestData.getUserToken(),
+            serviceAuthorization,
+            eventRequestData.getUserId(),
+            eventRequestData.getJurisdictionId(),
+            eventRequestData.getCaseTypeId(),
+            eventRequestData.isIgnoreWarning(),
+            caseDataContent
+        );
+    }
+
+}
