1b9e487e7f433a7ea251e76f44429dc2f7fc7e1e
Santhosh Avirneni <savirneni@Santhoshs-MacBook-Pro.local>
2018-03-13 15:10:01 +0000

    SSCS-2810[SA]:add e2e test for createCase

--- src/e2e/java/ccd/CreateCaseInCcd.java
+++ src/e2e/java/ccd/CreateCaseInCcd.java
@@ -9,18 +9,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.sscs.service.ccd.CaseDataUtils;
+import uk.gov.hmcts.sscs.service.ccd.CreateCoreCaseDataService;
 import uk.gov.hmcts.sscs.service.ccd.ReadCoreCaseDataService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ReadCaseFromCcd {
+public class CreateCaseInCcd {
 
     @Autowired
-    private ReadCoreCaseDataService readCoreCaseDataService;
+    private CreateCoreCaseDataService createCoreCaseDataService;
 
     @Test
-    public void givenACaseId_shouldRetrieveCaseDetails() {
-        CaseDetails caseDetails = readCoreCaseDataService.getCcdCaseDetails("1520116198612015");
+    public void givenACase_shouldBeSavedIntoCcd() {
+        CaseDetails caseDetails = createCoreCaseDataService.createCcdCase(CaseDataUtils.buildCaseData());
         assertNotNull(caseDetails);
     }
 
