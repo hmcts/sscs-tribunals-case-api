fc20a7d04aab23363324cc9269a774569521dd42
Santhosh Avirneni <savirneni@Santhoshs-MacBook-Pro.local>
2018-04-25 09:48:07 +0100

    SSCS-2501[SA]:update e2e test for sya

--- src/e2e/java/uk/gov/hmcts/sscs/ccd/CreateCaseInCcdTest.java
+++ src/e2e/java/uk/gov/hmcts/sscs/ccd/CreateCaseInCcdTest.java
@@ -1,17 +1,24 @@
 package uk.gov.hmcts.sscs.ccd;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static uk.gov.hmcts.sscs.util.SyaJsonMessageSerializer.ALL_DETAILS;
+
+import java.net.ConnectException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.ResourceAccessException;
 
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.sscs.domain.wrapper.SyaCaseWrapper;
+import uk.gov.hmcts.sscs.exception.CcdException;
+import uk.gov.hmcts.sscs.exception.EmailSendFailedException;
+import uk.gov.hmcts.sscs.exception.PdfGenerationException;
 import uk.gov.hmcts.sscs.model.ccd.CaseData;
 import uk.gov.hmcts.sscs.service.CcdService;
 import uk.gov.hmcts.sscs.service.SubmitAppealService;
@@ -46,8 +53,10 @@ public class CreateCaseInCcdTest {
     public void givenASyaCaseShouldBeSavedIntoCcdViaSubmitAppealService() {
         try {
             submitAppealService.submitAppeal(ALL_DETAILS.getDeserializeMessage());
-        } catch (Exception ex) {
+        } catch (EmailSendFailedException | PdfGenerationException ep) {
             assertTrue(true);
+        } catch (Exception ex) {
+            assertFalse(true);
         }
 
     }
