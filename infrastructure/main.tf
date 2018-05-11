a84708ebbf35b1dbcbc27f01298ac7dad72542e3
istredd <labno.b@gmail.com>
2018-05-11 08:11:14 +0100

    S2S Migration for nonproduction envs to CNP

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -79,12 +79,12 @@ locals {
   aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   app_full_name = "${var.product}-${var.component}"
 
-  localCcdApi = "http://ccd-data-store-api-${var.env}.service.${local.aseName}.internal"
-  CcdApi = "${var.env == "preview" ? "http://ccd-data-store-api-aat.service.core-compute-aat.internal" : local.localCcdApi}"
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+  local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.aseName}"
 
-  localPdfService = "http://cmc-pdf-service-${var.env}.service.${local.aseName}.internal"
-  pdfService = "${var.env == "preview" ? "http://cmc-pdf-service-aat.service.core-compute-aat.internal" : local.localPdfService}"
-
+  CcdApi = "http://ccd-data-store-api-${local.local_env}.service.${local.local_ase}.internal"
+  s2sCnpUrl = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
+  pdfService = "http://cmc-pdf-service-${local.local_env}.service.${local.local_ase}.internal"
 }
 
 
@@ -119,7 +119,7 @@ module "tribunals-case-api" {
 
     IDAM_S2S_AUTH_TOTP_SECRET="${data.vault_generic_secret.cmc_s2s_secret.data["value"]}"
     IDAM_S2S_AUTH_MICROSERVICE="${var.idam_s2s_auth_microservice}"
-    IDAM_S2S_AUTH_URL="${data.vault_generic_secret.idam_s2s_api.data["value"]}"
+    IDAM_S2S_AUTH_URL="${var.env == "prod" ? data.vault_generic_secret.idam_s2s_api.data["value"] : local.s2sCnpUrl}"
 
     PDF_API_URL="${local.pdfService}"
 
@@ -134,7 +134,7 @@ module "tribunals-case-api" {
     IDAM_URL = "${data.vault_generic_secret.idam_api.data["value"]}"
 
     IDAM.S2S-AUTH.TOTP_SECRET ="${data.vault_generic_secret.sscs_s2s_secret.data["value"]}"
-    IDAM.S2S-AUTH = "${data.vault_generic_secret.idam_s2s_api.data["value"]}"
+    IDAM.S2S-AUTH = "${var.env == "prod" ? data.vault_generic_secret.idam_s2s_api.data["value"] : local.s2sCnpUrl}"
     IDAM.S2S-AUTH.MICROSERVICE = "${var.ccd_idam_s2s_auth_microservice}"
 
     IDAM_SSCS_SYSTEMUPDATE_USER = "${data.vault_generic_secret.idam_sscs_systemupdate_user.data["value"]}"
