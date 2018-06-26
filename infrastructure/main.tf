bd7c0ff841b1c0f5cec0db36e22e1fa99ace9e5f
Jamie Burns <fantasticjamieburns@gmail.com>
2018-06-26 16:14:27 +0100

    Using consistent vault path for idam credentials between aat and saat

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -69,11 +69,11 @@ data "vault_generic_secret" "sscs_s2s_secret" {
 }
 
 data "vault_generic_secret" "idam_sscs_systemupdate_user" {
-  path = "secret/${var.infrastructure_env}/sscs/sidamuser"
+  path = "secret/${var.infrastructure_env}/ccidam/idam-api/sscs/systemupdate/user"
 }
 
 data "vault_generic_secret" "idam_sscs_systemupdate_password" {
-  path = "secret/${var.infrastructure_env}/sscs/sidampassword"
+  path = "secret/${var.infrastructure_env}/ccidam/idam-api/sscs/systemupdate/password"
 }
 
 data "vault_generic_secret" "idam_oauth2_client_secret" {
