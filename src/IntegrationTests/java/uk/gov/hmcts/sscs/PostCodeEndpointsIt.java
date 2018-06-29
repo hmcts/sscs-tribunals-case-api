48f9319ecafcd1f444558c205894fff27c9647a9
Chris <christopher.davidson@btinternet.com>
2018-06-29 16:43:09 +0100

    Fixed another checkstyle

--- src/IntegrationTests/java/uk/gov/hmcts/sscs/PostCodeEndpointsIt.java
+++ src/IntegrationTests/java/uk/gov/hmcts/sscs/PostCodeEndpointsIt.java
@@ -1,5 +1,9 @@
 package uk.gov.hmcts.sscs;
 
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
