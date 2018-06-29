aa2cd9b5a111d1f600460386a206d70fd9549b88
Chris <christopher.davidson@btinternet.com>
2018-06-29 17:38:54 +0100

    Fixed all style problems

--- src/main/java/uk/gov/hmcts/sscs/controller/PostCodeController.java
+++ src/main/java/uk/gov/hmcts/sscs/controller/PostCodeController.java
@@ -1,5 +1,11 @@
 package uk.gov.hmcts.sscs.controller;
 
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.http.ResponseEntity.notFound;
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -8,23 +14,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.sscs.service.AIRLookupService;
-
-import static org.slf4j.LoggerFactory.getLogger;
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
-import static org.springframework.http.ResponseEntity.ok;
-import static org.springframework.http.ResponseEntity.notFound;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import uk.gov.hmcts.sscs.service.AirLookupService;
 
 /**
- * Controller for handling post code queries
+ * Controller for handling post code queries.
  */
 @RestController
 public class PostCodeController {
     private static final org.slf4j.Logger LOG = getLogger(ReminderController.class);
 
     @Autowired
-    private AIRLookupService AIRLookupService;
+    private AirLookupService airLookupService;
 
     @ApiOperation(value = "getRegionalCentre",
             notes = "Returns the regional centre given the appellants post code",
@@ -32,10 +32,10 @@ public class PostCodeController {
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Regional Centre", response = String.class)})
     @RequestMapping(value = "/regionalcentre/{postCode}", method = GET, produces = APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<String> getRegionalCentre(@PathVariable(value = "postCode") String postCode) {
-        String regionalCentre = AIRLookupService.lookupRegionalCentre(postCode);
-        if (regionalCentre == null ){
+        String regionalCentre = airLookupService.lookupRegionalCentre(postCode);
+        if (regionalCentre == null) {
             return notFound().build();
         }
-        return ok("{\"regionalCentre\": \""+regionalCentre+"\"}");
+        return ok("{\"regionalCentre\": \"" + regionalCentre + "\"}");
     }
 }
