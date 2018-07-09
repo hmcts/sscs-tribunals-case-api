1d678b2cb7e359e518c440b4f3e61ffdce7fe652
Chris <christopher.davidson@btinternet.com>
2018-07-09 16:50:20 +0100

    fixed check style

--- src/main/java/uk/gov/hmcts/sscs/service/AirLookupService.java
+++ src/main/java/uk/gov/hmcts/sscs/service/AirLookupService.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-
 /**
  * Service that ingests a spreadsheet and a csv file containing the
  * venues and regional centres for handling all post codes.
