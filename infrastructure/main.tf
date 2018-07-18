7c198b485c7682e92d13768d8b1ae37dadbe9d59
David Crespo <davidcrespoarroyo@gmail.com>
2018-07-18 14:32:30 +0100

    Task/revert rename resource change (#184)
    * Revert "Task/sscs 3520 preview fix (#183)"
    
    This reverts commit 2f01c18a20fa6bb787e513013de3d5bd841044ef.
    
    * Clean up cmc stuff added here by mistake
    
    * comment out smoke/functional tests to cause the aat resource to be created
    
    * uncomment tests

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,8 +2,9 @@ provider "vault" {
   address = "https://vault.reform.hmcts.net:6200"
 }
 
-data "vault_generic_secret" "cmc_s2s_secret" {
-  path = "secret/${var.infrastructure_env}/ccidam/service-auth-provider/api/microservice-keys/cmc"
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.component}-${var.env}"
+  location = "${var.location}"
 }
 
 data "vault_generic_secret" "email_mac_secret" {
@@ -169,6 +170,6 @@ module "sscs-tca-key-vault" {
   env                 = "${var.env}"
   tenant_id           = "${var.tenant_id}"
   object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${module.tribunals-case-api.resource_group_name}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "300e771f-856c-45cc-b899-40d78281e9c1"
 }
