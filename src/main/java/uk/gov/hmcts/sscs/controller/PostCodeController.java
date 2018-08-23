d99958be79f4b8d710b49dfdfb7bdb37565af3ec
david.crespo <david.crespo@hmcts.net>
2018-08-23 11:08:37 +0100

    SSCS-3825 Stop throwing exception for invalid postcodes

--- src/main/java/uk/gov/hmcts/sscs/controller/PostCodeController.java
+++ src/main/java/uk/gov/hmcts/sscs/controller/PostCodeController.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.sscs.controller;
 
-import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.http.ResponseEntity.notFound;
 import static org.springframework.http.ResponseEntity.ok;
@@ -9,20 +8,20 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.sscs.exception.AirLookupServiceException;
 import uk.gov.hmcts.sscs.service.AirLookupService;
 
 /**
  * Controller for handling post code queries.
  */
 @RestController
+@Slf4j
 public class PostCodeController {
-    private static final org.slf4j.Logger LOG = getLogger(PostCodeController.class);
 
     @Autowired
     private AirLookupService airLookupService;
@@ -36,13 +35,11 @@ public class PostCodeController {
         String regionalCentre = airLookupService.lookupRegionalCentre(postCode);
 
         if (regionalCentre == null) {
-            String message = "Could not find postcode " + postCode;
-            AirLookupServiceException ex = new AirLookupServiceException(new Exception(message));
-            LOG.error(message, ex);
+            log.warn("Could not find postcode " + postCode);
             return notFound().build();
         }
 
-        LOG.debug("Found regional centre " + regionalCentre + " for post code " + postCode);
+        log.debug("Found regional centre " + regionalCentre + " for post code " + postCode);
 
         return ok("{\"regionalCentre\": \"" + regionalCentre + "\"}");
     }
