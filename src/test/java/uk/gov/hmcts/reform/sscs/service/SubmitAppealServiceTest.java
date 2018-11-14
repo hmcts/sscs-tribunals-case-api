ba1c24d63d8a39210e4de54db67451519e9bc96a
Mark Hunter <mark.hunter@agilesphere.co.uk>
2018-11-14 10:29:06 +0000

    SSCS-4290 Increase version number of pdf robotics dependency

--- src/test/java/uk/gov/hmcts/reform/sscs/service/SubmitAppealServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/SubmitAppealServiceTest.java
@@ -27,7 +27,6 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
