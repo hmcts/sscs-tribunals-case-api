604fd7d86899d5fd386ee39b5773a748ce6fba58
david.crespo <david.crespo@hmcts.net>
2018-11-16 13:28:26 +0100

    put back an ignore functional test that it passing fine locally.

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/ccd/CreateCaseInCcdTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/ccd/CreateCaseInCcdTest.java
@@ -69,7 +69,6 @@ public class CreateCaseInCcdTest {
     }
 
     @Test
-    @Ignore
     public void givenASyaCaseShouldBeSavedIntoCcdViaSubmitAppealService() {
         try {
             submitAppealService.submitAppeal(ALL_DETAILS.getDeserializeMessage());
