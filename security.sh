a7cdd86de8171bc044dc183fc41f0b1a1f4669d5
Adeleke Osinloye <michael@adelekes-mbp.home>
2019-03-19 22:34:29 +0000

    set up SSCS tribunals case api security scan

--- security.sh
+++ security.sh
@@ -5,6 +5,3 @@ cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 cp /zap/api-report.html functional-output/
 zap-cli -p 1001 alerts -l High
-git clone https://github.com/owasp/glue
-cd glue 
-ruby bin/glue -t zap --zap-host http://0.0.0.0 --zap-port 1001 --zap-passive-mode ${TEST_URL}/v2/api-docs -z 0 --finding-file glue.json
