9e9e628fa7abe112c6044d7b1a1809212e5ffe1e
david.crespo <david.crespo@hmcts.net>
2019-04-01 22:54:30 +0200

    configure the citizen user to be injected in the functional test
    Configure the test to generate token for the test citizen user in AAT

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/SubmitDraftTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/sya/SubmitDraftTest.java
@@ -9,6 +9,7 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
+import java.util.Base64;
 import org.eclipse.jetty.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,7 +21,8 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.sscs.domain.wrapper.SyaBenefitType;
 import uk.gov.hmcts.reform.sscs.domain.wrapper.SyaCaseWrapper;
-import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.Authorize;
+import uk.gov.hmcts.reform.sscs.idam.IdamApiClient;
 import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 import uk.gov.hmcts.reform.sscs.util.SyaServiceHelper;
 
@@ -32,16 +34,28 @@ public class SubmitDraftTest {
     @Value("${test-url}")
     private String testUrl;
 
-    private IdamTokens idamTokens;
-
     @Autowired
-    private IdamService idamService;
+    private IdamApiClient idamApiClient;
+
+    @Value("${idam.oauth2.client.secret}")
+    private String idamOauth2ClientSecret;
+
+    @Value("${idam.oauth2.redirectUrl}")
+    private String idamOauth2RedirectUrl;
+
+    @Value("${idam.oauth2.citizen.email}")
+    private String username;
+
+    @Value("${idam.oauth2.citizen.password}")
+    private String password;
+
+    private String userToken;
 
     @Before
     public void setUp() {
         baseURI = testUrl;
         useRelaxedHTTPSValidation();
-        idamTokens = idamService.getIdamTokens();
+        userToken = getIdamOauth2Token(username, password);
     }
 
     @Test
@@ -50,13 +64,38 @@ public class SubmitDraftTest {
         draftAppeal.setBenefitType(new SyaBenefitType("PIP", "pip benefit"));
 
         RestAssured.given()
-                .log().method().log().headers().log().uri().log().body(true)
-                .contentType(ContentType.JSON)
-                .header(new Header(AUTHORIZATION, idamTokens.getIdamOauth2Token()))
-                .body(SyaServiceHelper.asJsonString(draftAppeal))
-                .post("/drafts")
-                .then()
-                .statusCode(HttpStatus.CREATED_201)
-                .assertThat().body("id", not(isEmptyOrNullString()));
+            .log().method().log().headers().log().uri().log().body(true)
+            .contentType(ContentType.JSON)
+            .header(new Header(AUTHORIZATION, userToken))
+            .body(SyaServiceHelper.asJsonString(draftAppeal))
+            .post("/drafts")
+            .then()
+            .statusCode(HttpStatus.CREATED_201)
+            .assertThat().body("id", not(isEmptyOrNullString()));
     }
+
+    public String getIdamOauth2Token(String username, String password) {
+        String authorisation = username + ":" + password;
+        String base64Authorisation = Base64.getEncoder().encodeToString(authorisation.getBytes());
+
+        Authorize authorize = idamApiClient.authorizeCodeType(
+            "Basic " + base64Authorisation,
+            "code",
+            "sscs",
+            idamOauth2RedirectUrl,
+            " "
+        );
+
+        Authorize authorizeToken = idamApiClient.authorizeToken(
+            authorize.getCode(),
+            "authorization_code",
+            idamOauth2RedirectUrl,
+            "sscs",
+            idamOauth2ClientSecret,
+            " "
+        );
+
+        return "Bearer " + authorizeToken.getAccessToken();
+    }
+
 }
