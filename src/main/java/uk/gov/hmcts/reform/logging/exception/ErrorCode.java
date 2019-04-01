b0ba066e79d55d6fc226ee1fba4906fbc2aaa639
david.crespo <david.crespo@hmcts.net>
2019-04-01 22:23:35 +0200

    remove code related to java logging version 5

--- src/main/java/uk/gov/hmcts/reform/logging/exception/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.logging.exception;
-
-public final class ErrorCode {
-
-    public static final String UNKNOWN = "UNKNOWN";
-
-    private ErrorCode() {
-    }
-}
--- src/main/java/uk/gov/hmcts/reform/logging/exception/InvalidExceptionImplementation.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.logging.exception;
-
-public class InvalidExceptionImplementation extends AbstractLoggingException {
-
-    public InvalidExceptionImplementation(String message, Throwable cause) {
-        super(AlertLevel.P1, "0", message, cause);
-    }
-}
