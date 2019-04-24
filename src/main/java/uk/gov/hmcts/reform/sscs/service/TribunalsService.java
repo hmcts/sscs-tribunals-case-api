dc1211865217e56ecf854727c8f5e5a3d2941f6c
Chris <christopher.davidson@btinternet.com>
2019-04-24 12:19:28 +0100

    fixed import order

--- src/main/java/uk/gov/hmcts/reform/sscs/service/TribunalsService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/TribunalsService.java
@@ -2,9 +2,10 @@ package uk.gov.hmcts.reform.sscs.service;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.builder.TrackYourAppealJsonBuilder;
 import uk.gov.hmcts.reform.sscs.ccd.domain.RegionalProcessingCenter;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
