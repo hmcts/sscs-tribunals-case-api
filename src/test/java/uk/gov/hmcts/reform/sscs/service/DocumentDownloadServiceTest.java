bc3a0813f55414c3bfb7bf055a808bab73ae4602
david.crespo <david.crespo@hmcts.net>
2019-05-23 13:04:24 +0200

    increase unit coverage

--- src/test/java/uk/gov/hmcts/reform/sscs/service/DocumentDownloadServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/DocumentDownloadServiceTest.java
@@ -5,18 +5,22 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.document.DocumentDownloadClientApi;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(JUnitParamsRunner.class)
 public class DocumentDownloadServiceTest {
 
     @Mock
@@ -25,6 +29,8 @@ public class DocumentDownloadServiceTest {
     private AuthTokenGenerator authTokenGenerator;
     private DocumentDownloadService documentDownloadService;
 
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Before
     public void setUp() {
@@ -61,5 +67,26 @@ public class DocumentDownloadServiceTest {
         long size = documentDownloadService
             .getFileSize("http://dm-store:4506/documents/19cd94a8-4280-406b-92c7-090b735159ca");
         assertEquals(0L, size);
+    }
+
+    @Test
+    @Parameters(method = "getDifferentResponseScenarios")
+    public void givenResponseIsNull_shouldReturnZeroSizeByDefault(ResponseEntity<Resource> response) {
+        //noinspection unchecked
+        given(documentDownloadClientApi.downloadBinary(any(), any(), any(), any(), any()))
+            .willReturn(response);
+        long size = documentDownloadService
+            .getFileSize("http://dm-store:4506/documents/19cd94a8-4280-406b-92c7-090b735159ca");
+        assertEquals(0L, size);
+    }
+
+
+    @SuppressWarnings("unused")
+    private Object[][] getDifferentResponseScenarios() {
+        ResponseEntity<Resource> response = ResponseEntity.notFound().build();
+        return new Object[][]{
+            new Object[]{response},
+            new Object[]{null}
+        };
     }
 }
