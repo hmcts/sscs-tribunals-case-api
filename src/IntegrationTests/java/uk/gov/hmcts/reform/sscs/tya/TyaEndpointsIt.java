509263ca05cd322bce9ee9702f27124300c90bcf
chrisgrimble <emailme@chrisgrimble.com>
2019-06-07 09:58:48 +0100

    Add an endpoint to load cases for MYA by case id. There used to be one that loaded them by TYA number but we many not have that for MYA.

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/tya/TyaEndpointsIt.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/tya/TyaEndpointsIt.java
@@ -7,7 +7,9 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.reform.sscs.util.SerializeJsonMessageManager.APPEAL_RECEIVED;
+import static uk.gov.hmcts.reform.sscs.util.SerializeJsonMessageManager.APPEAL_RECEIVED_CASE_ID;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.Test;
@@ -19,6 +21,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.client.CcdClient;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
@@ -33,6 +37,7 @@ import uk.gov.hmcts.reform.sscs.util.SerializeJsonMessageManager;
 @AutoConfigureMockMvc
 public class TyaEndpointsIt {
 
+    private static final long CASE_ID = 123456789L;
     @Autowired
     private MockMvc mockMvc;
 
@@ -41,6 +46,9 @@ public class TyaEndpointsIt {
 
     @MockBean
     CcdService ccdService;
+
+    @MockBean
+    CcdClient ccdClient;
 
     @MockBean
     MessageAuthenticationService macService;
@@ -63,6 +71,27 @@ public class TyaEndpointsIt {
         assertJsonEquals(APPEAL_RECEIVED.getSerializedMessage(), result);
 
 
+    }
+
+    @Test
+    public void shouldReturnAnAppealGivenACaseId() throws Exception {
+        idamTokens = IdamTokens.builder().build();
+        when(idamService.getIdamTokens()).thenReturn(idamTokens);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("caseCreated", "2019-06-06");
+        data.put("appeal", Collections.singletonMap(
+                "benefitType", Collections.singletonMap("code", "PIP")
+        ));
+
+        when(ccdClient.readForCaseworker(idamTokens, CASE_ID))
+                .thenReturn(CaseDetails.builder().data(data).id(CASE_ID).build());
+
+        MvcResult mvcResult = mockMvc.perform(get("/appeals?caseId=" + CASE_ID))
+                .andExpect(status().isOk())
+                .andReturn();
+        String result = mvcResult.getResponse().getContentAsString();
+        assertJsonEquals(APPEAL_RECEIVED_CASE_ID.getSerializedMessage(), result);
     }
 
     @Test
