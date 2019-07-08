982e4f1f0fc0ed249e8767957cf1954ee389287a
david.crespo <david.crespo@hmcts.net>
2019-07-08 13:17:52 +0100

    add scenarios for the handle method

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/ActionFurtherEvidenceSubmittedCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/ActionFurtherEvidenceSubmittedCallbackHandler.java
@@ -11,6 +11,7 @@ import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 
@@ -50,11 +51,11 @@ public class ActionFurtherEvidenceSubmittedCallbackHandler implements PreSubmitC
 
         SscsCaseData caseData = callback.getCaseDetails().getCaseData();
         caseData.setInterlocReviewState("interlocutoryReview");
-        ccdService.updateCase(caseData, callback.getCaseDetails().getId(),
+        SscsCaseDetails sscsCaseDetails = ccdService.updateCase(caseData, callback.getCaseDetails().getId(),
             EventType.INTERLOC_INFORMATION_RECEIVED.getCcdType(), "update to Information received event",
             "update to Information received event", idamService.getIdamTokens());
 
-        return new PreSubmitCallbackResponse<>(caseData);
+        return new PreSubmitCallbackResponse<>(sscsCaseDetails.getData());
     }
 
 }
--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/ActionFurtherEvidenceSubmittedCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/ActionFurtherEvidenceSubmittedCallbackHandlerTest.java
@@ -1,15 +1,26 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.ACTION_FURTHER_EVIDENCE;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Optional;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
@@ -17,16 +28,28 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicList;
 import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicListItem;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.State;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 
 @RunWith(JUnitParamsRunner.class)
 public class ActionFurtherEvidenceSubmittedCallbackHandlerTest {
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
     @Mock
     private CcdService ccdService;
     @Mock
     private IdamService idamService;
+
+    private ActionFurtherEvidenceSubmittedCallbackHandler handler;
+
+    @Before
+    public void setUp() {
+        handler = new ActionFurtherEvidenceSubmittedCallbackHandler(ccdService, idamService);
+    }
+
 
     @Test
     @Parameters({
@@ -37,9 +60,6 @@ public class ActionFurtherEvidenceSubmittedCallbackHandlerTest {
     })
     public void givenCanHandleIsCalled_shouldReturnCorrectResult(CallbackType callbackType, String dynamicListItemCode,
                                                                  EventType eventType, boolean expectedResult) {
-        ActionFurtherEvidenceSubmittedCallbackHandler handler =
-            new ActionFurtherEvidenceSubmittedCallbackHandler(ccdService, idamService);
-
         boolean actualResult = handler.canHandle(callbackType,
             buildCallback(dynamicListItemCode, eventType));
 
@@ -55,5 +75,23 @@ public class ActionFurtherEvidenceSubmittedCallbackHandlerTest {
         CaseDetails<SscsCaseData> caseDetails = new CaseDetails<>(123L, "sscs",
             State.INTERLOCUTORY_REVIEW_STATE, sscsCaseData, LocalDateTime.now());
         return new Callback<>(caseDetails, Optional.empty(), eventType);
+    }
+
+    @Test
+    public void givenHandleMethodIsCalled_shouldUpdateCaseCorrectly() {
+        Callback<SscsCaseData> callback = buildCallback("informationReceivedForInterloc",
+            ACTION_FURTHER_EVIDENCE);
+
+        given(idamService.getIdamTokens()).willReturn(IdamTokens.builder().build());
+
+        ArgumentCaptor<SscsCaseData> captor = ArgumentCaptor.forClass(SscsCaseData.class);
+
+        given(ccdService.updateCase(captor.capture(), anyLong(), eq("interlocInformationReceived"),
+            anyString(), anyString(), any(IdamTokens.class)))
+            .willReturn(SscsCaseDetails.builder().data(SscsCaseData.builder().build()).build());
+
+        handler.handle(CallbackType.SUBMITTED, callback);
+
+        assertEquals("interlocutoryReview", captor.getValue().getInterlocReviewState());
     }
 }
