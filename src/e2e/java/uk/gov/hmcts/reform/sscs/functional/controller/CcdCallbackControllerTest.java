0fc7c33e35ad0e53d435d6b72fc98cfbc333e65b
david.crespo <david.crespo@hmcts.net>
2019-07-09 16:13:39 +0100

    fix e2e test to work in both preview and aat

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/controller/CcdCallbackControllerTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/controller/CcdCallbackControllerTest.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.sscs.functional.controller;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.useRelaxedHTTPSValidation;
 import static org.hamcrest.Matchers.equalTo;
 import static uk.gov.hmcts.reform.sscs.functional.ccd.UpdateCaseInCcdTest.buildSscsCaseDataForTestingWithValidMobileNumbers;
 
@@ -16,6 +18,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,27 +26,35 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource(locations = "classpath:config/application_e2e.properties")
 @SpringBootTest
 public class CcdCallbackControllerTest {
 
+    @Value("${test-url}")
+    private String testUrl;
     @Autowired
     private IdamService idamService;
     @Autowired
     private CcdService ccdService;
     private Long ccdCaseId;
+    private IdamTokens idamTokens;
 
     @Before
     public void setUp() {
+        baseURI = testUrl;
+        useRelaxedHTTPSValidation();
         ccdCaseId = createCaseInCcdInterlocutoryReviewState().getId();
+        idamTokens = idamService.getIdamTokens();
     }
 
     private SscsCaseDetails createCaseInCcdInterlocutoryReviewState() {
+
         return ccdService.createCase(buildSscsCaseDataForTestingWithValidMobileNumbers(),
             EventType.NON_COMPLIANT.getCcdType(), "non compliant created test case",
-            "non compliant created test case", idamService.getIdamTokens());
+            "non compliant created test case", idamTokens);
     }
 
     @Test
@@ -51,7 +62,7 @@ public class CcdCallbackControllerTest {
         RestAssured.given()
             .log().method().log().headers().log().uri().log().body(true)
             .contentType(ContentType.JSON)
-            .header(new Header("ServiceAuthorization", idamService.generateServiceAuthorization()))
+            .header(new Header("ServiceAuthorization", idamTokens.getServiceAuthorization()))
             .body(getJsonCallbackForTest())
             .post("/ccdSubmittedEvent")
             .then()
