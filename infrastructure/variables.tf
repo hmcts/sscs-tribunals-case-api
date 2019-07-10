331b8d1a835cd0333d6d93093dd20a934a3a9bbb
jackmaloney <jack.maloney@transformuk.com>
2019-07-10 11:08:11 +0100

    SSCS-6089 Remove send to dwp feature flag

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -100,8 +100,3 @@ variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
   default     = ""
 }
-
-variable "send_to_dwp_enabled" {
-  type    = "string"
-  default = "false"
-}
