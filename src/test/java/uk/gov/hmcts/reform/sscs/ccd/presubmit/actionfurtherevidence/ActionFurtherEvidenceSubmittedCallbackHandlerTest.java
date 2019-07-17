23b70c793fa62316f4466fccddf215f5c7ae6abd
Jack Maloney <jack.maloney@transformuk.com>
2019-07-17 16:33:51 +0100

    SSCS-6016 set evidence issued to no when action evidence event processed (#613)

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/actionfurtherevidence/ActionFurtherEvidenceSubmittedCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/actionfurtherevidence/ActionFurtherEvidenceSubmittedCallbackHandlerTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.sscs.ccd.presubmit;
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.actionfurtherevidence;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
