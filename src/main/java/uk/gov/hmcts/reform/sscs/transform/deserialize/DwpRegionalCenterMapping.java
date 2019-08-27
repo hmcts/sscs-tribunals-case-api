c3f4f07fcddac95e5bd8213ee7fe1e16591d87c2
david.crespo <david.crespo@hmcts.net>
2019-08-27 19:17:41 +0100

    Add dwp regional center to the case
    upgrade to dev version of common library
    Add enum to store mapping between dwp issuing office and regional center
    Add logic in the enum to get dwp regional center for given office number
    Add production code to make the test scenario pass
    Add test scenarios
    fix tests

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/transform/deserialize/DwpRegionalCenterMapping.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.sscs.transform.deserialize;
+
+import java.util.stream.Stream;
+
+public enum DwpRegionalCenterMapping {
+    NEWCASTLE("Newcastle", "1"),
+    GLASGOW("Glasgow", "2,4"),
+    BELLEVALE("Bellevale", "3"),
+    SPRINGBURN("Springburn", "5"),
+    BLACKPOOL("Blackpool", "6,7,8,9"),
+    NEWPORT("Newport", "10");
+
+
+    private String dwpRegion;
+    private String dwpIssuingOfficeNumber;
+
+    DwpRegionalCenterMapping(String dwpRegion, String dwpIssuingOfficeNumber) {
+        this.dwpRegion = dwpRegion;
+        this.dwpIssuingOfficeNumber = dwpIssuingOfficeNumber;
+    }
+
+    public static String getDwpRegionForGivenDwpIssuingOfficeNum(String issuingOfficeNum) {
+        return Stream.of(DwpRegionalCenterMapping.values())
+            .filter(e -> e.dwpIssuingOfficeNumber.contains(issuingOfficeNum))
+            .map(e -> e.dwpRegion)
+            .findFirst()
+            .orElseThrow(RuntimeException::new);
+    }
+
+}
