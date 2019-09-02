d90b4204e541ad76e521d6199c67ad5d97811ff2
david.crespo <david.crespo@hmcts.net>
2019-09-02 17:52:51 +0200

    refactor test code to reuse buildTestcallback logic from the common library

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/AdminAppealWithdrawnHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/AdminAppealWithdrawnHandlerTest.java
@@ -1,26 +1,50 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.READ_ENUMS_USING_TO_STRING;
+import static com.fasterxml.jackson.databind.DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_ENUMS_USING_TO_STRING;
 import static org.junit.Assert.assertEquals;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import junitparams.converters.Nullable;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
-import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.deserialisation.SscsCaseCallbackDeserializer;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
-import uk.gov.hmcts.reform.sscs.ccd.domain.State;
 
 @RunWith(JUnitParamsRunner.class)
 public class AdminAppealWithdrawnHandlerTest {
 
     private final AdminAppealWithdrawnHandler adminAppealWithdrawnHandler = new AdminAppealWithdrawnHandler();
+    private ObjectMapper mapper;
+
+    @Before
+    public void setUp() {
+        Jackson2ObjectMapperBuilder objectMapperBuilder =
+            new Jackson2ObjectMapperBuilder()
+                .featuresToEnable(READ_ENUMS_USING_TO_STRING)
+                .featuresToEnable(READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
+                .featuresToEnable(WRITE_ENUMS_USING_TO_STRING)
+                .serializationInclusion(JsonInclude.Include.NON_ABSENT);
+
+        mapper = objectMapperBuilder.createXmlMapper(false).build();
+        mapper.registerModule(new JavaTimeModule());
+    }
 
     @Test
     @Parameters({
@@ -32,20 +56,25 @@ public class AdminAppealWithdrawnHandlerTest {
         "null,ADMIN_APPEAL_WITHDRAWN,false",
         "ABOUT_TO_SUBMIT,null,false",
     })
-    public void canHandle(@Nullable CallbackType callbackType, @Nullable EventType eventType, boolean expectedResult) {
-        boolean actualResult = adminAppealWithdrawnHandler.canHandle(callbackType, buildTestCallback(eventType));
+    public void canHandle(@Nullable CallbackType callbackType, @Nullable EventType eventType, boolean expectedResult)
+        throws IOException {
+        Callback<SscsCaseData> callback = buildTestCallback(eventType);
+        boolean actualResult = adminAppealWithdrawnHandler.canHandle(callbackType, callback);
         assertEquals(expectedResult, actualResult);
     }
 
-    private Callback<SscsCaseData> buildTestCallback(EventType eventType) {
+    private Callback<SscsCaseData> buildTestCallback(EventType eventType) throws IOException {
         if (eventType == null) return null;
-        CaseDetails<SscsCaseData> caseDetails = new CaseDetails<>(1L, "SSCS", State.DORMANT_APPEAL_STATE,
-            SscsCaseData.builder().build(), LocalDateTime.now());
-        return new Callback<>(caseDetails, Optional.empty(), eventType);
+        String file = Objects.requireNonNull(getClass().getClassLoader().getResource(
+            "callback/adminAppealWithdrawnCallback.json")).getFile();
+        String json = FileUtils.readFileToString(new File(file), StandardCharsets.UTF_8.name());
+        String jsonCallback = json.replace("EVENT_ID_PLACEHOLDER", eventType.getCcdType());
+        SscsCaseCallbackDeserializer sscsCaseCallbackDeserializer = new SscsCaseCallbackDeserializer(mapper);
+        return sscsCaseCallbackDeserializer.deserialize(jsonCallback);
     }
 
     @Test
-    public void handle() {
+    public void handle() throws IOException {
         PreSubmitCallbackResponse<SscsCaseData> actualResult = adminAppealWithdrawnHandler.handle(
             CallbackType.ABOUT_TO_SUBMIT, buildTestCallback(EventType.ADMIN_APPEAL_WITHDRAWN));
         assertEquals("withdrawalReceived", actualResult.getData().getDwpState());
