cf3f8b7f2e2c27624fc5343d68c3b895d39cefad
david.crespo <david.crespo@hmcts.net>
2019-09-18 19:15:20 +0200

    upgrade common to release version
    remove logging

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentHandler.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.uploaddocuments;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
@@ -12,20 +11,13 @@ import uk.gov.hmcts.reform.sscs.ccd.presubmit.DwpState;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 
 @Service
-@Slf4j
 public class UploadDocumentHandler implements PreSubmitCallbackHandler<SscsCaseData> {
 
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
-        log.info("UploadDocumentHandler.canHandle begin...");
-        log.info("callbackType: " + callbackType);
-        log.info("caseId: " + callback.getCaseDetails().getId());
-        log.info("caseData: " + callback.getCaseDetails().getCaseData());
-        boolean result = callbackType != null && callback != null && callbackType.equals(CallbackType.ABOUT_TO_SUBMIT)
+        return callbackType != null && callback != null && callbackType.equals(CallbackType.ABOUT_TO_SUBMIT)
             && callback.getEvent().equals(EventType.UPLOAD_DOCUMENT)
             && State.WITH_DWP.equals(callback.getCaseDetails().getState());
-        log.info("UploadDocumentHandler.canHandle.result: " + result);
-        return result;
     }
 
     @Override
@@ -33,12 +25,8 @@ public class UploadDocumentHandler implements PreSubmitCallbackHandler<SscsCaseD
         if (!canHandle(callbackType, callback)) {
             throw new IllegalStateException("Cannot handle callback");
         }
-        log.info("callbackType: " + callbackType);
-        log.info("caseId: " + callback.getCaseDetails().getId());
-        log.info("caseData: " + callback.getCaseDetails().getCaseData());
         SscsCaseData caseData = callback.getCaseDetails().getCaseData();
         caseData.setDwpState(DwpState.FE_RECEIVED.getValue());
-        log.info("UploadDocumentHandler.handle FE_RECEIVED end");
         return new PreSubmitCallbackResponse<>(caseData);
     }
 }
--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentHandlerTest.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import junitparams.converters.Nullable;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
@@ -22,7 +21,6 @@ public class UploadDocumentHandlerTest extends BaseHandlerTest {
     private static final String UPLOAD_DOCUMENT_CALLBACK_JSON = "uploaddocument/uploadDocumentCallback.json";
     private UploadDocumentHandler handler = new UploadDocumentHandler();
 
-    @Ignore
     @Test
     @Parameters({
         "ABOUT_TO_SUBMIT,UPLOAD_DOCUMENT,withDwp,true",
@@ -51,7 +49,6 @@ public class UploadDocumentHandlerTest extends BaseHandlerTest {
         assertEquals("feReceived", actualCaseData.getData().getDwpState());
     }
 
-    @Ignore
     @Test(expected = IllegalStateException.class)
     @Parameters({
         "ABOUT_TO_START,UPLOAD_DOCUMENT,withDwp",
