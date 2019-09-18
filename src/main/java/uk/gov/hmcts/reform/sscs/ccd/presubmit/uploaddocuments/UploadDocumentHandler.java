8092239591bdb1ac77b7cb0eb919be8a7ad5a213
david.crespo <david.crespo@hmcts.net>
2019-09-18 16:44:38 +0200

    add logs to debug failing test in the PR

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentHandler.java
@@ -18,6 +18,9 @@ public class UploadDocumentHandler implements PreSubmitCallbackHandler<SscsCaseD
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
         log.info("UploadDocumentHandler.canHandle begin...");
+        log.info("callbackType: " + callbackType);
+        log.info("caseId: " + callback.getCaseDetails().getId());
+        log.info("caseData: " + callback.getCaseDetails().getCaseData());
         boolean result = callbackType != null && callback != null && callbackType.equals(CallbackType.ABOUT_TO_SUBMIT)
             && callback.getEvent().equals(EventType.UPLOAD_DOCUMENT)
             && State.WITH_DWP.equals(callback.getCaseDetails().getState());
@@ -30,7 +33,9 @@ public class UploadDocumentHandler implements PreSubmitCallbackHandler<SscsCaseD
         if (!canHandle(callbackType, callback)) {
             throw new IllegalStateException("Cannot handle callback");
         }
-        log.info("UploadDocumentHandler.handle FE_RECEIVED begin...");
+        log.info("callbackType: " + callbackType);
+        log.info("caseId: " + callback.getCaseDetails().getId());
+        log.info("caseData: " + callback.getCaseDetails().getCaseData());
         SscsCaseData caseData = callback.getCaseDetails().getCaseData();
         caseData.setDwpState(DwpState.FE_RECEIVED.getValue());
         log.info("UploadDocumentHandler.handle FE_RECEIVED end");
