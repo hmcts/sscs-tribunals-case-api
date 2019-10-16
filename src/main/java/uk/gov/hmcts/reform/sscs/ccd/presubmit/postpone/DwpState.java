360e53f3820c983abbb8dc2a7cbebf5e58f9d682
Chris <christopher.davidson@btinternet.com>
2019-10-16 19:37:13 +0100

    undo test

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postpone/DwpState.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postpone/DwpState.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.postpone;
 
 public enum DwpState {
-    HEARING_POSTPONED("inProgress");
+    HEARING_POSTPONED("hearingPostponed");
 
     private String value;
 
