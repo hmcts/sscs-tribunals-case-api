3cbf21ce914ebe52bb80e938cc5bf3467a93594a
jackmaloney <jack.maloney@transformuk.com>
2019-10-16 10:23:54 +0100

    Fix broken test

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/ReissueFurtherEvidenceIt.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/ReissueFurtherEvidenceIt.java
@@ -102,7 +102,7 @@ public class ReissueFurtherEvidenceIt {
 
         DynamicListItem listItem1 = new DynamicListItem("http://www.bbc.com", "11111.pdf -  Appellant evidence");
         DynamicListItem listItem2 = new DynamicListItem("http://www.itv.com", "22222.pdf -  Representative evidence");
-        DynamicList expected = new DynamicList(listItem1, Arrays.asList(listItem1, listItem2));
+        DynamicList expected = new DynamicList(listItem2, Arrays.asList(listItem2, listItem1));
 
         PreSubmitCallbackResponse<SscsCaseData> result = deserialize(response.getContentAsString());
 
