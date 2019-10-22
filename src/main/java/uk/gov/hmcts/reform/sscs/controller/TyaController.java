bbbe5ec0052f66873c32ba45552381083d1591b9
david.crespo <david.crespo@hmcts.net>
2019-10-22 15:50:36 +0100

    fix sonar issues

--- src/main/java/uk/gov/hmcts/reform/sscs/controller/TyaController.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/controller/TyaController.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.sscs.controller;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.ResponseEntity.ok;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -49,7 +48,7 @@ public class TyaController {
         notes = "Returns an appeal given the appeal number",
         response = String.class, responseContainer = "Appeal details")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Appeal", response = String.class)})
-    @RequestMapping(value = "/appeals/{appealNumber}", method = GET, produces = APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/appeals/{appealNumber}", method = GET, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getAppeal(
             @PathVariable(value = "appealNumber") String appealNumber) {
         return ok(tribunalsService.findAppeal(appealNumber).toString());
@@ -59,7 +58,7 @@ public class TyaController {
             notes = "Returns an appeal given the CCD case id",
             response = String.class, responseContainer = "Appeal details")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Appeal", response = String.class)})
-    @RequestMapping(value = "/appeals", method = GET, produces = APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/appeals", method = GET, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getAppealByCaseId(
             @RequestParam(value = "caseId") Long caseId) {
         return ok(tribunalsService.findAppeal(caseId).toString());
