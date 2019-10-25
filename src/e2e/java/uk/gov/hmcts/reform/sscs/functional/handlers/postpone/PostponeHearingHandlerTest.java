57e4ffaa27f1b036be1f99dd804d3dd20793b4a4
Chris Davidson <christopher.davidson@btinternet.com>
2019-10-25 16:10:25 +0100

    Sscs 6437 postponement (#785)
    * Added postponed handler

---
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/handlers/postpone/PostponeHearingHandlerTest.java
@@ -0,0 +1,59 @@
+package uk.gov.hmcts.reform.sscs.functional.handlers.postpone;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
+import static uk.gov.hmcts.reform.sscs.util.SyaJsonMessageSerializer.*;
+
+import io.restassured.http.ContentType;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
+
+@RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:config/application_e2e.properties")
+@SpringBootTest
+public class PostponeHearingHandlerTest {
+
+    @Value("${test-url}")
+    private String testUrl;
+    private IdamTokens idamTokens;
+    @Autowired
+    private IdamService idamService;
+
+    @Before
+    public void setUp() {
+        baseURI = testUrl;
+        useRelaxedHTTPSValidation();
+        idamTokens = idamService.getIdamTokens();
+    }
+
+    @Test
+    public void hitCallback() {
+
+        String body = POSTPONE_HEARING_CALLBACK_CCD.getSerializedMessage();
+
+        String response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", idamTokens.getIdamOauth2Token())
+                .header("ServiceAuthorization", idamTokens.getServiceAuthorization())
+                .body(body)
+                .expect()
+                .statusCode(200)
+                .when()
+                .post("/ccdAboutToSubmit/")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .and()
+                .extract().body().jsonPath().get("data.dwpState");
+
+        assertEquals("hearingPostponed", response);
+    }
+}
