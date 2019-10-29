e51210edb3e2fafdcbe5b32577cc30d3dafbdd06
jackmaloney <jack.maloney@transformuk.com>
2019-10-29 13:56:37 +0000

    SSCS-6308 Rep added to case flag

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/addrep/AddRepAboutToSubmitHandlerTest.java
@@ -0,0 +1,71 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.addrep;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+
+@RunWith(JUnitParamsRunner.class)
+public class AddRepAboutToSubmitHandlerTest {
+
+    private static final String USER_AUTHORISATION = "Bearer token";
+
+    private AddRepEvidenceAboutToSubmitHandler handler;
+
+    @Mock
+    private Callback<SscsCaseData> callback;
+
+    @Mock
+    private CaseDetails<SscsCaseData> caseDetails;
+
+    private SscsCaseData sscsCaseData;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        handler = new AddRepEvidenceAboutToSubmitHandler();
+
+        when(callback.getEvent()).thenReturn(EventType.ADD_REPRESENTATIVE);
+
+        sscsCaseData = SscsCaseData.builder()
+                .ccdCaseId("1234")
+                .createdInGapsFrom(State.READY_TO_LIST.getId())
+                .appeal(Appeal.builder().build())
+                .build();
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+    }
+
+    @Test
+    @Parameters({"APPEAL_RECEIVED"})
+    public void givenANonAddRepresentativeEvent_thenReturnFalse(EventType eventType) {
+        when(callback.getEvent()).thenReturn(eventType);
+        assertFalse(handler.canHandle(ABOUT_TO_SUBMIT, callback));
+    }
+
+    @Test
+    @Parameters({"ADD_REPRESENTATIVE"})
+    public void givenAHandleAddRepresentativeEvent_thenReturnTrue(EventType eventType) {
+        when(callback.getEvent()).thenReturn(eventType);
+        assertTrue(handler.canHandle(ABOUT_TO_SUBMIT, callback));
+    }
+
+    @Test
+    public void setDwpStateToAddRepresentative() {
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        assertEquals("repAdded", response.getData().getDwpState());
+    }
+}
