5142edb231639a9cffdb047c18781c7c850a91b6
Ryan Da Costa <ryan.dacosta@hmcts.net>
2019-10-30 13:51:33 +0000

    add integration test

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/DirectionIssuedIt.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/DirectionIssuedIt.java
@@ -8,69 +8,34 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.sscs.helper.IntegrationTestHelper.*;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
-import java.util.Objects;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi;
 import uk.gov.hmcts.reform.document.domain.UploadResponse;
 import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
-import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
-import uk.gov.hmcts.reform.sscs.ccd.deserialisation.SscsCaseCallbackDeserializer;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
-import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackDispatcher;
-import uk.gov.hmcts.reform.sscs.controller.CcdCallbackController;
 import uk.gov.hmcts.reform.sscs.docassembly.GenerateFile;
 import uk.gov.hmcts.reform.sscs.idam.IdamApiClient;
 import uk.gov.hmcts.reform.sscs.model.docassembly.DirectionOrDecisionIssuedTemplateBody;
 import uk.gov.hmcts.reform.sscs.model.docassembly.GenerateFileParams;
-import uk.gov.hmcts.reform.sscs.service.AuthorisationService;
 import uk.gov.hmcts.reform.sscs.service.EvidenceManagementService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class DirectionIssuedIt {
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
-
-    private MockMvc mockMvc;
-
-    @MockBean
-    private AuthorisationService authorisationService;
-
-    @Autowired
-    private SscsCaseCallbackDeserializer deserializer;
-
-    @Autowired
-    private PreSubmitCallbackDispatcher dispatcher;
-
-    @Autowired
-    private ObjectMapper mapper;
+public class DirectionIssuedIt extends AbstractEventIt {
 
     @MockBean
     private CoreCaseDataApi coreCaseDataApi;
@@ -87,15 +52,9 @@ public class DirectionIssuedIt {
     @MockBean
     private GenerateFile generateFile;
 
-    private String json;
-
     @Before
     public void setup() throws IOException {
-        CcdCallbackController controller = new CcdCallbackController(authorisationService, deserializer, dispatcher);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-        mapper.registerModule(new JavaTimeModule());
-        json = getJson("callback/directionIssuedForPreview.json");
-
+        setup("callback/directionIssuedForPreview.json");
     }
 
     @Test
@@ -147,26 +106,4 @@ public class DirectionIssuedIt {
         assertEquals("awaitingInformation", result.getData().getInterlocReviewState());
     }
 
-    private MockHttpServletResponse getResponse(MockHttpServletRequestBuilder requestBuilder) throws Exception {
-        return mockMvc.perform(requestBuilder).andReturn().getResponse();
-    }
-
-    private PreSubmitCallbackResponse<SscsCaseData> deserialize(String source) {
-        try {
-            return mapper.readValue(
-                    source,
-                    new TypeReference<PreSubmitCallbackResponse<SscsCaseData>>() {
-                    }
-            );
-
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Could not deserialize object", e);
-        }
-    }
-
-    private String getJson(String fileLocation) throws IOException {
-        String path = Objects.requireNonNull(getClass().getClassLoader()
-                .getResource(fileLocation)).getFile();
-        return FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8.name());
-    }
 }
