27833c1c196ae90138236cf95fab2f4739e31969
david.crespo <david.crespo@hmcts.net>
2019-11-05 01:08:09 +0000

    add functional test

---
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/handlers/timeextension/DwpRequestTimeExtensionAboutToSubmitHandlerTest.java
@@ -0,0 +1,71 @@
+package uk.gov.hmcts.reform.sscs.functional.handlers.timeextension;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.useRelaxedHTTPSValidation;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasItem;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
+
+@RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:config/application_e2e.properties")
+@SpringBootTest
+public class DwpRequestTimeExtensionAboutToSubmitHandlerTest {
+
+    @Value("${test-url}")
+    private String testUrl;
+    @Autowired
+    private IdamService idamService;
+    private IdamTokens idamTokens;
+
+    @Before
+    public void setUp() {
+        baseURI = testUrl;
+        useRelaxedHTTPSValidation();
+        idamTokens = idamService.getIdamTokens();
+    }
+
+    @Test
+    public void givenAboutToSubmitCallbackForEvent_shouldSetFieldsAndTl1Form() throws Exception {
+        RestAssured.given()
+            .log().method().log().headers().log().uri().log().body(true)
+            .contentType(ContentType.JSON)
+            .header(new Header("ServiceAuthorization", idamTokens.getServiceAuthorization()))
+            .header(new Header("Authorization", idamTokens.getIdamOauth2Token()))
+            .body(getJsonCallbackForTest())
+            .post("/ccdAboutToSubmit")
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .log().all(true)
+            .rootPath("data")
+            .assertThat().body("interlocReviewState", equalTo("reviewByTcw"))
+            .assertThat().body("dwpState", equalTo("extensionRequested"))
+            .assertThat().body("interlocReferralReason", equalTo("timeExtension"))
+            .assertThat().body("sscsDocument.value", hasItem(hasEntry("documentType", "tl1Form1")));
+    }
+
+    private String getJsonCallbackForTest() throws IOException {
+        String path = Objects.requireNonNull(getClass().getClassLoader()
+            .getResource("handlers/timeextension/dwpRequestTimeExtensionAboutToSubmitCallback.json")).getFile();
+        return FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8.name());
+    }
+}
