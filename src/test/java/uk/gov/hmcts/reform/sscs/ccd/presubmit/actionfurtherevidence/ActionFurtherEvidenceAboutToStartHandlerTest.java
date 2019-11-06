fd8f7465af867a02325615df81947ab5d935ca7e
david.crespo <david.crespo@hmcts.net>
2019-11-06 17:27:39 +0000

    Parametrise test method to cover all different scenario reusing code

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/actionfurtherevidence/ActionFurtherEvidenceAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/actionfurtherevidence/ActionFurtherEvidenceAboutToStartHandlerTest.java
@@ -1,18 +1,31 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.actionfurtherevidence;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_START;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.converters.Nullable;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
-import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicList;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicListItem;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Representative;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 
+@RunWith(JUnitParamsRunner.class)
 public class ActionFurtherEvidenceAboutToStartHandlerTest {
 
     private static final String USER_AUTHORISATION = "Bearer token";
@@ -54,57 +67,49 @@ public class ActionFurtherEvidenceAboutToStartHandlerTest {
     }
 
     @Test
-    public void populateFurtherEvidenceDropdownWithIssueFurtherEvidenceFeatureFlagTrue_whenCaseInInterloc() {
-        sscsCaseData = SscsCaseData.builder().appeal(Appeal.builder().build()).interlocReviewState("any").build();
+    @Parameters({
+        "true, any, 4, true, true, true, true",
+        "true, null, 2, true, true, false, false",
+        "false, null, 1, false, true, false, false"
+    })
+    public void GivenActionFurtherEvidenceAboutToStart_populateFurtherEvidenceDropdown(
+        boolean issueFurtherEvidenceFeature,
+        @Nullable String interlocReviewState,
+        int expectedListItemSize,
+        boolean expectedIssueFurtherEvidenceItem,
+        boolean expectedOtherDocumentManualItem,
+        boolean informationReceivedForInterlocJudgeItem,
+        boolean informationReceivedForInterlocTcwItem
+    ) {
+
+        sscsCaseData = SscsCaseData.builder().appeal(Appeal.builder().build())
+            .interlocReviewState(interlocReviewState).build();
         when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
-        ReflectionTestUtils.setField(handler, "issueFurtherEvidenceFeature", true);
+        ReflectionTestUtils.setField(handler, "issueFurtherEvidenceFeature", issueFurtherEvidenceFeature);
 
         PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_START, callback, USER_AUTHORISATION);
 
-        assertEquals("issueFurtherEvidence", response.getData().getFurtherEvidenceAction().getListItems().get(0).getCode());
-        assertEquals("otherDocumentManual", response.getData().getFurtherEvidenceAction().getListItems().get(1).getCode());
-        assertEquals("informationReceivedForInterlocJudge", response.getData().getFurtherEvidenceAction().getListItems().get(2).getCode());
-        assertEquals("informationReceivedForInterlocTcw", response.getData().getFurtherEvidenceAction().getListItems().get(3).getCode());
-        assertEquals(4, response.getData().getFurtherEvidenceAction().getListItems().size());
+        assertEquals(expectedIssueFurtherEvidenceItem, "issueFurtherEvidence".equals(getItemCodeInList(
+            response.getData().getFurtherEvidenceAction(), "issueFurtherEvidence")));
+
+        assertEquals(expectedOtherDocumentManualItem, "otherDocumentManual".equals(getItemCodeInList(
+            response.getData().getFurtherEvidenceAction(), "otherDocumentManual")));
+
+        assertEquals(informationReceivedForInterlocJudgeItem, "informationReceivedForInterlocJudge".equals(
+            getItemCodeInList(response.getData().getFurtherEvidenceAction(), "informationReceivedForInterlocJudge")));
+
+        assertEquals(informationReceivedForInterlocTcwItem, "informationReceivedForInterlocTcw".equals(
+            getItemCodeInList(response.getData().getFurtherEvidenceAction(), "informationReceivedForInterlocTcw")));
+
+        assertEquals(expectedListItemSize, response.getData().getFurtherEvidenceAction().getListItems().size());
     }
 
-    @Test
-    public void populateFurtherEvidenceDropdownWithIssueFurtherEvidenceFeatureFlagFalse_whenCaseInInterloc() {
-        sscsCaseData = SscsCaseData.builder().appeal(Appeal.builder().build()).interlocReviewState("any").build();
-        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
-        ReflectionTestUtils.setField(handler, "issueFurtherEvidenceFeature", false);
-
-        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_START, callback, USER_AUTHORISATION);
-
-        assertEquals("otherDocumentManual", response.getData().getFurtherEvidenceAction().getListItems().get(0).getCode());
-        assertEquals("informationReceivedForInterlocJudge", response.getData().getFurtherEvidenceAction().getListItems().get(1).getCode());
-        assertEquals("informationReceivedForInterlocTcw", response.getData().getFurtherEvidenceAction().getListItems().get(2).getCode());
-        assertEquals(3, response.getData().getFurtherEvidenceAction().getListItems().size());
-    }
-
-    @Test
-    public void populateFurtherEvidenceDropdownWithIssueFurtherEvidenceFeatureFlagTrue_whenCaseNotInInterloc() {
-        sscsCaseData = SscsCaseData.builder().appeal(Appeal.builder().build()).build();
-        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
-        ReflectionTestUtils.setField(handler, "issueFurtherEvidenceFeature", true);
-
-        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_START, callback, USER_AUTHORISATION);
-
-        assertEquals("issueFurtherEvidence", response.getData().getFurtherEvidenceAction().getListItems().get(0).getCode());
-        assertEquals("otherDocumentManual", response.getData().getFurtherEvidenceAction().getListItems().get(1).getCode());
-        assertEquals(2, response.getData().getFurtherEvidenceAction().getListItems().size());
-    }
-
-    @Test
-    public void populateFurtherEvidenceDropdownWithIssueFurtherEvidenceFeatureFlagFalse_whenCaseNotInInterloc() {
-        sscsCaseData = SscsCaseData.builder().appeal(Appeal.builder().build()).build();
-        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
-        ReflectionTestUtils.setField(handler, "issueFurtherEvidenceFeature", false);
-
-        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_START, callback, USER_AUTHORISATION);
-
-        assertEquals("otherDocumentManual", response.getData().getFurtherEvidenceAction().getListItems().get(0).getCode());
-        assertEquals(1, response.getData().getFurtherEvidenceAction().getListItems().size());
+    private String getItemCodeInList(DynamicList dynamicList, String item) {
+        return dynamicList.getListItems().stream()
+            .filter(o -> item.equals(o.getCode()))
+            .findFirst()
+            .map(DynamicListItem::getCode)
+            .orElse(null);
     }
 
     @Test
