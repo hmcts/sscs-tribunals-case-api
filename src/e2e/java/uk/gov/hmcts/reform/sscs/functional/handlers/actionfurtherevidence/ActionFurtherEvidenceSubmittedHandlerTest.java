057d81590ee697d4b20ee8504115dc68c23f2e99
david.crespo <david.crespo@hmcts.net>
2019-11-08 11:52:56 +0100

    parametrise submitted functional test to add more AC scenarios and reuse the code

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/handlers/actionfurtherevidence/ActionFurtherEvidenceSubmittedHandlerTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/handlers/actionfurtherevidence/ActionFurtherEvidenceSubmittedHandlerTest.java
@@ -11,61 +11,69 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpStatus;
-import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.functional.handlers.BaseHandler;
 
-@RunWith(SpringRunner.class)
+@RunWith(JUnitParamsRunner.class)
 @TestPropertySource(locations = "classpath:config/application_e2e.properties")
 @SpringBootTest
 public class ActionFurtherEvidenceSubmittedHandlerTest extends BaseHandler {
 
-    private Long ccdCaseId;
+    @ClassRule
+    public static final SpringClassRule scr = new SpringClassRule();
 
-    @Before
-    public void setUp() {
-        super.setUp();
-        ccdCaseId = createCaseInCcdInterlocutoryReviewState().getId();
-    }
+    @Rule
+    public final SpringMethodRule smr = new SpringMethodRule();
 
-    private SscsCaseDetails createCaseInCcdInterlocutoryReviewState() {
-
-        return ccdService.createCase(buildSscsCaseDataForTestingWithValidMobileNumbers(),
-            EventType.NON_COMPLIANT.getCcdType(), "non compliant created test case",
-            "non compliant created test case", idamTokens);
-    }
 
     @Test
-    public void givenSubmittedCallbackForActionFurtherEvidence_shouldUpdateFieldAndTriggerEvent() throws Exception {
+    @Parameters({
+        "NON_COMPLIANT, interlocutoryReviewState"
+    })
+    public void givenSubmittedCallbackForActionFurtherEvidence_shouldUpdateFieldAndTriggerEvent(
+        EventType eventType, String expectedState) throws Exception {
+
+        Long caseId = createCaseTriggeringGivenEvent(eventType).getId();
+
         RestAssured.given()
             .log().method().log().headers().log().uri().log().body(true)
             .contentType(ContentType.JSON)
             .header(new Header("ServiceAuthorization", idamTokens.getServiceAuthorization()))
             .header(new Header("Authorization", idamTokens.getIdamOauth2Token()))
-            .body(getJsonCallbackForTest())
+            .body(getJsonCallbackForTest(caseId))
             .post("/ccdSubmittedEvent")
             .then()
             .statusCode(HttpStatus.SC_OK)
             .log().all(true)
             .assertThat().body("data.interlocReviewState", equalTo("reviewByJudge"));
 
-        assertEquals("interlocutoryReviewState",
-            ccdService.getByCaseId(ccdCaseId, idamTokens).getState());
+        assertEquals(expectedState, ccdService.getByCaseId(caseId, idamTokens).getState());
     }
 
-    private String getJsonCallbackForTest() throws IOException {
+    private SscsCaseDetails createCaseTriggeringGivenEvent(EventType eventType) {
+        return ccdService.createCase(buildSscsCaseDataForTestingWithValidMobileNumbers(),
+            eventType.getCcdType(), "non compliant created test case",
+            "non compliant created test case", idamTokens);
+    }
+
+    private String getJsonCallbackForTest(Long caseId) throws IOException {
         String path = Objects.requireNonNull(getClass().getClassLoader()
             .getResource("handlers/actionfurtherevidence/actionFurtherEvidenceSubmittedCallback.json")).getFile();
         String jsonCallback = FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8.name());
-        return jsonCallback.replace("CASE_ID_TO_BE_REPLACED", ccdCaseId.toString());
+        return jsonCallback.replace("CASE_ID_TO_BE_REPLACED", caseId.toString());
     }
 }
 
