f3e0f5a1feeb6be7cb999fc3c8c43e84d52f3b68
Jack Maloney <jack.maloney@transformuk.com>
2019-11-13 15:58:35 +0000

    Sscs 6380 response reviewed callbacks (#833)
    * SSCS-6380 Response reviewed callbacks
    
    * Refactoring of response reviewed callbacks
    
    * Implement further callbacks for response reviewed event
    
    * Use common release candidate
    
    * Event comment dependent on which option is selected
    
    * Fix integration test

---
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/HmctsResponseReviewedIt.java
@@ -0,0 +1,101 @@
+package uk.gov.hmcts.reform.sscs.callback;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.sscs.helper.IntegrationTestHelper.assertHttpStatus;
+import static uk.gov.hmcts.reform.sscs.helper.IntegrationTestHelper.getRequestWithAuthHeader;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Collections;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicListItem;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
+import uk.gov.hmcts.reform.sscs.controller.CcdCallbackController;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(JUnitParamsRunner.class)
+public class HmctsResponseReviewedIt extends AbstractEventIt {
+
+    @MockBean
+    private CcdService ccdService;
+
+    @MockBean
+    private IdamService idamService;
+
+    @Before
+    public void setup() throws IOException {
+        CcdCallbackController controller = new CcdCallbackController(authorisationService, deserializer, dispatcher);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mapper.registerModule(new JavaTimeModule());
+        json = getJson("callback/hmctsResponseReviewedCallback.json");
+
+        when(idamService.getIdamTokens()).thenReturn(IdamTokens.builder().build());
+    }
+
+    @Test
+    public void callAboutToStartHandler_willPopulateDropdowns() throws Exception {
+        MockHttpServletResponse response = getResponse(getRequestWithAuthHeader(json, "/ccdAboutToStart"));
+
+        assertHttpStatus(response, HttpStatus.OK);
+
+        DynamicListItem listItem = new DynamicListItem("DWP PIP (2)", "DWP PIP (2)");
+
+        PreSubmitCallbackResponse<SscsCaseData> result = deserialize(response.getContentAsString());
+
+        assertEquals(Collections.EMPTY_SET, result.getErrors());
+        assertEquals(24, result.getData().getDwpPresentingOffice().getListItems().size());
+        assertEquals(listItem, result.getData().getDwpPresentingOffice().getValue());
+        assertEquals(24, result.getData().getDwpOriginatingOffice().getListItems().size());
+        assertEquals(listItem, result.getData().getDwpOriginatingOffice().getValue());
+    }
+
+    @Test
+    public void callToAboutToSubmitHandler_willSetCaseCode() throws Exception {
+        MockHttpServletResponse response = getResponse(getRequestWithAuthHeader(json, "/ccdAboutToSubmit"));
+
+        assertHttpStatus(response, HttpStatus.OK);
+
+        PreSubmitCallbackResponse<SscsCaseData> result = deserialize(response.getContentAsString());
+
+        assertEquals(Collections.EMPTY_SET, result.getErrors());
+        assertEquals("002DD", result.getData().getCaseCode());
+        assertEquals(LocalDate.now().toString(), result.getData().getDwpResponseDate());
+    }
+
+
+    @Test
+    @Parameters({"Yes,VALID_SEND_TO_INTERLOC", "No,READY_TO_LIST"})
+    public void callToSubmittedHandler_willTriggerValidSendToInterlocEvent(String yesOrNo, EventType eventType) throws Exception {
+        json = json.replaceAll("IS_INTERLOC_REQUIRED_VALUE", yesOrNo);
+
+        MockHttpServletResponse response = getResponse(getRequestWithAuthHeader(json, "/ccdSubmittedEvent"));
+
+        assertHttpStatus(response, HttpStatus.OK);
+
+        PreSubmitCallbackResponse<SscsCaseData> result = deserialize(response.getContentAsString());
+
+        assertEquals(Collections.EMPTY_SET, result.getErrors());
+        verify(ccdService).updateCase(any(SscsCaseData.class), eq(12345656789L), eq(eventType.getCcdType()), any(String.class), any(String.class), any(IdamTokens.class));
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/hmctsresponsereviewed/HmctsResponseReviewedSubmittedHandlerTest.java
@@ -0,0 +1,119 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.hmctsresponsereviewed;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.SUBMITTED;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.READY_TO_LIST;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.VALID_SEND_TO_INTERLOC;
+
+import java.util.Collections;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
+
+@RunWith(JUnitParamsRunner.class)
+public class HmctsResponseReviewedSubmittedHandlerTest {
+    private static final String USER_AUTHORISATION = "Bearer token";
+    private HmctsResponseReviewedSubmittedHandler handler;
+
+    @Mock
+    private Callback<SscsCaseData> callback;
+
+    @Mock
+    private CaseDetails<SscsCaseData> caseDetails;
+    private SscsCaseData sscsCaseData;
+
+    @Mock
+    private CcdService ccdService;
+
+    @Mock
+    private IdamService idamService;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        handler = new HmctsResponseReviewedSubmittedHandler(ccdService, idamService);
+
+        when(callback.getEvent()).thenReturn(EventType.HMCTS_RESPONSE_REVIEWED);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getId()).thenReturn(123L);
+        sscsCaseData = SscsCaseData.builder().ccdCaseId("ccdId").appeal(Appeal.builder().build())
+                .selectWhoReviewsCase(new DynamicList(new DynamicListItem("reviewByTcw", "Review by TCW"), null))
+                .build();
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+
+        when(idamService.getIdamTokens()).thenReturn(IdamTokens.builder().build());
+
+    }
+
+    @Test
+    public void givenANonHmctsResponseReviewedEvent_thenReturnFalse() {
+        when(callback.getEvent()).thenReturn(EventType.APPEAL_RECEIVED);
+        assertFalse(handler.canHandle(ABOUT_TO_SUBMIT, callback));
+    }
+
+    @Test
+    @Parameters({"ABOUT_TO_START", "MID_EVENT", "ABOUT_TO_SUBMIT"})
+    public void givenANonCallbackType_thenReturnFalse(CallbackType callbackType) {
+        assertFalse(handler.canHandle(callbackType, callback));
+    }
+
+    @Test
+    public void givenAHmctsResponseReviewedSubmittedEventAndInterlocIsRequiredForJudge_thenTriggerValidSendToInterlocEvent() {
+
+        sscsCaseData = sscsCaseData.toBuilder().isInterlocRequired("Yes").selectWhoReviewsCase(new DynamicList(new DynamicListItem("reviewByJudge", "Review by Judge"), null)).build();
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(SUBMITTED, callback, USER_AUTHORISATION);
+
+        assertEquals(Collections.EMPTY_SET, response.getErrors());
+        verify(ccdService).updateCase(any(SscsCaseData.class), eq(123L), eq(VALID_SEND_TO_INTERLOC.getCcdType()), eq("Send to interloc"), eq("Send a case to a Judge for review"), any(IdamTokens.class));
+    }
+
+    @Test
+    public void givenAHmctsResponseReviewedSubmittedEventAndInterlocIsRequiredForTcw_thenTriggerValidSendToInterlocEvent() {
+
+        sscsCaseData = sscsCaseData.toBuilder().isInterlocRequired("Yes").selectWhoReviewsCase(new DynamicList(new DynamicListItem("reviewByTcw", "Review by TCW"), null)).build();
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(SUBMITTED, callback, USER_AUTHORISATION);
+
+        assertEquals(Collections.EMPTY_SET, response.getErrors());
+        verify(ccdService).updateCase(any(SscsCaseData.class), eq(123L), eq(VALID_SEND_TO_INTERLOC.getCcdType()), eq("Send to interloc"), eq("Send a case to a TCW for review"), any(IdamTokens.class));
+    }
+
+    @Test
+    public void givenAHmctsResponseReviewedSubmittedEventAndInterlocIsNotRequired_thenTriggerReadyToListEvent() {
+
+        sscsCaseData = sscsCaseData.toBuilder().isInterlocRequired("No").build();
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(SUBMITTED, callback, USER_AUTHORISATION);
+
+        assertEquals(Collections.EMPTY_SET, response.getErrors());
+        verify(ccdService).updateCase(any(SscsCaseData.class), eq(123L), eq(READY_TO_LIST.getCcdType()), eq("Ready to list"), eq("Makes an appeal ready to list"), any(IdamTokens.class));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void throwsExceptionIfItCannotHandleTheAppeal() {
+        when(callback.getEvent()).thenReturn(EventType.APPEAL_RECEIVED);
+        handler.handle(SUBMITTED, callback, USER_AUTHORISATION);
+    }
+
+}
