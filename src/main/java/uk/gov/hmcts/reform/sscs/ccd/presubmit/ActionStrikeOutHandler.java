d17c901ae5a5b6967066c4c99b38f08c0ecba9c5
david.crespo <david.crespo@hmcts.net>
2019-11-21 13:24:39 +0000

    add junit test to cover teh new handler
    add first unit test scenario to cover the canHandle method
    add production code to pass it

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/ActionStrikeOutHandler.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+
+@Service
+public class ActionStrikeOutHandler extends EventToFieldPreSubmitCallbackHandler {
+
+    @Autowired
+    ActionStrikeOutHandler() {
+        super(createMappings());
+    }
+
+    private static Map<EventType, String> createMappings() {
+        Map<EventType, String> eventFieldMappings = new HashMap<>();
+        eventFieldMappings.put(EventType.ACTION_STRIKE_OUT, null);
+        return eventFieldMappings;
+    }
+
+
+    @Override
+    protected SscsCaseData setField(SscsCaseData sscsCaseData, String newValue, EventType eventType) {
+        return null;
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/ActionStrikeOutHandlerTest.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+
+@RunWith(JUnitParamsRunner.class)
+public class ActionStrikeOutHandlerTest {
+
+    private ActionStrikeOutHandler actionStrikeOutHandler;
+
+    private SscsCaseData sscsCaseData;
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    private CaseDetails<SscsCaseData> caseDetails;
+
+    @Mock
+    private Callback<SscsCaseData> callback;
+
+    @Before
+    public void setUp() {
+        actionStrikeOutHandler = new ActionStrikeOutHandler();
+
+        sscsCaseData = SscsCaseData.builder().build();
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+    }
+
+
+    @Test
+    @Parameters({
+        "ACTION_STRIKE_OUT, ABOUT_TO_SUBMIT, true",
+    })
+    public void givenEvent_thenCanHandle(EventType eventType, CallbackType callbackType, boolean expected) {
+        when(callback.getEvent()).thenReturn(eventType);
+        assertEquals(expected, actionStrikeOutHandler.canHandle(callbackType, callback));
+    }
+
+    @Test
+    public void setField() {
+    }
+}
