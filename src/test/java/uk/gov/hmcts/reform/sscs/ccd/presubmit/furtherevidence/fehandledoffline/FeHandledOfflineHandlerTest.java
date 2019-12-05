af1ecaa1ef4bc20ce62867be3ae8429a5bbaf620
david.crespo <david.crespo@hmcts.net>
2019-12-05 13:39:32 +0000

    parametrise test to add more scenarios easily including edge scenarios

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/fehandledoffline/FeHandledOfflineHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/fehandledoffline/FeHandledOfflineHandlerTest.java
@@ -4,29 +4,40 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(JUnitParamsRunner.class)
 public class FeHandledOfflineHandlerTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Mock
     private Callback<SscsCaseData> callback;
 
     @Test
-    public void givenEventIsTriggered_thenCanHandle() {
+    @Parameters({
+        "FURTHER_EVIDENCE_HANDLED_OFFLINE, ABOUT_TO_SUBMIT, true"
+    })
+    public void givenEventIsTriggered_thenCanHandle(EventType eventType, CallbackType callbackType,
+                                                    boolean expectation) {
         FeHandledOfflineHandler feHandledOfflineHandler = new FeHandledOfflineHandler();
-        given(callback.getEvent()).willReturn(EventType.FURTHER_EVIDENCE_HANDLED_OFFLINE);
+        given(callback.getEvent()).willReturn(eventType);
 
-        boolean actual = feHandledOfflineHandler.canHandle(CallbackType.ABOUT_TO_SUBMIT, callback);
+        boolean actual = feHandledOfflineHandler.canHandle(callbackType, callback);
 
-        assertThat(actual, is(true));
+        assertThat(actual, is(expectation));
     }
 
     @Test
