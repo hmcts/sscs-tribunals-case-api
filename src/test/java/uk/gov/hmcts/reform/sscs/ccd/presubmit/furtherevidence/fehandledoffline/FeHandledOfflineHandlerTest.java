23f324e4ec9a4886cd5d0d25d044c3ff7f577764
david.crespo <david.crespo@hmcts.net>
2019-12-05 13:53:25 +0000

    add more scenarios to cover the canHandle including edge cases
    Add production code to make them pass

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/fehandledoffline/FeHandledOfflineHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/fehandledoffline/FeHandledOfflineHandlerTest.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.furtherevidence.fehandledoffline;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import junitparams.converters.Nullable;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,19 +27,28 @@ public class FeHandledOfflineHandlerTest {
 
     @Mock
     private Callback<SscsCaseData> callback;
+    private final FeHandledOfflineHandler feHandledOfflineHandler = new FeHandledOfflineHandler();
 
     @Test
     @Parameters({
-        "FURTHER_EVIDENCE_HANDLED_OFFLINE, ABOUT_TO_SUBMIT, true"
+        "FURTHER_EVIDENCE_HANDLED_OFFLINE, ABOUT_TO_SUBMIT, true",
+        "APPEAL_RECEIVED, ABOUT_TO_SUBMIT, false",
+        "FURTHER_EVIDENCE_HANDLED_OFFLINE, ABOUT_TO_START, false",
+        "null, ABOUT_TO_SUBMIT, false",
+        "FURTHER_EVIDENCE_HANDLED_OFFLINE, null, false"
     })
-    public void givenEventIsTriggered_thenCanHandle(EventType eventType, CallbackType callbackType,
+    public void givenEventIsTriggered_thenCanHandle(@Nullable EventType eventType, @Nullable CallbackType callbackType,
                                                     boolean expectation) {
-        FeHandledOfflineHandler feHandledOfflineHandler = new FeHandledOfflineHandler();
         given(callback.getEvent()).willReturn(eventType);
 
         boolean actual = feHandledOfflineHandler.canHandle(callbackType, callback);
 
         assertThat(actual, is(expectation));
+    }
+
+    @Test
+    public void givenCallbackIsNull_shouldReturnFalse() {
+        assertFalse(feHandledOfflineHandler.canHandle(CallbackType.ABOUT_TO_SUBMIT, null));
     }
 
     @Test
