e103c03c29a3042baeaa4e53c5ed750eb6e06c48
jackmaloney <jack.maloney@transformuk.com>
2019-12-16 14:40:17 +0000

    SSCS-6979 Callback handler for request info event

---
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/RequestInfoIt.java
@@ -0,0 +1,59 @@
+package uk.gov.hmcts.reform.sscs.callback;
+
+import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertEquals;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.State.INCOMPLETE_APPLICATION_INFORMATION_REQUESTED;
+import static uk.gov.hmcts.reform.sscs.helper.IntegrationTestHelper.assertHttpStatus;
+import static uk.gov.hmcts.reform.sscs.helper.IntegrationTestHelper.getRequestWithAuthHeader;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.IOException;
+import java.util.Collections;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.controller.CcdCallbackController;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RequestInfoIt extends AbstractEventIt {
+
+    @Before
+    public void setup() throws IOException {
+        CcdCallbackController controller = new CcdCallbackController(authorisationService, deserializer, dispatcher);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mapper.registerModule(new JavaTimeModule());
+        json = getJson("callback/requestInfoCallback.json");
+    }
+
+    @Test
+    public void callbackToRequestInfoHandler_willUpdateStateToIncompleteApplicationInfoRequested() throws Exception {
+        json = json.replaceAll("STATE_TO_UPDATE", "interlocutoryReviewState");
+
+        MockHttpServletResponse response = getResponse(getRequestWithAuthHeader(json, "/ccdAboutToSubmit"));
+        assertHttpStatus(response, HttpStatus.OK);
+        PreSubmitCallbackResponse<SscsCaseData> result = deserialize(response.getContentAsString());
+
+        assertEquals(Collections.EMPTY_SET, result.getErrors());
+
+        assertEquals(INCOMPLETE_APPLICATION_INFORMATION_REQUESTED, result.getData().getState());
+    }
+
+    @Test
+    public void callbackToRequestInfoHandler_willNotUpdateUpdateStateWhenNotAppropriate() throws Exception {
+        MockHttpServletResponse response = getResponse(getRequestWithAuthHeader(json, "/ccdAboutToSubmit"));
+        assertHttpStatus(response, HttpStatus.OK);
+        PreSubmitCallbackResponse<SscsCaseData> result = deserialize(response.getContentAsString());
+
+        assertEquals(Collections.EMPTY_SET, result.getErrors());
+
+        assertNull(result.getData().getState());
+    }
+
+}
