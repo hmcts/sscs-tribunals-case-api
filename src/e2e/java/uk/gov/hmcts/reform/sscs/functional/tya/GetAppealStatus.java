ef328f92a6ba296e5b76d8c908ecd17b37c8c0d0
Chris <christopher.davidson@btinternet.com>
2019-12-17 12:17:50 +0000

    order imports checkstyle

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/tya/GetAppealStatus.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/tya/GetAppealStatus.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.sscs.functional.tya;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.restassured.RestAssured;
+import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,9 +16,6 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.functional.handlers.BaseHandler;
 
-import java.io.IOException;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource(locations = "classpath:config/application_e2e.properties")
