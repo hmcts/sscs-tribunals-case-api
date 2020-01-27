4cc4c858db0b0491f5c054041334502453134a19
srochani <sunil.rochani@hotmail.com>
2020-01-27 15:59:41 +0000

    SSCS-7079 Interloc – Clear the interlocReviewState flag if case goes dormant or void

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/hmctslapsecase/HmctsLapseCaseAboutToSubmitHandlerTest.java
@@ -0,0 +1,75 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.hmctslapsecase;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+
+import java.util.Collections;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.InterlocReviewState;
+
+
+
+@RunWith(JUnitParamsRunner.class)
+public class HmctsLapseCaseAboutToSubmitHandlerTest {
+    private static final String USER_AUTHORISATION = "Bearer token";
+    private HmctsLapseCaseAboutToSubmitHandler handler;
+
+    @Mock
+    private Callback<SscsCaseData> callback;
+
+    @Mock
+    private CaseDetails<SscsCaseData> caseDetails;
+    private SscsCaseData sscsCaseData;
+
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        handler = new HmctsLapseCaseAboutToSubmitHandler();
+
+        when(callback.getEvent()).thenReturn(EventType.HMCTS_LAPSE_CASE);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        sscsCaseData = SscsCaseData.builder().ccdCaseId("ccdId").appeal(Appeal.builder().build())
+                .interlocReviewState(InterlocReviewState.AWAITING_ADMIN_ACTION.getId()).build();
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+    }
+
+    @Test
+    @Parameters({"ABOUT_TO_START", "MID_EVENT", "SUBMITTED"})
+    public void givenANonCallbackType_thenReturnFalse(CallbackType callbackType) {
+        assertFalse(handler.canHandle(callbackType, callback));
+    }
+
+    @Test
+    public void setsInterlocReviewStateForHmctsLapseCaseEvent() {
+
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        assertEquals(Collections.EMPTY_SET, response.getErrors());
+
+        assertNull(response.getData().getInterlocReviewState());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void throwsExceptionIfItCannotHandleTheAppeal() {
+        when(callback.getEvent()).thenReturn(EventType.APPEAL_RECEIVED);
+        handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+    }
+
+}
