69ca8e628058e73bbc6cf9d74c887c98bbd6c251
jackmaloney <jack.maloney@transformuk.com>
2020-02-19 14:44:47 +0000

    Add log statement before hitting bundling service

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createbundle/CreateBundleAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createbundle/CreateBundleAboutToStartHandler.java
@@ -63,6 +63,7 @@ public class CreateBundleAboutToStartHandler implements PreSubmitCallbackHandler
                 }
             }
         }
+
         return bundleRequestExecutor.post(callback, bundleUrl + CREATE_BUNDLE_ENDPOINT);
     }
 
