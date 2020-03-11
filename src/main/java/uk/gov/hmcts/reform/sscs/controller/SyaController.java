dfd36cf850c9e32f6315a0981566ecc1f76289a8
srochani <sunil.rochani@hotmail.com>
2020-03-11 08:31:42 +0000

    SSCS-7299: IDAM - Review

--- src/main/java/uk/gov/hmcts/reform/sscs/controller/SyaController.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/controller/SyaController.java
@@ -16,7 +16,6 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
