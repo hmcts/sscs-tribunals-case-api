60ee56fb9725c352f774dff57f8671fe82804507
srochani <sunil.rochani@hotmail.com>
2020-03-17 11:41:08 +0000

    SSCS-7299: IDAM Review

--- src/main/java/uk/gov/hmcts/reform/sscs/TribunalsCaseApiApplication.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/TribunalsCaseApiApplication.java
@@ -20,7 +20,8 @@ import uk.gov.hmcts.reform.sscs.ccd.config.CcdRequestDetails;
                 "uk.gov.hmcts.reform.authorisation",
                 "uk.gov.hmcts.reform.sscs.idam",
                 "uk.gov.hmcts.reform.sscs.document",
-                "uk.gov.hmcts.reform.docassembly"
+                "uk.gov.hmcts.reform.docassembly",
+                "uk.gov.hmcts.reform.auth"
         })
 @EnableScheduling
 public class TribunalsCaseApiApplication {
