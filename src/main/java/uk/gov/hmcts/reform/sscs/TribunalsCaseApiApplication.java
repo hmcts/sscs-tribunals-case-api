daf6a5af4d2a3d1f4e95ad843a8b88c91d9f4893
srochani <sunil.rochani@hotmail.com>
2020-03-17 11:58:49 +0000

    SSCS-7299: IDAM Review

--- src/main/java/uk/gov/hmcts/reform/sscs/TribunalsCaseApiApplication.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/TribunalsCaseApiApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -21,8 +22,8 @@ import uk.gov.hmcts.reform.sscs.ccd.config.CcdRequestDetails;
                 "uk.gov.hmcts.reform.sscs.idam",
                 "uk.gov.hmcts.reform.sscs.document",
                 "uk.gov.hmcts.reform.docassembly",
-                "uk.gov.hmcts.reform.auth"
         })
+@ComponentScan(basePackages = {"uk.gov.hmcts.reform"})
 @EnableScheduling
 public class TribunalsCaseApiApplication {
 
