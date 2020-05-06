01e1acd2a0cbb5add1c56cdb10989c83ebd32284
jackmaloney <jack.maloney@transformuk.com>
2020-05-06 13:07:37 +0100

    Functional test for sscs1 form

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/issuedecision/IssueDecisionAboutToSubmitHandler.java
@@ -0,0 +1,54 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.issuedecision;
+
+import static uk.gov.hmcts.reform.sscs.ccd.callback.DecisionType.STRIKE_OUT;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.IssueDocumentHandler;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.sscs.service.FooterService;
+
+@Service
+@Slf4j
+public class IssueDecisionAboutToSubmitHandler extends IssueDocumentHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    private final FooterService footerService;
+
+    @Autowired
+    public IssueDecisionAboutToSubmitHandler(FooterService footerService) {
+        this.footerService = footerService;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        return callbackType == CallbackType.ABOUT_TO_SUBMIT
+                && callback.getEvent().getCcdType().equals("issueDecision")
+                && Objects.nonNull(callback.getCaseDetails())
+                && Objects.nonNull(callback.getCaseDetails().getCaseData());
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback, String userAuthorisation) {
+
+        SscsCaseData caseData = callback.getCaseDetails().getCaseData();
+
+
+
+        PreSubmitCallbackResponse<SscsCaseData> sscsCaseDataPreSubmitCallbackResponse = new PreSubmitCallbackResponse<>(caseData);
+
+        return sscsCaseDataPreSubmitCallbackResponse;
+    }
+
+}
