e85fbcf759a3acde2339047a02a6a8edbfe7bbfa
srochani <sunil.rochani@hotmail.com>
2020-05-13 08:29:58 +0100

    SSCS-7331: COR/MYA/Tribunals – Move sscs-cor-backend to tribunals

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/mya/BaseFunctionTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/mya/BaseFunctionTest.java
@@ -21,7 +21,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.sscs.TribunalsCaseApiApplication;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
