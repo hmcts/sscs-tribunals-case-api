f30354599877743b99a3ab7ccb4e076705739103
Michael lavelle <michael@lavelle.name>
2020-06-12 10:14:15 +0100

    SSCS-7555 Further unit tests

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/ActivityAnswerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/ActivityAnswerTest.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 
 import org.junit.Test;
 
@@ -19,6 +21,56 @@ public class ActivityAnswerTest {
         assertEquals("answerLetter", activityAnswer.getActivityAnswerLetter());
         assertEquals(10, activityAnswer.getActivityAnswerPoints());
         assertEquals("1", activityAnswer.getActivityAnswerNumber());
+
+        assertEquals("ActivityAnswer(activityAnswerNumber=1, "
+            + "activityAnswerLetter=answerLetter, activityAnswerValue=answerValue, activityAnswerPoints=10)", activityAnswer.toString());
+
+    }
+
+    @Test
+    public void testActivityAnswerConstructor() {
+        ActivityAnswer activityAnswer = new ActivityAnswer(
+            "1",
+            "answerLetter",
+            "answerValue",
+            10);
+
+        assertEquals("answerValue", activityAnswer.getActivityAnswerValue());
+        assertEquals("answerLetter", activityAnswer.getActivityAnswerLetter());
+        assertEquals(10, activityAnswer.getActivityAnswerPoints());
+        assertEquals("1", activityAnswer.getActivityAnswerNumber());
+
+        assertEquals("ActivityAnswer(activityAnswerNumber=1, "
+            + "activityAnswerLetter=answerLetter, activityAnswerValue=answerValue, activityAnswerPoints=10)", activityAnswer.toString());
+
+    }
+
+    @Test
+    public void testActivityAnswerHashcodeAndEquals() {
+        ActivityAnswer activityAnswer1 = new ActivityAnswer(
+            "1",
+            "answerLetter",
+            "answerValue",
+            10);
+
+        ActivityAnswer activityAnswer2 = new ActivityAnswer(
+            "1",
+            "answerLetter",
+            "answerValue",
+            10);
+
+
+        ActivityAnswer activityAnswer3 = new ActivityAnswer(
+            "2",
+            "answerLetter",
+            "answerValue",
+            10);
+
+        assertEquals(activityAnswer1, activityAnswer2);
+        assertEquals(activityAnswer1.hashCode(), activityAnswer2.hashCode());
+        assertNotEquals(activityAnswer1, activityAnswer3);
+        assertNotEquals(activityAnswer1.hashCode(), activityAnswer3.hashCode());
+
     }
 
 }
--- src/test/java/uk/gov/hmcts/reform/sscs/model/docassembly/DescriptorTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/model/docassembly/DescriptorTest.java
@@ -21,6 +21,61 @@ public class DescriptorTest {
         assertEquals("1", descriptor.getActivityQuestionNumber());
         assertEquals(10, descriptor.getActivityAnswerPoints());
         assertEquals("answerLetter", descriptor.getActivityAnswerLetter());
+
+        assertEquals("Descriptor(activityQuestionNumber=1, "
+            + "activityQuestionValue=questionValue, activityAnswerLetter=answerLetter, activityAnswerValue=answerValue, activityAnswerPoints=10)", descriptor.toString());
+    }
+
+    @Test
+    public void testConstructor() {
+        Descriptor descriptor = new Descriptor(
+            "1",
+            "questionValue",
+            "answerLetter",
+           "answerValue",
+            10);
+
+        assertEquals("questionValue", descriptor.getActivityQuestionValue());
+        assertEquals("answerValue", descriptor.getActivityAnswerValue());
+        assertEquals("1", descriptor.getActivityQuestionNumber());
+        assertEquals(10, descriptor.getActivityAnswerPoints());
+        assertEquals("answerLetter", descriptor.getActivityAnswerLetter());
+
+        assertEquals("Descriptor(activityQuestionNumber=1, "
+            + "activityQuestionValue=questionValue, activityAnswerLetter=answerLetter, activityAnswerValue=answerValue, activityAnswerPoints=10)", descriptor.toString());
+    }
+
+    @Test
+    public void testDescriptorEqualsAndHashcode() {
+        Descriptor descriptor1 = Descriptor.builder()
+            .activityQuestionValue("questionValue")
+            .activityAnswerValue("answerValue")
+            .activityAnswerLetter("answerLetter")
+            .activityQuestionNumber("1")
+            .activityAnswerPoints(10)
+            .build();
+
+        Descriptor descriptor2 = Descriptor.builder()
+            .activityQuestionValue("questionValue")
+            .activityAnswerValue("answerValue")
+            .activityAnswerLetter("answerLetter")
+            .activityQuestionNumber("1")
+            .activityAnswerPoints(10)
+            .build();
+
+        Descriptor descriptor3 = Descriptor.builder()
+            .activityQuestionValue("questionValue2")
+            .activityAnswerValue("answerValue")
+            .activityAnswerLetter("answerLetter")
+            .activityQuestionNumber("1")
+            .activityAnswerPoints(10)
+            .build();
+
+        assertEquals(descriptor1, descriptor2);
+        assertEquals(descriptor1.hashCode(), descriptor2.hashCode());
+        assertNotEquals(descriptor1, descriptor3);
+        assertNotEquals(descriptor1.hashCode(), descriptor3.hashCode());
+
     }
 
 }
