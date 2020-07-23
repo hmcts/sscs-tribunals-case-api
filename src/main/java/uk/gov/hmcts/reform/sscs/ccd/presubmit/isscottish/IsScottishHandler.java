fad6fd4c7c62cb268c43f1dcfe33022d3b6b6004
Dan Hamilton <dan@emagineers.co.uk>
2020-07-23 11:19:47 +0100

    fix isScottish logging

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/isscottish/IsScottishHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/isscottish/IsScottishHandler.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.sscs.ccd.presubmit.isscottish;
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.isScottish;
 
 import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
