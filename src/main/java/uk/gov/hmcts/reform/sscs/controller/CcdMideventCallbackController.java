13f7b886dfa4760e5830704fb8a2a8339e4a0bbd
Michael lavelle <michael@lavelle.name>
2020-08-06 16:13:40 +0100

    SSCS-7868 Adding mid event callback controller for population of dynamic venue list

--- src/main/java/uk/gov/hmcts/reform/sscs/controller/CcdMideventCallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/controller/CcdMideventCallbackController.java
@@ -4,6 +4,8 @@ import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.ResponseEntity.ok;
 import static uk.gov.hmcts.reform.sscs.service.AuthorisationService.SERVICE_AUTHORISATION_HEADER;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +17,14 @@ import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.deserialisation.SscsCaseCallbackDeserializer;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicList;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicListItem;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.adjourncase.AdjournCasePreviewService;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.WriteFinalDecisionPreviewDecisionService;
+import uk.gov.hmcts.reform.sscs.model.VenueDetails;
 import uk.gov.hmcts.reform.sscs.service.AuthorisationService;
+import uk.gov.hmcts.reform.sscs.service.VenueDataLoader;
 
 @RestController
 @Slf4j
@@ -28,16 +34,61 @@ public class CcdMideventCallbackController {
     private final SscsCaseCallbackDeserializer deserializer;
     private final WriteFinalDecisionPreviewDecisionService writeFinalDecisionPreviewDecisionService;
     private final AdjournCasePreviewService adjournCasePreviewService;
+    private final VenueDataLoader venueDataLoader;
 
     @Autowired
     public CcdMideventCallbackController(AuthorisationService authorisationService, SscsCaseCallbackDeserializer deserializer,
                                          WriteFinalDecisionPreviewDecisionService writeFinalDecisionPreviewDecisionService,
-                                            AdjournCasePreviewService adjournCasePreviewService) {
+                                            AdjournCasePreviewService adjournCasePreviewService, VenueDataLoader venueDataLoader) {
         this.authorisationService = authorisationService;
         this.deserializer = deserializer;
         this.writeFinalDecisionPreviewDecisionService = writeFinalDecisionPreviewDecisionService;
         this.adjournCasePreviewService = adjournCasePreviewService;
+        this.venueDataLoader = venueDataLoader;
     }
+
+    @PostMapping(path = "/ccdMidEventAdjournCasePopulateVenueDropdown")
+    public ResponseEntity<PreSubmitCallbackResponse<SscsCaseData>> ccdMidEventAdjournCasePopulateVenueDropdown(
+        @RequestHeader(SERVICE_AUTHORISATION_HEADER) String serviceAuthHeader,
+        @RequestHeader(AUTHORIZATION) String userAuthorisation,
+        @RequestBody String message) {
+        Callback<SscsCaseData> callback = deserializer.deserialize(message);
+        log.info("About to start ccdMidEventPreviewFinalDecision callback `{}` received for Case ID `{}`", callback.getEvent(),
+            callback.getCaseDetails().getId());
+
+        authorisationService.authorise(serviceAuthHeader);
+
+        SscsCaseData sscsCaseData = callback.getCaseDetails().getCaseData();
+
+        PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
+
+        List<DynamicListItem> itemList = venueDataLoader.getVenueDetailsMap()
+            .entrySet().stream()
+            .filter(e -> isRegionalProcessingCentreMatch(e.getValue().getRegionalProcessingCentre(),
+                sscsCaseData.getRegionalProcessingCenter().getName()))
+            .map(e -> new DynamicListItem(e.getKey(),
+                getVenueDisplayString(e.getValue()))).collect(Collectors.toList());
+
+        DynamicList list = new DynamicList(new DynamicListItem("", ""), itemList);
+
+        sscsCaseData.setAdjournCaseNextHearingVenueSelected(list);
+
+        return ok(preSubmitCallbackResponse);
+    }
+
+    private String getVenueDisplayString(VenueDetails venueDetails) {
+        return venueDetails.getVenName() + ", "
+            + venueDetails.getVenAddressLine1() + ", "
+            + venueDetails.getVenAddressLine2() + ", "
+            + venueDetails.getVenAddressTown() + ", "
+            + venueDetails.getVenAddressCounty() + ", "
+            + venueDetails.getVenAddressPostcode();
+    }
+
+    private boolean isRegionalProcessingCentreMatch(String value, String rpc) {
+        return value.substring(5).equalsIgnoreCase(rpc);
+    }
+
 
     @PostMapping(path = "/ccdMidEventPreviewFinalDecision")
     public ResponseEntity<PreSubmitCallbackResponse<SscsCaseData>> ccdMidEventPreviewFinalDecision(
