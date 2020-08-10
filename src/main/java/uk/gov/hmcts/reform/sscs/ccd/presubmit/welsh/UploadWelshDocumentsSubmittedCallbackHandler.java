a73c0b6c9465e143722d2c0cd443b7d6f19d9bda
Rishi Sharma <rishi.sharma@hmcts.net>
2020-08-10 08:30:44 +0100

    WLA-608: SSCS - Upload Translated Document Test added

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/welsh/UploadWelshDocumentsSubmittedCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/welsh/UploadWelshDocumentsSubmittedCallbackHandler.java
@@ -6,27 +6,21 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
-import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentLink;
-import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicList;
-import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicListItem;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocument;
-import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocumentDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocumentTranslationStatus;
-import uk.gov.hmcts.reform.sscs.ccd.domain.SscsWelshDocumentDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsWelshDocuments;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
@@ -35,6 +29,14 @@ import static java.util.Objects.requireNonNull;
 public class UploadWelshDocumentsSubmittedCallbackHandler implements PreSubmitCallbackHandler<SscsCaseData> {
     private final CcdService ccdService;
     private final IdamService idamService;
+
+    private static Map<String, String> nextEventMap = new HashMap<>();
+
+    static {
+        nextEventMap.put("sscs1","sendToDwp");
+        nextEventMap.put("sscs1","sendToDwp");
+        nextEventMap.put("sscs1","sendToDwp");
+    }
 
     @Autowired
     public UploadWelshDocumentsSubmittedCallbackHandler(CcdService ccdService, IdamService idamService) {
@@ -46,7 +48,7 @@ public class UploadWelshDocumentsSubmittedCallbackHandler implements PreSubmitCa
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
         requireNonNull(callback, "callback must not be null");
         requireNonNull(callbackType, "callbackType must not be null");
-        DynamicList welshDocuments = callback.getCaseDetails().getCaseData().getOriginalDocuments();
+
         return callbackType.equals(CallbackType.SUBMITTED)
                 && callback.getEvent().equals(EventType.UPLOAD_WELSH_DOCUMENT);
     }
@@ -64,64 +66,47 @@ public class UploadWelshDocumentsSubmittedCallbackHandler implements PreSubmitCa
     }
 
     private SscsCaseDetails updateCase(Callback<SscsCaseData> callback, SscsCaseData caseData) {
-
-        List<SscsDocument> documents = new ArrayList<>();
-        List<SscsWelshDocuments> sscsWelshDocumentsList = new ArrayList<>();
-        List<SscsDocument> sscsDocuments =  Optional.ofNullable(caseData).map(SscsCaseData::getSscsDocument)
-                .orElse(Collections.emptyList())
-                .stream()
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-
-        List<SscsWelshDocuments> sscsWelshDocuments =
-                new ArrayList<>(Optional.ofNullable(caseData).map(SscsCaseData::getSscsWelshDocuments)
-                .orElse(Collections.emptyList()));
-
-
-        if(sscsDocuments.size() > 0) {
-            for (SscsDocument sscsDocument : sscsDocuments) {
-                if(sscsDocument.getValue().getDocumentLink().getDocumentFilename().equals(caseData.getOriginalDocuments().getValue().getCode()) &&
-                        sscsDocument.getValue().getDocumentTranslationStatus().equals(SscsDocumentTranslationStatus.TRANSLATION_REQUESTED))
-                 {
-                     SscsDocument  _sscsDocument = SscsDocument.builder().value(SscsDocumentDetails.builder()
-                            .documentType(sscsDocument.getValue().getDocumentType())
-                            .documentFileName(sscsDocument.getValue().getDocumentLink().getDocumentFilename())
-                            .documentLink(sscsDocument.getValue().getDocumentLink())
-                            .documentTranslationStatus(SscsDocumentTranslationStatus.TRANSLATION_COMPLETE)
-                            .build()).build();
-                        documents.add(_sscsDocument);
-                } else {
-                    SscsDocument _sscsDocument = SscsDocument.builder().value(SscsDocumentDetails.builder()
-                            .documentType(sscsDocument.getValue().getDocumentType())
-                            .documentFileName(sscsDocument.getValue().getDocumentLink().getDocumentFilename())
-                            .documentLink(sscsDocument.getValue().getDocumentLink())
-                            .documentTranslationStatus(SscsDocumentTranslationStatus.TRANSLATION_NOT_REQUIRED)
-                            .build()).build();
-                    documents.add(_sscsDocument);
+        int outStandingDocumentFlag = 0;
+        String nextEvent;
+        for (SscsDocument sscsDocument : caseData.getSscsDocument()) {
+            if (sscsDocument.getValue().getDocumentTranslationStatus() !=null &&
+                    sscsDocument.getValue().getDocumentTranslationStatus().equals(SscsDocumentTranslationStatus.TRANSLATION_REQUESTED)) {
+                outStandingDocumentFlag++;
+                if (sscsDocument.getValue().getDocumentLink().getDocumentFilename().equals(caseData.getOriginalDocuments().getValue().getCode())) {
+                    sscsDocument.getValue().setDocumentTranslationStatus(SscsDocumentTranslationStatus.TRANSLATION_COMPLETE);
                 }
             }
-            caseData.setSscsDocument(documents);
+        }
+        log.info("outStandingDocumentFlag  {}",outStandingDocumentFlag);
+        for (SscsWelshDocuments sscsWelshPreviewDocuments : caseData.getSscsWelshPreviewDocuments()) {
+            sscsWelshPreviewDocuments.getValue().setOriginalDocumentFileName(caseData.getOriginalDocuments().getValue().getCode());
+            nextEvent = sscsWelshPreviewDocuments.getValue().getDocumentType();
+            log.info("nextEvent  {}",nextEvent);
+            if(caseData.getSscsWelshDocuments() != null) {
+                caseData.getSscsWelshDocuments().add(sscsWelshPreviewDocuments);
+            } else {
+                List<SscsWelshDocuments> sscsWelshDocumentsList =  new ArrayList<>();
+                sscsWelshDocumentsList.add(sscsWelshPreviewDocuments);
+                caseData.setSscsWelshDocuments(sscsWelshDocumentsList);
+            }
         }
 
-        if(sscsWelshDocuments.size() > 0) {
-            for (SscsWelshDocuments sscsWelshDocument : sscsWelshDocuments) {
-                SscsWelshDocuments _sscsWelshDocument =
-                        sscsWelshDocument.builder().value(SscsWelshDocumentDetails.builder()
-                        .documentType(sscsWelshDocument.getValue().getDocumentType())
-                        .documentFileName(sscsWelshDocument.getValue().getDocumentLink().getDocumentFilename())
-                        .documentLink(sscsWelshDocument.getValue().getDocumentLink())
-                        .originalDocumentFileName(caseData.getOriginalDocuments().getValue().getCode())
-                        .documentComment(sscsWelshDocument.getValue().getDocumentComment())
-                        .documentLanguage(sscsWelshDocument.getValue().getDocumentLanguage())
-                        .build()).build();
-
-                sscsWelshDocumentsList.add(_sscsWelshDocument);
-            }
-            caseData.setSscsWelshDocuments(sscsWelshDocumentsList);
+        //clear the Preview collection
+        caseData.setSscsWelshPreviewDocuments(new ArrayList<>());
+        if(outStandingDocumentFlag <= 1) {
+            log.info("outStandingDocumentFlag if {}",outStandingDocumentFlag);
+            caseData.setTranslationWorkOutstanding("No");
+        } else {
+            log.info("outStandingDocumentFlag else {}",outStandingDocumentFlag);
         }
 
         return ccdService.updateCase(caseData, callback.getCaseDetails().getId(),
                 EventType.UPLOAD_WELSH_DOCUMENT.getCcdType(), "Update document translation status",
                 "Update document translation status", idamService.getIdamTokens());
+    }
+
+    private String getNextEvent(String documentType){
+        return nextEventMap.get(documentType) != null ? nextEventMap.get(documentType):
+                EventType.UPLOAD_WELSH_DOCUMENT.getCcdType();
     }
 }
