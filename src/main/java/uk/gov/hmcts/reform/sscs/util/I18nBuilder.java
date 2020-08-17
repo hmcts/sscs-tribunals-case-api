111f13ad438c4a675c2b86112ec1338ef67bbbb4
jackmaloney <jack.maloney@transformuk.com>
2020-08-17 10:57:29 +0100

    SSCS-7934 Fortify - close access to resource

--- src/main/java/uk/gov/hmcts/reform/sscs/util/I18nBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/util/I18nBuilder.java
@@ -5,13 +5,38 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.util.IOUtils;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class I18nBuilder {
+
     public Map build() throws IOException {
-        InputStream in = getClass().getResourceAsStream("/json/en.json");
-        return new ObjectMapper().readValue(IOUtils.toByteArray(in), HashMap.class);
+        InputStream in = null;
+        try {
+            in = getClass().getResourceAsStream("/json/en.json");
+        } finally {
+            if (in != null) {
+                byte[] byteArray = IOUtils.toByteArray(in);
+                HashMap hashObject = new ObjectMapper().readValue(byteArray, HashMap.class);
+
+                safeClose(in);
+
+                return hashObject;
+            }
+        }
+        return null;
+    }
+
+    public static void safeClose(InputStream in) {
+        if (in != null) {
+            try {
+                in.close();
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+        }
     }
 }
