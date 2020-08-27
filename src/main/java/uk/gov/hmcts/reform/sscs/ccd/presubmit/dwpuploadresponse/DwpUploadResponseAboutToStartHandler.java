2cc549ad62e39eda42ca136bd6e266779b2c7ced
Michael lavelle <michael@lavelle.name>
2020-08-27 16:47:03 +0100

    SSCS-8049 Checkstyle

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/dwpuploadresponse/DwpUploadResponseAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/dwpuploadresponse/DwpUploadResponseAboutToStartHandler.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.dwpuploadresponse;
 
 import static java.util.Objects.requireNonNull;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
