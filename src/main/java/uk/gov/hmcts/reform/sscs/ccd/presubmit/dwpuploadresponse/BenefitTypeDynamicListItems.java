eac090faf51501d65f6dcbec7a375afc0c3d3741
Michael lavelle <michael@lavelle.name>
2020-08-27 16:39:23 +0100

    SSCS-8049 Adding DwpUploadResponse about to start handler to populate benefit type dropdown

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/dwpuploadresponse/BenefitTypeDynamicListItems.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.dwpuploadresponse;
+
+import java.util.Arrays;
+import java.util.Optional;
+import lombok.Getter;
+import uk.gov.hmcts.reform.sscs.ccd.domain.BenefitType;
+
+@Getter
+public enum BenefitTypeDynamicListItems {
+    PIP("pip", "PIP"),
+    ESA("esa", "ESA"),
+    UC("uc", "UC");
+
+    String code;
+    String label;
+
+    public static Optional<BenefitTypeDynamicListItems> findByBenefitType(BenefitType benefitType) {
+        return Arrays.stream(BenefitTypeDynamicListItems.values())
+            .filter(b -> b.getCode().equalsIgnoreCase(benefitType.getCode())).findFirst();
+    }
+
+    BenefitTypeDynamicListItems(String code, String label) {
+        this.code = code;
+        this.label = label;
+    }
+}
