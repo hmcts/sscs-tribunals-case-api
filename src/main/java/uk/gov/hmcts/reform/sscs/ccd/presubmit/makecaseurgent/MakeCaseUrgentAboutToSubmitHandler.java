79478c2d6dc97bdaf84f990d2b7c9a9613da2345
sunilrochani <sunil.rochani@hotmail.com>
2020-10-13 17:51:37 +0100

    SSCS-7985: CCD UI – Repurpose the makeCaseUrgent event

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/makecaseurgent/MakeCaseUrgentAboutToSubmitHandler.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.makecaseurgent;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.RequestOutcome;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.InterlocReviewState;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+
+@Component
+@Slf4j
+public class MakeCaseUrgentAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    @Autowired
+    public MakeCaseUrgentAboutToSubmitHandler() {
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        return callbackType == CallbackType.ABOUT_TO_SUBMIT
+            && callback.getEvent() == EventType.MAKE_CASE_URGENT
+            && Objects.nonNull(callback.getCaseDetails())
+            && Objects.nonNull(callback.getCaseDetails().getCaseData());
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback, String userAuthorisation) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        SscsCaseData sscsCaseData = callback.getCaseDetails().getCaseData();
+
+        sscsCaseData.setUrgentCase("Yes");
+        sscsCaseData.setInterlocReviewState(InterlocReviewState.REVIEW_BY_JUDGE.getId());
+        sscsCaseData.setUrgentHearingRegistered(LocalDate.now());
+        sscsCaseData.setUrgentHearingOutcome(RequestOutcome.IN_PROGRESS.getValue());
+
+        PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
+
+        return preSubmitCallbackResponse;
+    }
+
+}
