515a5c09fb518049c928afbf8a417aec0657448c
sunilrochani <sunil.rochani@hotmail.com>
2020-10-15 10:02:41 +0100

    SSCS-7985: CCD UI – Repurpose the makeCaseUrgent event

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/makecaseurgent/MakeCaseUrgentAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/makecaseurgent/MakeCaseUrgentAboutToSubmitHandler.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.makecaseurgent;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
