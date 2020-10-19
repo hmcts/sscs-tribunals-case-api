ecff62e82f6288cafe9d387ce991a585db77bd4a
chrisdavecm <christopher.davidson@btinternet.com>
2020-10-19 16:55:37 +0100

    sscs 8237 New DWP raise exception

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/dwpraiseexception/DwpRaiseExceptionAboutToSubmitHandler.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.dwpraiseexception;
+
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.State;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+
+
+@Service
+@Slf4j
+public class DwpRaiseExceptionAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        return callbackType == CallbackType.ABOUT_TO_SUBMIT
+                && callback.getEvent() == EventType.DWP_RAISE_EXCEPTION_NOT_LISTABLE
+                && Objects.nonNull(callback.getCaseDetails())
+                && Objects.nonNull(callback.getCaseDetails().getCaseData());
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback, String userAuthorisation) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        SscsCaseData sscsCaseData = callback.getCaseDetails().getCaseData();
+
+        sscsCaseData.setIsProgressingViaGaps("Yes");
+        sscsCaseData.setState(State.NOT_LISTABLE);
+        sscsCaseData.setCreatedInGapsFrom(State.VALID_APPEAL.getId());
+
+        PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
+
+        return preSubmitCallbackResponse;
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/dwpraiseexception/DwpRaiseExceptionAboutToSubmitHandlerTest.java
@@ -0,0 +1,72 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.dwpraiseexception;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+
+import java.io.IOException;
+import junitparams.JUnitParamsRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+
+@RunWith(JUnitParamsRunner.class)
+public class DwpRaiseExceptionAboutToSubmitHandlerTest {
+
+    private static final String USER_AUTHORISATION = "Bearer token";
+    private DwpRaiseExceptionAboutToSubmitHandler handler;
+
+    @Mock
+    private Callback<SscsCaseData> callback;
+
+    @Mock
+    private CaseDetails<SscsCaseData> caseDetails;
+
+    private SscsCaseData sscsCaseData;
+
+    @Before
+    public void setUp() throws IOException {
+        openMocks(this);
+        handler = new DwpRaiseExceptionAboutToSubmitHandler();
+
+        when(callback.getEvent()).thenReturn(EventType.DWP_RAISE_EXCEPTION_NOT_LISTABLE);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+
+        sscsCaseData = SscsCaseData.builder().ccdCaseId("ccdId")
+                .appeal(Appeal.builder().build())
+                .build();
+
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+    }
+
+    @Test
+    public void givenANonReviewConfidentialityRequestEvent_thenReturnFalse() {
+        when(callback.getEvent()).thenReturn(EventType.DWP_RAISE_EXCEPTION);
+        assertFalse(handler.canHandle(ABOUT_TO_SUBMIT, callback));
+    }
+
+    @Test
+    public void givenAHandleDwpLapseEvent_thenReturnTrue() {
+        assertTrue(handler.canHandle(ABOUT_TO_SUBMIT, callback));
+    }
+
+    @Test
+    public void setInterlocReviewToAdmin() {
+        sscsCaseData = sscsCaseData.toBuilder().state(State.WITH_DWP).build();
+
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        assertEquals("Yes", response.getData().getIsProgressingViaGaps());
+        assertEquals(State.NOT_LISTABLE, response.getData().getState());
+        assertEquals(State.VALID_APPEAL.getId(), response.getData().getCreatedInGapsFrom());
+
+    }
+}
