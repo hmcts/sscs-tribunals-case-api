bf8e6d9d8f74a8af25171ba3d83e6ba0dd29360d
Michael lavelle <michael@lavelle.name>
2020-10-21 14:29:59 +0100

    SSCS-7515 SSCS-7516 Minor refactor and integration test fix

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/PointsCondition.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/PointsCondition.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision;
 
+import static org.apache.commons.collections4.CollectionUtils.emptyIfNull;
+
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
@@ -19,5 +22,25 @@ public interface PointsCondition<E extends Enum<E> & PointsCondition> {
 
     Function<SscsCaseData, List<String>> getAnswersExtractor();
 
-    Optional<String> getOptionalErrorMessage(DecisionNoticeQuestionService questionService, SscsCaseData sscsCaseData);
+    /**
+     * Given a points condition, and an SscsCaseData instance, obtain an error message for that condition if the condition has failed to be satified, or an empty optional if the condition is met.
+     *
+     * @param questionService The question service to use
+     * @param sscsCaseData The SscsCaseData to evaluate against the condition.
+     * @return An optional error message if the condition has failed to be satified, or an empty optional if the condition is met.
+     */
+    default Optional<String> getOptionalErrorMessage(DecisionNoticeQuestionService questionService, SscsCaseData sscsCaseData) {
+
+        Collection<String> collection = emptyIfNull(getAnswersExtractor().apply(sscsCaseData));
+
+        if (collection.isEmpty()) {
+            return Optional.empty();
+        }
+        int totalPoints = collection.stream().map(answerText -> questionService.getAnswerForActivityQuestionKey(sscsCaseData,
+            answerText)).filter(Optional::isPresent).map(Optional::get).mapToInt(ActivityAnswer::getActivityAnswerPoints).sum();
+
+        return getPointsRequirementCondition().test(totalPoints) ? Optional.empty() :
+            Optional.of(getErrorMessage());
+
+    }
 }
