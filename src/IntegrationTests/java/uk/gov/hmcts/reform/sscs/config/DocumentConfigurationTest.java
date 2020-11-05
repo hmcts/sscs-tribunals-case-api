993cfc9bb68950e32631a1dcda6735419ae0499a
Michael lavelle <michael@lavelle.name>
2020-11-05 10:22:54 +0000

    SSCS-7578 Checkstyle

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/config/DocumentConfigurationTest.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/config/DocumentConfigurationTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 import java.util.Optional;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
