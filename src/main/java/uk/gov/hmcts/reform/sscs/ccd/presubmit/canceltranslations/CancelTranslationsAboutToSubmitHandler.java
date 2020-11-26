5bf6415a59f55092e69b693f10aa6992e7afba5b
srochani <sunil.rochani@hotmail.com>
2020-11-26 16:44:24 +0000

    Sscs 8259 auto start stop judge urgenthearing (#1489)
    * SSCS-8258: Tribunals/CCD UI – Welsh auto stop/start for processing urgent hearing docs
    
    * SSCS-8259: Tribunals/CCD UI – Welsh auto stop/start for judge processing urgent hearing
    
    * SSCS-8259: Tribunals/CCD UI – Welsh auto stop/start for judge processing urgent hearing
    
    * SSCS-8259: Tribunals/CCD UI – Welsh auto stop/start for judge processing urgent hearing
    
    * SSCS-8259: Tribunals/CCD UI – Welsh auto stop/start for judge processing urgent hearing
    
    * SSCS-8259: Tribunals/CCD UI – Welsh auto stop/start for judge processing urgent hearing
    
    * SSCS-8259: Tribunals/CCD UI – Welsh auto stop/start for judge processing urgent hearing

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/canceltranslations/CancelTranslationsAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/canceltranslations/CancelTranslationsAboutToSubmitHandler.java
@@ -24,7 +24,7 @@ public class CancelTranslationsAboutToSubmitHandler implements PreSubmitCallback
 
     static {
         nextEventMap.put(DocumentType.SSCS1.getValue(), EventType.SEND_TO_DWP.getCcdType());
-        nextEventMap.put(DocumentType.URGENT_HEARING_REQUEST.getValue(), EventType.MAKE_CASE_URGENT.getCcdType());
+        nextEventMap.put(DocumentType.URGENT_HEARING_REQUEST.getValue(), EventType.UPDATE_CASE_ONLY.getCcdType());
         nextEventMap.put(DocumentType.DECISION_NOTICE.getValue(), EventType.DECISION_ISSUED_WELSH.getCcdType());
         nextEventMap.put(DocumentType.DIRECTION_NOTICE.getValue(), EventType.DIRECTION_ISSUED_WELSH.getCcdType());
     }
