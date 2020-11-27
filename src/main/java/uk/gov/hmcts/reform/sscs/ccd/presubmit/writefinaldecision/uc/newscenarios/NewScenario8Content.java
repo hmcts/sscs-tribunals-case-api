0aa7686d64e622ce57af3b8f22decb49bc2e8c28
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2020-11-27 16:55:12 +0000

    SSCS-8083  - Scenario 8 and 9 (#1591)
    SSCS-8083 - Scenarion 8 and Scenario 9

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/uc/newscenarios/NewScenario8Content.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/uc/newscenarios/NewScenario8Content.java
@@ -7,22 +7,27 @@ import uk.gov.hmcts.reform.sscs.model.docassembly.DescriptorTable;
 import uk.gov.hmcts.reform.sscs.model.docassembly.Paragraph;
 import uk.gov.hmcts.reform.sscs.model.docassembly.WriteFinalDecisionTemplateBody;
 
-public class NewScenario1Content extends UcNewTemplateContent {
 
-    public NewScenario1Content(WriteFinalDecisionTemplateBody writeFinalDecisionTemplateBody) {
+public class NewScenario8Content extends UcNewTemplateContent {
+
+    public NewScenario8Content(WriteFinalDecisionTemplateBody writeFinalDecisionTemplateBody) {
         addComponent(new Paragraph(UcTemplateComponentId.ALLOWED_OR_REFUSED_PARAGRAPH.name(), getAllowedOrRefusedSentence(writeFinalDecisionTemplateBody.isAllowed())));
         addComponent(new Paragraph(
             UcTemplateComponentId.CONFIRMED_OR_SET_ASIDE_PARAGRAPH.name(), getConfirmedOrSetAsideSentence(writeFinalDecisionTemplateBody.isSetAside(), writeFinalDecisionTemplateBody.getDateOfDecision())));
-        addComponent(new Paragraph(UcTemplateComponentId.DOES_NOT_HAVE_LIMITED_CAPABILITY_FOR_WORK_RELATED_ACTIVITY_PARAGRAPH.name(), getDoesNotHaveLimitedCapabilityForWorkSentence(writeFinalDecisionTemplateBody.getAppellantName())));
-        addComponent(new Paragraph(UcTemplateComponentId.INSUFFICIENT_POINTS_PARAGRAPH.name(), getSchedule6PointsSentence(writeFinalDecisionTemplateBody.getUcNumberOfPoints(), false, writeFinalDecisionTemplateBody.getUcSchedule6Descriptors())));
+        addComponent(new Paragraph(
+            UcTemplateComponentId.DOES_HAVE_LIMITED_CAPABILITY_FOR_WORK_PARAGRAPH.name(), getDoesHaveLimitedCapabilityForWorkSentence(writeFinalDecisionTemplateBody.getAppellantName())));
+        addComponent(new Paragraph(UcTemplateComponentId.INSUFFICIENT_POINTS_PARAGRAPH_SCHEDULE_8_PARAGRAPH_4_APPLIED.name(), getInsufficientPointsSentenceSchedule8Paragraph4Applied()));
         addDescriptorTableIfPopulated(new DescriptorTable(UcTemplateComponentId.SCHEDULE_6_DESCRIPTORS.name(), writeFinalDecisionTemplateBody.getUcSchedule6Descriptors(), false));
+        addComponent(new Paragraph(UcTemplateComponentId.DISEASE_OR_DISABLEMENT_PARAGRAPH.name(), getSchedule8Paragraph4AndSchedule9Paragraph4DiseaseOrDisablementSentence(true, true)));
+        addComponent(new Paragraph(UcTemplateComponentId.DOES_NOT_HAVE_LIMITED_CAPABILITY_FOR_SCHEDULE_9_PARAGRAPH_4_APPLIED_PARAGRAPH.name(), getNoSchedule7Sentence()));
         addReasonsIfPresent(writeFinalDecisionTemplateBody);
         addAnythingElseIfPresent(writeFinalDecisionTemplateBody);
         addHearingType(writeFinalDecisionTemplateBody);
+        addRecommendationIfPresent(writeFinalDecisionTemplateBody);
     }
 
     @Override
     public UcScenario getScenario() {
-        return UcScenario.SCENARIO_1;
+        return UcScenario.SCENARIO_8;
     }
 }
--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/uc/scenarios/UcScenario.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/uc/scenarios/UcScenario.java
@@ -2,18 +2,14 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.scenarios;
 
 import java.util.function.Function;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.UcTemplateContent;
-import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.newscenarios.NewScenario12Content;
-import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.newscenarios.NewScenario1Content;
-import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.newscenarios.NewScenario5Content;
-import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.newscenarios.NewScenario6Content;
-import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.newscenarios.NewScenario7Content;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.newscenarios.*;
 import uk.gov.hmcts.reform.sscs.model.docassembly.WriteFinalDecisionTemplateBody;
 
 public enum UcScenario {
 
     SCENARIO_1(NewScenario1Content::new), SCENARIO_2(Scenario2Content::new), SCENARIO_3(Scenario3Content::new),
     SCENARIO_4(Scenario4Content::new), SCENARIO_5(NewScenario5Content::new), SCENARIO_6(NewScenario6Content::new),
-    SCENARIO_7(NewScenario7Content::new), SCENARIO_8(Scenario8Content::new), SCENARIO_9(Scenario9Content::new),
+    SCENARIO_7(NewScenario7Content::new), SCENARIO_8(NewScenario8Content::new), SCENARIO_9(NewScenario9Content::new),
     SCENARIO_10(Scenario10Content::new), SCENARIO_12(NewScenario12Content::new);
 
     Function<WriteFinalDecisionTemplateBody, UcTemplateContent> contentSupplier;
