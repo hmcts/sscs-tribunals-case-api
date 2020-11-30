1df8333327db403c85150bea4730f6b83a995fe2
srochani <sunil.rochani@hotmail.com>
2020-11-30 15:25:39 +0000

    SSCS-7673: Tech debt – Tribunals API - changes to health checks (#1199)
    * SSCS-7673: Tech debt – Tribunals API - changes to health checks
    
    * Fixed health aggregator
    
    * SSCS-7673: Tech debt – Tribunals API - changes to health checks
    
    Co-authored-by: sunil <sunil.rochani@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/sscs/healthcheck/TribunalCaseApiHealthAggregator.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/healthcheck/TribunalCaseApiHealthAggregator.java
@@ -1,21 +1,14 @@
 package uk.gov.hmcts.reform.sscs.healthcheck;
 
-import java.util.Map;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthAggregator;
+import java.util.Set;
+import org.springframework.boot.actuate.health.*;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TribunalCaseApiHealthAggregator implements HealthAggregator {
+public class TribunalCaseApiHealthAggregator implements StatusAggregator {
 
     @Override
-    public Health aggregate(Map<String, Health> healths) {
-        // default status is up
-        Health overallHealth = Health.up().build();
-        long downStatusCount = healths.keySet().stream()
-                .filter(k -> healths.get(k).getStatus().equals(Health.down().build().getStatus())).count();
-        return new Health.Builder(overallHealth.getStatus())
-                .withDetail("All downstream systems", (downStatusCount > 0) ? healths : Health.up().build())
-                .build();
+    public Status getAggregateStatus(Set<Status> statuses) {
+        return Status.UP;
     }
 }
