a4ae4182b5ad9dbe01054d232231871f78315ed7
chrisdavecm <christopher.davidson@btinternet.com>
2021-01-08 10:11:25 +0000

    sscs-8458 taking out ignores

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/CreateBundleIt.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/CreateBundleIt.java
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.IOException;
 import junitparams.JUnitParamsRunner;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -66,7 +65,6 @@ public class CreateBundleIt extends AbstractEventIt {
         when(idamService.getIdamTokens()).thenReturn(IdamTokens.builder().build());
     }
 
-    @Ignore
     @Test
     public void callToAboutToSubmitHandler_willCallExternalCreateBundleService() throws Exception {
         SscsCaseData caseData = SscsCaseData.builder()
