4ec747e3cc8eaaed0dc1319cd698b07fbb68865e
Michael lavelle <michael@lavelle.name>
2021-01-10 16:42:35 +0000

    SSCS-8520 Adding default constructor

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/pip/scenarios/Scenario1Content.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/pip/scenarios/Scenario1Content.java
@@ -5,6 +5,10 @@ import uk.gov.hmcts.reform.sscs.model.docassembly.WriteFinalDecisionTemplateBody
 
 public class Scenario1Content extends PipTemplateContent {
 
+    public Scenario1Content(WriteFinalDecisionTemplateBody writeFinalDecisionTemplateBody) {
+        // Default constructor
+    }
+
     @Override
     public PipScenario getScenario() {
         return PipScenario.SCENARIO_1;
