d16a1fc5a61e65b8410bdebaa9295486d84da38b
Jason.Hart <jason.hart@enginegroup.com>
2021-02-25 17:23:58 +0000

    Added handler tests for process audio video evidence event

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/procesaudiovideo/ProcessAudioVideoEvidenceAboutToStartHandlerTest.java
@@ -0,0 +1,87 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.procesaudiovideo;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_START;
+
+import java.util.List;
+import junitparams.JUnitParamsRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
+import uk.gov.hmcts.reform.sscs.ccd.domain.AudioVideoEvidence;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicList;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicListItem;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.MrnDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.processaudiovideo.ProcessAudioVideoEvidenceAboutToStartHandler;
+
+@RunWith(JUnitParamsRunner.class)
+public class ProcessAudioVideoEvidenceAboutToStartHandlerTest {
+
+    private static final String USER_AUTHORISATION = "Bearer token";
+
+    private ProcessAudioVideoEvidenceAboutToStartHandler handler;
+
+    @Mock
+    private Callback<SscsCaseData> callback;
+
+    @Mock
+    private CaseDetails<SscsCaseData> caseDetails;
+
+    private SscsCaseData sscsCaseData;
+
+    @Before
+    public void setUp() {
+        openMocks(this);
+        handler = new ProcessAudioVideoEvidenceAboutToStartHandler();
+
+        sscsCaseData = SscsCaseData.builder().appeal(Appeal.builder().mrnDetails(MrnDetails.builder().dwpIssuingOffice("3").build()).build()).build();
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(callback.getEvent()).thenReturn(EventType.PROCESS_AUDIO_VIDEO);
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+    }
+
+    @Test
+    public void givenAudioEvidenceListIsNull_ReturnError() {
+        sscsCaseData.setAudioVideoEvidence(null);
+        assertNoEvidenceError();
+    }
+
+    @Test
+    public void givenAudioEvidenceListIsEmpty_ReturnError() {
+        sscsCaseData.setAudioVideoEvidence(List.of());
+        assertNoEvidenceError();
+    }
+
+    private void assertNoEvidenceError() {
+        final PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_START, callback, USER_AUTHORISATION);
+        assertEquals(1, response.getErrors().size());
+        assertEquals("Before running this event audio and video evidence must be uploaded", response.getErrors().toArray()[0]);
+    }
+
+    @Test
+    public void givenAudioEvidenceListIsNotEmpty_ProcessAudioVideoActionListIsCreated() {
+        sscsCaseData.setAudioVideoEvidence(List.of(AudioVideoEvidence.builder().build()));
+        final PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_START, callback, USER_AUTHORISATION);
+        SscsCaseData responseData = response.getData();
+        assertEquals(1, responseData.getProcessAudioVideoAction().getListItems().size());
+        assertEquals("issueDirectionsNotice", getItemCodeInList(responseData.getProcessAudioVideoAction(), "issueDirectionsNotice"));
+    }
+
+    private String getItemCodeInList(DynamicList dynamicList, String item) {
+        return dynamicList.getListItems().stream()
+                .filter(o -> item.equals(o.getCode()))
+                .findFirst()
+                .map(DynamicListItem::getCode)
+                .orElse(null);
+    }
+}
