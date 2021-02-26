f0dadc62947ca772cc2752dd6707c0d38ec2d666
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2021-02-26 11:53:07 +0000

    SSCS-8734 : Exclude A/V evidence (#1840)
    * SSCS-8734 - Exclude evidence

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processaudiovideo/ProcessAudioVideoEvidenceAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processaudiovideo/ProcessAudioVideoEvidenceAboutToStartHandler.java
@@ -1,10 +1,11 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.processaudiovideo;
 
 import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.sscs.ccd.presubmit.processaudiovideo.ProcessAudioVideoActionDynamicListItems.ISSUE_DIRECTIONS_NOTICE;
+import static uk.gov.hmcts.reform.sscs.ccd.presubmit.processaudiovideo.ProcessAudioVideoActionDynamicListItems.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
@@ -15,9 +16,18 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicListItem;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.UserDetails;
 
 @Service
 public class ProcessAudioVideoEvidenceAboutToStartHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    private final IdamService idamService;
+
+    @Autowired
+    public ProcessAudioVideoEvidenceAboutToStartHandler(IdamService idamService) {
+        this.idamService = idamService;
+    }
 
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
@@ -42,16 +52,21 @@ public class ProcessAudioVideoEvidenceAboutToStartHandler implements PreSubmitCa
             errorResponse.addError("Before running this event audio and video evidence must be uploaded");
             return errorResponse;
         }
-
-        setProcessAudioVideoActionDropdown(sscsCaseData);
+        final UserDetails userDetails = idamService.getUserDetails(userAuthorisation);
+        final boolean hasJudgeRole = userDetails.hasJudgeRole();
+        setProcessAudioVideoActionDropdown(sscsCaseData, hasJudgeRole);
 
         return new PreSubmitCallbackResponse<>(sscsCaseData);
     }
 
-    private void setProcessAudioVideoActionDropdown(SscsCaseData sscsCaseData) {
+    private void setProcessAudioVideoActionDropdown(SscsCaseData sscsCaseData, boolean hasJudgeRole) {
         List<DynamicListItem> listOptions = new ArrayList<>();
 
         populateListWithItems(listOptions, ISSUE_DIRECTIONS_NOTICE);
+
+        if (hasJudgeRole) {
+            populateListWithItems(listOptions, EXCLUDE_EVIDENCE);
+        }
 
         sscsCaseData.setProcessAudioVideoAction(new DynamicList(listOptions.get(0), listOptions));
     }
--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processaudiovideo/ProcessAudioVideoEvidenceAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processaudiovideo/ProcessAudioVideoEvidenceAboutToSubmitHandler.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.processaudiovideo;
 
-import static java.util.Objects.nonNull;
-import static java.util.Objects.requireNonNull;
+import static java.util.Objects.*;
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 import static uk.gov.hmcts.reform.sscs.ccd.presubmit.InterlocReviewState.AWAITING_INFORMATION;
+import static uk.gov.hmcts.reform.sscs.ccd.presubmit.processaudiovideo.ProcessAudioVideoActionDynamicListItems.EXCLUDE_EVIDENCE;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
@@ -18,7 +20,6 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentLink;
 import uk.gov.hmcts.reform.sscs.ccd.domain.DwpState;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
-import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocumentTranslationStatus;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.sscs.service.FooterService;
 
@@ -46,23 +47,58 @@ public class ProcessAudioVideoEvidenceAboutToSubmitHandler implements PreSubmitC
 
         CaseDetails<SscsCaseData> caseDetails = callback.getCaseDetails();
         SscsCaseData caseData = caseDetails.getCaseData();
-        SscsDocumentTranslationStatus documentTranslationStatus = caseData.isLanguagePreferenceWelsh() && callback.getEvent() == EventType.DIRECTION_ISSUED ? SscsDocumentTranslationStatus.TRANSLATION_REQUIRED : null;
 
         DocumentLink url = null;
 
+        PreSubmitCallbackResponse<SscsCaseData> response = new PreSubmitCallbackResponse<>(caseData);
+
         if (nonNull(caseData.getPreviewDocument())) {
             url = caseData.getPreviewDocument();
+        } else {
+            response.addError("There is no document notice");
+        }
+
+        if (isNull(caseData.getProcessAudioVideoAction()) || isNull(caseData.getProcessAudioVideoAction().getValue())) {
+            response.addError("There action to process the audio/video evidence");
+        }
+
+        if (!isEmpty(response.getErrors())) {
+            return response;
         }
 
         footerService.createFooterAndAddDocToCase(url, caseData, DocumentType.DIRECTION_NOTICE,
                 Optional.ofNullable(caseData.getDateAdded()).orElse(LocalDate.now())
-                        .format(DateTimeFormatter.ofPattern("dd-MM-YYYY")),
-                caseData.getDateAdded(), null, documentTranslationStatus);
+                        .format(DateTimeFormatter.ofPattern("dd-MM-yyyy")),
+                caseData.getDateAdded(), null, null);
 
         caseData.setInterlocReviewState(AWAITING_INFORMATION.getId());
         caseData.setDwpState(DwpState.DIRECTION_ACTION_REQUIRED.getId());
 
+        processIfExcludeEvidence(caseData);
+
+        clearTransientFields(caseData);
+
         return new PreSubmitCallbackResponse<>(caseData);
 
+    }
+
+    private void processIfExcludeEvidence(SscsCaseData caseData) {
+        if (nonNull(caseData.getProcessAudioVideoAction())
+                && nonNull(caseData.getProcessAudioVideoAction().getValue())
+                && StringUtils.equals(caseData.getProcessAudioVideoAction().getValue().getCode(), EXCLUDE_EVIDENCE.getCode())) {
+            caseData.setInterlocReviewState(null);
+            caseData.setDwpState(DwpState.DIRECTION_ACTION_REQUIRED.getId());
+            caseData.setAudioVideoEvidence(null);
+        }
+    }
+
+    private void clearTransientFields(SscsCaseData caseData) {
+        caseData.setBodyContent(null);
+        caseData.setPreviewDocument(null);
+        caseData.setGenerateNotice(null);
+        caseData.setGenerateNotice(null);
+        caseData.setSignedBy(null);
+        caseData.setSignedRole(null);
+        caseData.setDateAdded(null);
     }
 }
