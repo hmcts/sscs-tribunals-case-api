5d9c5defe3fd2a0e692492f89ae0eb7cd6d9a76b
Jason.Hart <jason.hart@enginegroup.com>
2021-03-01 15:49:41 +0000

    changed label for issue direction notice

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processaudiovideo/ProcessAudioVideoActionDynamicListItems.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processaudiovideo/ProcessAudioVideoActionDynamicListItems.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProcessAudioVideoActionDynamicListItems {
 
-    ISSUE_DIRECTIONS_NOTICE("issueDirectionsNotice", "Issue directions notice to all parties"),
+    ISSUE_DIRECTIONS_NOTICE("issueDirectionsNotice", "Issue directions notice"),
     EXCLUDE_EVIDENCE("excludeEvidence", "Exclude audio/video evidence");
 
     String code;
