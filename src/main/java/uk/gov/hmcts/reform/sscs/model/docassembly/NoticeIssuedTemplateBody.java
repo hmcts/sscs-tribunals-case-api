0db3959ca0a417dfc89a4b9f22d99d2903744df4
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-03-26 15:59:39 +0000

    SSCS-8975: Scottish logo not appearing on decision noticie

--- src/main/java/uk/gov/hmcts/reform/sscs/model/docassembly/NoticeIssuedTemplateBody.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/docassembly/NoticeIssuedTemplateBody.java
@@ -16,7 +16,7 @@ import uk.gov.hmcts.reform.docassembly.domain.FormPayload;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class NoticeIssuedTemplateBody implements FormPayload {
     @JsonIgnore
-    public static final String SCOTTISH_IMAGE = "[userImage:schmcts2.png]";
+    public static final String SCOTTISH_IMAGE = "[userImage:schmcts.png]";
     @JsonIgnore
     public static final String ENGLISH_IMAGE = "[userImage:enhmcts.png]";
     @JsonIgnore
