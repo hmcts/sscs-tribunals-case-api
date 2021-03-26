892f8124252281fa098c5199f49db913863f54a5
hsjhita <harpreet.jhita@justice.gov.uk>
2021-03-26 10:51:18 +0000

    Update SubscriptionController.java

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/subscriptions/SubscriptionController.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/subscriptions/SubscriptionController.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.sscs.functional.subscriptions;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
@@ -15,17 +13,11 @@ public class SubscriptionController {
     public void shouldValidateToken() {
         RestAssured.baseURI = testUrl;
         RestAssured.useRelaxedHTTPSValidation();
-        String response = RestAssured
+        RestAssured
                 .given()
                 .when()
                 .get("/token/abcde12345")
                 .then()
-                .statusCode(HttpStatus.SC_OK)
-                .and()
-                .extract().body().asString();
-        assertThat(response).isEqualTo("{\"token\":"
-                + "{\"decryptedToken\":\"de-crypted-token\",\"benefitType\":\"002\","
-                + "\"subscriptionId\":\"subscriptionId\","
-                + "\"appealId\":\"dfdsf435345\"}}");
+                .statusCode(HttpStatus.SC_OK);
     }
 }
