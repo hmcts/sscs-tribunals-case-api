d917a2d12385acdd31f67863135b5d9619436fef
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-04-23 12:01:53 +0100

    minor refactoring

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/actionfurtherevidence/ActionFurtherEvidenceMidEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/actionfurtherevidence/ActionFurtherEvidenceMidEventHandler.java
@@ -1,13 +1,20 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.furtherevidence.actionfurtherevidence;
 
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static org.apache.commons.collections4.CollectionUtils.emptyIfNull;
 import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 import static uk.gov.hmcts.reform.sscs.ccd.presubmit.furtherevidence.actionfurtherevidence.ActionFurtherEvidenceAboutToSubmitHandler.checkWarningsAndErrors;
+import static uk.gov.hmcts.reform.sscs.domain.wrapper.pdf.PdfState.PASSWORD_ENCRYPTED;
+import static uk.gov.hmcts.reform.sscs.domain.wrapper.pdf.PdfState.UNREADABLE;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
@@ -15,7 +22,6 @@ import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
-import uk.gov.hmcts.reform.sscs.ccd.domain.ScannedDocument;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.sscs.domain.wrapper.pdf.PdfState;
@@ -63,52 +69,83 @@ public class ActionFurtherEvidenceMidEventHandler implements PreSubmitCallbackHa
     private void buildSscsDocumentFromScan(SscsCaseData sscsCaseData, long caseId, Boolean ignoreWarnings,
                                            PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse) {
 
-        if (isNotEmpty(sscsCaseData.getScannedDocuments())) {
-            List<String> unreadablePdfLinks = new ArrayList<>();
-            List<String> encryptedPdfLinks = new ArrayList<>();
-            for (ScannedDocument scannedDocument : sscsCaseData.getScannedDocuments()) {
-                if (scannedDocument != null && scannedDocument.getValue() != null) {
-                    if (scannedDocument.getValue().getUrl() != null
-                        && scannedDocument.getValue().getUrl().getDocumentUrl() != null) {
-                        PdfState pdfState =
-                            footerService.isReadablePdf(scannedDocument.getValue().getUrl().getDocumentUrl());
-                        switch (pdfState) {
-                            case UNREADABLE:
-                                unreadablePdfLinks.add(scannedDocument.getValue().getFileName());
-                                break;
-                            case PASSWORD_ENCRYPTED:
-                                encryptedPdfLinks.add(scannedDocument.getValue().getFileName());
-                                break;
-                            case OK:
-                            default:
-                                break;
-                        }
-                    }
-                    if (isEmpty(unreadablePdfLinks) && isEmpty(encryptedPdfLinks)) {
-                        checkWarningsAndErrors(sscsCaseData, scannedDocument, sscsCaseData.getCcdCaseId(),
-                            ignoreWarnings,
-                            preSubmitCallbackResponse);
-                    }
-                }
-            }
-
-            if (isNotEmpty(unreadablePdfLinks)) {
-                preSubmitCallbackResponse
-                    .addError("The below PDF document(s) are not readable, please correct this");
-                addFileErrors(unreadablePdfLinks, preSubmitCallbackResponse);
-                log.error("{} – {} failed due to broken PDF(s)\n{}", caseId, EventType.ACTION_FURTHER_EVIDENCE,
-                    getFormattedFileUrl(unreadablePdfLinks));
-            }
-            if (isNotEmpty(encryptedPdfLinks)) {
-                preSubmitCallbackResponse.addError(
-                    "The below PDF document(s) cannot be password protected, please correct this");
-                addFileErrors(encryptedPdfLinks, preSubmitCallbackResponse);
-                log.error("{} – {} failed due to encrypted PDF(s)\n{}", caseId, EventType.ACTION_FURTHER_EVIDENCE,
-                    getFormattedFileUrl(encryptedPdfLinks));
-            }
-        } else {
+        if (isEmpty(sscsCaseData.getScannedDocuments())) {
             preSubmitCallbackResponse.addError("Please add a scanned document");
+            return;
         }
+        List<PdfReadable> pdfReadableErrorList = getPdfReadableErrorList(sscsCaseData);
+
+        if (pdfReadableErrorList.isEmpty()) {
+            checkForWarningsAndErrorsOnScannedDocuments(sscsCaseData, ignoreWarnings, preSubmitCallbackResponse);
+        }
+
+        addErrorsIfPdfsHaveErrors(caseId, preSubmitCallbackResponse, pdfReadableErrorList);
+
+    }
+
+    private void addErrorsIfPdfsHaveErrors(long caseId, PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse, List<PdfReadable> pdfReadableErrorList) {
+        addErrorsIfAnyPdfsAreUnreadable(caseId, preSubmitCallbackResponse, pdfReadableErrorList);
+        addErrorsIfAnyPdfsAreEncrypted(caseId, preSubmitCallbackResponse, pdfReadableErrorList);
+    }
+
+    private void addErrorsIfAnyPdfsAreEncrypted(long caseId, PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse, List<PdfReadable> pdfReadableErrorList) {
+        List<String> encryptedPdfLinks = getPdfLinksWithError(pdfReadableErrorList, PASSWORD_ENCRYPTED);
+        if (isNotEmpty(encryptedPdfLinks)) {
+            addPdfEncryptedError(caseId, preSubmitCallbackResponse, encryptedPdfLinks);
+        }
+    }
+
+    private void addErrorsIfAnyPdfsAreUnreadable(long caseId, PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse, List<PdfReadable> pdfReadableErrorList) {
+        List<String> unreadablePdfLinks = getPdfLinksWithError(pdfReadableErrorList, UNREADABLE);
+        if (isNotEmpty(unreadablePdfLinks)) {
+            addPdfUnreadableError(caseId, preSubmitCallbackResponse, unreadablePdfLinks);
+        }
+    }
+
+    @NotNull
+    private List<String> getPdfLinksWithError(List<PdfReadable> pdfReadableErrorList, PdfState pdfState) {
+        return pdfReadableErrorList.stream()
+                .filter(pdfReadable -> pdfReadable.getPdfState().equals(pdfState))
+                .map(PdfReadable::getFilename)
+                .collect(toUnmodifiableList());
+    }
+
+    private void checkForWarningsAndErrorsOnScannedDocuments(SscsCaseData sscsCaseData, Boolean ignoreWarnings, PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse) {
+        emptyIfNull(sscsCaseData.getScannedDocuments())
+                .forEach(scannedDocument -> checkWarningsAndErrors(
+                        sscsCaseData,
+                        scannedDocument,
+                        sscsCaseData.getCcdCaseId(),
+                        ignoreWarnings,
+                        preSubmitCallbackResponse));
+    }
+
+    @NotNull
+    private List<PdfReadable> getPdfReadableErrorList(SscsCaseData sscsCaseData) {
+        return emptyIfNull(sscsCaseData.getScannedDocuments()).stream()
+                .filter(Objects::nonNull)
+                .filter(doc -> doc.getValue() != null)
+                .filter(doc -> doc.getValue().getUrl() != null)
+                .filter(doc -> doc.getValue().getUrl().getDocumentUrl() != null)
+                .map(doc -> new PdfReadable(doc.getValue().getFileName(), footerService.isReadablePdf(doc.getValue().getUrl().getDocumentUrl())))
+                .filter(pdfReadable -> pdfReadable.getPdfState().equals(UNREADABLE) || pdfReadable.getPdfState().equals(PASSWORD_ENCRYPTED))
+                .collect(toUnmodifiableList());
+    }
+
+    private void addPdfEncryptedError(long caseId, PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse, List<String> encryptedPdfLinks) {
+        preSubmitCallbackResponse.addError(
+            "The below PDF document(s) cannot be password protected, please correct this");
+        addFileErrors(encryptedPdfLinks, preSubmitCallbackResponse);
+        log.error("{} – {} failed due to encrypted PDF(s)\n{}", caseId, EventType.ACTION_FURTHER_EVIDENCE,
+            getFormattedFileUrl(encryptedPdfLinks));
+    }
+
+    private void addPdfUnreadableError(long caseId, PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse, List<String> unreadablePdfLinks) {
+        preSubmitCallbackResponse
+            .addError("The below PDF document(s) are not readable, please correct this");
+        addFileErrors(unreadablePdfLinks, preSubmitCallbackResponse);
+        log.error("{} – {} failed due to broken PDF(s)\n{}", caseId, EventType.ACTION_FURTHER_EVIDENCE,
+            getFormattedFileUrl(unreadablePdfLinks));
     }
 
     private String getFormattedFileUrl(List<String> errors) {
@@ -126,4 +163,11 @@ public class ActionFurtherEvidenceMidEventHandler implements PreSubmitCallbackHa
         }
     }
 
+    @Getter
+    @AllArgsConstructor
+    private static class PdfReadable {
+        private final String filename;
+        private final PdfState pdfState;
+
+    }
 }
