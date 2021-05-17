8bb370c7f36dcee6be7baf61d6b120bb414733cb
Jason.Hart <jason.hart@enginegroup.com>
2021-05-17 08:48:21 +0100

    add note added to update welsh preference submit handler

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/updatewelshpreference/UpdateWelshPreferenceAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/updatewelshpreference/UpdateWelshPreferenceAboutToSubmitHandler.java
@@ -1,19 +1,33 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.updatewelshpreference;
 
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.sscs.ccd.presubmit.InterlocReviewState.AWAITING_ADMIN_ACTION;
+import static uk.gov.hmcts.reform.sscs.ccd.presubmit.InterlocReviewState.WELSH_TRANSLATION;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.addnote.AddNoteAboutToSubmitHandler;
+import uk.gov.hmcts.reform.sscs.service.FooterService;
+import uk.gov.hmcts.reform.sscs.service.UserDetailsService;
 
 @Service
 @Slf4j
 public class UpdateWelshPreferenceAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    public AddNoteAboutToSubmitHandler addNote;
+
+    @Autowired
+    public UpdateWelshPreferenceAboutToSubmitHandler(AddNoteAboutToSubmitHandler addNote) {
+        this.addNote = addNote;
+    }
 
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
@@ -29,6 +43,11 @@ public class UpdateWelshPreferenceAboutToSubmitHandler implements PreSubmitCallb
                                                           String userAuthorisation) {
         SscsCaseData caseData = callback.getCaseDetails().getCaseData();
         if (!caseData.isLanguagePreferenceWelsh()) {
+            if (WELSH_TRANSLATION.getId().equals(caseData.getInterlocReviewState())) {
+                caseData.setInterlocReviewState(AWAITING_ADMIN_ACTION.getId());
+                caseData.setTempNoteDetail("Assigned to admin - Case no longer Welsh. Please cancel any Welsh translations");
+                addNote.handle(callbackType, callback, userAuthorisation);
+            }
             caseData.setTranslationWorkOutstanding("No");
         }
         return new PreSubmitCallbackResponse<>(caseData);
