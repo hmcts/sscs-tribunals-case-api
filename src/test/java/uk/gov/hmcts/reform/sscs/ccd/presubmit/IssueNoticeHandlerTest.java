c7d7a372cad42293dac23f5a327d5bff893a476a
sunilrochani <sunil.rochani@hotmail.com>
2021-05-18 15:08:57 +0100

    SSCS-9187: Appointee should be shown as attending hearing on decision notice

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/IssueNoticeHandlerTest.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit;
+
+import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.ccd.domain.LanguagePreference;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.pip.PipWriteFinalDecisionPreviewDecisionService;
+import uk.gov.hmcts.reform.sscs.config.DocumentConfiguration;
+import uk.gov.hmcts.reform.sscs.docassembly.GenerateFile;
+import uk.gov.hmcts.reform.sscs.service.PipDecisionNoticeOutcomeService;
+import uk.gov.hmcts.reform.sscs.service.PipDecisionNoticeQuestionService;
+import uk.gov.hmcts.reform.sscs.service.UserDetailsService;
+
+import java.util.function.Function;
+
+public class IssueNoticeHandlerTest extends IssueNoticeHandlerTestBase {
+    @Mock
+    public GenerateFile generateFile;
+    @Mock
+    public UserDetailsService userDetailsService;
+    @Mock
+    public Function<LanguagePreference, String> templateId;
+    @Mock
+    public PipDecisionNoticeQuestionService decisionNoticeQuestionService;
+    @Mock
+    public PipDecisionNoticeOutcomeService decisionNoticeOutcomeService;
+    @Mock
+    public DocumentConfiguration documentConfiguration;
+
+    public IssueNoticeHandlerTest() {
+        super("PIP");
+    }
+
+    @Override
+    protected IssueNoticeHandler createIssueNoticeHandler(GenerateFile generateFile, UserDetailsService userDetailsService, Function<LanguagePreference, String> templateId) {
+        return new PipWriteFinalDecisionPreviewDecisionService(generateFile, userDetailsService, decisionNoticeQuestionService, decisionNoticeOutcomeService, documentConfiguration);
+    }
+}
