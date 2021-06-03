4de93621d3ca1f2c0c4dc3ba9b1134f1339d15b6
Chris Davidson <christopher.davidson@btinternet.com>
2021-06-03 17:49:39 +0100

    sscs 8854 Move legacy docs to dwpdocuments

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createbundle/CreateBundleAboutToStartEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createbundle/CreateBundleAboutToStartEventHandlerTest.java
@@ -110,6 +110,20 @@ public class CreateBundleAboutToStartEventHandlerTest {
     }
 
     @Test
+    public void givenOldPattern_thenNoError() {
+        callback.getCaseDetails().getCaseData().setDwpEvidenceBundleDocument(DwpResponseDocument.builder().documentLink(DocumentLink.builder().documentFilename("Testing").build()).build());
+        callback.getCaseDetails().getCaseData().setDwpResponseDocument(DwpResponseDocument.builder().documentLink(DocumentLink.builder().documentFilename("Testing").build()).build());
+
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_START, callback, USER_AUTHORISATION);
+
+
+        assertEquals(0, response.getErrors().size());
+        assertEquals(0, response.getWarnings().size());
+
+        verifyNoInteractions(serviceRequestExecutor);
+    }
+
+    @Test
     public void givenEmptyDwpResponseDocumentLinkWithDwpDocumentsPattern_thenReturnError() {
         List<DwpDocument> dwpDocuments = new ArrayList<>();
         dwpDocuments.add(DwpDocument.builder().value(DwpDocumentDetails.builder().documentType(DWP_RESPONSE.getValue()).build()).build());
@@ -210,4 +224,5 @@ public class CreateBundleAboutToStartEventHandlerTest {
         assertEquals("The bundle cannot be created as mandatory DWP documents are missing, do you want to proceed?", warning);
         verifyNoInteractions(serviceRequestExecutor);
     }
+
 }
