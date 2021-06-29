0ba447cc08f6818303723b372c2aff650dfd89ce
jackmaloney <jack.maloney@transformuk.com>
2021-06-29 13:51:44 +0100

    SSCS-9365 Dynamic list spike

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/abatecase/TestEventAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/abatecase/TestEventAboutToStartHandler.java
@@ -1,22 +1,18 @@
-package uk.gov.hmcts.reform.sscs.ccd.presubmit.dwpuploadresponse;
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.abatecase;
 
 import static java.util.Objects.requireNonNull;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.State.READY_TO_LIST;
 
 import java.util.ArrayList;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
-import uk.gov.hmcts.reform.sscs.ccd.presubmit.ResponseEventsAboutToSubmit;
 
 @Component
-@Slf4j
-public class DwpUploadResponseAboutToStartHandler extends ResponseEventsAboutToSubmit implements PreSubmitCallbackHandler<SscsCaseData> {
+public class TestEventAboutToStartHandler implements PreSubmitCallbackHandler<SscsCaseData> {
 
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
@@ -24,25 +20,31 @@ public class DwpUploadResponseAboutToStartHandler extends ResponseEventsAboutToS
         requireNonNull(callbackType, "callbacktype must not be null");
 
         return callbackType.equals(CallbackType.ABOUT_TO_START)
-                && callback.getEvent() == EventType.DWP_UPLOAD_RESPONSE;
+                && callback.getEvent() == EventType.TEST_EVENT;
     }
 
     @Override
     public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback, String userAuthorisation) {
+
         if (!canHandle(callbackType, callback)) {
             throw new IllegalStateException("Cannot handle callback");
         }
 
         final CaseDetails<SscsCaseData> caseDetails = callback.getCaseDetails();
         final SscsCaseData sscsCaseData = caseDetails.getCaseData();
-
         PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
 
-        if (!READY_TO_LIST.getId().equals(sscsCaseData.getCreatedInGapsFrom())) {
-            preSubmitCallbackResponse.addError("This case cannot be updated by DWP");
-        }
+        List<DynamicListItem> listOptions = new ArrayList<>();
+        listOptions.add(new DynamicListItem("reviewByTcw", "Review by TCW"));
+        listOptions.add(new DynamicListItem("reviewByJudge", "Review by Judge"));
+
+        List<DynamicListItem> listOptions2 = new ArrayList<>();
+        listOptions2.add(new DynamicListItem("reviewByTcw2", "Review by TCW2"));
+        listOptions2.add(new DynamicListItem("reviewByJudge2", "Review by Judge2"));
+
+        sscsCaseData.setTestField(TestField.builder().testFieldDl(new DynamicList(new DynamicListItem("", ""), listOptions)).build());
+        sscsCaseData.setTestFieldTop(new DynamicList(new DynamicListItem("", ""), listOptions2));
 
         return preSubmitCallbackResponse;
     }
-
 }
