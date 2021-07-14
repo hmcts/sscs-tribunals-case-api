eeadd9c8c9088263bdc17c998b0443ca95c8658a
Jason.Hart <jason.hart@enginegroup.com>
2021-07-14 09:25:00 +0100

    SSCS-9309-add integration test

---
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/AdminAppealWithdrawnHandlerIt.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.reform.sscs.callback;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.DwpState.WITHDRAWAL_RECEIVED;
+import static uk.gov.hmcts.reform.sscs.helper.IntegrationTestHelper.getRequestWithAuthHeader;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AdminAppealWithdrawnHandlerIt extends AbstractEventIt {
+
+
+    @Before
+    public void setup() throws IOException {
+        setup("callback/adminAppealWithdrawn.json");
+    }
+
+    @Test
+    public void callToAboutToSubmit_willAddWithdrawalDocumentToSscsDocuments_andSetDwpState() throws Exception {
+        MockHttpServletResponse response = getResponse(getRequestWithAuthHeader(json, "/ccdAboutToSubmit"));
+        PreSubmitCallbackResponse<SscsCaseData> result = deserialize(response.getContentAsString());
+
+        assertEquals(4, result.getData().getSscsDocument().size());
+        assertEquals("withdrawalRequest", result.getData().getSscsDocument().get(3).getValue().getDocumentType());
+        assertEquals(WITHDRAWAL_RECEIVED.getId(), result.getData().getDwpState());
+    }
+
+
+}
+
+
