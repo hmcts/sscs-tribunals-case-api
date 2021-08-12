b633c46c2af2d8e029ce3a1bb6b4c5de72bacf7a
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-08-12 12:14:11 +0100

    complete mid event test

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processhearingrecordingrequest/ProcessHearingRecordingRequestMidEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processhearingrecordingrequest/ProcessHearingRecordingRequestMidEventHandlerTest.java
@@ -1,27 +1,49 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.processhearingrecordingrequest;
 
 import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.sscs.ccd.presubmit.processhearingrecordingrequest.ProcessHearingRecordingRequestAboutToStartHandlerTest.HEARING;
-import static uk.gov.hmcts.reform.sscs.ccd.presubmit.processhearingrecordingrequest.ProcessHearingRecordingRequestAboutToStartHandlerTest.recording;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.MID_EVENT;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.YES;
+import static uk.gov.hmcts.reform.sscs.ccd.presubmit.processhearingrecordingrequest.ProcessHearingRecordingRequestAboutToStartHandlerTest.*;
+import static uk.gov.hmcts.reform.sscs.model.PartyItemList.*;
+import static uk.gov.hmcts.reform.sscs.model.RequestStatus.GRANTED;
+import static uk.gov.hmcts.reform.sscs.model.RequestStatus.REFUSED;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
-import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicList;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicListItem;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.HearingRecordingRequest;
+import uk.gov.hmcts.reform.sscs.ccd.domain.HearingRecordingRequestDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.MrnDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.ProcessHearingRecordingRequest;
+import uk.gov.hmcts.reform.sscs.ccd.domain.ProcessHearingRecordingRequestDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsHearingRecordingCaseData;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 import uk.gov.hmcts.reform.sscs.idam.UserDetails;
 import uk.gov.hmcts.reform.sscs.idam.UserRole;
+import uk.gov.hmcts.reform.sscs.model.PartyItemList;
+import uk.gov.hmcts.reform.sscs.model.RequestStatus;
 import uk.gov.hmcts.reform.sscs.service.processhearingrecordingrequest.ProcessHearingRecordingRequestService;
 
 @RunWith(JUnitParamsRunner.class)
@@ -47,8 +69,8 @@ public class ProcessHearingRecordingRequestMidEventHandlerTest {
 
         sscsCaseData = SscsCaseData.builder()
                 .appeal(Appeal.builder().mrnDetails(MrnDetails.builder().dwpIssuingOffice("3").build()).build())
-                .hearings(List.of(HEARING))
-
+                .jointParty(YES.getValue())
+                .hearings(List.of(HEARING, getHearing(2)))
                 .sscsHearingRecordingCaseData(SscsHearingRecordingCaseData.builder()
                         .sscsHearingRecordings(List.of(recording(1), recording(2)))
                         .build())
@@ -60,6 +82,84 @@ public class ProcessHearingRecordingRequestMidEventHandlerTest {
         when(idamService.getUserDetails(USER_AUTHORISATION)).thenReturn(userDetails);
     }
 
+
+    @Test
+    @Parameters({"APPELLANT", "DWP", "JOINT_PARTY"})
+    public void changingRequestFromGrantedToRefusedReturnsWarning(PartyItemList party) {
+
+        setReleasedHearingsForParty(party);
+
+        sscsCaseData.getSscsHearingRecordingCaseData().setProcessHearingRecordingRequests(
+                List.of(getProcessHearingRecordingRequestDetails(party))
+        );
+        final PreSubmitCallbackResponse<SscsCaseData> response =
+                handler.handle(MID_EVENT, callback, USER_AUTHORISATION);
+        assertThat(response.getErrors().size(), is(0));
+        assertThat(response.getWarnings().size(), is(1));
+        assertThat(response.getWarnings().iterator().next(), is("Are you sure you want to change the request status"));
+    }
+
+    private ProcessHearingRecordingRequest getProcessHearingRecordingRequestDetails(PartyItemList party) {
+        return ProcessHearingRecordingRequest.builder()
+                .value(ProcessHearingRecordingRequestDetails.builder()
+                        .hearingId(HEARING.getValue().getHearingId())
+                        .appellant(getDynamicList(party, APPELLANT))
+                        .dwp(getDynamicList(party, DWP))
+                        .jointParty(getDynamicList(party, JOINT_PARTY))
+                        .build())
+                .build();
+    }
+
+    @Test
+    @Parameters({"APPELLANT", "DWP", "JOINT_PARTY"})
+    public void changingFromRequestedToRefusedHasNoWarningsOrErrors(PartyItemList party) {
+        sscsCaseData.getSscsHearingRecordingCaseData().setRequestedHearings(
+                List.of(hearingRecordingRequest(party))
+        );
+
+        sscsCaseData.getSscsHearingRecordingCaseData().setProcessHearingRecordingRequests(
+                List.of(getProcessHearingRecordingRequestDetails(party))
+        );
+        final PreSubmitCallbackResponse<SscsCaseData> response =
+                handler.handle(MID_EVENT, callback, USER_AUTHORISATION);
+        assertThat(response.getErrors().size(), is(0));
+        assertThat(response.getWarnings().size(), is(0));
+    }
+
+
+    private void setReleasedHearingsForParty(PartyItemList party) {
+        if (party.equals(DWP)) {
+            sscsCaseData.getSscsHearingRecordingCaseData().setDwpReleasedHearings(
+                    List.of(hearingRecordingRequest(party)));
+        } else {
+            sscsCaseData.getSscsHearingRecordingCaseData().setCitizenReleasedHearings(
+                    List.of(hearingRecordingRequest(party)));
+        }
+    }
+
+    private HearingRecordingRequest hearingRecordingRequest(PartyItemList party) {
+        return HearingRecordingRequest.builder()
+                .value(HearingRecordingRequestDetails.builder()
+                        .requestingParty(party.getCode())
+                        .sscsHearingRecordingList(List.of(recording(1)))
+                        .build())
+                .build();
+    }
+
+    @NotNull
+    private DynamicList getDynamicList(PartyItemList party, PartyItemList appellant) {
+        return new DynamicList(refusedSelectedIfTrueElseGranted(party.equals(appellant)), getListItems());
+    }
+
+    @NotNull
+    private List<DynamicListItem> getListItems() {
+        return List.of(REFUSED, GRANTED).stream().map(s -> new DynamicListItem(s.getLabel(), s.getLabel())).collect(Collectors.toList());
+    }
+
+    private DynamicListItem refusedSelectedIfTrueElseGranted(boolean trueIfRefused) {
+        RequestStatus status = trueIfRefused ? REFUSED : GRANTED;
+        return new DynamicListItem(status.getLabel(), status.getLabel());
+    }
 
     @Test
     @Parameters({"ABOUT_TO_START", "ABOUT_TO_SUBMIT", "SUBMITTED"})
