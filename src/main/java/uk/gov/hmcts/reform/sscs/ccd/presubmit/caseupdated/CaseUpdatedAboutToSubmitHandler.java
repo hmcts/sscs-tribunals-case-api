c4c7d439173fe66e41164a0c95ce1d6cdfb30d7f
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-10-21 12:45:00 +0100

    SSCS-9637: CCD UI – Tribunals – Set dwpRegionalCentre when case is updated by user

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/caseupdated/CaseUpdatedAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/caseupdated/CaseUpdatedAboutToSubmitHandler.java
@@ -3,8 +3,11 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.caseupdated;
 import static java.util.Objects.requireNonNull;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
@@ -15,7 +18,9 @@ import uk.gov.hmcts.reform.sscs.ccd.presubmit.AssociatedCaseLinkHelper;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.ResponseEventsAboutToSubmit;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.isscottish.IsScottishHandler;
+import uk.gov.hmcts.reform.sscs.model.dwp.OfficeMapping;
 import uk.gov.hmcts.reform.sscs.service.AirLookupService;
+import uk.gov.hmcts.reform.sscs.service.DwpAddressLookupService;
 import uk.gov.hmcts.reform.sscs.service.RegionalProcessingCenterService;
 
 @Component
@@ -77,7 +82,52 @@ public class CaseUpdatedAboutToSubmitHandler extends ResponseEventsAboutToSubmit
 
         }
 
+        validateAndUpdateDwpHandlingOffice(sscsCaseData,preSubmitCallbackResponse);
+
         return preSubmitCallbackResponse;
+    }
+
+    private void validateAndUpdateDwpHandlingOffice(SscsCaseData sscsCaseData, PreSubmitCallbackResponse<SscsCaseData> response) {
+        DwpAddressLookupService dwpLookup = new DwpAddressLookupService();
+        MrnDetails mrnDetails = sscsCaseData.getAppeal().getMrnDetails();
+        BenefitType benefitType = sscsCaseData.getAppeal().getBenefitType();
+        boolean validBenefitType = validateBenefitType(benefitType,response);
+        boolean validDwpIssuingOffice = validateDwpIssuingOffice(mrnDetails, benefitType, response, dwpLookup);
+
+        if (validBenefitType && validDwpIssuingOffice) {
+            String regionalCenter = dwpLookup.getDwpRegionalCenterByBenefitTypeAndOffice(benefitType.getCode(), mrnDetails.getDwpIssuingOffice());
+            sscsCaseData.setDwpRegionalCentre(regionalCenter);
+        }
+    }
+
+    private boolean validateBenefitType(BenefitType benefitType, PreSubmitCallbackResponse<SscsCaseData> response) {
+        if (benefitType == null || StringUtils.isEmpty(benefitType.getCode())) {
+            response.addWarning("Benefit type code is empty");
+            return false;
+        } else if (Benefit.findBenefitByShortName(benefitType.getCode()).isEmpty()) {
+            String validBenefitTypes = Arrays.stream(Benefit.values()).sequential().map(Benefit::getShortName).collect(Collectors.joining(", "));
+            response.addWarning("Benefit type code is invalid, should be one of: " + validBenefitTypes);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateDwpIssuingOffice(MrnDetails mrnDetails, BenefitType benefitType, PreSubmitCallbackResponse<SscsCaseData> response, DwpAddressLookupService dwpLookup) {
+        if (mrnDetails != null) {
+            if (StringUtils.isEmpty(mrnDetails.getDwpIssuingOffice())) {
+                response.addWarning("DWP issuing office is empty");
+                return false;
+            } else if (Benefit.findBenefitByShortName(benefitType.getCode()).isPresent()) {
+                if (!dwpLookup.validateIssuingOffice(benefitType.getCode(), mrnDetails.getDwpIssuingOffice())) {
+                    OfficeMapping[] officeMappings = dwpLookup.getDwpOfficeMappings(benefitType.getCode());
+                    String validOffice = Arrays.stream(officeMappings).map(OfficeMapping::getCode).collect(Collectors.joining(", "));
+                    response.addWarning("DWP issuing office is invalid, should one of: " + validOffice);
+                    return false;
+                }
+                return true;
+            }
+        }
+        return false;
     }
 
     public void maybeChangeIsScottish(RegionalProcessingCenter oldRpc, RegionalProcessingCenter newRpc, SscsCaseData caseData) {
