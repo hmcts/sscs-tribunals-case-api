9a91d529a44770e9ec94ae0d0c16ff3a3eda1c48
jackmaloney <jack.maloney@transformuk.com>
2021-11-01 15:26:47 +0000

    SSCS-9729 Request hearing recording in upload document fe event

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentFurtherEvidenceAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentFurtherEvidenceAboutToSubmitHandler.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.uploaddocuments;
 
 import static java.util.stream.Collectors.toList;
-import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static uk.gov.hmcts.reform.sscs.ccd.presubmit.InterlocReferralReason.REVIEW_AUDIO_VIDEO_EVIDENCE;
 import static uk.gov.hmcts.reform.sscs.ccd.presubmit.InterlocReviewState.REVIEW_BY_JUDGE;
 import static uk.gov.hmcts.reform.sscs.ccd.presubmit.uploaddocuments.DocumentType.REQUEST_FOR_HEARING_RECORDING;
@@ -13,9 +11,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
@@ -23,24 +20,12 @@ import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.InterlocReviewState;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
-import uk.gov.hmcts.reform.sscs.domain.wrapper.pdf.PdfState;
-import uk.gov.hmcts.reform.sscs.service.FooterService;
 import uk.gov.hmcts.reform.sscs.util.DocumentUtil;
 
 @Service
 public class UploadDocumentFurtherEvidenceAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
 
     private static final String UPLOAD_DATE_FORMATTER = "yyyy-MM-dd";
-    private final boolean uploadAudioVideoEvidenceEnabled;
-    private final FooterService footerService;
-
-    @Autowired
-    public UploadDocumentFurtherEvidenceAboutToSubmitHandler(
-        @Value("${feature.upload-audio-video-evidence.enabled}") boolean uploadAudioVideoEvidenceEnabled,
-        FooterService footerService) {
-        this.uploadAudioVideoEvidenceEnabled = uploadAudioVideoEvidenceEnabled;
-        this.footerService = footerService;
-    }
 
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
@@ -57,33 +42,6 @@ public class UploadDocumentFurtherEvidenceAboutToSubmitHandler implements PreSub
         }
 
         SscsCaseData caseData = callback.getCaseDetails().getCaseData();
-        PreSubmitCallbackResponse<SscsCaseData> response = new PreSubmitCallbackResponse<>(caseData);
-
-        if (!validDraftFurtherEvidenceDocument(caseData.getDraftSscsFurtherEvidenceDocument())) {
-            response.addError("You need to provide a file and a document type");
-        } else if (!uploadAudioVideoEvidenceEnabled
-            && !isFileUploadedAPdf(caseData.getDraftSscsFurtherEvidenceDocument())) {
-            response.addError("You need to upload PDF documents only");
-        } else if (uploadAudioVideoEvidenceEnabled
-            && !isFileUploadedAValid(caseData.getDraftSscsFurtherEvidenceDocument())) {
-            response.addError("You need to upload PDF, MP3 or MP4 file only");
-        }
-
-        PdfState pdfState = isPdfReadable(caseData.getDraftSscsFurtherEvidenceDocument());
-        switch (pdfState) {
-            case UNKNOWN:
-            case UNREADABLE:
-                initDraftSscsFurtherEvidenceDocument(caseData);
-                response.addError("Your PDF Document is not readable.");
-                return response;
-            case PASSWORD_ENCRYPTED:
-                initDraftSscsFurtherEvidenceDocument(caseData);
-                response.addError("Your PDF Document cannot be password protected.");
-                return response;
-            case OK:
-            default:
-                break;
-        }
 
         moveDraftsToSscsDocs(caseData);
         moveDraftsToAudioVideoEvidence(caseData);
@@ -92,67 +50,9 @@ public class UploadDocumentFurtherEvidenceAboutToSubmitHandler implements PreSub
 
         initDraftSscsFurtherEvidenceDocument(caseData);
         setHasUnprocessedAudioVideoEvidenceFlag(caseData);
+        clearTransientFields(caseData);
 
-        return response;
-    }
-
-    private boolean validDraftFurtherEvidenceDocument(List<SscsFurtherEvidenceDoc> draftSscsFurtherEvidenceDocuments) {
-        if (draftSscsFurtherEvidenceDocuments != null && !draftSscsFurtherEvidenceDocuments.isEmpty()) {
-            return draftSscsFurtherEvidenceDocuments.stream()
-                .allMatch(doc -> {
-                    String docType = doc.getValue() != null ? doc.getValue().getDocumentType() : null;
-                    return isValidDocumentType(docType) && isFileUploaded(doc);
-                });
-        }
-        return false;
-    }
-
-    private boolean isFileUploadedAPdf(List<SscsFurtherEvidenceDoc> draftSscsFurtherEvidenceDocuments) {
-        return draftSscsFurtherEvidenceDocuments.stream().allMatch(doc ->
-            DocumentUtil.isFileAPdf(doc.getValue().getDocumentLink()));
-    }
-
-    private boolean isFileUploadedAValid(List<SscsFurtherEvidenceDoc> draftSscsFurtherEvidenceDocuments) {
-        return draftSscsFurtherEvidenceDocuments.stream().allMatch(doc ->
-            DocumentUtil.isFileAPdf(doc.getValue().getDocumentLink())
-                || DocumentUtil.isFileAMedia(doc.getValue().getDocumentLink()));
-    }
-
-    private PdfState isPdfReadable(List<SscsFurtherEvidenceDoc> docs) {
-        PdfState pdfState = PdfState.UNKNOWN;
-        if (isNotEmpty(docs)) {
-            for (SscsFurtherEvidenceDoc doc : docs) {
-                if (DocumentUtil.isFileAPdf(doc.getValue().getDocumentLink())) {
-                    pdfState = isPdfReadable(doc);
-                    if (!PdfState.OK.equals(pdfState)) {
-                        return pdfState;
-                    }
-                } else if (DocumentUtil.isFileAMedia(doc.getValue().getDocumentLink())) {
-                    pdfState = PdfState.OK;
-                }
-            }
-        }
-        return pdfState;
-    }
-
-    private PdfState isPdfReadable(SscsFurtherEvidenceDoc doc) {
-        if (doc.getValue().getDocumentLink() != null) {
-            return footerService.isReadablePdf(doc.getValue().getDocumentLink().getDocumentUrl());
-        }
-        return PdfState.UNKNOWN;
-    }
-
-    private boolean isFileUploaded(SscsFurtherEvidenceDoc doc) {
-        return doc.getValue().getDocumentLink() != null
-            && isNotBlank(doc.getValue().getDocumentLink().getDocumentUrl());
-    }
-
-    private boolean isValidDocumentType(String docType) {
-        return DocumentType.MEDICAL_EVIDENCE.getId().equals(docType)
-            || DocumentType.OTHER_EVIDENCE.getId().equals(docType)
-            || DocumentType.APPELLANT_EVIDENCE.getId().equals(docType)
-            || DocumentType.REPRESENTATIVE_EVIDENCE.getId().equals(docType)
-            || DocumentType.REQUEST_FOR_HEARING_RECORDING.getId().equals(docType);
+        return new PreSubmitCallbackResponse<>(caseData);
     }
 
     private void initDraftSscsFurtherEvidenceDocument(SscsCaseData caseData) {
@@ -234,9 +134,16 @@ public class UploadDocumentFurtherEvidenceAboutToSubmitHandler implements PreSub
                 && sscsCaseData.getSscsHearingRecordingCaseData().getRequestingParty().getValue().getCode() != null
                 && sscsFurtherEvidenceDocList != null && sscsFurtherEvidenceDocList.size() >= 1) {
 
+                DynamicListItem selectedRequestable = sscsCaseData.getSscsHearingRecordingCaseData().getRequestableHearingDetails().getValue();
+                String hearingId = selectedRequestable.getCode();
+
+                Optional<SscsHearingRecording> sscsHearingRecording = sscsCaseData.getSscsHearingRecordingCaseData().getSscsHearingRecordings()
+                        .stream().filter(r -> r.getValue().getHearingId().equals(hearingId)).findAny();
+
                 HearingRecordingRequest hearingRecordingRequest = HearingRecordingRequest.builder().value(HearingRecordingRequestDetails.builder()
                     .requestingParty(sscsCaseData.getSscsHearingRecordingCaseData().getRequestingParty().getValue().getCode())
                     .requestDocument(sscsFurtherEvidenceDocList.get(0).getValue().getDocumentLink())
+                    .sscsHearingRecording(sscsHearingRecording.get().getValue())
                     .dateRequested(LocalDateTime.now().format(DateTimeFormatter.ofPattern(UPLOAD_DATE_FORMATTER))).build()).build();
 
                 List<HearingRecordingRequest> hearingRecordingRequests = sscsCaseData.getSscsHearingRecordingCaseData().getRequestedHearings();
@@ -249,5 +156,9 @@ public class UploadDocumentFurtherEvidenceAboutToSubmitHandler implements PreSub
                 sscsCaseData.getSscsHearingRecordingCaseData().setHearingRecordingRequestOutstanding(YesNo.YES);
             }
         }
+    }
+
+    private void clearTransientFields(SscsCaseData sscsCaseData) {
+        sscsCaseData.getSscsHearingRecordingCaseData().setRequestingParty(null);
     }
 }
--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentFurtherEvidenceMidEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/uploaddocuments/UploadDocumentFurtherEvidenceMidEventHandlerTest.java
@@ -1,19 +1,30 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.uploaddocuments;
 
+import static java.lang.String.format;
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.MID_EVENT;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.APPEAL_RECEIVED;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.UPLOAD_DOCUMENT_FURTHER_EVIDENCE;
 import static uk.gov.hmcts.reform.sscs.ccd.presubmit.uploaddocuments.DocumentType.REQUEST_FOR_HEARING_RECORDING;
+import static uk.gov.hmcts.reform.sscs.ccd.presubmit.uploaddocuments.FileUploadScenario.FILE_UPLOAD_IS_EMPTY;
+import static uk.gov.hmcts.reform.sscs.ccd.presubmit.uploaddocuments.FileUploadScenario.FILE_UPLOAD_IS_NULL;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import junitparams.converters.Nullable;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,9 +33,15 @@ import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.domain.wrapper.pdf.PdfState;
+import uk.gov.hmcts.reform.sscs.model.PartyItemList;
+import uk.gov.hmcts.reform.sscs.service.FooterService;
+import uk.gov.hmcts.reform.sscs.service.HearingRecordingRequestService;
 
 @RunWith(JUnitParamsRunner.class)
-public class UploadDocumentFurtherEvidenceMidEventHandlerTest {
+public class UploadDocumentFurtherEvidenceMidEventHandlerTest extends BaseHandlerTest {
+
+    private static final String UPLOAD_DOCUMENT_FE_CALLBACK_JSON = "uploaddocument/uploadDocumentFECallback.json";
 
     private static final String USER_AUTHORISATION = "Bearer token";
     private UploadDocumentFurtherEvidenceMidEventHandler handler;
@@ -32,18 +49,41 @@ public class UploadDocumentFurtherEvidenceMidEventHandlerTest {
     private Callback<SscsCaseData> callback;
     @Mock
     private CaseDetails<SscsCaseData> caseDetails;
+    @Mock
+    private HearingRecordingRequestService hearingRecordingRequestService;
     private SscsCaseData sscsCaseData;
+
+    @Mock
+    private FooterService footerService;
 
     @Before
     public void setUp() {
         openMocks(this);
-        handler = new UploadDocumentFurtherEvidenceMidEventHandler();
-        sscsCaseData = SscsCaseData.builder().appeal(
-                Appeal.builder().mrnDetails(MrnDetails.builder().dwpIssuingOffice("3").build()).build()).build();
+        handler = new UploadDocumentFurtherEvidenceMidEventHandler(hearingRecordingRequestService, footerService);
+        sscsCaseData = SscsCaseData.builder()
+                .sscsHearingRecordingCaseData(SscsHearingRecordingCaseData.builder()
+                        .sscsHearingRecordings(List.of(recording(1)))
+                        .build())
+                .appeal(Appeal.builder().mrnDetails(MrnDetails.builder().dwpIssuingOffice("3").build()).build()).build();
 
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(callback.getEvent()).thenReturn(EventType.UPLOAD_DOCUMENT_FURTHER_EVIDENCE);
         when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+        when(footerService.isReadablePdf(any())).thenReturn(PdfState.OK);
+
+        super.setUp();
+    }
+
+    static SscsHearingRecording recording(int hearingId) {
+        return SscsHearingRecording.builder()
+                .value(SscsHearingRecordingDetails.builder()
+                        .hearingType("Adjourned")
+                        .uploadDate(LocalDate.now().toString())
+                        .recordings(List.of(HearingRecordingDetails.builder()
+                                .value(DocumentLink.builder().documentBinaryUrl(format("https://example/%s", hearingId)).build())
+                                .build()))
+                        .build())
+                .build();
     }
 
     @Test
@@ -71,7 +111,8 @@ public class UploadDocumentFurtherEvidenceMidEventHandlerTest {
 
     @Test
     public void givenOneHearingRecordingRequestDocumentIsUploaded_thenShowHearingsPageAndSetPartiesListForAppellant() {
-        SscsFurtherEvidenceDoc furtherEvidenceDoc = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder().documentType(REQUEST_FOR_HEARING_RECORDING.getId()).build()).build();
+        SscsFurtherEvidenceDoc furtherEvidenceDoc = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder()
+                .documentType(REQUEST_FOR_HEARING_RECORDING.getId()).documentLink(DocumentLink.builder().documentUrl("url.com").documentFilename("file.pdf").build()).build()).build();
 
         sscsCaseData.setDraftSscsFurtherEvidenceDocument(Collections.singletonList(furtherEvidenceDoc));
         PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(MID_EVENT, callback, USER_AUTHORISATION);
@@ -84,7 +125,8 @@ public class UploadDocumentFurtherEvidenceMidEventHandlerTest {
     @Test
     public void givenOneHearingRecordingRequestDocumentIsUploaded_thenShowHearingsPageAndSetPartiesListForAppellantAndRep() {
         sscsCaseData.getAppeal().setRep(Representative.builder().hasRepresentative("yes").build());
-        SscsFurtherEvidenceDoc furtherEvidenceDoc = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder().documentType(REQUEST_FOR_HEARING_RECORDING.getId()).build()).build();
+        SscsFurtherEvidenceDoc furtherEvidenceDoc = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder()
+                .documentType(REQUEST_FOR_HEARING_RECORDING.getId()).documentLink(DocumentLink.builder().documentUrl("url.com").documentFilename("file.pdf").build()).build()).build();
 
         sscsCaseData.setDraftSscsFurtherEvidenceDocument(Collections.singletonList(furtherEvidenceDoc));
         PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(MID_EVENT, callback, USER_AUTHORISATION);
@@ -98,7 +140,8 @@ public class UploadDocumentFurtherEvidenceMidEventHandlerTest {
     @Test
     public void givenOneHearingRecordingRequestDocumentIsUploaded_thenShowHearingsPageAndSetPartiesListForAppellantAndJointParty() {
         sscsCaseData.setJointParty("Yes");
-        SscsFurtherEvidenceDoc furtherEvidenceDoc = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder().documentType(REQUEST_FOR_HEARING_RECORDING.getId()).build()).build();
+        SscsFurtherEvidenceDoc furtherEvidenceDoc = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder()
+                .documentType(REQUEST_FOR_HEARING_RECORDING.getId()).documentLink(DocumentLink.builder().documentUrl("url.com").documentFilename("file.pdf").build()).build()).build();
 
         sscsCaseData.setDraftSscsFurtherEvidenceDocument(Collections.singletonList(furtherEvidenceDoc));
         PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(MID_EVENT, callback, USER_AUTHORISATION);
@@ -111,8 +154,10 @@ public class UploadDocumentFurtherEvidenceMidEventHandlerTest {
 
     @Test
     public void givenMoreThanOneHearingRecordingRequestDocumentIsUploaded_thenShowError() {
-        SscsFurtherEvidenceDoc furtherEvidenceDoc1 = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder().documentType(REQUEST_FOR_HEARING_RECORDING.getId()).build()).build();
-        SscsFurtherEvidenceDoc furtherEvidenceDoc2 = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder().documentType(REQUEST_FOR_HEARING_RECORDING.getId()).build()).build();
+        SscsFurtherEvidenceDoc furtherEvidenceDoc1 = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder()
+                .documentType(REQUEST_FOR_HEARING_RECORDING.getId()).documentLink(DocumentLink.builder().documentUrl("url.com").documentFilename("file.pdf").build()).build()).build();
+        SscsFurtherEvidenceDoc furtherEvidenceDoc2 = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder()
+                .documentType(REQUEST_FOR_HEARING_RECORDING.getId()).documentLink(DocumentLink.builder().documentUrl("url.com").documentFilename("file.pdf").build()).build()).build();
 
         List<SscsFurtherEvidenceDoc> sscsFurtherEvidenceDocList = new ArrayList<>();
         sscsFurtherEvidenceDocList.add(furtherEvidenceDoc1);
@@ -130,6 +175,151 @@ public class UploadDocumentFurtherEvidenceMidEventHandlerTest {
         PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(MID_EVENT, callback, USER_AUTHORISATION);
 
         assertNull(response.getData().getSscsHearingRecordingCaseData().getShowRequestingPartyPage());
+    }
+
+    @Test
+    @Parameters({"APPELLANT", "REPRESENTATIVE", "JOINT_PARTY"})
+    public void givenARequestHearingRecordingEvent_thenBuildTheUi(PartyItemList partyItem) {
+        PreSubmitCallbackResponse response = new PreSubmitCallbackResponse(callback.getCaseDetails().getCaseData());
+
+        SscsFurtherEvidenceDoc furtherEvidenceDoc = SscsFurtherEvidenceDoc.builder().value(SscsFurtherEvidenceDocDetails.builder()
+                .documentType(REQUEST_FOR_HEARING_RECORDING.getId()).documentLink(DocumentLink.builder().documentUrl("url.com").documentFilename("file.pdf").build()).build()).build();
+        sscsCaseData.setDraftSscsFurtherEvidenceDocument(Collections.singletonList(furtherEvidenceDoc));
+        sscsCaseData.getSscsHearingRecordingCaseData().setRequestingParty(new DynamicList(partyItem.getCode()));
+
+        when(hearingRecordingRequestService.buildHearingRecordingUi(any(PreSubmitCallbackResponse.class), eq(partyItem)))
+                .thenReturn(response);
+
+        handler.handle(MID_EVENT, callback, USER_AUTHORISATION);
+
+        verify(hearingRecordingRequestService).buildHearingRecordingUi(any(PreSubmitCallbackResponse.class), eq(partyItem));
+    }
+
+    @Test
+    @Parameters({
+            "MID_EVENT,UPLOAD_DOCUMENT_FURTHER_EVIDENCE,withDwp,representativeEvidence,appellantEvidence,FILE_UPLOAD_IS_EMPTY",
+            "MID_EVENT,UPLOAD_DOCUMENT_FURTHER_EVIDENCE,withDwp,representativeEvidence,appellantEvidence,FILE_UPLOAD_IS_NULL",
+            "MID_EVENT,UPLOAD_DOCUMENT_FURTHER_EVIDENCE,withDwp,,appellantEvidence,null",
+            "MID_EVENT,UPLOAD_DOCUMENT_FURTHER_EVIDENCE,withDwp,,representativeEvidence,null",
+            "MID_EVENT,UPLOAD_DOCUMENT_FURTHER_EVIDENCE,withDwp,,,null",
+            "MID_EVENT,UPLOAD_DOCUMENT_FURTHER_EVIDENCE,withDwp,,,FILE_UPLOAD_IS_NULL"
+    })
+    public void handleErrorScenariosWhenSomeFieldsAreNotProvided(@Nullable CallbackType callbackType,
+                                                                 @Nullable EventType eventType, String state,
+                                                                 @Nullable String documentType,
+                                                                 @Nullable String documentType2,
+                                                                 @Nullable FileUploadScenario fileUploadScenario)
+            throws IOException {
+        Callback<SscsCaseData> callback = buildTestCallbackGivenData(eventType,state, documentType, documentType2,
+                UPLOAD_DOCUMENT_FE_CALLBACK_JSON);
+
+        if (FILE_UPLOAD_IS_EMPTY.equals(fileUploadScenario)) {
+            callback.getCaseDetails().getCaseData().setDraftSscsFurtherEvidenceDocument(Collections.emptyList());
+        }
+        if (FILE_UPLOAD_IS_NULL.equals(fileUploadScenario)) {
+            callback.getCaseDetails().getCaseData().setDraftSscsFurtherEvidenceDocument(null);
+        }
+
+        PreSubmitCallbackResponse<SscsCaseData> actualResponse = handler.handle(callbackType, callback,
+                USER_AUTHORISATION);
+
+        assertThatJson(actualResponse.getData()).isEqualTo(callback.getCaseDetails().getCaseData());
+        assertNull(actualResponse.getData().getDwpState());
+        long numberOfExpectedError = actualResponse.getErrors().stream()
+                .filter(error -> error.equalsIgnoreCase("You need to provide a file and a document type"))
+                .count();
+        assertEquals(1, numberOfExpectedError);
+    }
+
+    @Test
+    public void handleDocumentUploadWhereUploadedFileIsNotAValid() throws IOException {
+        Callback<SscsCaseData> callback = buildTestCallbackGivenData(UPLOAD_DOCUMENT_FURTHER_EVIDENCE,
+                "withDwp",
+                "representativeEvidence", "appellantEvidence",
+                UPLOAD_DOCUMENT_FE_CALLBACK_JSON);
+        List<SscsFurtherEvidenceDoc> draftDocuments = Collections.singletonList(SscsFurtherEvidenceDoc.builder()
+                .value(SscsFurtherEvidenceDocDetails.builder()
+                        .documentFileName("word.docx")
+                        .documentType("representativeEvidence")
+                        .documentLink(DocumentLink.builder()
+                                .documentUrl("http://dm-store:5005/documents/abe3b75a-7a72-4e68-b136-4349b7d4f655")
+                                .documentFilename("word.docx").build())
+                        .build())
+                .build());
+        callback.getCaseDetails().getCaseData().setDraftSscsFurtherEvidenceDocument(draftDocuments);
+        PreSubmitCallbackResponse<SscsCaseData> actualResponse = handler.handle(CallbackType.MID_EVENT, callback, USER_AUTHORISATION);
+        assertThatJson(actualResponse.getData()).isEqualTo(callback.getCaseDetails().getCaseData());
+        assertNull(actualResponse.getData().getDwpState());
+        assertNull(actualResponse.getData().getDraftSscsFurtherEvidenceDocument());
+        long numberOfExpectedError = actualResponse.getErrors().stream()
+                .filter(error -> error.equalsIgnoreCase("You need to upload PDF, MP3 or MP4 file only"))
+                .count();
+        assertEquals(1, numberOfExpectedError);
+    }
+
+
+    @Test
+    public void handleDocumentUploadWhereUploadedFileIsNotAReadablePdf() throws Exception {
+        Callback<SscsCaseData> callback = buildTestCallbackGivenData(UPLOAD_DOCUMENT_FURTHER_EVIDENCE,
+                "withDwp",
+                "representativeEvidence", "appellantEvidence",
+                UPLOAD_DOCUMENT_FE_CALLBACK_JSON);
+
+        List<SscsFurtherEvidenceDoc> draftDocuments = Collections.singletonList(SscsFurtherEvidenceDoc.builder()
+                .value(SscsFurtherEvidenceDocDetails.builder()
+                        .documentFileName("badPdf.pdf")
+                        .documentType("representativeEvidence")
+                        .documentLink(DocumentLink.builder()
+                                .documentUrl("http://dm-store:5005/documents/abe3b75a-7a72-4e68-b136-4349b7d4f655")
+                                .documentFilename("bdPdf.pdf").build())
+                        .build())
+                .build());
+
+        when(footerService.isReadablePdf(any())).thenReturn(PdfState.UNREADABLE);
+
+        callback.getCaseDetails().getCaseData().setDraftSscsFurtherEvidenceDocument(draftDocuments);
+        PreSubmitCallbackResponse<SscsCaseData> actualResponse = handler.handle(CallbackType.MID_EVENT, callback, USER_AUTHORISATION);
+
+        assertThatJson(actualResponse.getData()).isEqualTo(callback.getCaseDetails().getCaseData());
+        assertNull(actualResponse.getData().getDwpState());
+        assertNull(actualResponse.getData().getDraftSscsFurtherEvidenceDocument());
+
+        long numberOfExpectedError = actualResponse.getErrors().stream()
+                .filter(error -> error.equalsIgnoreCase("Your PDF Document is not readable."))
+                .count();
+        assertEquals(1, numberOfExpectedError);
+    }
+
+    @Test
+    public void handleDocumentUploadWhereUploadedFileIsPasswordProtectedPdf() throws Exception {
+        Callback<SscsCaseData> callback = buildTestCallbackGivenData(UPLOAD_DOCUMENT_FURTHER_EVIDENCE,
+                "withDwp",
+                "representativeEvidence", "appellantEvidence",
+                UPLOAD_DOCUMENT_FE_CALLBACK_JSON);
+
+        List<SscsFurtherEvidenceDoc> draftDocuments = Collections.singletonList(SscsFurtherEvidenceDoc.builder()
+                .value(SscsFurtherEvidenceDocDetails.builder()
+                        .documentFileName("badPdf.pdf")
+                        .documentType("representativeEvidence")
+                        .documentLink(DocumentLink.builder()
+                                .documentUrl("http://dm-store:5005/documents/abe3b75a-7a72-4e68-b136-4349b7d4f655")
+                                .documentFilename("bdPdf.pdf").build())
+                        .build())
+                .build());
+
+        when(footerService.isReadablePdf(any())).thenReturn(PdfState.PASSWORD_ENCRYPTED);
+
+        callback.getCaseDetails().getCaseData().setDraftSscsFurtherEvidenceDocument(draftDocuments);
+        PreSubmitCallbackResponse<SscsCaseData> actualResponse = handler.handle(CallbackType.MID_EVENT, callback, USER_AUTHORISATION);
+
+        assertThatJson(actualResponse.getData()).isEqualTo(callback.getCaseDetails().getCaseData());
+        assertNull(actualResponse.getData().getDwpState());
+        assertNull(actualResponse.getData().getDraftSscsFurtherEvidenceDocument());
+
+        long numberOfExpectedError = actualResponse.getErrors().stream()
+                .filter(error -> error.equalsIgnoreCase("Your PDF Document cannot be password protected."))
+                .count();
+        assertEquals(1, numberOfExpectedError);
     }
 
     @Test(expected = IllegalStateException.class)
