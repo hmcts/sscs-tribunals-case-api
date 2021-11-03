304f830cfe93dcb18c6a7f160c8e4a7e72821ee8
jackmaloney <jack.maloney@transformuk.com>
2021-11-03 10:25:58 +0000

    SSCS-9711 Callback for attach scanned docs event

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/attachscanneddocs/AttachScannedDocsAboutToSubmitHandlerTest.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.attachscanneddocs;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+
+public class AttachScannedDocsAboutToSubmitHandlerTest {
+
+    private static final String USER_AUTHORISATION = "Bearer token";
+
+    private AttachScannedDocsAboutToSubmitHandler handler;
+
+    @Mock
+    private Callback<SscsCaseData> callback;
+
+    @Mock
+    private CaseDetails<SscsCaseData> caseDetails;
+
+    private SscsCaseData sscsCaseData;
+
+    @Before
+    public void setUp() {
+        openMocks(this);
+        handler = new AttachScannedDocsAboutToSubmitHandler();
+
+        when(callback.getEvent()).thenReturn(EventType.ATTACH_SCANNED_DOCS);
+        sscsCaseData = SscsCaseData.builder().createdInGapsFrom("readyToList").appeal(Appeal.builder().build()).build();
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+    }
+
+    @Test
+    public void givenAValidEvent_thenReturnTrue() {
+        assertTrue(handler.canHandle(ABOUT_TO_SUBMIT, callback));
+    }
+
+    @Test
+    public void givenANonAttachScannedDocsEvent_thenReturnFalse() {
+        when(callback.getEvent()).thenReturn(EventType.APPEAL_RECEIVED);
+
+        assertFalse(handler.canHandle(ABOUT_TO_START, callback));
+    }
+
+    @Test
+    public void givenAnAttachScannedDocsEvent_thenSetEvidenceFlagsToNo() {
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        assertEquals(YesNo.NO.getValue(), response.getData().getEvidenceHandled());
+        assertEquals(YesNo.NO, response.getData().getHasUnprocessedAudioVideoEvidence());
+    }
+}
