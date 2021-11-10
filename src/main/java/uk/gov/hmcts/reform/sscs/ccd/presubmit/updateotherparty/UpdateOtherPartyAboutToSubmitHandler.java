85b4f4ab5908b0dd1303348f8efdf6520f6153fe
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-11-10 14:16:17 +0000

    SSCS-9682: Child Support - Upload response event: Add/Review Other Parties

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/updateotherparty/UpdateOtherPartyAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/updateotherparty/UpdateOtherPartyAboutToSubmitHandler.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.updateotherparty;
 
 import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.sscs.util.OtherPartyDataUtil.assignOtherPartyId;
+import static uk.gov.hmcts.reform.sscs.util.OtherPartyDataUtil.getIdComparator;
 
 import java.util.Comparator;
 import java.util.List;
@@ -43,32 +45,4 @@ public class UpdateOtherPartyAboutToSubmitHandler implements PreSubmitCallbackHa
 
         return new PreSubmitCallbackResponse<>(sscsCaseData);
     }
-
-    private void assignOtherPartyId(List<CcdValue<OtherParty>> otherParties) {
-        int maxId = 0;
-        for (CcdValue<OtherParty> otherParty : otherParties) {
-            if (otherParty.getValue().getId() != null) {
-                maxId = Integer.parseInt(otherParty.getValue().getId());
-            } else {
-                otherParty.getValue().setId(Integer.toString(++maxId));
-            }
-        }
-    }
-
-    @NotNull
-    private Comparator<CcdValue<OtherParty>> getIdComparator() {
-        return new Comparator<CcdValue<OtherParty>>() {
-            @Override
-            public int compare(CcdValue<OtherParty> a, CcdValue<OtherParty> b) {
-                if (a.getValue().getId() == null) {
-                    return b.getValue().getId() == null ? 0 : 1;
-                } else if (b.getValue().getId() == null) {
-                    return -1;
-                } else {
-                    return a.getValue().getId().compareTo(b.getValue().getId());
-                }
-            }
-        };
-    }
-
 }
