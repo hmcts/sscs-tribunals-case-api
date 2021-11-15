697a85a5791710171cb40fc1e82ce0785bbb1974
Chris Davidson <christopher.davidson@btinternet.com>
2021-11-15 16:56:05 +0000

    sscs 9735 callbacks

--- src/main/java/uk/gov/hmcts/reform/sscs/util/OtherPartyDataUtil.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/util/OtherPartyDataUtil.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.sscs.util;
 
+import java.util.Comparator;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CcdValue;
 import uk.gov.hmcts.reform.sscs.ccd.domain.OtherParty;
 
-import java.util.Comparator;
-import java.util.List;
-
 public class OtherPartyDataUtil {
+
+    private OtherPartyDataUtil() {
+        //not called
+    }
+
     public static void assignOtherPartyId(List<CcdValue<OtherParty>> otherParties) {
         int maxId = 0;
         for (CcdValue<OtherParty> otherParty : otherParties) {
@@ -33,5 +37,25 @@ public class OtherPartyDataUtil {
                 }
             }
         };
+    }
+
+    public static boolean haveOtherPartiesChanged(List<CcdValue<OtherParty>> before, List<CcdValue<OtherParty>> after) {
+        if (before == null && after == null) {
+            return false;
+        }
+        if (before == null ^ after == null) {
+            return true;
+        }
+        if (before.size() != after.size()) {
+            return true;
+        }
+        before.sort(getIdComparator());
+        after.sort(getIdComparator());
+        for (int i = 0; i < before.size(); i++) {
+            if (!before.get(i).getValue().getId().equals(after.get(i).getValue().getId())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
