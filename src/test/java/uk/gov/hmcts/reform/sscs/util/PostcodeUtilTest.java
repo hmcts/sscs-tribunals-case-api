b6d347c5f3876f114c907440f46171a3f907fdd2
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2021-11-18 12:32:25 +0000

    SSCS-9726 - MYA Other party login (#2449)

--- src/test/java/uk/gov/hmcts/reform/sscs/util/PostcodeUtilTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/util/PostcodeUtilTest.java
@@ -3,13 +3,20 @@ package uk.gov.hmcts.reform.sscs.util;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
-import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Address;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appellant;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CcdValue;
+import uk.gov.hmcts.reform.sscs.ccd.domain.OtherParty;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 
 public class PostcodeUtilTest {
 
-    private String postcode = "CM11 1AB";
+    private final String postcode = "CM11 1AB";
     private SscsCaseDetails sscsCaseDetails;
 
     @Before
@@ -29,26 +36,42 @@ public class PostcodeUtilTest {
 
     @Test
     public void exactMatchIsEquivalent() {
-        assertThat(new PostcodeUtil().hasAppellantPostcode(sscsCaseDetails, postcode), is(true));
+        assertThat(new PostcodeUtil().hasAppellantOrOtherPartyPostcode(sscsCaseDetails, postcode), is(true));
     }
 
     @Test
     public void noSpaceIsEquivalent() {
-        assertThat(new PostcodeUtil().hasAppellantPostcode(sscsCaseDetails, "CM111AB"), is(true));
+        assertThat(new PostcodeUtil().hasAppellantOrOtherPartyPostcode(sscsCaseDetails, "CM111AB"), is(true));
     }
 
     @Test
     public void multipleSpacesAreEquivalent() {
-        assertThat(new PostcodeUtil().hasAppellantPostcode(sscsCaseDetails, "CM11    1AB"), is(true));
+        assertThat(new PostcodeUtil().hasAppellantOrOtherPartyPostcode(sscsCaseDetails, "CM11    1AB"), is(true));
     }
 
     @Test
     public void lowerCaseIsEquivalent() {
-        assertThat(new PostcodeUtil().hasAppellantPostcode(sscsCaseDetails, "cm11 1ab"), is(true));
+        assertThat(new PostcodeUtil().hasAppellantOrOtherPartyPostcode(sscsCaseDetails, "cm11 1ab"), is(true));
     }
 
     @Test
     public void differentIsNotEquivalent() {
-        assertThat(new PostcodeUtil().hasAppellantPostcode(sscsCaseDetails, "CM12 1AB"), is(false));
+        assertThat(new PostcodeUtil().hasAppellantOrOtherPartyPostcode(sscsCaseDetails, "CM12 1AB"), is(false));
+    }
+
+    @Test
+    public void willMatchAndOtherPartyPostcode() {
+        String otherPartyPostCode = "W1 1LA";
+        OtherParty otherParty = OtherParty.builder().address(Address.builder().postcode(otherPartyPostCode).build()).build();
+        sscsCaseDetails.getData().setOtherParties(List.of(new CcdValue<>(otherParty)));
+        assertThat(new PostcodeUtil().hasAppellantOrOtherPartyPostcode(sscsCaseDetails, "w1 1lA"), is(true));
+    }
+
+    @Test
+    public void willNotMatchAndOtherPartyOrAppellantsPostcodeIfIncorrectPostcodeIsGiven() {
+        String otherPartyPostCode = "W1 1LA";
+        OtherParty otherParty = OtherParty.builder().address(Address.builder().postcode(otherPartyPostCode).build()).build();
+        sscsCaseDetails.getData().setOtherParties(List.of(new CcdValue<>(otherParty)));
+        assertThat(new PostcodeUtil().hasAppellantOrOtherPartyPostcode(sscsCaseDetails, "inc 1ab"), is(false));
     }
 }
