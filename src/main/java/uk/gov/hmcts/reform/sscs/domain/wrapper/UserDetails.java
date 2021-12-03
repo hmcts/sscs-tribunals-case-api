229d205281f0460ace44e59b046a2e7be0635eca
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-12-03 14:30:54 +0000

    SSCS-9824: Child Support - MYA: Other party Log in

--- src/main/java/uk/gov/hmcts/reform/sscs/domain/wrapper/UserDetails.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/domain/wrapper/UserDetails.java
@@ -4,22 +4,41 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
+import java.util.List;
 import java.util.Objects;
 
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class AppellantDetails {
+public class UserDetails {
 
+    private final String type;
+    private final String name;
     private final AddressDetails addressDetails;
     private final String email;
     private final String phone;
     private final String mobile;
+    private final List<Subscription> subscriptions;
 
-    public AppellantDetails(AddressDetails addressDetails, String email, String phone, String mobile) {
+    public UserDetails(String type, String name, AddressDetails addressDetails, String email, String phone, String mobile, List<Subscription> subscriptions) {
+        this.type = type;
+        this.name = name;
         this.addressDetails = addressDetails;
         this.email = email;
         this.phone = phone;
         this.mobile = mobile;
+        this.subscriptions = subscriptions;
+    }
+
+    @ApiModelProperty(example = "Appellant", required = true)
+    @JsonProperty(value = "type")
+    public String getType() {
+        return type;
+    }
+
+    @ApiModelProperty(example = "manish sharma", required = true)
+    @JsonProperty(value = "name")
+    public String getName() {
+        return name;
     }
 
     @JsonProperty(value = "address_details")
@@ -45,6 +64,11 @@ public class AppellantDetails {
         return mobile;
     }
 
+    @JsonProperty(value = "subscriptions")
+    public List<Subscription> getSubscriptions() {
+        return subscriptions;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -53,8 +77,10 @@ public class AppellantDetails {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        AppellantDetails that = (AppellantDetails) o;
-        return Objects.equals(addressDetails, that.addressDetails)
+        UserDetails that = (UserDetails) o;
+        return Objects.equals(name, that.name)
+                && Objects.equals(type, that.type)
+                && Objects.equals(addressDetails, that.addressDetails)
                 && Objects.equals(email, that.email)
                 && Objects.equals(phone, that.phone)
                 && Objects.equals(mobile, that.mobile);
@@ -63,13 +89,15 @@ public class AppellantDetails {
     @Override
     public int hashCode() {
 
-        return Objects.hash(addressDetails, email, phone, mobile);
+        return Objects.hash(type, name, addressDetails, email, phone, mobile);
     }
 
     @Override
     public String toString() {
         return "AppellantDetails{"
-                + "addressDetails=" + addressDetails
+                + "type=" + type
+                + ", name=" + name
+                + ", addressDetails=" + addressDetails + '\''
                 + ", email='" + email + '\''
                 + ", phone='" + phone + '\''
                 + ", mobile='" + mobile + '\''
--- src/main/java/uk/gov/hmcts/reform/sscs/service/OnlineHearingService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/OnlineHearingService.java
@@ -1,14 +1,18 @@
 package uk.gov.hmcts.reform.sscs.service;
 
 import static java.util.Collections.emptyList;
+import static java.util.stream.Stream.of;
+import static org.apache.commons.collections4.ListUtils.emptyIfNull;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Subscription;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
 import uk.gov.hmcts.reform.sscs.domain.wrapper.*;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
@@ -46,12 +50,23 @@ public class OnlineHearingService {
         });
     }
 
+    //Load hearing with appellant details as user details
     public Optional<OnlineHearing> loadHearing(SscsCaseDetails sscsCaseDeails) {
-        SscsCaseData data = sscsCaseDeails.getData();
+        UserDetails userDetails = convertUserDetails(sscsCaseDeails, null, null, true);
+        return populateHearing(sscsCaseDeails, userDetails);
+    }
+
+    //Load hearing with signed-in user details
+    public Optional<OnlineHearing> loadHearing(SscsCaseDetails sscsCaseDeails, String tya, String email) {
+        UserDetails userDetails = convertUserDetails(sscsCaseDeails, tya, email, false);
+        return populateHearing(sscsCaseDeails, userDetails);
+    }
+
+    private Optional<OnlineHearing> populateHearing(SscsCaseDetails sscsCaseDetails, UserDetails userDetails) {
+        SscsCaseData data = sscsCaseDetails.getData();
         HearingOptions hearingOptions = data.getAppeal().getHearingOptions();
-        Appellant appellant = sscsCaseDeails.getData().getAppeal().getAppellant();
-        AppellantDetails appellantDetails = convertAppellantDetails(appellant);
-        AppealDetails appealDetails = convertAppealDetails(sscsCaseDeails);
+        Appellant appellant = sscsCaseDetails.getData().getAppeal().getAppellant();
+        AppealDetails appealDetails = convertAppealDetails(sscsCaseDetails);
         Name name = appellant.getName();
         String nameString = name.getFirstName() + " " + name.getLastName();
 
@@ -59,8 +74,8 @@ public class OnlineHearingService {
                 ? hearingOptions.getArrangements() : emptyList();
         return Optional.of(new OnlineHearing(
                 nameString,
-                sscsCaseDeails.getData().getCaseReference(),
-                sscsCaseDeails.getId(),
+                sscsCaseDetails.getData().getCaseReference(),
+                sscsCaseDetails.getId(),
                 new HearingArrangements(
                         "yes".equalsIgnoreCase(hearingOptions.getLanguageInterpreter()),
                         hearingOptions.getLanguages(),
@@ -70,7 +85,7 @@ public class OnlineHearingService {
                         arrangements.contains("disabledAccess"),
                         hearingOptions.getOther()
                 ),
-                appellantDetails,
+                userDetails,
                 appealDetails
         ));
     }
@@ -83,12 +98,59 @@ public class OnlineHearingService {
         );
     }
 
-    private AppellantDetails convertAppellantDetails(Appellant appellant) {
-        Address address = appellant.getAddress();
-        Optional<Contact> contact = Optional.ofNullable(appellant.getContact());
+    private UserDetails convertUserDetails(SscsCaseDetails sscsCaseDetails, String tya, String email, boolean onlyForAppellant) {
+        Address address = null;
+        Optional<Contact> contact = Optional.empty();
+        Name name = null;
+        Map<UserType,Subscription> subscriptionsMap = null;
+        UserType type = null;
+
+
+        Subscriptions subscriptions = sscsCaseDetails.getData().getSubscriptions();
+        Map<UserType,Subscription> appellantSubscriptions = of(Pair.of(UserType.APPELLANT, subscriptions.getAppellantSubscription()),
+                Pair.of(UserType.APPOINTEE, subscriptions.getAppointeeSubscription()),
+                Pair.of(UserType.REP, subscriptions.getRepresentativeSubscription()),
+                Pair.of(UserType.SUPPORTER, subscriptions.getSupporterSubscription()),
+                Pair.of(UserType.JOINT_PARTY, subscriptions.getJointPartySubscription()))
+                .filter(p -> p.getLeft() != null && p.getRight() != null)
+                .filter(p -> p.getRight().getEmail() != null || p.getRight().getMobile() != null)
+                .collect(Collectors.toMap(Pair::getLeft, Pair::getValue));
+
+
+        if (onlyForAppellant || isSignInSubscription(appellantSubscriptions.values(), tya, email)) {
+            address =  sscsCaseDetails.getData().getAppeal().getAppellant().getAddress();
+            contact = Optional.ofNullable(sscsCaseDetails.getData().getAppeal().getAppellant().getContact());
+            name = sscsCaseDetails.getData().getAppeal().getAppellant().getName();
+            subscriptionsMap = appellantSubscriptions;
+            type = UserType.APPELLANT;
+        } else {
+            for (CcdValue<OtherParty> op : emptyIfNull(sscsCaseDetails.getData().getOtherParties())) {
+                Map<UserType,Subscription> otherPartySubscriptions = of(Pair.of(UserType.OTHER_PARTY, op.getValue().getOtherPartySubscription()),
+                        Pair.of(UserType.OTHER_PARTY_APPOINTEE, op.getValue().getOtherPartyAppointeeSubscription()),
+                        Pair.of(UserType.OTHER_PARTY_REP, op.getValue().getOtherPartyRepresentativeSubscription()))
+                        .filter(p -> p.getLeft() != null && p.getRight() != null)
+                        .filter(p -> p.getRight().getEmail() != null || p.getRight().getMobile() != null)
+                        .collect(Collectors.toMap(Pair::getLeft, Pair::getValue));
+                if (isSignInSubscription(otherPartySubscriptions.values(), tya, email)) {
+                    address =  op.getValue().getAddress();
+                    contact = Optional.ofNullable(op.getValue().getContact());
+                    name = op.getValue().getName();
+                    subscriptionsMap = otherPartySubscriptions;
+                    type = UserType.OTHER_PARTY;
+                    break;
+                }
+            }
+        }
+
+        assert address != null;
+        return populateUserDetails(type, name, address, contact, subscriptionsMap);
+    }
+
+    private UserDetails populateUserDetails(UserType type, Name name, Address address, Optional<Contact> contact, Map<UserType,Subscription> subscriptionsMap) {
         String email = null;
         String phone = null;
         String mobile = null;
+        String nameString = name.getFirstName() + " " + name.getLastName();
 
         AddressDetails addressDetails = new AddressDetails(address.getLine1(), address.getLine2(), address.getTown(), address.getCounty(), address.getPostcode());
 
@@ -99,8 +161,16 @@ public class OnlineHearingService {
             mobile = contactObj.getMobile();
         }
 
-        AppellantDetails appellantDetails = new AppellantDetails(addressDetails, email, phone, mobile);
+        List<uk.gov.hmcts.reform.sscs.domain.wrapper.Subscription> subscriptions = subscriptionsMap.entrySet().stream()
+                .filter(Objects::nonNull)
+                .filter(s -> !UserType.JOINT_PARTY.equals(s.getKey()))
+                .map(s -> new uk.gov.hmcts.reform.sscs.domain.wrapper.Subscription(s.getKey().getType(), s.getValue().getEmail(), s.getValue().getMobile()))
+                .collect(Collectors.toList());
 
-        return appellantDetails;
+        return new UserDetails(type.getType(), nameString, addressDetails, email, phone, mobile, subscriptions);
+    }
+
+    private boolean isSignInSubscription(Collection<Subscription> subscriptionStream, String tya, String email) {
+        return subscriptionStream.stream().anyMatch(subscription -> subscription != null && tya.equals(subscription.getTya()) && email.equalsIgnoreCase(subscription.getEmail()));
     }
 }
