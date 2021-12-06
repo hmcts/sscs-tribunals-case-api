0fba1f92ef4455f566444f3207e68bdddec00ccd
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-12-06 15:09:36 +0000

    refactored

--- src/main/java/uk/gov/hmcts/reform/sscs/service/OnlineHearingService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/OnlineHearingService.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
@@ -105,17 +106,7 @@ public class OnlineHearingService {
         Map<UserType,Subscription> subscriptionsMap = null;
         UserType type = null;
 
-
-        Subscriptions subscriptions = sscsCaseDetails.getData().getSubscriptions();
-        Map<UserType,Subscription> appellantSubscriptions = of(Pair.of(UserType.APPELLANT, subscriptions.getAppellantSubscription()),
-                Pair.of(UserType.APPOINTEE, subscriptions.getAppointeeSubscription()),
-                Pair.of(UserType.REP, subscriptions.getRepresentativeSubscription()),
-                Pair.of(UserType.SUPPORTER, subscriptions.getSupporterSubscription()),
-                Pair.of(UserType.JOINT_PARTY, subscriptions.getJointPartySubscription()))
-                .filter(p -> p.getLeft() != null && p.getRight() != null)
-                .filter(p -> p.getRight().getEmail() != null || p.getRight().getMobile() != null)
-                .collect(Collectors.toMap(Pair::getLeft, Pair::getValue));
-
+        Map<UserType, Subscription> appellantSubscriptions = getAppealSubscriptionMap(sscsCaseDetails);
 
         if (onlyForAppellant || isSignInSubscription(appellantSubscriptions.values(), tya, email)) {
             address =  sscsCaseDetails.getData().getAppeal().getAppellant().getAddress();
@@ -125,12 +116,7 @@ public class OnlineHearingService {
             type = UserType.APPELLANT;
         } else {
             for (CcdValue<OtherParty> op : emptyIfNull(sscsCaseDetails.getData().getOtherParties())) {
-                Map<UserType,Subscription> otherPartySubscriptions = of(Pair.of(UserType.OTHER_PARTY, op.getValue().getOtherPartySubscription()),
-                        Pair.of(UserType.OTHER_PARTY_APPOINTEE, op.getValue().getOtherPartyAppointeeSubscription()),
-                        Pair.of(UserType.OTHER_PARTY_REP, op.getValue().getOtherPartyRepresentativeSubscription()))
-                        .filter(p -> p.getLeft() != null && p.getRight() != null)
-                        .filter(p -> p.getRight().getEmail() != null || p.getRight().getMobile() != null)
-                        .collect(Collectors.toMap(Pair::getLeft, Pair::getValue));
+                Map<UserType, Subscription> otherPartySubscriptions = getOtherPartySubscriptionMap(op);
                 if (isSignInSubscription(otherPartySubscriptions.values(), tya, email)) {
                     address =  op.getValue().getAddress();
                     contact = Optional.ofNullable(op.getValue().getContact());
@@ -144,6 +130,29 @@ public class OnlineHearingService {
 
         assert address != null;
         return populateUserDetails(type, name, address, contact, subscriptionsMap);
+    }
+
+    @NotNull
+    private Map<UserType, Subscription> getOtherPartySubscriptionMap(CcdValue<OtherParty> op) {
+        return of(Pair.of(UserType.OTHER_PARTY, op.getValue().getOtherPartySubscription()),
+                Pair.of(UserType.OTHER_PARTY_APPOINTEE, op.getValue().getOtherPartyAppointeeSubscription()),
+                Pair.of(UserType.OTHER_PARTY_REP, op.getValue().getOtherPartyRepresentativeSubscription()))
+                .filter(p -> p.getLeft() != null && p.getRight() != null)
+                .filter(p -> p.getRight().getEmail() != null || p.getRight().getMobile() != null)
+                .collect(Collectors.toMap(Pair::getLeft, Pair::getValue));
+    }
+
+    @NotNull
+    private Map<UserType, Subscription> getAppealSubscriptionMap(SscsCaseDetails sscsCaseDetails) {
+        Subscriptions subscriptions = sscsCaseDetails.getData().getSubscriptions();
+        return of(Pair.of(UserType.APPELLANT, subscriptions.getAppellantSubscription()),
+                Pair.of(UserType.APPOINTEE, subscriptions.getAppointeeSubscription()),
+                Pair.of(UserType.REP, subscriptions.getRepresentativeSubscription()),
+                Pair.of(UserType.SUPPORTER, subscriptions.getSupporterSubscription()),
+                Pair.of(UserType.JOINT_PARTY, subscriptions.getJointPartySubscription()))
+                .filter(p -> p.getLeft() != null && p.getRight() != null)
+                .filter(p -> p.getRight().getEmail() != null || p.getRight().getMobile() != null)
+                .collect(Collectors.toMap(Pair::getLeft, Pair::getValue));
     }
 
     private UserDetails populateUserDetails(UserType type, Name name, Address address, Optional<Contact> contact, Map<UserType,Subscription> subscriptionsMap) {
@@ -161,13 +170,18 @@ public class OnlineHearingService {
             mobile = contactObj.getMobile();
         }
 
-        List<uk.gov.hmcts.reform.sscs.domain.wrapper.Subscription> subscriptions = subscriptionsMap.entrySet().stream()
+        List<uk.gov.hmcts.reform.sscs.domain.wrapper.Subscription> subscriptions = getSubscriptions(subscriptionsMap);
+
+        return new UserDetails(type.getType(), nameString, addressDetails, email, phone, mobile, subscriptions);
+    }
+
+    @NotNull
+    private List<uk.gov.hmcts.reform.sscs.domain.wrapper.Subscription> getSubscriptions(Map<UserType, Subscription> subscriptionsMap) {
+        return subscriptionsMap.entrySet().stream()
                 .filter(Objects::nonNull)
                 .filter(s -> !UserType.JOINT_PARTY.equals(s.getKey()))
                 .map(s -> new uk.gov.hmcts.reform.sscs.domain.wrapper.Subscription(s.getKey().getType(), s.getValue().getEmail(), s.getValue().getMobile()))
                 .collect(Collectors.toList());
-
-        return new UserDetails(type.getType(), nameString, addressDetails, email, phone, mobile, subscriptions);
     }
 
     private boolean isSignInSubscription(Collection<Subscription> subscriptionStream, String tya, String email) {
