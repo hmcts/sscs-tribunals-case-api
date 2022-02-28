27f901416044ed9fb17b15668fa777c4972bbecb
TuranG <gorkem.turan@hmcts.net>
2022-02-28 09:43:58 +0000

    scss-10288-extra seperation is removed

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/procesaudiovideo/ProcessAudioVideoEvidenceAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/procesaudiovideo/ProcessAudioVideoEvidenceAboutToSubmitHandlerTest.java
@@ -17,7 +17,6 @@ import static uk.gov.hmcts.reform.sscs.ccd.presubmit.processaudiovideo.ProcessAu
 
 import java.time.LocalDate;
 import java.util.*;
-
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import junitparams.converters.Nullable;
