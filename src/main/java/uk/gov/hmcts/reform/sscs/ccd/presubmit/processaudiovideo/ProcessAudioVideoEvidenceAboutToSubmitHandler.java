b24c97e4a602733dd4ac386736f3d7ff62f4da69
TuranG <gorkem.turan@hmcts.net>
2022-03-01 09:23:16 +0000

    scss-10288-pulling out remove evidence

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processaudiovideo/ProcessAudioVideoEvidenceAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/processaudiovideo/ProcessAudioVideoEvidenceAboutToSubmitHandler.java
@@ -85,7 +85,6 @@ public class ProcessAudioVideoEvidenceAboutToSubmitHandler implements PreSubmitC
         processIfIssueDirectionNotice(caseData);
         processIfAdmitEvidence(caseData, response);
         processIfExcludeEvidence(caseData);
-        processIfRemoveEvidence(caseData, userAuthorisation);
         processIfSendToJudge(caseData, userAuthorisation);
         processIfSendToAdmin(caseData, userAuthorisation);
         overrideInterlocReviewStateIfSelected(caseData);
@@ -254,13 +253,9 @@ public class ProcessAudioVideoEvidenceAboutToSubmitHandler implements PreSubmitC
     }
 
     private void addToNotesIfNoteExists(SscsCaseData caseData, String userAuthorisation)  {
-        String noteDetails  = caseData.getTempNoteDetail();
-        if (StringUtils.equals(caseData.getProcessAudioVideoAction().getValue().getCode(), REMOVE_EVIDENCE.getCode())) {
-            noteDetails =  caseData.getNoteDetailRemoveAudioVideo();
-        }
-        if (StringUtils.isNoneBlank(noteDetails)) {
+        if (StringUtils.isNoneBlank(caseData.getTempNoteDetail())) {
             ArrayList<Note> notes = new ArrayList<>(Optional.ofNullable(caseData.getAppealNotePad()).flatMap(f -> Optional.ofNullable(f.getNotesCollection())).orElse(Collections.emptyList()));
-            final NoteDetails noteDetail = NoteDetails.builder().noteDetail(noteDetails).noteDate(LocalDate.now().toString()).author(userDetailsService.buildLoggedInUserName(userAuthorisation)).build();
+            final NoteDetails noteDetail = NoteDetails.builder().noteDetail(caseData.getTempNoteDetail()).noteDate(LocalDate.now().toString()).author(userDetailsService.buildLoggedInUserName(userAuthorisation)).build();
             notes.add(Note.builder().value(noteDetail).build());
             caseData.setAppealNotePad(NotePad.builder().notesCollection(notes).build());
         }
@@ -271,13 +266,6 @@ public class ProcessAudioVideoEvidenceAboutToSubmitHandler implements PreSubmitC
             caseData.setInterlocReviewState(null);
             caseData.setInterlocReferralReason(InterlocReferralReason.NONE.getId());
             caseData.setDwpState(DIRECTION_ACTION_REQUIRED.getId());
-            caseData.getAudioVideoEvidence().removeIf(evidence -> isSelectedEvidence(evidence, caseData));
-        }
-    }
-
-    private void processIfRemoveEvidence(SscsCaseData caseData, String userAuthorisation) {
-        if (StringUtils.equals(caseData.getProcessAudioVideoAction().getValue().getCode(), REMOVE_EVIDENCE.getCode())) {
-            addToNotesIfNoteExists(caseData, userAuthorisation);
             caseData.getAudioVideoEvidence().removeIf(evidence -> isSelectedEvidence(evidence, caseData));
         }
     }
