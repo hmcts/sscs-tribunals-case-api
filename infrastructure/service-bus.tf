de239eb6608dcd54595f50468d49f756fad65f88
Del Attipoe <delali.attipoe@hmcts.net>
2022-04-13 11:47:50 +0100

    SSCS-10220 Fix resource group.

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -2,5 +2,5 @@ module "sscs-hearings-event-queue" {
   source                = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
   name                  = "tribunals-to-hearing-api-${var.env}"
   namespace_name        = "sscs-servicebus-${var.env}"
-  resource_group_name   = azurerm_resource_group.rg.name
+  resource_group_name   = "sscs-${var.env}"
 }
