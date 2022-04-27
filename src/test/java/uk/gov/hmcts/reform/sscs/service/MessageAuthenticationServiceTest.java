8925d54b5eb0c54e42f1454bcb7e82bcbf5ee9aa
Waqas Arshid <waqas.arshid@hmcts.net>
2022-04-27 20:42:12 +0100

    SSCS-10223 fixed tests

--- src/test/java/uk/gov/hmcts/reform/sscs/service/MessageAuthenticationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/MessageAuthenticationServiceTest.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.sscs.service;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
@@ -19,10 +17,8 @@ import uk.gov.hmcts.reform.sscs.exception.InvalidSubscriptionTokenException;
 @RunWith(MockitoJUnitRunner.class)
 public class MessageAuthenticationServiceTest {
 
-    public static final String SUBSCRIPTION_ID = "subscriptionId";
-    public static final String APPEAL_ID = "appealId";
-    public static final String BENEFIT_TYPE = "benefitType";
     public static final String APPEAL_NUMBER = "dfdsf435345";
+    public static final String SUBSCRIPTION_ID = "subscriptionId";
     public static final String BENEFIT_TYPE_VALUE = "002";
 
     private static final String macString = "our-big-secret";
@@ -58,7 +54,8 @@ public class MessageAuthenticationServiceTest {
         Exception exception = assertThrows(InvalidSubscriptionTokenException.class, () ->
             service.validateMacTokenAndReturnBenefitType(encryptedToken + "sdft5e"));
 
-        assertThat(exception.getMessage(), is("Error while decrypting HMAC token " + encryptedToken + "sdft5e"));
+        assertThat(exception.getMessage())
+            .contains("Error while decrypting HMAC token " + encryptedToken + "sdft5e");
     }
 
     @Test
@@ -67,9 +64,10 @@ public class MessageAuthenticationServiceTest {
 
         Map<String, Object> tokenDetailsMap = service.decryptMacToken(encryptedToken);
 
-        assertThat(tokenDetailsMap.get(APPEAL_ID), equalTo(APPEAL_NUMBER));
-        assertThat(tokenDetailsMap.get(SUBSCRIPTION_ID), equalTo(SUBSCRIPTION_ID));
-        assertThat(tokenDetailsMap.get(BENEFIT_TYPE), equalTo(BENEFIT_TYPE_VALUE));
+        assertThat(tokenDetailsMap)
+            .containsValue(APPEAL_NUMBER)
+            .containsValue(SUBSCRIPTION_ID)
+            .containsValue(BENEFIT_TYPE_VALUE);
     }
 
     @Test
@@ -80,6 +78,7 @@ public class MessageAuthenticationServiceTest {
                 service.decryptMacToken(encryptedToken + "sdft5e")
         );
 
-        assertThat(exception.getMessage(), is("Error while decrypting HMAC token " + encryptedToken + "sdft5e"));
+        assertThat(exception.getMessage())
+            .contains("Error while decrypting HMAC token " + encryptedToken + "sdft5e");
     }
 }
