70d7d618eba3de03b47458f07b5dd73f17a4638d
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-05-26 16:32:05 +0100

    SSCS-10220-enable-queue-sessions2

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -3,4 +3,5 @@ module "sscs-hearings-event-queue" {
   name                = "tribunals-to-hearing-api-${var.env}"
   namespace_name      = "sscs-servicebus-${var.env}"
   resource_group_name = "sscs-${var.env}"
+  requires_session    = true
 }
