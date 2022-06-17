f8c124feae89d7d76467c5a7922eb5b9f0c81bac
rmurphy <98958394+rmurphy-hmcts@users.noreply.github.com>
2022-06-17 17:25:55 +0100

    Sscs 10668 readd prev config (#2794)
    * SSCS-1068-re add prev config
    
    * SSCS-1068-re add prev config
    
    * SSCS-1068-re add prev config
    
    * SSCS-1068-re add prev config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,12 +12,13 @@ data "azurerm_key_vault" "sscs_key_vault" {
 # }
 
 
+
 ### FOLLOWING FILES ARE TEMPORARY AND SHOULD BE REMOVED ONCE PERFEST TFSTATE CAN PERFORM A TERRAFORM INIT 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
-  location = var.location
+  location = "${var.location}"
 
-  tags = var.common_tags
+  tags = "${var.common_tags}"
 }
 
 # data "azurerm_key_vault" "sscs_key_vault" {
@@ -26,92 +27,92 @@ resource "azurerm_resource_group" "rg" {
 # }
 
 data "azurerm_key_vault_secret" "email_mac_secret" {
-  name         = "sscs-email-mac-secret-text"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "sscs-email-mac-secret-text"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "idam_api" {
-  name         = "idam-api"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "idam-api"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "idam_s2s_api" {
-  name         = "idam-s2s-api"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "idam-s2s-api"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "appeal_email_from" {
-  name         = "appeal-email-from"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "appeal-email-from"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "appeal_email_to" {
-  name         = "appeal-email-to"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "appeal-email-to"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "robotics_email_from" {
-  name         = "robotics-email-from"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "robotics-email-from"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "robotics_email_to" {
-  name         = "robotics-email-to"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "robotics-email-to"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "robotics_email_scottish_to" {
-  name         = "robotics-email-scottish-to"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "robotics-email-scottish-to"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "smtp_host" {
-  name         = "smtp-host"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "smtp-host"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "smtp_port" {
-  name         = "smtp-port"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "smtp-port"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "sscs_s2s_secret" {
-  name         = "sscs-s2s-secret"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "sscs-s2s-secret"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "idam_sscs_systemupdate_user" {
-  name         = "idam-sscs-systemupdate-user"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "idam-sscs-systemupdate-user"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "idam_sscs_systemupdate_password" {
-  name         = "idam-sscs-systemupdate-password"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "idam-sscs-systemupdate-password"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "idam_oauth2_client_secret" {
-  name         = "idam-sscs-oauth2-client-secret"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "idam-sscs-oauth2-client-secret"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "pdf_service_base_url" {
-  name         = "docmosis-endpoint"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "docmosis-endpoint"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "pdf_service_access_key" {
-  name         = "docmosis-api-key"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "docmosis-api-key"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "appinsights_instrumentation_key" {
-  name         = "AppInsightsInstrumentationKey"
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.key_vault_id
+  name      = "AppInsightsInstrumentationKey"
+  key_vault_id = "${data.azurerm_key_vault.sscs_key_vault.key_vault_id}"
 }
 
 locals {
-  local_ase = data.terraform_remote_state.core_apps_compute.ase_name[0]
+  local_ase = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
 
   ccdApi        = "http://ccd-data-store-api-${var.env}.service.${local.local_ase}.internal"
   s2sCnpUrl     = "http://rpe-service-auth-provider-${var.env}.service.${local.local_ase}.internal"
@@ -123,5 +124,5 @@ locals {
 
   shared_app_service_plan     = "${var.product}-${var.env}"
   non_shared_app_service_plan = "${var.product}-${var.component}-${var.env}"
-  app_service_plan            = (var.env == "saat" || var.env == "sandbox") ? local.shared_app_service_plan : local.non_shared_app_service_plan
+  app_service_plan            = "${(var.env == "saat" || var.env == "sandbox") ?  local.shared_app_service_plan : local.non_shared_app_service_plan}"
 }
