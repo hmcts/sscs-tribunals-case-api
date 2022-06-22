c4bf939f325b8cdd81cf268b15d28a0282321f40
MohammedHMCTS <95023176+MohammedHMCTS@users.noreply.github.com>
2022-06-22 13:15:52 +0100

    SSCS-10651 Switching order of output and resource

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -6,13 +6,13 @@ module "sscs-hearings-event-queue" {
   requires_session    = true
 }
 
+output "sb_primary_send_and_listen_shared_access_key" {
+  value     = module.sscs-hearings-event-queue.primary_send_and_listen_shared_access_key
+  sensitive = true
+}
+
 resource "azurerm_key_vault_secret" "servicebus_primary_shared_access_key" {
   name         = "tribunals-hearing-queue-shared-access-key"
   value        = module.sscs-hearings-event-queue.primary_send_and_listen_shared_access_key
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
-}
-
-output "sb_primary_send_and_listen_shared_access_key" {
-  value     = module.sscs-hearings-event-queue.primary_send_and_listen_shared_access_key
-  sensitive = true
 }
