d3047aad6c948b091f8d56ae8f21ec885b6e4870
Mohammed Qureshi <95023176+MohammedHMCTS@users.noreply.github.com>
2022-06-23 13:57:01 +0100

    SSCS-10651 Remove terraform

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -5,14 +5,3 @@ module "sscs-hearings-event-queue" {
   resource_group_name = "sscs-${var.env}"
   requires_session    = true
 }
-
-output "sb_primary_send_and_listen_shared_access_key" {
-  value     = module.sscs-hearings-event-queue.primary_send_and_listen_shared_access_key
-  sensitive = true
-}
-
-resource "azurerm_key_vault_secret" "servicebus_primary_shared_access_key" {
-  name         = "tribunals-hearing-queue-shared-access-key"
-  value        = module.sscs-hearings-event-queue.primary_send_and_listen_shared_access_key
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
-}
