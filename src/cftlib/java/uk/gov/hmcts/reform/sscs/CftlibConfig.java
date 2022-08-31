2522ce542f531859e389226de51cf84709939978
careyma <matthew.carey@hmcts.net>
2022-08-31 12:43:14 +0100

    fix: format

--- src/cftlib/java/uk/gov/hmcts/reform/sscs/CftlibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/reform/sscs/CftlibConfig.java
@@ -23,10 +23,10 @@ public class CftlibConfig implements CFTLibConfigurer {
             "caseworker-sscs-registrar"
         );
         lib.createIdamUser("local.test@example.com",
-	     "caseworker",
-	     "caseworker-sscs"
-	);
-	lib.createIdamUser("super-user@example.com",
+            "caseworker",
+            "caseworker-sscs"
+        );
+        lib.createIdamUser("super-user@example.com",
             "caseworker",
             "caseworker-sscs",
             "caseworker-sscs-superuser"
