c6d9557c9fe3f1655a53383068466518622ea9cc
Del Attipoe <delali.attipoe@hmcts.net>
2022-09-02 11:18:50 +0100

    SSCS-10867 Common update, fix ref data test.

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/client/RefDataClientIt.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/client/RefDataClientIt.java
@@ -1,65 +1,91 @@
 package uk.gov.hmcts.reform.sscs.client;
 
 
-import static org.junit.Assert.assertNotNull;
-
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
-import uk.gov.hmcts.reform.sscs.ccd.domain.Benefit;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 import uk.gov.hmcts.reform.sscs.model.CourtVenue;
-import uk.gov.hmcts.reform.sscs.service.AirLookupService;
-import uk.gov.hmcts.reform.sscs.service.RefDataService;
+import uk.gov.hmcts.reform.sscs.service.VenueDataLoader;
+import uk.gov.hmcts.reform.sscs.service.VenueService;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
+/**
+ * Intention of this test is to ensure that for all active venues in sscs-venues.csv, reference data
+ * returns valid court venue information. The main incidence for this test is in sscs-common's RefDataService
+ * but the test is here to cache auth information for performance purposes.
+ */
 @SpringBootTest
-@RunWith(JUnitParamsRunner.class)
+@RunWith(SpringRunner.class)
 @TestPropertySource(locations = "classpath:config/application_refdata_it.properties")
 public class RefDataClientIt {
-    /**
-     * Intention of this test is to assert that reference data api returns case management location
-     * for all the venue names in air lookup service. This is one time test to run when Air lookup data is changed.
-     */
-
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+    public static final String SSCS_COURT_TYPE_ID = "31";
 
     @Autowired
-    private RefDataService refDataService;
+    private VenueService venueService;
 
-    private static final AirLookupService airLookupService;
+    @Autowired
+    private RefDataApi refDataApi;
 
-    static {
-        airLookupService = new AirLookupService();
-        airLookupService.init();
-    }
+    @Autowired
+    private IdamService idamService;
+
+    @Autowired
+    private VenueDataLoader venueDataLoader;
 
     @Test
-    @Parameters(method = "getVenueName")
-    @Ignore
-    public void testVenueRefDataForVenueName(String venueName) {
-        CourtVenue venue = refDataService.getVenueRefData(venueName);
-        assertNotNull(venue);
-        assertNotNull(venue.getEpimsId());
-        assertNotNull(venue.getRegionId());
+    public void testVenueRefDataForVenueName() {
+        List<String> failedIds = new ArrayList<>();
+        List<String> missingInformationIds = new ArrayList<>();
+
+        List<String> epimsIds = venueDataLoader.getActiveVenueDetailsMapByEpimsId()
+            .keySet().stream()
+            .distinct()
+            .collect(Collectors.toList());
+
+        IdamTokens idamTokens = idamService.getIdamTokens();
+
+        String serviceToken = idamTokens.getServiceAuthorization();
+
+        String oauth2Token = idamTokens.getIdamOauth2Token();
+
+        for (String epimsId : epimsIds) {
+            try {
+                List<CourtVenue> sscsCourtVenues = refDataApi.courtVenueByEpimsId(serviceToken,
+                    oauth2Token, epimsId).stream().filter(venue -> SSCS_COURT_TYPE_ID.equals(venue.getCourtTypeId()))
+                    .collect(Collectors.toList());
+
+                if (sscsCourtVenues.size() != 1) {
+                    failedIds.add(epimsId);
+                }
+
+                checkForMissingRegionId(missingInformationIds, epimsId, sscsCourtVenues.get(0));
+            } catch (Exception ex) {
+                failedIds.add(epimsId);
+            }
+        }
+
+        assertThat(failedIds).isEmpty();
+        assertThat(missingInformationIds).isEmpty();
     }
 
-    private Object[] getVenueName() {
-        return Stream.of(Benefit.values())
-                .map(airLookupService::lookupAirVenueNamesByBenefitCode)
-                .flatMap(List::stream)
-                .distinct()
-                .toArray(Object[]::new);
+    private static void checkForMissingRegionId(List<String> missingInformationIds, String epimsId,
+                                  CourtVenue sscsCourtVenue) {
+        if (StringUtils.isEmpty(sscsCourtVenue.getRegionId())) {
+            missingInformationIds.add(epimsId);
+        }
     }
+
+
 }
