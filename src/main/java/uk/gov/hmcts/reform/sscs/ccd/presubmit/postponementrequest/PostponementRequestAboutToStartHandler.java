02a4992da0842b8c00261f984b3f6a3a229852ec
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-09-09 11:02:33 +0100

    SSCS-10790 Adding hearing date to excluded date list
    SSCS-10790:Adding hearing date to excluded date list
    @EliandroGV1
    EliandroGV1 committed 8 days ago
    
    Merge branch 'master' into SSCS-10790
    @EliandroGV1
    EliandroGV1 committed 8 days ago
    
    Commits on Sep 2, 2022
    
        Snakeyaml bump to 1.31
        @dattipoeHMCTS
        dattipoeHMCTS committed 7 days ago
    
    Makingchanges as per review minus the Optional hearing change(once cl…
    @EliandroGV1
    EliandroGV1 committed 7 days ago
    
    SSCS-10790:Adding hearing date to excluded date list
    @EliandroGV1
    EliandroGV1 committed 7 days ago
    
    Makingchanges as per review minus the Optional hearing change(once cl…
    @EliandroGV1
    EliandroGV1 committed 7 days ago
    
    Makingchanges as per review to filter based on date on the hearing list.
    @EliandroGV1
    EliandroGV1 committed 7 days ago
    
    Merge remote-tracking branch 'origin/SSCS-10790' into SSCS-10790
    @EliandroGV1
    EliandroGV1 committed 7 days ago
    
    Makingchanges as per review to filter based on date on the hearing list.
    @EliandroGV1
    EliandroGV1 committed 7 days ago
    
    Merge remote-tracking branch 'origin/update-snake-yaml' into SSCS-10790
    @EliandroGV1
    EliandroGV1 committed 7 days ago
    
    Checkstyle change
    @EliandroGV1
    EliandroGV1 committed 7 days ago
    
    Checkstyle change
    @EliandroGV1
    EliandroGV1 committed 7 days ago
    
    Fixing unit tests
    @filippo-possenti-version1
    filippo-possenti-version1 committed 7 days ago
    
    Commits on Sep 7, 2022
    
        SSCS-10790 - code clean up
        @hajam09
        hajam09 committed 2 days ago
    
    SSCS-10790 Reworking
    
    Update ActionPostponementRequestAboutToSubmitHandlerTest.java
    
    Co-Authored-By: EliandroGV1 <110095414+EliandroGV1@users.noreply.github.com>
    Co-Authored-By: Mohamed Ashraf Hajamohideen <34398691+hajam09@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postponementrequest/PostponementRequestAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postponementrequest/PostponementRequestAboutToStartHandler.java
@@ -1,30 +1,38 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.postponementrequest;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
-import static org.apache.commons.collections4.CollectionUtils.emptyIfNull;
-import static uk.gov.hmcts.reform.sscs.model.AppConstants.POSTPONEMENTS_NOT_POSSIBLE_FOR_GAPS;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
-import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Hearing;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.sscs.util.SscsUtil;
 
 
 @Service
 public class PostponementRequestAboutToStartHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    public static final String NOT_LIST_ASSIST_CASE_ERROR = "Postponement requests can only be made for list"
+        + " assist cases";
+
+    @Value("${feature.snl.enabled}")
+    private boolean isScheduleListingEnabled;
 
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
         requireNonNull(callback, "callback must not be null");
         requireNonNull(callbackType, "callbacktype must not be null");
 
-        return callbackType.equals(CallbackType.ABOUT_TO_START)
-                && callback.getEvent() == EventType.POSTPONEMENT_REQUEST
-                && callback.getCaseDetails() != null;
+        return callbackType == CallbackType.ABOUT_TO_START
+            && callback.getEvent() == EventType.POSTPONEMENT_REQUEST
+            && isScheduleListingEnabled;
     }
 
     @Override
@@ -32,25 +40,26 @@ public class PostponementRequestAboutToStartHandler implements PreSubmitCallback
         final CaseDetails<SscsCaseData> caseDetails = callback.getCaseDetails();
         final SscsCaseData sscsCaseData = caseDetails.getCaseData();
 
-        final Optional<Hearing> optionalHearing = emptyIfNull(sscsCaseData.getHearings()).stream()
-                .filter(h -> h.getValue().getHearingDateTime().isAfter(LocalDateTime.now()))
-                .distinct()
-                .findFirst();
-
         final PreSubmitCallbackResponse<SscsCaseData> response = new PreSubmitCallbackResponse<>(sscsCaseData);
 
-        if (HearingRoute.GAPS.equals(sscsCaseData.getRegionalProcessingCenter().getHearingRoute())) {
-            response.addError(POSTPONEMENTS_NOT_POSSIBLE_FOR_GAPS);
+        if (!SscsUtil.isSAndLCase(sscsCaseData)) {
+            response.addError(NOT_LIST_ASSIST_CASE_ERROR);
+            return response;
         }
 
-        optionalHearing.ifPresentOrElse(hearing -> setPostponementRequest(hearing, sscsCaseData),
-                () -> response.addError("There are no hearing to postpone"));
+        Hearing hearing = sscsCaseData.getLatestHearing();
+        if (isNull(hearing)) {
+            response.addError("There are no hearing to postpone");
+            return response;
+        }
+
+        setPostponementRequest(hearing, sscsCaseData);
 
         return response;
     }
 
     private void setPostponementRequest(Hearing hearing, SscsCaseData sscsCaseData) {
-        sscsCaseData.getPostponementRequest().setPostponementRequestHearingDateAndTime(hearing.getValue().getHearingDateTime().toString());
+        sscsCaseData.getPostponementRequest().setPostponementRequestHearingDateAndTime(hearing.getValue().getStart().toString());
         sscsCaseData.getPostponementRequest().setPostponementRequestHearingVenue(hearing.getValue().getVenue().getName());
     }
 }
