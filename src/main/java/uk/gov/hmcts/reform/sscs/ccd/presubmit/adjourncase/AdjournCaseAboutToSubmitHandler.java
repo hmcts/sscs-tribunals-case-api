59879342a454651cdb51dc9dedcd59fec64acb3e
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2022-10-03 11:49:11 +0100

    SSCS-10950 adds tests
    calls sendListAssistCreateHearingMessage
    
    adds feature flag

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adjourncase/AdjournCaseAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adjourncase/AdjournCaseAboutToSubmitHandler.java
@@ -1,15 +1,11 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.adjourncase;
 
 import static uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType.DRAFT_ADJOURNMENT_NOTICE;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.HearingRoute.LIST_ASSIST;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.State.READY_TO_LIST;
 
-import java.time.LocalDate;
 import java.util.Objects;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
@@ -20,20 +16,19 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.resendtogaps.ListAssistHearingMessageHelper;
 import uk.gov.hmcts.reform.sscs.service.PreviewDocumentService;
+import uk.gov.hmcts.reform.sscs.util.SscsUtil;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class AdjournCaseAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
 
-    private PreviewDocumentService previewDocumentService;
+    private final PreviewDocumentService previewDocumentService;
 
     private final ListAssistHearingMessageHelper hearingMessageHelper;
 
-    @Autowired
-    public AdjournCaseAboutToSubmitHandler(PreviewDocumentService previewDocumentService) {
-        this.previewDocumentService = previewDocumentService;
-    }
+    @Value("${feature.snl.adjournment.enabled}")
+    private boolean isAdjournmentEnabled; // TODO SSCS-10951
 
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
@@ -51,23 +46,20 @@ public class AdjournCaseAboutToSubmitHandler implements PreSubmitCallbackHandler
 
         SscsCaseData sscsCaseData = callback.getCaseDetails().getCaseData();
 
-        if (LIST_ASSIST == sscsCaseData.getSchedulingAndListingFields().getHearingRoute()
-            && READY_TO_LIST == sscsCaseData.getState()) {
-            hearingMessageHelper.sendListAssistCreateHearingMessage(sscsCaseData.getCcdCaseId());
-        }
-
-        // Due to a bug with CCD related to hidden fields, this field not being set
-        // on the final submission from CCD, so we need to reset it here
-        // See https://tools.hmcts.net/jira/browse/RDM-8200
-        // This is a temporary workaround for this issue.
-        sscsCaseData.setAdjournCaseGeneratedDate(LocalDate.now().toString());
-
         PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
 
         previewDocumentService.writePreviewDocumentToSscsDocument(sscsCaseData, DRAFT_ADJOURNMENT_NOTICE, sscsCaseData.getAdjournCasePreviewDocument());
 
-        if (sscsCaseData.getAdjournCaseInterpreterRequired() != null) {
-            HearingOptions hearingOptions = sscsCaseData.getAppeal().getHearingOptions() != null ? sscsCaseData.getAppeal().getHearingOptions() : HearingOptions.builder().build();
+        if (SscsUtil.isSAndLCase(sscsCaseData)
+            && sscsCaseData.isAdjournCaseAbleToBeListedRightAway()
+            && isAdjournmentEnabled // TODO SSCS-10951
+        ) {
+            hearingMessageHelper.sendListAssistCreateHearingMessage(sscsCaseData.getCcdCaseId());
+        } else if (sscsCaseData.getAdjournCaseInterpreterRequired() != null) {
+            HearingOptions hearingOptions = HearingOptions.builder().build();
+            if (sscsCaseData.getAppeal().getHearingOptions() != null) {
+                hearingOptions = sscsCaseData.getAppeal().getHearingOptions();
+            }
             hearingOptions.setLanguages(sscsCaseData.getAdjournCaseInterpreterLanguage());
             hearingOptions.setLanguageInterpreter(sscsCaseData.getAdjournCaseInterpreterRequired());
 
