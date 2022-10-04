dd8eefc71487b9856ecfbc0f52d5ebc40ba6cecb
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2022-10-04 14:17:21 +0100

    SSCS-10950 modernizes CcdMidEventControllerFunctionalTest
    adds tests for LA json
    
    3 failing tests regarding preview response code

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/ccd/CcdMidEventControllerFunctionalTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/ccd/CcdMidEventControllerFunctionalTest.java
@@ -2,8 +2,7 @@ package uk.gov.hmcts.reform.sscs.functional.ccd;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.useRelaxedHTTPSValidation;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.sscs.functional.handlers.BaseHandler.getJsonCallbackForTest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,6 +16,7 @@ import org.apache.http.HttpStatus;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicList;
 import uk.gov.hmcts.reform.sscs.ccd.domain.DynamicListItem;
@@ -33,14 +33,16 @@ public class CcdMidEventControllerFunctionalTest extends BaseFunctionTest {
         useRelaxedHTTPSValidation();
     }
 
+    @DisplayName("Should return 400 for no body")
     @Test
-    public void shouldReturn400ForNoBody() throws IOException {
+    public void testNoBody() throws IOException {
         HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(""), "");
-        assertThat(httpResponse.getStatusLine().getStatusCode(), is(400));
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(400);
     }
 
+    @DisplayName("Should return 500 for unauthorised")
     @Test
-    public void shouldReturn500ForUnauthorised() throws IOException {
+    public void testUnauthorised() throws IOException {
         RestAssured.given()
                 .log().method().log().headers().log().uri().log().body(false)
                 .contentType(ContentType.JSON)
@@ -53,46 +55,75 @@ public class CcdMidEventControllerFunctionalTest extends BaseFunctionTest {
                 .statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
     }
 
+    @DisplayName("Should return 200 with valid request")
     @Test
-    public void shouldReturn200WithValidRequest() throws IOException {
+    public void testValidRequest() throws IOException {
         HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(getJsonCallbackForTest("handlers/writefinaldecision/writeFinalDecisionCallback.json")), "");
-        assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     }
 
+    @DisplayName("Adjourn case populate venue dropdown should populate next hearing venue dropdown")
     @Test
-    public void adjournCasePopulateVenueDropdown_shouldPopulateNextHearingVenueDropdown() throws IOException {
+    public void testAdjournCasePopulateVenueDropdown() throws IOException {
         HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(getJsonCallbackForTest("handlers/writefinaldecision/writeFinalDecisionCallback.json")), "AdjournCasePopulateVenueDropdown");
         CcdEventResponse ccdEventResponse = getCcdEventResponse(httpResponse);
-        assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
         DynamicList adjournCaseNextHearingVenueSelected = ccdEventResponse.getData().getAdjournCaseNextHearingVenueSelected();
-        assertThat(adjournCaseNextHearingVenueSelected.getValue(), is(new DynamicListItem("", "")));
-        assertThat(adjournCaseNextHearingVenueSelected.getListItems().size(), is(greaterThan(2)));
+        assertThat(adjournCaseNextHearingVenueSelected.getValue()).isEqualTo(new DynamicListItem("", ""));
+        assertThat(adjournCaseNextHearingVenueSelected.getListItems()).hasSizeGreaterThan(2);
     }
 
+    @DisplayName("Preview final decision should populate final decision preview document")
     @Test
-    public void previewFinalDecision_shouldPopulateFinalDecisionPreviewDocument() throws IOException {
+    public void testPreviewFinalDecision() throws IOException {
         HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(getJsonCallbackForTest("handlers/writefinaldecision/writeFinalDecisionCallback.json")), "PreviewFinalDecision");
         CcdEventResponse ccdEventResponse = getCcdEventResponse(httpResponse);
-        assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
-        assertThat(ccdEventResponse.getData().getSscsFinalDecisionCaseData().getWriteFinalDecisionPreviewDocument(), is(not(nullValue())));
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
+        assertThat(ccdEventResponse.getData().getSscsFinalDecisionCaseData().getWriteFinalDecisionPreviewDocument()).isNotNull();
     }
 
+    @DisplayName("Preview adjourn case should populate adjourn case preview document")
     @Test
-    public void previewAdjournCase_shouldPopulateAdjournCasePreviewDocument() throws IOException {
-        HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(getJsonCallbackForTest("handlers/adjourncase/adjournCaseCallback.json")), "PreviewAdjournCase");
+    public void testPreviewAdjournCaseGaps() throws IOException {
+        HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(getJsonCallbackForTest(
+            "handlers/adjourncase/adjournCaseGapsCallback.json")), "PreviewAdjournCase");
         CcdEventResponse ccdEventResponse = getCcdEventResponse(httpResponse);
-        assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
-        assertThat(ccdEventResponse.getData().getAdjournCasePreviewDocument(), is(not(nullValue())));
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
+        assertThat(ccdEventResponse.getData().getAdjournCasePreviewDocument()).isNotNull();
     }
 
+    @DisplayName("Admin restore cases should return an error for data without restore cases date")
     @Test
-    public void adminRestoreCases_shouldReturnAnErrorForDataWithoutRestoreCasesDate() throws IOException {
-        HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(getJsonCallbackForTest("handlers/adjourncase/adjournCaseCallback.json")), "AdminRestoreCases");
+    public void testAdminRestoreCasesGaps() throws IOException {
+        HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(getJsonCallbackForTest(
+            "handlers/adjourncase/adjournCaseGapsCallback.json")), "AdminRestoreCases");
         CcdEventResponse ccdEventResponse = getCcdEventResponse(httpResponse);
-        assertThat(httpResponse.getStatusLine().getStatusCode(), is(200));
-        assertThat(ccdEventResponse.getWarnings().size(), is(0));
-        assertThat(ccdEventResponse.getErrors().size(), is(1));
-        assertThat(ccdEventResponse.getErrors().iterator().next(), is("Unable to extract restoreCaseFileName"));
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
+        assertThat(ccdEventResponse.getWarnings().size()).isEqualTo(0);
+        assertThat(ccdEventResponse.getErrors().size()).isEqualTo(1);
+        assertThat(ccdEventResponse.getErrors().iterator().next()).isEqualTo("Unable to extract restoreCaseFileName");
+    }
+
+    @DisplayName("Preview adjourn case should populate adjourn case preview document")
+    @Test
+    public void testPreviewAdjournCaseLA() throws IOException {
+        HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(getJsonCallbackForTest(
+            "handlers/adjourncase/adjournCaseLACallback.json")), "PreviewAdjournCase");
+        CcdEventResponse ccdEventResponse = getCcdEventResponse(httpResponse);
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
+        assertThat(ccdEventResponse.getData().getAdjournCasePreviewDocument()).isNotNull();
+    }
+
+    @DisplayName("Admin restore cases should return an error for data without restore cases date")
+    @Test
+    public void testAdminRestoreCasesLA() throws IOException {
+        HttpResponse httpResponse = sscsMyaBackendRequests.midEvent(new StringEntity(getJsonCallbackForTest(
+            "handlers/adjourncase/adjournCaseLACallback.json")), "AdminRestoreCases");
+        CcdEventResponse ccdEventResponse = getCcdEventResponse(httpResponse);
+        assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
+        assertThat(ccdEventResponse.getWarnings().size()).isEqualTo(0);
+        assertThat(ccdEventResponse.getErrors().size()).isEqualTo(1);
+        assertThat(ccdEventResponse.getErrors().iterator().next()).isEqualTo("Unable to extract restoreCaseFileName");
     }
 
     private CcdEventResponse getCcdEventResponse(HttpResponse httpResponse) throws IOException {
