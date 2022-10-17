0bb911c9f25b0279730204413e66ec13519b8519
Lucas <18538046+TeaWithLucas@users.noreply.github.com>
2022-10-17 13:43:08 +0100

    SSCS-10985 CVE Fix Commons Bump (#2980)
    * CVE fix
    
    * Upgrade to gradle v7 configuration
    
    * Bump versions
    
    * Fixing Dependences

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adjourncase/AdjournCasePreviewService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adjourncase/AdjournCasePreviewService.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
