2c6fb213576cceaa20f0186b6c4658b8bfbd7700
Pankaj Khare <pankaj.khare@version1.com>
2022-10-19 12:55:58 +0100

    apply review comments

--- src/e2e/java/uk/gov/hmcts/reform/sscs/functional/handlers/writefinaldecision/WriteFinalDecisionAboutToSubmitHandlerTest.java
+++ src/e2e/java/uk/gov/hmcts/reform/sscs/functional/handlers/writefinaldecision/WriteFinalDecisionAboutToSubmitHandlerTest.java
@@ -1,94 +1,23 @@
 package uk.gov.hmcts.reform.sscs.functional.handlers.writefinaldecision;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
-import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import org.apache.http.HttpStatus;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
-import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
-import uk.gov.hmcts.reform.sscs.ccd.domain.*;
-import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.WriteFinalDecisionAboutToSubmitHandler;
 import uk.gov.hmcts.reform.sscs.functional.handlers.BaseHandler;
-import uk.gov.hmcts.reform.sscs.service.DecisionNoticeOutcomeService;
-import uk.gov.hmcts.reform.sscs.service.DecisionNoticeService;
-import uk.gov.hmcts.reform.sscs.service.PreviewDocumentService;
 
 @RunWith(SpringRunner.class)
 @TestPropertySource(locations = "classpath:config/application_e2e.properties")
 @SpringBootTest
 public class WriteFinalDecisionAboutToSubmitHandlerTest extends BaseHandler {
-
-    private static final String USER_AUTHORISATION = "Bearer token";
-    @Mock
-    private CaseDetails<SscsCaseData> caseDetails;
-    private WriteFinalDecisionAboutToSubmitHandler handler;
-    private SscsCaseData sscsCaseData;
-    @Mock
-    private DecisionNoticeService decisionNoticeService;
-    @Mock
-    private Callback<SscsCaseData> callback;
-    @Mock
-    private PreviewDocumentService previewDocumentService;
-    @Mock
-    private DecisionNoticeOutcomeService decisionNoticeOutcomeService;
-
-    @Before
-    public void setUp() {
-        super.setUp();
-        openMocks(this);
-
-        handler = new WriteFinalDecisionAboutToSubmitHandler(decisionNoticeService, previewDocumentService);
-        sscsCaseData = SscsCaseData.builder().ccdCaseId("ccdId").build();
-        sscsCaseData.setAppeal(Appeal.builder().hearingOptions(HearingOptions.builder().build()).build());
-        sscsCaseData.getAppeal().setBenefitType(BenefitType.builder().code("childSupport").build());
-
-        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
-        when(decisionNoticeService.getOutcomeService(any(String.class))).thenReturn(decisionNoticeOutcomeService);
-        when(callback.getEvent()).thenReturn(EventType.WRITE_FINAL_DECISION);
-        when(callback.getCaseDetails()).thenReturn(caseDetails);
-
-    }
-
-    @Test
-    public void givenAboutToSubmitCallbackForEvent_shouldUpdatePreviousStateWhenStateIsNotReadyToListOrWithFta() {
-        sscsCaseData.setPreviousState(State.VOID_STATE);
-        sscsCaseData.setState(State.APPEAL_CREATED);
-        PreSubmitCallbackResponse<SscsCaseData> response =
-                handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
-        assertThat(response.getData().getPreviousState(), is(State.APPEAL_CREATED));
-    }
-
-    @Test
-    public void givenAboutToSubmitCallbackForEvent_shouldNotUpdatePreviousStateWhenStateIsReadyToList() {
-        sscsCaseData.setPreviousState(State.VOID_STATE);
-        sscsCaseData.setState(State.READY_TO_LIST);
-        PreSubmitCallbackResponse<SscsCaseData> response =
-                handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
-        assertThat(response.getData().getPreviousState(), is(State.VOID_STATE));
-    }
-
-    @Test
-    public void givenAboutToSubmitCallbackForEvent_shouldNotUpdatePreviousStateWhenStateIsWithFta() {
-        sscsCaseData.setPreviousState(State.VOID_STATE);
-        sscsCaseData.setState(State.WITH_DWP);
-        PreSubmitCallbackResponse<SscsCaseData> response =
-                handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
-        assertThat(response.getData().getPreviousState(), is(State.VOID_STATE));
-    }
 
     @Test
     public void givenAboutToSubmitCallbackForEvent_shouldSetFields() throws Exception {
