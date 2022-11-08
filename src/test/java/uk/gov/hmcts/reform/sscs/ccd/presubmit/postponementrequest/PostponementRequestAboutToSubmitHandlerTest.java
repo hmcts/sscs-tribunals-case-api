5aa52c2ae73390b3e21398450f8d7e7e9fc4b4b1
hajam09 <hajam09@yahoo.com>
2022-11-08 15:58:12 +0000

    SSCS-10935 - Postponement request must be added to doc tab and as an addition in the bundle

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postponementrequest/PostponementRequestAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postponementrequest/PostponementRequestAboutToSubmitHandlerTest.java
@@ -6,24 +6,30 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType.POSTPONEMENT_REQUEST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.YES;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
-import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
@@ -43,6 +49,7 @@ import uk.gov.hmcts.reform.sscs.ccd.presubmit.InterlocReviewState;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 import uk.gov.hmcts.reform.sscs.idam.UserDetails;
 import uk.gov.hmcts.reform.sscs.idam.UserRole;
+import uk.gov.hmcts.reform.sscs.service.FooterService;
 import uk.gov.hmcts.reform.sscs.service.PostponementRequestService;
 
 @RunWith(JUnitParamsRunner.class)
@@ -61,14 +68,19 @@ public class PostponementRequestAboutToSubmitHandlerTest {
     @Mock
     private IdamService idamService;
 
+    @Mock
+    private FooterService footerService;
+
     private SscsCaseData sscsCaseData;
+
+    private SscsDocument expectedDocument;
 
     private final UserDetails userDetails = UserDetails.builder().roles(new ArrayList<>(asList("caseworker-sscs", UserRole.CTSC_CLERK.getValue()))).build();
 
     @Before
     public void setUp() {
         openMocks(this);
-        handler = new PostponementRequestAboutToSubmitHandler(new PostponementRequestService());
+        handler = new PostponementRequestAboutToSubmitHandler(new PostponementRequestService(), footerService);
 
         Hearing hearing = Hearing.builder().value(HearingDetails.builder()
             .hearingDate(LocalDate.now().plusDays(1).toString())
@@ -98,6 +110,13 @@ public class PostponementRequestAboutToSubmitHandlerTest {
         when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
         when(caseDetails.getState()).thenReturn(sscsCaseData.getState());
         when(idamService.getUserDetails(USER_AUTHORISATION)).thenReturn(userDetails);
+
+        expectedDocument = SscsDocument.builder().value(SscsDocumentDetails.builder()
+                .documentLink(sscsCaseData.getPostponementRequest().getPostponementPreviewDocument())
+                .documentFileName(sscsCaseData.getPostponementRequest().getPostponementPreviewDocument().getDocumentFilename())
+                .documentType(POSTPONEMENT_REQUEST.getValue())
+                .documentDateAdded(LocalDate.now().format(DateTimeFormatter.ISO_DATE))
+                .build()).build();
     }
 
     @Test
@@ -139,7 +158,7 @@ public class PostponementRequestAboutToSubmitHandlerTest {
 
         assertThat(sscsCaseData.getSscsDocument().size(), is(1));
         final SscsDocument document = sscsCaseData.getSscsDocument().get(0);
-        assertThat(document.getValue().getDocumentType(), is(DocumentType.POSTPONEMENT_REQUEST.getValue()));
+        assertThat(document.getValue().getDocumentType(), is(POSTPONEMENT_REQUEST.getValue()));
         assertThat(document.getValue().getDocumentLink().getDocumentFilename(), is("example.pdf"));
         assertThat(document.getValue().getOriginalPartySender(), is(UploadParty.DWP.getValue()));
         assertThat(sscsCaseData.getInterlocReviewState(), is(InterlocReviewState.REVIEW_BY_TCW.getId()));
@@ -147,4 +166,12 @@ public class PostponementRequestAboutToSubmitHandlerTest {
         assertThat(sscsCaseData.getPostponementRequest().getUnprocessedPostponementRequest(), is(YES));
     }
 
+    @Test
+    public void givenAPostponementRequest_AddGeneratedDocumentToBundle() {
+        final PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+        List<SscsDocument> sscsDocuments = response.getData().getSscsDocument();
+        Assert.assertEquals(sscsDocuments, Collections.singletonList(expectedDocument));
+        verify(footerService).createFooterAndAddDocToCase(eq(expectedDocument.getValue().getDocumentLink()), any(),
+                eq(POSTPONEMENT_REQUEST), any(), any(), eq(null), eq(null));
+    }
 }
