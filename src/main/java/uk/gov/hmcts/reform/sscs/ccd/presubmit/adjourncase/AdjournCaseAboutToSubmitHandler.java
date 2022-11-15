f302d012fb64e46787bab3dd72e8d43aad5d73fc
Nicholas Hill <nicholas.hill@HMCTS.net>
2022-11-15 16:32:47 +0000

    updates the region when adjourning SSCS-10293

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adjourncase/AdjournCaseAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adjourncase/AdjournCaseAboutToSubmitHandler.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.adjourncase;
 
+import static java.util.Objects.nonNull;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType.DRAFT_ADJOURNMENT_NOTICE;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.isNoOrNull;
 
 import java.time.LocalDate;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,7 +17,9 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.HearingOptions;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.resendtogaps.ListAssistHearingMessageHelper;
+import uk.gov.hmcts.reform.sscs.model.VenueDetails;
 import uk.gov.hmcts.reform.sscs.service.PreviewDocumentService;
+import uk.gov.hmcts.reform.sscs.service.VenueService;
 import uk.gov.hmcts.reform.sscs.util.SscsUtil;
 
 @Component
@@ -26,6 +28,8 @@ import uk.gov.hmcts.reform.sscs.util.SscsUtil;
 public class AdjournCaseAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
 
     private final PreviewDocumentService previewDocumentService;
+
+    private final VenueService venueService;
 
     private final ListAssistHearingMessageHelper hearingMessageHelper;
 
@@ -36,8 +40,8 @@ public class AdjournCaseAboutToSubmitHandler implements PreSubmitCallbackHandler
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
         return callbackType == CallbackType.ABOUT_TO_SUBMIT
             && callback.getEvent() == EventType.ADJOURN_CASE
-            && Objects.nonNull(callback.getCaseDetails())
-            && Objects.nonNull(callback.getCaseDetails().getCaseData());
+            && nonNull(callback.getCaseDetails())
+            && nonNull(callback.getCaseDetails().getCaseData());
     }
 
     @Override
@@ -50,8 +54,7 @@ public class AdjournCaseAboutToSubmitHandler implements PreSubmitCallbackHandler
 
         previewDocumentService.writePreviewDocumentToSscsDocument(sscsCaseData, DRAFT_ADJOURNMENT_NOTICE, sscsCaseData.getAdjournCasePreviewDocument());
 
-        if (SscsUtil.isSAndLCase(sscsCaseData)
-            && isAdjournmentEnabled // TODO SSCS-10951
+        if (isAdjournmentEnabled // TODO SSCS-10951
             && (sscsCaseData.isAdjournCaseAbleToBeListedRightAway()
             || isNoOrNull(sscsCaseData.getAdjournCaseAreDirectionsBeingMadeToParties()))
         ) {
@@ -61,10 +64,21 @@ public class AdjournCaseAboutToSubmitHandler implements PreSubmitCallbackHandler
             if (sscsCaseData.getAppeal().getHearingOptions() != null) {
                 hearingOptions = sscsCaseData.getAppeal().getHearingOptions();
             }
+
             hearingOptions.setLanguages(sscsCaseData.getAdjournCaseInterpreterLanguage());
             hearingOptions.setLanguageInterpreter(sscsCaseData.getAdjournCaseInterpreterRequired());
 
             sscsCaseData.getAppeal().setHearingOptions(hearingOptions);
+        }
+
+
+        if (nonNull(sscsCaseData.getAdjournCaseNextHearingVenueSelected())) {
+            String venueId = sscsCaseData.getAdjournCaseNextHearingVenueSelected().getValue().getCode();
+            String epimsId = venueService.getEpimsIdForVenueId(venueId);
+
+            VenueDetails venueDetails = venueService.getVenueDetailsForActiveVenueByEpimsId(epimsId);
+
+            sscsCaseData.setRegion(venueDetails.getRegionalProcessingCentre());
         }
 
         if (sscsCaseData.getAdjournCaseGeneratedDate() == null) {
