da4a53ad1c89c41d10cb6e30150ac38520e99638
Nicholas Hill <nicholas.hill@HMCTS.net>
2022-11-17 17:03:17 +0000

    remove unused import SSCS-11090

--- src/main/java/uk/gov/hmcts/reform/sscs/util/SscsUtil.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/util/SscsUtil.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentLink;
 import uk.gov.hmcts.reform.sscs.ccd.domain.OverrideFields;
