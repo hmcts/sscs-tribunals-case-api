a14077b336615cd85177a305fff6d041ee6552ce
Mohamed Ashraf Hajamohideen <34398691+hajam09@users.noreply.github.com>
2022-12-06 13:36:51 +0000

    SSCS-10935 - Postponement request must be added to doc tab and as an … (#3049)
    * SSCS-10935 - Postponement request must be added to doc tab and as an addition in the bundle
    
    * SSCS-10935 - minor fix

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postponementrequest/PostponementRequestAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postponementrequest/PostponementRequestAboutToSubmitHandler.java
@@ -2,24 +2,30 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.postponementrequest;
 
 import static java.util.Objects.requireNonNull;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.sscs.service.FooterService;
 import uk.gov.hmcts.reform.sscs.service.PostponementRequestService;
 
 @Service
 public class PostponementRequestAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
 
     private final PostponementRequestService postponementRequestService;
+    private final FooterService footerService;
 
     @Autowired
-    public PostponementRequestAboutToSubmitHandler(PostponementRequestService postponementRequestService) {
+    public PostponementRequestAboutToSubmitHandler(PostponementRequestService postponementRequestService, FooterService footerService) {
         this.postponementRequestService = postponementRequestService;
+        this.footerService = footerService;
     }
 
     @Override
@@ -40,9 +46,17 @@ public class PostponementRequestAboutToSubmitHandler implements PreSubmitCallbac
 
         if (response.getErrors().isEmpty()) {
             postponementRequestService.processPostponementRequest(sscsCaseData, UploadParty.DWP);
+            addDocumentToBundle(sscsCaseData, sscsCaseData.getSscsDocument().get(sscsCaseData.getSscsDocument().size() - 1));
         }
 
         return response;
+    }
+
+    private void addDocumentToBundle(SscsCaseData sscsCaseData, SscsDocument sscsDocument) {
+        DocumentLink url = sscsDocument.getValue().getDocumentLink();
+        DocumentType documentType = DocumentType.fromValue(sscsDocument.getValue().getDocumentType());
+        String dateIssued = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        footerService.createFooterAndAddDocToCase(url, sscsCaseData, documentType, dateIssued, null, null, null);
     }
 
     @NotNull
