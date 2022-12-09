8862c6ba2da19098bb00395d7c01f20ae58ac2a7
amoslv1 <lucian.amos@version1.com>
2022-12-09 15:29:55 +0000

    SSCS-11254 adds feature flag check to AdjournCaseService.clearTransientFields

--- src/main/java/uk/gov/hmcts/reform/sscs/service/adjourncase/AdjournCaseService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/adjourncase/AdjournCaseService.java
@@ -12,10 +12,13 @@ public class AdjournCaseService {
         // remove default constructor
     }
 
-    public static void clearTransientFields(SscsCaseData sscsCaseData) {
+    public static void clearTransientFields(SscsCaseData sscsCaseData, boolean isAdjournmentEnabled) {
         log.info("Clearing transient adjournment case fields for caseId {}", sscsCaseData.getCcdCaseId());
-        sscsCaseData.setAdjournment(Adjournment.builder()
-            .adjournmentInProgress(YesNo.NO)
-            .build());
+
+        sscsCaseData.setAdjournment(Adjournment.builder().build());
+
+        if (isAdjournmentEnabled) {
+            sscsCaseData.getAdjournment().setAdjournmentInProgress(YesNo.NO);
+        }
     }
 }
