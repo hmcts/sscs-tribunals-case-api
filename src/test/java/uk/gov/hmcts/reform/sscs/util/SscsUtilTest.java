5b73bfa5047e95fa9d2a9e0d6d1164f39c04c445
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2023-01-16 10:53:19 +0000

    SSCS-10983 alters helper method getLatestEventOfSpecifiedTypes to be more generic with varargs
    adjusts tests accordingly

--- src/test/java/uk/gov/hmcts/reform/sscs/util/SscsUtilTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/util/SscsUtilTest.java
@@ -1,8 +1,11 @@
 package uk.gov.hmcts.reform.sscs.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.ISSUE_FINAL_DECISION;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.ISSUE_FINAL_DECISION_WELSH;
 
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,8 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.EventDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 
 class SscsUtilTest {
+
+    public static final String DATE_TEMPLATE = "2017-06-2%dT12:00:00";
 
     private SscsCaseData caseData;
     private Event issueFinalDecisionEarliest;
@@ -22,60 +27,76 @@ class SscsUtilTest {
 
     @BeforeEach
     void setUp() {
-        final String date1 = String.format("2017-06-2%dT12:00:00", 1);
-        issueFinalDecisionEarliest = new Event(new EventDetails(date1, "issueFinalDecision", "issueFinalDecision earliest"));
-        final String date2 = String.format("2017-06-2%dT12:00:00", 2);
-        issueFinalDecisionLatest = new Event(new EventDetails(date2, "issueFinalDecision", "issueFinalDecision latest"));
-        final String date3 = String.format("2017-06-2%dT12:00:00", 3);
-        issueFinalDecisionWelshEarliest = new Event(new EventDetails(date3, "issueFinalDecisionWelsh", "issueFinalDecisionWelsh earliest"));
-        final String date4 = String.format("2017-06-2%dT12:00:00", 4);
-        issueFinalDecisionWelshLatest = new Event(new EventDetails(date4, "issueFinalDecisionWelsh", "issueFinalDecisionWelsh latest"));
-        evidenceReceived = new Event(new EventDetails(date4, "evidenceReceived", "evidence received"));
+        issueFinalDecisionEarliest = new Event(new EventDetails(String.format(DATE_TEMPLATE, 1), "issueFinalDecision", "issueFinalDecision earliest"));
+        issueFinalDecisionLatest = new Event(new EventDetails(String.format(DATE_TEMPLATE, 2), "issueFinalDecision", "issueFinalDecision latest"));
+        issueFinalDecisionWelshEarliest = new Event(new EventDetails(String.format(DATE_TEMPLATE, 3), "issueFinalDecisionWelsh", "issueFinalDecisionWelsh earliest"));
+        issueFinalDecisionWelshLatest = new Event(new EventDetails(String.format(DATE_TEMPLATE, 4), "issueFinalDecisionWelsh", "issueFinalDecisionWelsh latest"));
+        evidenceReceived = new Event(new EventDetails(String.format(DATE_TEMPLATE, 4), "evidenceReceived", "evidence received"));
 
         caseData = SscsCaseData.builder().build();
     }
 
     @Test
-    @DisplayName("Given multiple issue final decision events, getLatestIssueFinalDecision returns latest")
-    void getLatestEventOfSpecifiedType_returnsLatestEventEnglish() {
-        eventsList = List.of(issueFinalDecisionEarliest, evidenceReceived, issueFinalDecisionLatest);
+    @DisplayName("Given event type exists on case data, getLatestEventOfSpecifiedTypes returns it")
+    void getLatestEventOfSpecifiedTypes_returnsEvent() {
+        eventsList = List.of(issueFinalDecisionEarliest);
         caseData.setEvents(eventsList);
 
-        Event result = SscsUtil.getLatestIssueFinalDecision(caseData);
+        Optional<Event> result = SscsUtil.getLatestEventOfSpecifiedTypes(caseData, ISSUE_FINAL_DECISION);
 
-        assertThat(result).isEqualTo(issueFinalDecisionLatest);
+        assertThat(result)
+            .isPresent()
+            .contains(issueFinalDecisionEarliest);
     }
 
     @Test
-    @DisplayName("Given multiple issue final decision Welsh events, getLatestIssueFinalDecision returns latest")
-    void getLatestEventOfSpecifiedType_returnsLatestEventWelsh() {
-        eventsList = List.of(issueFinalDecisionWelshEarliest, evidenceReceived, issueFinalDecisionWelshLatest);
+    @DisplayName("Given multiple issue final decision events, getLatestEventOfSpecifiedTypes returns latest")
+    void getLatestEventOfSpecifiedTypes_returnsLatestEventEnglish() {
+        eventsList = List.of(evidenceReceived, issueFinalDecisionEarliest, issueFinalDecisionLatest);
         caseData.setEvents(eventsList);
 
-        Event result = SscsUtil.getLatestIssueFinalDecision(caseData);
+        Optional<Event> result = SscsUtil.getLatestEventOfSpecifiedTypes(caseData, ISSUE_FINAL_DECISION, ISSUE_FINAL_DECISION_WELSH);
 
-        assertThat(result).isEqualTo(issueFinalDecisionWelshLatest);
+        assertThat(result)
+            .isPresent()
+            .contains(issueFinalDecisionLatest);
     }
 
     @Test
-    @DisplayName("Given multiple issue final decision English and Welsh events, getLatestIssueFinalDecision returns latest")
-    void getLatestIssueFinalDecision_returnsLatestEnglishOrWelsh() {
-        eventsList = List.of(issueFinalDecisionWelshEarliest, issueFinalDecisionEarliest, evidenceReceived, issueFinalDecisionWelshLatest, issueFinalDecisionLatest);
+    @DisplayName("Given multiple issue final decision Welsh events, getLatestEventOfSpecifiedTypes returns latest")
+    void getLatestEventOfSpecifiedTypes_returnsLatestEventWelsh() {
+        eventsList = List.of(evidenceReceived, issueFinalDecisionWelshEarliest, issueFinalDecisionWelshLatest);
         caseData.setEvents(eventsList);
 
-        Event result = SscsUtil.getLatestIssueFinalDecision(caseData);
+        Optional<Event> result = SscsUtil.getLatestEventOfSpecifiedTypes(caseData, ISSUE_FINAL_DECISION, ISSUE_FINAL_DECISION_WELSH);
 
-        assertThat(result).isEqualTo(issueFinalDecisionWelshLatest);
+        assertThat(result)
+            .isPresent()
+            .contains(issueFinalDecisionWelshLatest);
     }
 
     @Test
-    @DisplayName("Given issue final decision event is not in case data events, getLatestIssueFinalDecision returns null")
-    void getLatestEventOfSpecifiedType_returnsNull() {
+    @DisplayName("Given multiple issue final decision English and Welsh events, getLatestEventOfSpecifiedTypes returns latest")
+    void getLatestEventOfSpecifiedTypes_returnsLatestEvent() {
+        eventsList = List.of(issueFinalDecisionWelshEarliest, evidenceReceived, issueFinalDecisionWelshLatest,
+            issueFinalDecisionEarliest, issueFinalDecisionLatest);
+        caseData.setEvents(eventsList);
+
+        Optional<Event> result = SscsUtil.getLatestEventOfSpecifiedTypes(caseData, ISSUE_FINAL_DECISION, ISSUE_FINAL_DECISION_WELSH);
+
+        assertThat(result)
+            .isPresent()
+            .contains(issueFinalDecisionWelshLatest);
+    }
+
+    @Test
+    @DisplayName("Given issue final decision event is not in case data events, getLatestEventOfSpecifiedTypes returns empty Optional")
+    void getLatestEventOfSpecifiedTypes_returnsEmptyOptional() {
         eventsList = List.of(evidenceReceived);
         caseData.setEvents(eventsList);
 
-        Event result = SscsUtil.getLatestIssueFinalDecision(caseData);
+        Optional<Event> result = SscsUtil.getLatestEventOfSpecifiedTypes(caseData, ISSUE_FINAL_DECISION, ISSUE_FINAL_DECISION_WELSH);
 
-        assertThat(result).isNull();
+        assertThat(result).isNotPresent();
     }
 }
