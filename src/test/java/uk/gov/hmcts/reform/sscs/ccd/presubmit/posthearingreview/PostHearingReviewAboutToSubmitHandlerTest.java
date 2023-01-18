7bfb134f7d3c588676f31c67d4e211ae166e49a5
Pankaj Khare <pankaj.khare@version1.com>
2023-01-18 09:24:03 +0000

    add tests

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/posthearingreview/PostHearingReviewAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/posthearingreview/PostHearingReviewAboutToSubmitHandlerTest.java
@@ -11,6 +11,7 @@ import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.POST_HEARING_REVIEW;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.READY_TO_LIST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.HearingRoute.LIST_ASSIST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.PostHearingReviewType.SET_ASIDE;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocumentTranslationStatus.TRANSLATION_REQUIRED;
 
 import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
@@ -124,6 +125,25 @@ class PostHearingReviewAboutToSubmitHandlerTest {
 
         verify(footerService).createFooterAndAddDocToCase(eq(expectedDocument.getValue().getDocumentLink()), any(),
             eq(SET_ASIDE_APPLICATION), any(),  eq(null), eq(null), eq(null));
+
+    }
+
+    @Test
+    public void givenARefusedPostponementAndApplicationTypeSetAsideWithWelsh_thenAddSetAsideDoc() {
+        caseData.setLanguagePreferenceWelsh("YES");
+        caseData.setPostponementRequest(PostponementRequest.builder()
+            .actionPostponementRequestSelected("refuse")
+            .build());
+        caseData.getPostHearing().setReviewType(SET_ASIDE);
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(caseData);
+
+        PreSubmitCallbackResponse<SscsCaseData> response =
+            handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        verify(footerService).createFooterAndAddDocToCase(eq(expectedDocument.getValue().getDocumentLink()), any(),
+            eq(SET_ASIDE_APPLICATION), any(),  eq(null), eq(null), eq(TRANSLATION_REQUIRED));
 
     }
 }
