a409f99c25897407991227a86705bb91ade2f208
eliandro goncalves <eliandro.goncalves@version1.com>
2023-01-19 13:38:12 +0000

    template name corrections

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/config/DocumentConfigurationTest.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/config/DocumentConfigurationTest.java
@@ -32,7 +32,7 @@ public class DocumentConfigurationTest {
 
     public static Object[][] documentParameters() {
         return new Object[][] {
-                {LanguagePreference.ENGLISH, EventType.DECISION_ISSUED, Optional.empty(), "TB-SCS-GNO-ENG-draft-decision-notice.docx"},
+                {LanguagePreference.ENGLISH, EventType.DECISION_ISSUED, Optional.empty(), "TB-SCS-GNO-ENG-directions-notice.docx"},
                 {LanguagePreference.ENGLISH, EventType.DIRECTION_ISSUED, Optional.empty(), "TB-SCS-GNO-ENG-directions-notice.docx"},
                 {LanguagePreference.ENGLISH, EventType.ISSUE_FINAL_DECISION, Optional.empty(), "TB-SCS-GNO-ENG-final-decision-notice.docx"},
                 {LanguagePreference.WELSH, EventType.DECISION_ISSUED, Optional.empty(), "TB-SCS-GNO-WEL-00473.docx"},
--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/decisionissued/DecisionIssuedMidEventHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/decisionissued/DecisionIssuedMidEventHandlerTest.java
@@ -76,7 +76,7 @@ public class DecisionIssuedMidEventHandlerTest {
         Map<EventType, String> englishEventTypeDocs = new HashMap<>();
         englishEventTypeDocs.put(EventType.DIRECTION_ISSUED, "TB-SCS-GNO-ENG-directions-notice.docx");
         englishEventTypeDocs.put(EventType.DECISION_ISSUED, "TB-SCS-GNO-ENG-draft-decision-notice.docx");
-        englishEventTypeDocs.put(EventType.ISSUE_FINAL_DECISION, "TB-SCS-GNO-ENG-00453.docx");
+        englishEventTypeDocs.put(EventType.ISSUE_FINAL_DECISION, "TB-SCS-GNO-ENG-directions-notice.docx");
 
 
         Map<EventType, String> welshEventTypeDocs = new HashMap<>();
