4fc5e314c45b5f497d628da61082287b6d38d223
Basker Nadar <basker.nadar@version1.com>
2023-02-10 16:40:16 +0000

    SSCS-10544: Added the changes for set aside grant flow

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/posthearingreview/PostHearingReviewAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/posthearingreview/PostHearingReviewAboutToSubmitHandlerTest.java
@@ -4,8 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.MID_EVENT;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.POST_HEARING_REVIEW;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.READY_TO_LIST;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.*;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.HearingRoute.LIST_ASSIST;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -15,12 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
-import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
-import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentGeneration;
-import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentLink;
-import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentStaging;
-import uk.gov.hmcts.reform.sscs.ccd.domain.SchedulingAndListingFields;
-import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
 
 @ExtendWith(MockitoExtension.class)
 class PostHearingReviewAboutToSubmitHandlerTest {
@@ -94,5 +88,27 @@ class PostHearingReviewAboutToSubmitHandlerTest {
             handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
 
         assertThat(response.getErrors()).isEmpty();
+    }
+
+    @Test
+    void givenSetAsideStateIsNull_thenCaseStatusNotChanged() {
+        caseData.setState(State.DORMANT_APPEAL_STATE);
+        handler.updateCaseStatus(caseData);
+        assertThat(caseData.getState()).isEqualTo(State.DORMANT_APPEAL_STATE);
+    }
+
+    @Test
+    void givenSetAsideState_thenCaseStatusChanged() {
+        caseData = SscsCaseData.builder()
+                    .state(State.DORMANT_APPEAL_STATE)
+                    .postHearing(PostHearing.builder()
+                        .setAside(SetAside.builder()
+                            .action(SetAsideActions.GRANT)
+                            .build())
+                        .build())
+                    .build();
+
+        handler.updateCaseStatus(caseData);
+        assertThat(caseData.getState()).isEqualTo(State.NOT_LISTABLE);
     }
 }
