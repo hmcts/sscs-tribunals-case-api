64b6ebef33250af8bde531fe7883f853d5361399
DGordonV1 <david.gordon@hmcts.net>
2023-03-06 08:24:35 +0000

    SSCS-11357 - Refactor queue & topic names resolution.

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -1,6 +1,6 @@
 module "sscs-hearings-event-queue" {
   source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
-  name                = "tribunals-to-hearings-api-${var.env}"
+  name                = "tribunals-to-hearing-api-${var.env}"
   namespace_name      = "sscs-servicebus-${var.env}"
   resource_group_name = "sscs-${var.env}"
   requires_session    = false
