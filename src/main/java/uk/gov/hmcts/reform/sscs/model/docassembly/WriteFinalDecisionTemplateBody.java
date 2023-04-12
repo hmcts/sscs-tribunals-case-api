45b90b6bef86581c3d24ab93c5c9530912c973f0
Simon Stevenson <simon.stevenson@version1.com>
2023-04-12 21:33:51 +0100

    adding appointee details to template body

--- src/main/java/uk/gov/hmcts/reform/sscs/model/docassembly/WriteFinalDecisionTemplateBody.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/model/docassembly/WriteFinalDecisionTemplateBody.java
@@ -33,6 +33,8 @@ public class WriteFinalDecisionTemplateBody {
     private boolean isIndefinite;
     @JsonProperty("appellant_name")
     private String appellantName;
+    @JsonProperty("appointee_name")
+    private String appointeeName;
     @JsonProperty("start_date")
     private String startDate;
     @JsonProperty("end_date")
@@ -63,6 +65,8 @@ public class WriteFinalDecisionTemplateBody {
     private String pageNumber;
     @JsonProperty("attended_hearing")
     private boolean attendedHearing;
+    @JsonProperty("appointee_attended_hearing")
+    private boolean appointeeAttendedHearing;
     @JsonProperty("other_party_names_attended_hearing")
     private String otherPartyNamesAttendedHearing;
     @JsonProperty("presenting_officer_attended")
