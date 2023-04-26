3de68e785659cf02e93a55243abc0e0aff4954fb
Mohammed Ali <mohammed.ali@hmcts.net>
2023-04-26 13:33:32 +0100

    SSCS-10934 - 26.04.2023 - Sonar cloud coverage

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/WriteFinalDecisionPreviewDecisionServiceBase.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/WriteFinalDecisionPreviewDecisionServiceBase.java
@@ -63,12 +63,12 @@ public abstract class WriteFinalDecisionPreviewDecisionServiceBase extends Issue
 
     @Override
     protected NoticeIssuedTemplateBody createPayload(PreSubmitCallbackResponse<SscsCaseData> response, SscsCaseData caseData, String documentTypeLabel, LocalDate dateAdded, LocalDate generatedDate, boolean isScottish,
-        String userAuthorisation) throws IllegalStateException, NullPointerException {
+        String userAuthorisation) throws IllegalStateException {
 
         String benefitType = WriteFinalDecisionBenefitTypeHelper.getBenefitType(caseData);
 
         if (benefitType == null) {
-            throw new NullPointerException("Unable to determine benefit type");
+            throw new IllegalStateException("Unable to determine benefit type");
         }
 
         decisionNoticeOutcomeService.performPreOutcomeIntegrityAdjustments(caseData);
