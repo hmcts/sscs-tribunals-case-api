0c995ded4accc84fe285e801a95f013fe103b7b5
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-05-09 10:23:44 +0100

    update functional tests SSCS-11431

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/handlers/actionfurtherevidence/ActionFurtherEvidenceAboutToStartHandlerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/handlers/actionfurtherevidence/ActionFurtherEvidenceAboutToStartHandlerTest.java
@@ -53,7 +53,9 @@ public class ActionFurtherEvidenceAboutToStartHandlerTest extends BaseHandler {
             .assertThat().body("data.furtherEvidenceAction.list_items", hasItem(hasEntry("code", "informationReceivedForInterlocTcw")))
             .assertThat().body("data.furtherEvidenceAction.list_items", hasItem(hasEntry("code", "sendToInterlocReviewByJudge")))
             .assertThat().body("data.furtherEvidenceAction.list_items", hasItem(hasEntry("code", "sendToInterlocReviewByTcw")))
-            .assertThat().body("data.furtherEvidenceAction.list_items", hasSize(6));
+            .assertThat().body("data.furtherEvidenceAction.list_items", hasItem(hasEntry("code", "sendToInterlocReviewByTcw")))
+            .assertThat().body("data.furtherEvidenceAction.list_items", hasItem(hasEntry("code", "adminActionCorrection")))
+            .assertThat().body("data.furtherEvidenceAction.list_items", hasSize(7));
 
     }
 }
