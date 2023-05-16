0c5fc6da81d8be3b5cc9d190e3b1b512e1a54f09
Mohammed Ali <mohammed.ali@hmcts.net>
2023-05-16 16:27:50 +0100

    SSCS-10934 - Functional test fixes

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/ccd/EsaDecisionNoticeFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/ccd/EsaDecisionNoticeFunctionalTest.java
@@ -164,7 +164,7 @@ public class EsaDecisionNoticeFunctionalTest extends BaseFunctionTest {
             assertThat(pdfTextWithoutNewLines, containsString("7. Anything else"));
             assertThat(pdfTextWithoutNewLines, containsString("8. This has been a remote hearing in the form of a telephone hearing. Joe Bloggs the appellant did not attend the hearing today. First Tier Agency representative attended on behalf of the Respondent."));
             assertThat(pdfTextWithoutNewLines, containsString("9. Having considered the appeal bundle to page B7 and the requirements of rules 2 and 31 of The Tribunal Procedure (First-tier Tribunal)(Social Entitlement Chamber) Rules 2008 the Tribunal is satisfied that reasonable steps were taken to notify Joe Bloggs of the hearing and that it is in the interests of justice to proceed today."));
-            assertThat(pdfTextWithoutNewLines, containsString("10. Any recommendation given below does not form part of the Tribunal's decision and is not binding on the Secretary of State. The Tribunal makes no recommendation as to when the Department should reassess Joe Bloggs. Tribunal Judge: Account Date: 11/05/2023 Issued to the parties on: "));
+            assertThat(pdfTextWithoutNewLines, containsString("10. Any recommendation given below does not form part of the Tribunal's decision and is not binding on the Secretary of State. The Tribunal makes no recommendation as to when the Department should reassess Joe Bloggs. Tribunal Judge: Account Date: 16/05/2023 Issued to the parties on: "));
             assertThat(pdfTextWithoutNewLines, not(containsString("11.")));
         }
     }
