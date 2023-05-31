74f71f05f3cc012ba92c0eede484e613bc925213
amoslv1 <lucian.amos@version1.com>
2023-05-31 12:51:08 +0100

    SSCS-10602 removes flag enabled test
    test will need to be adjusted when the flag is enabled for real when the feature goes live

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/handlers/actionfurtherevidence/ActionFurtherEvidenceAboutToStartHandlerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/handlers/actionfurtherevidence/ActionFurtherEvidenceAboutToStartHandlerTest.java
@@ -22,8 +22,8 @@ import uk.gov.hmcts.reform.sscs.functional.handlers.BaseHandler;
 
 @RunWith(JUnitParamsRunner.class)
 @TestPropertySource(locations = "classpath:config/application_functional.properties")
-@SpringBootTest(properties = {"feature.postHearings.enabled=true"})
-public class ActionFurtherEvidenceAboutToStartHandlerPostHearingsEnabledTest extends BaseHandler {
+@SpringBootTest
+public class ActionFurtherEvidenceAboutToStartHandlerTest extends BaseHandler {
 
     @ClassRule
     public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
@@ -53,8 +53,7 @@ public class ActionFurtherEvidenceAboutToStartHandlerPostHearingsEnabledTest ext
             .assertThat().body("data.furtherEvidenceAction.list_items", hasItem(hasEntry("code", "informationReceivedForInterlocTcw")))
             .assertThat().body("data.furtherEvidenceAction.list_items", hasItem(hasEntry("code", "sendToInterlocReviewByJudge")))
             .assertThat().body("data.furtherEvidenceAction.list_items", hasItem(hasEntry("code", "sendToInterlocReviewByTcw")))
-            .assertThat().body("data.furtherEvidenceAction.list_items", hasItem(hasEntry("code", "adminActionCorrection")))
-            .assertThat().body("data.furtherEvidenceAction.list_items", hasSize(7));
+            .assertThat().body("data.furtherEvidenceAction.list_items", hasSize(6));
 
     }
 }
