e878e910dfde0fee98bd7e8b33c60260aa9d5eb6
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-06-13 13:55:22 +0100

    add extra test for reserving SSCS-10865

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/updatelistingrequirements/UpdateListingRequirementsAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/updatelistingrequirements/UpdateListingRequirementsAboutToSubmitHandlerTest.java
@@ -265,6 +265,32 @@ class UpdateListingRequirementsAboutToSubmitHandlerTest {
     }
 
     @Test
+    void givenPanelMembersAreReserved_updateTheirValues() {
+        ReflectionTestUtils.setField(handler, "isAdjournmentEnabled", true);
+        given(callback.getEvent()).willReturn(EventType.UPDATE_LISTING_REQUIREMENTS);
+        given(callback.getCaseDetails()).willReturn(caseDetails);
+        given(caseDetails.getCaseData()).willReturn(sscsCaseData);
+        given(caseDetails.getState()).willReturn(State.READY_TO_LIST);
+
+        String personalCode = "1";
+        String idamId = "2";
+        given(judicialRefDataService.getPersonalCode(idamId)).willReturn(personalCode);
+
+        sscsCaseData.getSchedulingAndListingFields().setPanelMemberExclusions(PanelMemberExclusions.builder()
+            .arePanelMembersReserved(YES).reservedPanelMembers(List.of(new CollectionItem<>(idamId, null))).build());
+
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(
+            ABOUT_TO_SUBMIT,
+            callback,
+            USER_AUTHORISATION);
+
+        assertThat(response.getErrors()).isEmpty();
+        List<CollectionItem<JudicialUserBase>> result = response.getData().getSchedulingAndListingFields().getPanelMemberExclusions().getReservedPanelMembers();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0)).isEqualTo(new CollectionItem<>(idamId, JudicialUserBase.builder().idamId(idamId).personalCode(personalCode).build()));
+    }
+
+    @Test
     void givenPanelMembersAreExcludedButNoValuesInList_updateTheirValues() {
         ReflectionTestUtils.setField(handler, "isAdjournmentEnabled", true);
         given(callback.getEvent()).willReturn(EventType.UPDATE_LISTING_REQUIREMENTS);
