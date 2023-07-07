9619c7d3c6c440a4187be239b6ad09e6e8d653ca
amoslv1 <110455092+amoslv1@users.noreply.github.com>
2023-07-07 08:41:22 +0100

    SSCS-10581 adds test for setFinalDecisionGeneratedDate

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/WriteFinalDecisionAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/WriteFinalDecisionAboutToSubmitHandlerTest.java
@@ -1,15 +1,23 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
@@ -17,6 +25,7 @@ import uk.gov.hmcts.reform.sscs.service.DecisionNoticeOutcomeService;
 import uk.gov.hmcts.reform.sscs.service.DecisionNoticeService;
 import uk.gov.hmcts.reform.sscs.service.PreviewDocumentService;
 
+@ExtendWith(MockitoExtension.class)
 public class WriteFinalDecisionAboutToSubmitHandlerTest {
     private static final String USER_AUTHORISATION = "Bearer token";
     @Mock
@@ -32,10 +41,8 @@ public class WriteFinalDecisionAboutToSubmitHandlerTest {
     @Mock
     private DecisionNoticeOutcomeService decisionNoticeOutcomeService;
 
-    @Before
+    @BeforeEach
     public void setUp() {
-        openMocks(this);
-
         handler = new WriteFinalDecisionAboutToSubmitHandler(decisionNoticeService, previewDocumentService);
         sscsCaseData = SscsCaseData.builder().ccdCaseId("ccdId").build();
         sscsCaseData.setAppeal(Appeal.builder().hearingOptions(HearingOptions.builder().build()).build());
@@ -54,7 +61,7 @@ public class WriteFinalDecisionAboutToSubmitHandlerTest {
         sscsCaseData.setState(State.APPEAL_CREATED);
         PreSubmitCallbackResponse<SscsCaseData> response =
             handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
-        assertThat(response.getData().getPreviousState(), is(State.APPEAL_CREATED));
+        assertThat(response.getData().getPreviousState()).isEqualTo(State.APPEAL_CREATED);
     }
 
     @Test
@@ -63,7 +70,7 @@ public class WriteFinalDecisionAboutToSubmitHandlerTest {
         sscsCaseData.setState(State.READY_TO_LIST);
         PreSubmitCallbackResponse<SscsCaseData> response =
             handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
-        assertThat(response.getData().getPreviousState(), is(State.VOID_STATE));
+        assertThat(response.getData().getPreviousState()).isEqualTo(State.VOID_STATE);
     }
 
     @Test
@@ -72,6 +79,34 @@ public class WriteFinalDecisionAboutToSubmitHandlerTest {
         sscsCaseData.setState(State.WITH_DWP);
         PreSubmitCallbackResponse<SscsCaseData> response =
             handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
-        assertThat(response.getData().getPreviousState(), is(State.VOID_STATE));
+        assertThat(response.getData().getPreviousState()).isEqualTo(State.VOID_STATE);
+    }
+
+    @Test
+    public void givenPostHearingsDisabled_shouldNotUpdateFinalDecisionGeneratedDate() {
+        ReflectionTestUtils.setField(handler, "isPostHearingsEnabled", false);
+        PreSubmitCallbackResponse<SscsCaseData> response =
+            handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+        assertThat(response.getData().getFinalDecisionGeneratedDate()).isNull();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = YesNo.class, names = "NO")
+    @NullSource
+    public void givenPostHearingsEnabled_andNotACorrection_shouldUpdateFinalDecisionGeneratedDate(YesNo yesNo) {
+        ReflectionTestUtils.setField(handler, "isPostHearingsEnabled", true);
+        sscsCaseData.getPostHearing().getCorrection().setCorrectionFinalDecisionInProgress(yesNo);
+        PreSubmitCallbackResponse<SscsCaseData> response =
+            handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+        assertThat(response.getData().getFinalDecisionGeneratedDate()).isEqualTo(LocalDate.now());
+    }
+
+    @Test
+    public void givenPostHearingsEnabled_andIsACorrection_shouldNotUpdateFinalDecisionGeneratedDate() {
+        ReflectionTestUtils.setField(handler, "isPostHearingsEnabled", true);
+        sscsCaseData.getPostHearing().getCorrection().setCorrectionFinalDecisionInProgress(YesNo.YES);
+        PreSubmitCallbackResponse<SscsCaseData> response =
+            handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+        assertThat(response.getData().getFinalDecisionGeneratedDate()).isNull();
     }
 }
