6d61c2617948b4d53e576a54653b4143857d0f56
paul-pearson <paul.pearson@version1.com>
2023-07-14 16:55:57 +0100

    SSCS-11878 Switch back to citizen idam tokens

--- src/main/java/uk/gov/hmcts/reform/sscs/service/CitizenLoginService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/CitizenLoginService.java
@@ -53,7 +53,7 @@ public class CitizenLoginService {
 
     public List<OnlineHearing> findCasesForCitizen(IdamTokens idamTokens, String tya) {
         log.info(format("Find case: Searching for case with tya [%s] for user [%s]", tya, idamTokens.getUserId()));
-        List<CaseDetails> caseDetails = citizenCcdService.searchForCitizenAllCases(idamTokens.getUserId(), idamService.getIdamTokens());
+        List<CaseDetails> caseDetails = citizenCcdService.searchForCitizenAllCases(idamTokens.getUserId(), idamTokens);
         List<SscsCaseDetails> sscsCaseDetails = caseDetails.stream()
                 .map(sscsCcdConvertService::getCaseDetails)
                 .filter(AppealNumberGenerator::filterCaseNotDraftOrArchivedDraft)
