3e4bc3a1306bef0f97b498ed800b09d3f3f3c3e3
Mark Brooks <mark.brooks@hmcts.net>
2023-07-19 15:55:36 +0100

    Add role caseworker-wa-task-configuration to cftlib config

--- src/cftlib/java/uk/gov/hmcts/reform/sscs/CftlibConfig.java
+++ src/cftlib/java/uk/gov/hmcts/reform/sscs/CftlibConfig.java
@@ -69,7 +69,8 @@ public class CftlibConfig implements CFTLibConfigurer {
                 "ccd-import", "manage-user", "caseworker");
         lib.createIdamUser("wa-system-user@fake.hmcts.net",
              "caseworker-wa",
-             "caseworker-wa-configuration"
+            "caseworker-wa-configuration",
+            "caseworker-wa-task-configuration"
         );
         lib.createRoles(
                 "caseworker-sscs-superuser",
@@ -87,7 +88,8 @@ public class CftlibConfig implements CFTLibConfigurer {
                 "caseworker-sscs-pcqextractor",
                 "citizen",
                 "caseworker-sscs",
-                "caseworker"
+                "caseworker",
+                "caseworker-wa-task-configuration"
         );
         var def = Files.readAllBytes(Path.of("../sscs-ccd-definitions/releases/CCD_SSCSDefinition_vdev_LOCAL.xlsx"));
         lib.importDefinition(def);
