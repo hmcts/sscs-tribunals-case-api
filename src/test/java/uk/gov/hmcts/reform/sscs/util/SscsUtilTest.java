1ce909d6247b700a89225f7ad5fd51f482c912a1
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-09-12 12:36:43 +0100

    remove unused import SSCS-10593

--- src/test/java/uk/gov/hmcts/reform/sscs/util/SscsUtilTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/util/SscsUtilTest.java
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
