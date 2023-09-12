e043375ad6a5d7255f8ab98885affc72c0178697
Nicholas Hill <nicholas.hill@HMCTS.net>
2023-09-12 15:28:37 +0100

    fix mocks SSCS-10865

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adjourncase/AdjournCaseAboutToSubmitHandlerTestBase.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adjourncase/AdjournCaseAboutToSubmitHandlerTestBase.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.adjourncase;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -15,6 +14,7 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.OverrideFields;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SchedulingAndListingFields;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.service.PreviewDocumentService;
+import uk.gov.hmcts.reform.sscs.service.UserDetailsService;
 import uk.gov.hmcts.reform.sscs.service.VenueDataLoader;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,7 +23,7 @@ abstract class AdjournCaseAboutToSubmitHandlerTestBase {
     protected static final String USER_AUTHORISATION = "Bearer token";
     protected static final String OLD_DRAFT_DOC = "oldDraft.doc";
 
-    @InjectMocks
+    @Mock
     protected AdjournCaseAboutToSubmitHandler handler;
 
     @Mock
@@ -37,14 +37,17 @@ abstract class AdjournCaseAboutToSubmitHandlerTestBase {
     protected PreviewDocumentService previewDocumentService;
 
     @Mock
+    protected UserDetailsService userDetailsService;
+
+    @Mock
     protected VenueDataLoader venueDataLoader;
 
     protected SscsCaseData sscsCaseData;
 
     @BeforeEach
     protected void setUp() {
+        handler = new AdjournCaseAboutToSubmitHandler(previewDocumentService, userDetailsService, true);
         ReflectionTestUtils.setField(handler, "isAdjournmentEnabled", true);
-
         sscsCaseData = SscsCaseData.builder().ccdCaseId("ccdId")
             .appeal(Appeal.builder().hearingOptions(HearingOptions.builder().build()).build())
             .schedulingAndListingFields(SchedulingAndListingFields.builder()
