177f9466a66e85eca76e63ab1205417fa4312811
Mark Brooks <mark.brooks@hmcts.net>
2023-11-01 16:12:16 +0000

    SSCS-12124 Fix NPE

--- src/test/java/uk/gov/hmcts/reform/sscs/util/AddedDocumentsUtilTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/util/AddedDocumentsUtilTest.java
@@ -8,7 +8,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
 import org.junit.Before;
 import org.junit.Test;
 import uk.gov.hmcts.reform.sscs.ccd.domain.AbstractDocument;
