6154ae9e7099f18b841d4466fe018aecab259fe9
Gemma Talbot <gemma.talbot@solirius.com>
2023-11-03 15:53:50 +0000

    add canHandle test

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/actionpostponementrequest/ActionPostponementRequestAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/actionpostponementrequest/ActionPostponementRequestAboutToStartHandlerTest.java
@@ -1,16 +1,23 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.actionpostponementrequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_START;
 
 import java.time.LocalDate;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentGeneration;
@@ -24,6 +31,7 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.SchedulingAndListingFields;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsHearingRecordingCaseData;
 
+@RunWith(JUnitParamsRunner.class)
 public class ActionPostponementRequestAboutToStartHandlerTest {
 
     private static final String USER_AUTHORISATION = "Bearer token";
@@ -34,7 +42,7 @@ public class ActionPostponementRequestAboutToStartHandlerTest {
     private CaseDetails<SscsCaseData> caseDetails;
     private SscsCaseData sscsCaseData;
 
-    @BeforeEach
+    @Before
     public void setUp() {
         openMocks(this);
         handler = new ActionPostponementRequestAboutToStartHandler();
@@ -58,16 +66,25 @@ public class ActionPostponementRequestAboutToStartHandlerTest {
                         .actionPostponementRequestSelected("grant")
                         .build())
                 .build();
-    }
 
-    @Test
-    void givenCaseHasActionPostponementRequestSelected_thenClearActionPostponementRequestSelected() {
         when(callback.getEvent()).thenReturn(EventType.ACTION_POSTPONEMENT_REQUEST);
         when(callback.getCaseDetails()).thenReturn(caseDetails);
         when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+    }
 
+    @Test
+    public void givenCaseHasActionPostponementRequestSelected_thenClearActionPostponementRequestSelected() {
         handler.handle(ABOUT_TO_START, callback, USER_AUTHORISATION);
-
         assertThat(sscsCaseData.getPostponementRequest().getActionPostponementRequestSelected()).isNull();
+    }
+    @Test
+    public void givenValidCallback_thenReturnTrue() {
+        assertTrue(handler.canHandle(ABOUT_TO_START, callback));
+    }
+
+    @Test
+    @Parameters({"ABOUT_TO_SUBMIT", "MID_EVENT", "SUBMITTED"})
+    public void givenANonCallbackType_thenReturnFalse(CallbackType callbackType) {
+        assertFalse(handler.canHandle(callbackType, callback));
     }
 }
