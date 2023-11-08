2f8c7fd7a7350a78f3bfcae043c87b8272295b51
hashimalisolirius <HashimAli@HMCTS.NET>
2023-11-08 10:43:41 +0000

    Revert "handle empty organisation field"
    This reverts commit 8940ccf7c508c3e59af891e80b89abe82e048161.

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/caseupdated/CaseUpdatedAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/caseupdated/CaseUpdatedAboutToSubmitHandler.java
@@ -347,9 +347,7 @@ public class CaseUpdatedAboutToSubmitHandler extends ResponseEventsAboutToSubmit
         final boolean hasRepresentative = sscsCaseData.isThereARepresentative();
         if (hasRepresentative) {
             Representative representativeInfo = sscsCaseData.getAppeal().getRep();
-            if (representativeInfo.getOrganisation() == null
-                    || representativeInfo.getOrganisation() != null
-                    && representativeInfo.getOrganisation().isEmpty()) {
+            if (representativeInfo.getOrganisation() == null) {
                 List<String> warnings = validateRepAndJointPartyCaseData(representativeInfo, "Representative");
                 if (!warnings.isEmpty()) {
                     response.addWarnings(warnings);
