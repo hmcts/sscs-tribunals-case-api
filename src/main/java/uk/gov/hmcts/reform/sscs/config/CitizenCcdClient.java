ac46be6d8b582419ba69ce882a51e6a887e2f7f0
hashimalisolirius <HashimAli@HMCTS.NET>
2023-11-30 12:23:29 +0000

    Remove wildcard import

--- src/main/java/uk/gov/hmcts/reform/sscs/config/CitizenCcdClient.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/CitizenCcdClient.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.reform.sscs.config;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
