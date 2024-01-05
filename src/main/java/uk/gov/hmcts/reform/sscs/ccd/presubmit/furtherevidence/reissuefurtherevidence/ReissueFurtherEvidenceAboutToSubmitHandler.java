cc7646f53b8eeb2969aedb7b6f0008b75221f4ae
Gemma Talbot <gemma.talbot@solirius.com>
2024-01-05 16:54:34 +0000

    update handler for null exception

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/reissuefurtherevidence/ReissueFurtherEvidenceAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/reissuefurtherevidence/ReissueFurtherEvidenceAboutToSubmitHandler.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.furtherevidence.reissuefurtherevidence;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.DocumentType.*;
 import static uk.gov.hmcts.reform.sscs.model.PartyItemList.*;
@@ -63,7 +64,8 @@ public class ReissueFurtherEvidenceAboutToSubmitHandler implements PreSubmitCall
                 optionalSelectedDocument  = Stream
                         .of(sscsCaseData.getSscsDocument(), sscsCaseData.getSscsWelshDocuments())
                         .flatMap(x -> x == null ? null : x.stream())
-                        .filter(f -> selectedDocumentUrl.get().equals(f.getValue().getEditedDocumentLink().getDocumentUrl()))
+                        .filter(f -> f.getValue().getEditedDocumentLink()!=null
+                                && selectedDocumentUrl.get().equals(f.getValue().getEditedDocumentLink().getDocumentUrl()))
                         .findFirst();
             }
             if (optionalSelectedDocument.isEmpty()) {
