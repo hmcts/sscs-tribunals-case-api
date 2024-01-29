5599bb3621ef50042ee2a2619d210e610a86ba38
markbrooks725 <107925801+markbrooks725@users.noreply.github.com>
2024-01-29 15:18:56 +0000

    Update ActionFurtherEvidenceSubmittedCallbackHandlerTest.java

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/actionfurtherevidence/ActionFurtherEvidenceSubmittedCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/actionfurtherevidence/ActionFurtherEvidenceSubmittedCallbackHandlerTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
