f8468b807cbe563c1561b57dc3d0bba0d71352e5
NHAL22 <113360585+NHAL22@users.noreply.github.com>
2024-03-14 15:27:25 +0000

    change default value for dropdown list

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/actionfurtherevidence/ActionFurtherEvidenceAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/actionfurtherevidence/ActionFurtherEvidenceAboutToStartHandler.java
@@ -47,7 +47,7 @@ public class ActionFurtherEvidenceAboutToStartHandler implements PreSubmitCallba
 
     private void setFurtherEvidenceActionDropdown(SscsCaseData sscsCaseData) {
         List<DynamicListItem> listOptions = getFurtherActionEvidenceItems(List.of(values()));
-        sscsCaseData.setFurtherEvidenceAction(new DynamicList(listOptions.get(0), listOptions));
+        sscsCaseData.setFurtherEvidenceAction(new DynamicList(new DynamicListItem("", ""), listOptions));
     }
 
     private List<DynamicListItem> getFurtherActionEvidenceItems(List<FurtherEvidenceActionDynamicListItems> items) {
