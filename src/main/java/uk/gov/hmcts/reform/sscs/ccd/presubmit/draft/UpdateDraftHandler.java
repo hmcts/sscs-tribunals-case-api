2183c22ef316273440333bc0ee1d05ecc103fda4
Mark Brooks <mark.brooks@hmcts.net>
2024-03-14 16:28:30 +0000

    Add UpdateDraft callback handler

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/draft/UpdateDraftHandler.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.draft;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+
+@Component
+@Slf4j
+public class UpdateDraftHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        return callbackType == CallbackType.ABOUT_TO_SUBMIT
+            && callback.getEvent() == EventType.UPDATE_DRAFT;
+    }
+
+    git @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback, String userAuthorisation) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        SscsCaseData caseData = callback.getCaseDetails().getCaseData();
+        return new PreSubmitCallbackResponse<>(caseData);
+    }
+}
