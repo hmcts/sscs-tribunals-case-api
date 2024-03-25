628da44074da42c20625950a7945d588c884aecf
DGordonV1 <david.gordon@hmcts.net>
2024-03-25 16:55:10 +0000

    SSCS-12061 - Fix tests

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/caseupdated/AbstractedCaseUpdatedAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/caseupdated/AbstractedCaseUpdatedAboutToSubmitHandlerTest.java
@@ -1678,27 +1678,6 @@ public abstract class AbstractedCaseUpdatedAboutToSubmitHandlerTest {
         assertNull(appeal.getHearingOptions().getLanguages());
     }
 
-    @ParameterizedTest
-    @CsvSource({
-        "Spanish", "Chittagonain", "Czech", "Danish", "Dinka", "Maldivian", "Toura", "Douala", "Dutch", "Dioula",
-        "Efik", "Estonian", "Ewe", "Ewondo", "Farsi", "Fanti", "Fijian", "French"
-    })
-    public void givenAnyCaseAndLanguageIsSet_thenSetTheLanguageValue(String language) {
-        Appeal appeal = callback.getCaseDetails().getCaseData().getAppeal();
-        appeal.getBenefitType().setCode("PIP");
-        appeal.setHearingType("paper");
-        HearingOptions hearingOptions = HearingOptions.builder()
-                .wantsToAttend("Yes")
-                .languages(language)
-                .build();
-        appeal.setHearingOptions(hearingOptions);
-
-        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
-
-        assertEquals(0, response.getWarnings().size());
-        assertEquals(language, appeal.getHearingOptions().getLanguages());
-    }
-
     @Test
     void givenNewBenefitTypeAndCodeIsSelected_thenCaseCodeShouldChange() {
         DynamicListItem item = new DynamicListItem("088", "");
