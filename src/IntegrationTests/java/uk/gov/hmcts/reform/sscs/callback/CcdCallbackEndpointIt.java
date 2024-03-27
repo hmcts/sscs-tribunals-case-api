77a302aea1bd90143596114fc2c2e7dc7e064fab
Ruban <ruban72@hotmail.com>
2024-03-27 10:37:23 +0000

    SSCSSI-135: Fix failing FTs for action further evidence

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/CcdCallbackEndpointIt.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/callback/CcdCallbackEndpointIt.java
@@ -175,7 +175,7 @@ public class CcdCallbackEndpointIt extends AbstractEventIt {
 
         given(coreCaseDataApi.startEventForCaseWorker(eq("Bearer authToken"), eq("s2s token"),
                 eq("userId"), eq("SSCS"), eq("Benefit"), eq("12345656789"),
-                eq("actionFurtherEvidence")))
+                eq("updateCaseOnly")))
                 .willReturn(StartEventResponse.builder()
                         .caseDetails(CaseDetails.builder()
                                 .id(12345656789L)
@@ -223,7 +223,7 @@ public class CcdCallbackEndpointIt extends AbstractEventIt {
 
         given(coreCaseDataApi.startEventForCaseWorker(eq("Bearer authToken"), eq("s2s token"),
                 eq("userId"), eq("SSCS"), eq("Benefit"), eq("12345656789"),
-                eq("actionFurtherEvidence")))
+                eq("updateCaseOnly")))
                 .willReturn(StartEventResponse.builder()
                         .caseDetails(CaseDetails.builder()
                                 .id(12345656789L)
