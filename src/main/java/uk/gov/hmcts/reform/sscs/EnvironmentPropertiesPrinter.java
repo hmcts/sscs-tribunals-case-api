5dcf336e771bbf6073a72525be44557e161ad1e1
Ruban <ruban72@hotmail.com>
2024-04-08 15:55:24 +0100

    SSCSSI-34: print out aliases for evidence share secrets

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/EnvironmentPropertiesPrinter.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.sscs;
+
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class EnvironmentPropertiesPrinter {
+
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void logApplicationProperties() {
+        log.info("{}={}", "robotics.email.to", env.getProperty("robotics.email.to"));
+        log.info("{}={}", "robotics.email.from", env.getProperty("robotics.email.from"));
+        log.info("{}={}", "robotics.email.pipAeTo", env.getProperty("robotics.email.pipAeTo"));
+        log.info("{}={}", "robotics.email.scottishTo", env.getProperty("robotics.email.scottishTo"));
+        log.info("{}={}", "ld.sdk-key", env.getProperty("ld.sdk-key"));
+        log.info("{}={}", "send-grid.apiKey", env.getProperty("send-grid.apiKey"));
+    }
+}
