f6b256b26357cbc63fd25198a275662350546719
SomeshRA <somesh.acharya1@hmcts.net>
2024-05-07 13:03:43 +0100

    SSCSSI-257: Fix Broken Trigger in PostponeHearingHandler

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postpone/PostponeHearingHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postpone/PostponeHearingHandlerTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.postpone;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
@@ -15,6 +14,7 @@ import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.READY_TO_LIST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.NO;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.YES;
 
+import java.util.function.Consumer;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.After;
@@ -22,6 +22,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -35,10 +37,10 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.HearingRoute;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Postponement;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SchedulingAndListingFields;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
-import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.service.UpdateCcdCaseService;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
-
 
 @RunWith(JUnitParamsRunner.class)
 public class PostponeHearingHandlerTest {
@@ -58,10 +60,13 @@ public class PostponeHearingHandlerTest {
     private CaseDetails<SscsCaseData> caseDetails;
 
     @Mock
-    private CcdService ccdService;
+    private IdamService idamService;
 
     @Mock
-    private IdamService idamService;
+    private UpdateCcdCaseService updateCcdCaseService;
+
+    @Captor
+    private ArgumentCaptor<Consumer<SscsCaseData>> caseDetailsCaptor;
 
     @InjectMocks
     private PostponeHearingHandler handler;
@@ -126,24 +131,37 @@ public class PostponeHearingHandlerTest {
     public void handleReadyToListPostponementRelistEventType() {
         caseData.getPostponement().setPostponementEvent(READY_TO_LIST);
 
-        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(SUBMITTED, callback, USER_AUTHORISATION);
-
-        assertThat(response.getErrors()).isEmpty();
-
-        SscsCaseData result = response.getData();
-
-        assertThat(result.getPostponement()).isNotNull();
-        assertThat(result.getPostponement().getPostponementEvent()).isNull();
-        assertThat(result.getPostponement().getUnprocessedPostponement()).isEqualTo(NO);
-        assertThat(result.getDwpState()).isEqualTo(DwpState.HEARING_POSTPONED);
-
-        verify(ccdService,times(1))
-            .updateCase(any(SscsCaseData.class),
+        SscsCaseDetails sscsCaseDetails = SscsCaseDetails.builder()
+                .data(caseData)
+                .build();
+        when(updateCcdCaseService.updateCaseV2(
                 eq(Long.valueOf(CASE_ID)),
                 eq(READY_TO_LIST.getCcdType()),
                 eq("Ready to List after Hearing Postponed"),
                 eq("Setting case to Ready to List after Hearing Postponed"),
-                eq(idamTokens));
+                eq(idamTokens),
+                any(Consumer.class))
+        ).thenReturn(sscsCaseDetails);
+
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(SUBMITTED, callback, USER_AUTHORISATION);
+
+        assertThat(response.getErrors()).isEmpty();
+
+        verify(updateCcdCaseService)
+                .updateCaseV2(
+                        eq(Long.valueOf(CASE_ID)),
+                        eq(READY_TO_LIST.getCcdType()),
+                        eq("Ready to List after Hearing Postponed"),
+                        eq("Setting case to Ready to List after Hearing Postponed"),
+                        eq(idamTokens),
+                        caseDetailsCaptor.capture());
+
+        SscsCaseData sscsCaseData = sscsCaseDetails.getData();
+        caseDetailsCaptor.getValue().accept(sscsCaseData);
+        assertThat(sscsCaseData.getPostponement()).isNotNull();
+        assertThat(sscsCaseData.getPostponement().getPostponementEvent()).isNull();
+        assertThat(sscsCaseData.getPostponement().getUnprocessedPostponement()).isEqualTo(NO);
+        assertThat(sscsCaseData.getDwpState()).isEqualTo(DwpState.HEARING_POSTPONED);
     }
 
     @DisplayName("When given Not Listable event is given and postponement hearing has not been already handled "
@@ -151,25 +169,37 @@ public class PostponeHearingHandlerTest {
     @Test
     public void handleNotListablePostponementRelistEventType() {
         caseData.getPostponement().setPostponementEvent(NOT_LISTABLE);
+        SscsCaseDetails sscsCaseDetails = SscsCaseDetails.builder()
+                .data(caseData)
+                .build();
+        when(updateCcdCaseService.updateCaseV2(
+                eq(Long.valueOf(CASE_ID)),
+                eq(NOT_LISTABLE.getCcdType()),
+                eq("Not Listable after Hearing Postponed"),
+                eq("Setting case to Not Listable after Hearing Postponed"),
+                eq(idamTokens),
+                any(Consumer.class))
+        ).thenReturn(sscsCaseDetails);
 
         PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(SUBMITTED, callback, USER_AUTHORISATION);
 
         assertThat(response.getErrors()).isEmpty();
 
-        SscsCaseData result = response.getData();
-
-        assertThat(result.getPostponement()).isNotNull();
-        assertThat(result.getPostponement().getPostponementEvent()).isNull();
-        assertThat(result.getPostponement().getUnprocessedPostponement()).isEqualTo(NO);
-        assertThat(result.getDwpState()).isEqualTo(DwpState.HEARING_POSTPONED);
-
-        verify(ccdService,times(1))
-            .updateCase(any(SscsCaseData.class),
-                eq(Long.valueOf(CASE_ID)),
-                eq(NOT_LISTABLE.getCcdType()),
-                eq("Not Listable after Hearing Postponed"),
-                eq("Setting case to Not Listable after Hearing Postponed"),
-                eq(idamTokens));
+        verify(updateCcdCaseService)
+            .updateCaseV2(
+                    eq(Long.valueOf(CASE_ID)),
+                    eq(NOT_LISTABLE.getCcdType()),
+                    eq("Not Listable after Hearing Postponed"),
+                    eq("Setting case to Not Listable after Hearing Postponed"),
+                    eq(idamTokens),
+                    caseDetailsCaptor.capture()
+            );
+        SscsCaseData sscsCaseData = sscsCaseDetails.getData();
+        caseDetailsCaptor.getValue().accept(sscsCaseData);
+        assertThat(sscsCaseData.getPostponement()).isNotNull();
+        assertThat(sscsCaseData.getPostponement().getPostponementEvent()).isNull();
+        assertThat(sscsCaseData.getPostponement().getUnprocessedPostponement()).isEqualTo(NO);
+        assertThat(sscsCaseData.getDwpState()).isEqualTo(DwpState.HEARING_POSTPONED);
     }
 
     @DisplayName("When a case is not List Assist the method handled does not update any fields or call update ccd "
@@ -184,7 +214,7 @@ public class PostponeHearingHandlerTest {
             .hasSize(1)
             .contains("Invalid event type: APPEAL_RECEIVED for Hearing Postponed Event");
 
-        verifyNoInteractions(ccdService);
+        verifyNoInteractions(updateCcdCaseService);
 
         SscsCaseData result = response.getData();
 
@@ -205,7 +235,7 @@ public class PostponeHearingHandlerTest {
             .contains("Cannot process hearing postponed event on a case that has already been processed");
 
 
-        verifyNoInteractions(ccdService);
+        verifyNoInteractions(updateCcdCaseService);
 
         SscsCaseData result = response.getData();
 
@@ -227,7 +257,7 @@ public class PostponeHearingHandlerTest {
             .hasSize(1)
             .contains("Cannot process hearing postponed on non Scheduling & Listing Case");
 
-        verifyNoInteractions(ccdService);
+        verifyNoInteractions(updateCcdCaseService);
 
         SscsCaseData result = response.getData();
 
