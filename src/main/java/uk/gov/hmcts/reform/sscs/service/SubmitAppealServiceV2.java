7fa19d97a1846743f5a1f8ac25ae5f6819f0970f
andrewmcmahon <andrew.mcmahon@solirius.com>
2024-05-07 12:30:55 +0100

    Add in env values

--- src/main/java/uk/gov/hmcts/reform/sscs/service/SubmitAppealServiceV2.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/SubmitAppealServiceV2.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.sscs.service;
 
 import static java.util.Objects.nonNull;
-import static uk.gov.hmcts.reform.sscs.transform.deserialize.SubmitYourAppealToCcdCaseDataDeserializer.convertSyaToCcdCaseDataV1;
 import static uk.gov.hmcts.reform.sscs.transform.deserialize.SubmitYourAppealToCcdCaseDataDeserializer.convertSyaToCcdCaseDataV2;
 
 import feign.FeignException;
