53a39eeeb0dc59f21cac6701ce8787709c8c24ee
taleb <tech@benouaer.com>
2024-05-10 18:26:56 +0100

    SSCSCI-855: fix checkstyle -remove unused imports

--- src/main/java/uk/gov/hmcts/reform/sscs/config/MessagingConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/MessagingConfig.java
@@ -1,13 +1,7 @@
 package uk.gov.hmcts.reform.sscs.config;
 
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
 import javax.jms.ConnectionFactory;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.qpid.jms.JmsConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +14,6 @@ import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 import uk.gov.hmcts.reform.sscs.service.servicebus.messaging.JmsErrorHandler;
-
 
 @Configuration
 @Slf4j
