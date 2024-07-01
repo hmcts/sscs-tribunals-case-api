e2fcdd0436e63eac65a951a149e40374719000f0
NHAL22 <113360585+NHAL22@users.noreply.github.com>
2024-07-01 13:03:00 +0100

    adjust indent

--- src/main/java/uk/gov/hmcts/reform/sscs/transform/deserialize/SubmitYourAppealToCcdCaseDataDeserializer.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/transform/deserialize/SubmitYourAppealToCcdCaseDataDeserializer.java
@@ -38,7 +38,7 @@ public final class SubmitYourAppealToCcdCaseDataDeserializer {
             .toBuilder()
             .region(region)
             .regionalProcessingCenter(rpc)
-                .schedulingAndListingFields(SchedulingAndListingFields.builder().hearingRoute(rpc.getHearingRoute()).build())
+            .schedulingAndListingFields(SchedulingAndListingFields.builder().hearingRoute(rpc.getHearingRoute()).build())
             .build();
     }
 
