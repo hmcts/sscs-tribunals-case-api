d41808634546108e68d8486f1d54bad4866e51ed
SomeshRA <somesh.acharya1@hmcts.net>
2024-07-05 14:24:02 +0100

    SSCSSI-338: Fix broken trigger for WriteStatementOfReasonsSubmittedHandler

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writestatementofreasons/WriteStatementOfReasonsSubmittedHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writestatementofreasons/WriteStatementOfReasonsSubmittedHandler.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.writestatementofreasons;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CcdCallbackMap;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.WriteStatementOfReasons;
@@ -23,6 +25,8 @@ public class WriteStatementOfReasonsSubmittedHandler implements PreSubmitCallbac
     private final CcdCallbackMapService ccdCallbackMapService;
     @Value("${feature.postHearings.enabled}")
     private final boolean isPostHearingsEnabled;
+    @Value("${feature.handle-ccd-callbackMap-v2.enabled}")
+    private boolean isHandleCcdCallbackMapV2Enabled;
 
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
@@ -39,14 +43,24 @@ public class WriteStatementOfReasonsSubmittedHandler implements PreSubmitCallbac
                                                           String userAuthorisation) {
         SscsCaseData caseData = callback.getCaseDetails().getCaseData();
 
-        Long caseId = Long.valueOf(caseData.getCcdCaseId());
+        long caseId = Long.parseLong(caseData.getCcdCaseId());
 
         log.info("Write Statement of Reasons: processing submitted handler for case {}", caseId);
 
-        caseData = ccdCallbackMapService.handleCcdCallbackMap(WriteStatementOfReasons.IN_TIME, caseData);
+        CcdCallbackMap callbackMap = WriteStatementOfReasons.IN_TIME;
 
-        SscsUtil.clearPostHearingFields(caseData, isPostHearingsEnabled);
+        Consumer<SscsCaseData> sscsCaseDataConsumer = sscsCaseData -> SscsUtil.clearPostHearingFields(sscsCaseData, isPostHearingsEnabled);
 
+        if (isHandleCcdCallbackMapV2Enabled) {
+            caseData = ccdCallbackMapService.handleCcdCallbackMapV2(
+                    callbackMap,
+                    caseId,
+                    sscsCaseDataConsumer
+            ).orElse(caseData);
+        } else {
+            sscsCaseDataConsumer.accept(caseData);
+            caseData = ccdCallbackMapService.handleCcdCallbackMap(callbackMap, caseData);
+        }
         return new PreSubmitCallbackResponse<>(caseData);
     }
 }
