cce4cee5626b7a87d5f84420799984d7f75c96b0
adusumillipraveen <praveen.adusumilli@hmcts.net>
2024-07-05 11:34:07 +0100

    fix imports

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/TemplateServiceTest.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/TemplateServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
