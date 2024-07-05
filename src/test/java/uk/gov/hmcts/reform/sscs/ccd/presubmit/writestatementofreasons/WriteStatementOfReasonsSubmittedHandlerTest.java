56be5ced2de3085406f48754e0592e22d17cf9b2
SomeshRA <somesh.acharya1@hmcts.net>
2024-07-05 14:24:02 +0100

    SSCSSI-338: Fix broken trigger for WriteStatementOfReasonsSubmittedHandler

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writestatementofreasons/WriteStatementOfReasonsSubmittedHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writestatementofreasons/WriteStatementOfReasonsSubmittedHandlerTest.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.writestatementofreasons;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -9,14 +12,24 @@ import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.READY_TO_LIST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.SOR_WRITE;
 
+import java.util.Optional;
+import java.util.function.Consumer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CcdCallbackMap;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentGeneration;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentStaging;
+import uk.gov.hmcts.reform.sscs.ccd.domain.PostHearing;
+import uk.gov.hmcts.reform.sscs.ccd.domain.PostHearingRequestType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.WriteStatementOfReasons;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdCallbackMapService;
@@ -39,6 +52,9 @@ class WriteStatementOfReasonsSubmittedHandlerTest {
     private CaseDetails<SscsCaseData> caseDetails;
 
     private SscsCaseData caseData;
+
+    @Captor
+    private ArgumentCaptor<Consumer<SscsCaseData>> consumerArgumentCaptor;
 
     @BeforeEach
     void setUp() {
@@ -91,4 +107,40 @@ class WriteStatementOfReasonsSubmittedHandlerTest {
             .handleCcdCallbackMap(WriteStatementOfReasons.IN_TIME, caseData);
     }
 
+
+    @Test
+    void givenWriteStatementOfReasons_shouldReturnCallCorrectCallback_WhenCcdCallbackMapV2IsEnabled() {
+        ReflectionTestUtils.setField(handler, "isHandleCcdCallbackMapV2Enabled", true);
+        caseData.getPostHearing().setRequestType(PostHearingRequestType.STATEMENT_OF_REASONS);
+
+        CcdCallbackMap callbackMap = WriteStatementOfReasons.IN_TIME;
+
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+
+        when(caseDetails.getCaseData()).thenReturn(caseData);
+        long caseId = Long.parseLong(caseData.getCcdCaseId());
+
+        when(ccdCallbackMapService.handleCcdCallbackMapV2(eq(callbackMap), eq(caseId), any()))
+                .thenReturn(Optional.of(SscsCaseData.builder().build()));
+
+        PreSubmitCallbackResponse<SscsCaseData> response =
+                handler.handle(SUBMITTED, callback, USER_AUTHORISATION);
+
+        assertThat(response.getErrors()).isEmpty();
+
+        verify(ccdCallbackMapService)
+                .handleCcdCallbackMapV2(eq(callbackMap), eq(caseId), consumerArgumentCaptor.capture());
+
+        verify(ccdCallbackMapService, never())
+                .handleCcdCallbackMap(callbackMap, caseData);
+
+        consumerArgumentCaptor.getValue().accept(caseData);
+
+        assertThat(caseData.getPostHearing())
+                .isEqualTo(PostHearing.builder().build());
+        assertThat(caseData.getDocumentGeneration())
+                .isEqualTo(DocumentGeneration.builder().build());
+        assertThat(caseData.getDocumentStaging())
+                .isEqualTo(DocumentStaging.builder().build());
+    }
 }
