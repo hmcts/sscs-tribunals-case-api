d076a5a76a20ebba23d2687543d390b42767ff55
Gemma Talbot <gemma.talbot@solirius.com>
2024-08-02 14:13:57 +0100

    add isCorrection check and tests

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/WriteFinalDecisionPreviewDecisionServiceBase.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/WriteFinalDecisionPreviewDecisionServiceBase.java
@@ -101,9 +101,10 @@ public abstract class WriteFinalDecisionPreviewDecisionServiceBase extends Issue
         setHearings(writeFinalDecisionBuilder, caseData);
 
         String heldAt = writeFinalDecisionBuilder.build().getHeldAt();
-        String heldBefore = buildHeldBefore(caseData, userAuthorisation);
+        String heldBefore = buildHeldBefore(caseData, userAuthorisation, isPostHearingsEnabled);
 
-        if (isPostHearingsEnabled && nonNull(finalDecisionCaseData.getFinalDecisionHeldAt())) {
+        if (isPostHearingsEnabled && nonNull(finalDecisionCaseData.getFinalDecisionHeldAt())
+                && SscsUtil.isCorrectionInProgress(caseData, isPostHearingsEnabled)) {
             heldAt = finalDecisionCaseData.getFinalDecisionHeldAt();
         }
 
@@ -262,10 +263,12 @@ public abstract class WriteFinalDecisionPreviewDecisionServiceBase extends Issue
         }
     }
 
-    protected String buildHeldBefore(SscsCaseData caseData, String userAuthorisation) {
-        String judgeName = caseData.getSscsFinalDecisionCaseData().getFinalDecisionJudge();
-
-        if (isNull(judgeName)) {
+    protected String buildHeldBefore(SscsCaseData caseData, String userAuthorisation, boolean isPostHearingsEnabled) {
+        String judgeName = null;
+        String originalJudgeName = caseData.getSscsFinalDecisionCaseData().getFinalDecisionJudge();
+        if (SscsUtil.isCorrectionInProgress(caseData, isPostHearingsEnabled) && !isNull(originalJudgeName)) {
+            judgeName = originalJudgeName;
+        } else {
             judgeName = buildSignedInJudgeName(userAuthorisation);
         }
 
