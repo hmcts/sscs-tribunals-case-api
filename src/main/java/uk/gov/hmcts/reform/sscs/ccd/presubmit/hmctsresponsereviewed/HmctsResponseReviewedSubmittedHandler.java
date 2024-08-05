d871edb13a2cd0e2ccd93b2d9d1f07be04a6f08f
SomeshRA <somesh.acharya1@hmcts.net>
2024-08-05 11:27:32 +0100

    Revert "SSCSSI-338: Merged with master"
    This reverts commit 215506bbcb94a2848302f511f00db3622d657456.

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/hmctsresponsereviewed/HmctsResponseReviewedSubmittedHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/hmctsresponsereviewed/HmctsResponseReviewedSubmittedHandler.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.hmctsresponsereviewed;
 
 import static java.util.Objects.requireNonNull;
 
-import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -61,16 +60,6 @@ public class HmctsResponseReviewedSubmittedHandler extends ResponseEventsAboutTo
     }
 
     private void updateCase(SscsCaseData caseData, Long caseId, EventType eventType, String summary, String description) {
-        try {
-            ccdService.updateCase(caseData, caseId, eventType.getCcdType(), summary, description, idamService.getIdamTokens());
-        } catch (FeignException e) {
-            log.error(
-                    "{}. CCD response: {}",
-                    String.format("Could not update event %s for case %d", eventType, caseId),
-                    // exception.contentUTF8() uses response body internally
-                    e.responseBody().isPresent() ? e.contentUTF8() : e.getMessage()
-            );
-            throw e;
-        }
+        ccdService.updateCase(caseData, caseId, eventType.getCcdType(), summary, description, idamService.getIdamTokens());
     }
 }
