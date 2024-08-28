0a6a132e7173b7527b1bfbe1db1086e93c7d9de6
NHAL22 <113360585+NHAL22@users.noreply.github.com>
2024-08-28 10:22:46 +0100

    add functional test - unfinished

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/evidenceshare/IssueDirectionFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/evidenceshare/IssueDirectionFunctionalTest.java
@@ -1,14 +1,18 @@
 package uk.gov.hmcts.reform.sscs.functional.evidenceshare;
 
 import static junit.framework.TestCase.assertNull;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.DIRECTION_ISSUED;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.NON_COMPLIANT;
+import static org.junit.Assert.assertTrue;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.*;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReviewState.REVIEW_BY_TCW;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
+
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.apache.groovy.json.internal.IO;
 import org.junit.Test;
-import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
-import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
 
 public class IssueDirectionFunctionalTest extends AbstractFunctionalTest {
 
@@ -32,5 +36,39 @@ public class IssueDirectionFunctionalTest extends AbstractFunctionalTest {
         SscsCaseData caseData = caseDetails.getData();
 
         assertNull(caseData.getDirectionTypeDl());
+    }
+
+    @Test
+    public void processAnIssueDirectionEvent_ifPastHearingExcludedDatesAreOnCaseDetails() throws IOException {
+    //set case to have past hearing excluded dates
+        //set case to be in appeal to proceed
+        //set case to be in interloc review state
+
+        //assert that issue directions event is created.
+        SscsCaseDetails caseDetails = createDigitalCaseWithEvent(NON_COMPLIANT);
+        SscsCaseData caseData = caseDetails.getData();
+
+        caseData.getAppeal().setHearingOptions(HearingOptions.builder()
+                        .excludeDates(List.of(
+                                ExcludeDate.builder().value(new DateRange("2024-04-03", "2024-05-04")).build()
+                        )).build());
+
+        caseData.setInterlocReviewState(REVIEW_BY_TCW);
+
+//        caseData.setDirectionTypeDl(APPEAL_TO_PROCEED);
+
+        updateCaseEvent(UPDATE_CASE_ONLY, caseDetails);
+
+        String json = getJson(DIRECTION_ISSUED.getCcdType());
+        json = json.replace("CASE_ID_TO_BE_REPLACED", ccdCaseId);
+        json = json.replace("MRN_DATE_TO_BE_REPLACED", LocalDate.now().toString());
+
+        simulateCcdCallback(json);
+
+        caseDetails = findCaseById(ccdCaseId); //get the case again to validate
+        caseData = caseDetails.getData();
+
+        assertTrue(caseData.getEvents().stream().anyMatch(event -> event.getValue().getEventType().equals(DIRECTION_ISSUED)));
+
     }
 }
