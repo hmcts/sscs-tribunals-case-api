a0b75ca9f1fd917e102c57f35f1258629168f1f0
Gemma Talbot <gemma.talbot@solirius.com>
2024-09-23 22:40:22 +0100

    checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/handlers/decisionissued/DecisionIssuedAboutToSubmitHandlerTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/handlers/decisionissued/DecisionIssuedAboutToSubmitHandlerTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.sscs.ccd.domain.DwpState;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.functional.handlers.BaseHandler;
 
