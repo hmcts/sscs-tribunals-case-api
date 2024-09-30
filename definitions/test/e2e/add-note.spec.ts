bede1cc522be5b8b083e1edef9fef11372a80a1a
Linus Norton <linusnorton@gmail.com>
2024-09-30 08:52:13 +0100

    run preview tests

--- definitions/test/e2e/add-note.spec.ts
+++ definitions/test/e2e/add-note.spec.ts
@@ -4,7 +4,7 @@ import performAppealDormantOnCase from "../api/client/sscs/appeal.event";
 
 let caseId: string;
 
-test.beforeAll("Case has to be Created", {tag:'@danny'}, async () => {
+test.beforeAll("Case has to be Created", async () => {
     caseId = await createCaseBasedOnCaseType('PIP');
 });
 
