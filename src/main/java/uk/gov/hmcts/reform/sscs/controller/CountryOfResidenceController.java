534048de7ae39c962c19a3e842f5b2cc19e30f2e
nilay <nilay.patel@hmcts.net>
2024-10-07 18:46:29 +0100

    exposing endpoint to access country of residence enum from commons from frontend

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/controller/CountryOfResidenceController.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.sscs.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.sscs.ccd.domain.UkPortOfEntry;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/countries-of-residence")
+public class CountryOfResidenceController {
+
+    @GetMapping
+    public List<Map<String, String>> getCountryOfResidence() {
+        return Arrays.stream(CountryOfResidence.values())
+            .map(enumVal -> Map.of(
+                "label", enumVal.getLabel(),
+                "officialName", enumVal.getOfficialName(),
+                "value", enumVal.getValue()))
+            .collect(Collectors.toList());
+    }
+}
