4ab49a81b76ce63583c26839c03162fdc9a75cc0
nilay <nilay.patel@hmcts.net>
2024-10-08 16:22:46 +0100

    fixing and making adding another ingress for iba so no reliance on github label

--- src/main/java/uk/gov/hmcts/reform/sscs/controller/PortOfEntryController.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/controller/PortOfEntryController.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.sscs.controller;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.hmcts.reform.sscs.ccd.domain.UkPortOfEntry;
@@ -19,6 +20,6 @@ public class PortOfEntryController {
                 "label", enumVal.getLabel(),
                 "trafficType", enumVal.getTrafficType(),
                 "locationCode", enumVal.getLocationCode()))
-            .toList();
+            .collect(Collectors.toList());
     }
 }
