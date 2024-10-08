8d59ec900524e72a61c29af5fa53bd6440d5da09
nilay <nilay.patel@hmcts.net>
2024-10-08 12:36:31 +0100

    pointing to new sscs-common release and fixing checkstyle issues

--- src/test/java/uk/gov/hmcts/reform/sscs/controller/PortOfEntryControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/controller/PortOfEntryControllerTest.java
@@ -1,13 +1,12 @@
 package uk.gov.hmcts.reform.sscs.controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import uk.gov.hmcts.reform.sscs.ccd.domain.UkPortOfEntry;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.sscs.ccd.domain.UkPortOfEntry;
 
 class PortOfEntryControllerTest {
 
