96c2988b6771b2ed8e08b087a40c126c0a889b44
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2024-10-11 17:24:24 +0100

    SSCSSI-253: testing deprecated3

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/PreSubmitCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/PreSubmitCallbackHandler.java
@@ -7,6 +7,9 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.CaseData;
 
 public interface PreSubmitCallbackHandler<T extends CaseData> {
 
+    /**
+     * @deprecated (since = "10/2024 - some comment use something else)
+     */
     @Deprecated(since = "10/2024 - some comment")
     boolean canHandle(CallbackType callbackType, Callback<T> callback);
 
