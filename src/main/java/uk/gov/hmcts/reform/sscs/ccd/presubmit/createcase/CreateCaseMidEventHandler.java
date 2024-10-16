aa387056375842813e3775b30b40d866abd99809
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-10-16 20:12:29 +0100

    SSCSCI-1128: Update mid event to ensure validation only happens for ibca cases

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createcase/CreateCaseMidEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createcase/CreateCaseMidEventHandler.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.createcase;
 import static java.util.Objects.isNull;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.NO;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.YES;
+import static uk.gov.hmcts.reform.sscs.model.AppConstants.IBCA_BENEFIT_CODE;
 
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
@@ -37,28 +39,37 @@ public class CreateCaseMidEventHandler implements PreSubmitCallbackHandler<SscsC
         SscsCaseData caseData = callback.getCaseDetails().getCaseData();
         PreSubmitCallbackResponse<SscsCaseData> errorResponse = new PreSubmitCallbackResponse<>(caseData);
 
-        if (NO.equals(caseData.getAppeal().getAppellant().getAddress().getIsInUk())) {
-            final String selectedPortOfEntryLocationCode = caseData.getAppeal().getAppellant().getAddress().getUkPortOfEntryList().getValue().getCode();
-            caseData.getAppeal().getAppellant().getAddress().setPortOfEntry(selectedPortOfEntryLocationCode);
-        } else {
-            validateAddressAndPostcode(errorResponse, caseData.getAppeal().getAppellant());
+        if (IBCA_BENEFIT_CODE.equals(caseData.getBenefitCode())) {
+            if (NO.equals(caseData.getAppeal().getAppellant().getAddress().getIsInUk())) {
+                final String selectedPortOfEntryLocationCode = caseData.getAppeal().getAppellant().getAddress().getUkPortOfEntryList().getValue().getCode();
+                caseData.getAppeal().getAppellant().getAddress().setPortOfEntry(selectedPortOfEntryLocationCode);
+            }
+            validateAddress(errorResponse, caseData.getAppeal().getAppellant());
+
+            if (YES.equals(caseData.getHasRepresentative())
+                    && isNull(caseData.getAppeal().getRep().getAddress())
+                    && isNull(caseData.getAppeal().getRep().getAddress().getIsInUk())) {
+                errorResponse.addError("You must enter Living in the UK for the representative");
+            }
         }
 
         return errorResponse;
     }
 
-    private void validateAddressAndPostcode(PreSubmitCallbackResponse<SscsCaseData> response, Entity party) {
-        if (isNull(party.getAddress())) {
+    private void validateAddress(PreSubmitCallbackResponse<SscsCaseData> response, Entity party) {
+        String addressLine1 = party.getAddress().getLine1();
+        String postcode = party.getAddress().getPostcode();
+        String country = party.getAddress().getCountry();
+
+        if (isBlank(addressLine1)) {
             response.addError("You must enter address line 1 for the appellant");
-            response.addError("You must enter a valid UK postcode for the appellant");
-        } else {
-            String addressLine1 = party.getAddress().getLine1();
-            String postcode = party.getAddress().getPostcode();
+        }
 
-            if (isBlank(addressLine1)) {
-                response.addError("You must enter address line 1 for the appellant");
+        if (NO.equals(party.getAddress().getIsInUk())) {
+            if (isBlank(country)) {
+                response.addError("You must enter a valid country for the appellant");
             }
-
+        } else {
             if (isBlank(postcode) || !postcodeValidator.isValid(postcode, null)) {
                 response.addError("You must enter a valid UK postcode for the appellant");
             }
