975b9799bc09408ccd943f8173f8fe3ac096010c
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2024-10-17 17:50:38 +0100

    SSCSCI-1128: Address issues/comments

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createcase/CreateCaseMidEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createcase/CreateCaseMidEventHandler.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.createcase;
 
 import static java.util.Objects.isNull;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.NO;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.YES;
 import static uk.gov.hmcts.reform.sscs.model.AppConstants.IBCA_BENEFIT_CODE;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
@@ -44,10 +48,11 @@ public class CreateCaseMidEventHandler implements PreSubmitCallbackHandler<SscsC
                 final String selectedPortOfEntryLocationCode = caseData.getAppeal().getAppellant().getAddress().getUkPortOfEntryList().getValue().getCode();
                 caseData.getAppeal().getAppellant().getAddress().setPortOfEntry(selectedPortOfEntryLocationCode);
             }
-            validateAddress(errorResponse, caseData.getAppeal().getAppellant());
+
+            errorResponse.addErrors(validateAddress(caseData.getAppeal().getAppellant()));
 
             if (YES.equals(caseData.getHasRepresentative())
-                    && isNull(caseData.getAppeal().getRep().getAddress())
+                    && isNotEmpty(caseData.getAppeal().getRep().getAddress())
                     && isNull(caseData.getAppeal().getRep().getAddress().getIsInUk())) {
                 errorResponse.addError("You must enter Living in the UK for the representative");
             }
@@ -56,23 +61,26 @@ public class CreateCaseMidEventHandler implements PreSubmitCallbackHandler<SscsC
         return errorResponse;
     }
 
-    private void validateAddress(PreSubmitCallbackResponse<SscsCaseData> response, Entity party) {
+    private Collection<String> validateAddress(Entity party) {
+        Set<String> validationErrors = new HashSet<>();
         String addressLine1 = party.getAddress().getLine1();
         String postcode = party.getAddress().getPostcode();
         String country = party.getAddress().getCountry();
 
         if (isBlank(addressLine1)) {
-            response.addError("You must enter address line 1 for the appellant");
+            validationErrors.add("You must enter address line 1 for the appellant");
         }
 
         if (NO.equals(party.getAddress().getIsInUk())) {
             if (isBlank(country)) {
-                response.addError("You must enter a valid country for the appellant");
+                validationErrors.add("You must enter a valid country for the appellant");
             }
         } else {
             if (isBlank(postcode) || !postcodeValidator.isValid(postcode, null)) {
-                response.addError("You must enter a valid UK postcode for the appellant");
+                validationErrors.add("You must enter a valid UK postcode for the appellant");
             }
         }
+
+        return validationErrors;
     }
 }
