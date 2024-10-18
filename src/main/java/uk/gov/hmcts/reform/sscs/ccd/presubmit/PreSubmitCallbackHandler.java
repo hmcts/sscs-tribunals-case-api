dd2ffb181e92041317a5d8c22383548df3db46a2
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2024-10-18 09:11:44 +0100

    SSCSSI-253: reverted all

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/PreSubmitCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/PreSubmitCallbackHandler.java
@@ -5,17 +5,8 @@ import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseData;
 
-/**
- * Example deprecated.
- */
 public interface PreSubmitCallbackHandler<T extends CaseData> {
 
-    /**
-     * Some description here.
-     * @deprecated (since = "10/2024 - some comment use something else)
-     */
-    @Deprecated(since = "10/2024 - some comment", forRemoval = true)
-    @SuppressWarnings(value = "deprecated")
     boolean canHandle(CallbackType callbackType, Callback<T> callback);
 
     PreSubmitCallbackResponse<T> handle(CallbackType callbackType, Callback<T> callback, String userAuthorisation);
