53492f75215a2f299e8579d96e6f091fc5884f14
hashimalisolirius <HashimAli@HMCTS.NET>
2024-10-28 12:23:26 +0000

    canhandle check in handle method

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/addhearingoutcome/AddHearingOutcomeAboutToStartHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/addhearingoutcome/AddHearingOutcomeAboutToStartHandler.java
@@ -43,6 +43,9 @@ public class AddHearingOutcomeAboutToStartHandler implements PreSubmitCallbackHa
     public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType,
                                                           Callback<SscsCaseData> callback,
                                                           String userAuthorisation) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
         final CaseDetails<SscsCaseData> caseDetails = callback.getCaseDetails();
         final SscsCaseData sscsCaseData = caseDetails.getCaseData();
         PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
