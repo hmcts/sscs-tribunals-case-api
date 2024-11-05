21eb74bb4c090d8d6ec0d6377f12c0e709d83f08
nilay <nilay.patel@hmcts.net>
2024-11-05 13:36:15 +0000

    changing error message depending on IBCA or not

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/fenoaction/FeNoActionAboutToStartHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/furtherevidence/fenoaction/FeNoActionAboutToStartHandlerTest.java
@@ -2,7 +2,10 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.furtherevidence.fenoaction;
 
 import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.DwpState.FE_RECEIVED;
+import static uk.gov.hmcts.reform.sscs.model.AppConstants.IBCA_BENEFIT_CODE;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,7 +15,12 @@ import junitparams.Parameters;
 import junitparams.converters.Nullable;
 import net.javacrumbs.jsonunit.core.Option;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
@@ -24,6 +32,9 @@ public class FeNoActionAboutToStartHandlerTest extends BaseHandlerTest {
 
     private FeNoActionAboutToStartHandler handler = new FeNoActionAboutToStartHandler();
     private static final String FE_NO_ACTION_CALLBACK_JSON = "fenoaction/feNoActionAboutToStartCallback.json";
+    @Mock private Callback<SscsCaseData> mockCallback;
+    @Mock private CaseDetails<SscsCaseData> mockCaseDetails;
+    @Mock private SscsCaseData mockCaseData;
 
     @Test
     @Parameters({
@@ -80,4 +91,31 @@ public class FeNoActionAboutToStartHandlerTest extends BaseHandlerTest {
             "appellantEvidence", "", FE_NO_ACTION_CALLBACK_JSON), USER_AUTHORISATION);
     }
 
+    @ParameterizedTest
+    @EnumSource(value = DwpState.class, mode = EnumSource.Mode.EXCLUDE, names = {"FE_RECEIVED"})
+    public void returns_dwp_error_for_not_FE_received_non_ibca(DwpState dwpState) {
+        MockitoAnnotations.openMocks(this);
+        when(mockCallback.getEvent()).thenReturn(EventType.FE_NO_ACTION);
+        when(mockCallback.getCaseDetails()).thenReturn(mockCaseDetails);
+        when(mockCaseDetails.getCaseData()).thenReturn(mockCaseData);
+        when(mockCaseData.getDwpState()).thenReturn(dwpState);
+        when(mockCaseData.getBenefitCode()).thenReturn("000");
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(CallbackType.ABOUT_TO_START, mockCallback, USER_AUTHORISATION);
+        assertEquals(1, response.getErrors().size());
+        assertTrue(response.getErrors().contains("The dwp state value has to be 'FE received' in order to run this event"));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = DwpState.class, mode = EnumSource.Mode.EXCLUDE, names = {"FE_RECEIVED"})
+    public void returns_fta_error_for_not_FE_received_non_ibca(DwpState dwpState) {
+        MockitoAnnotations.openMocks(this);
+        when(mockCallback.getEvent()).thenReturn(EventType.FE_NO_ACTION);
+        when(mockCallback.getCaseDetails()).thenReturn(mockCaseDetails);
+        when(mockCaseDetails.getCaseData()).thenReturn(mockCaseData);
+        when(mockCaseData.getDwpState()).thenReturn(dwpState);
+        when(mockCaseData.getBenefitCode()).thenReturn(IBCA_BENEFIT_CODE);
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(CallbackType.ABOUT_TO_START, mockCallback, USER_AUTHORISATION);
+        assertEquals(1, response.getErrors().size());
+        assertTrue(response.getErrors().contains("The FTA state value has to be 'FE received' in order to run this event"));
+    }
 }
