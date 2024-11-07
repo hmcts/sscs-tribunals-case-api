e38cd25595234f828667eb0c9b08dcdab55c60bc
SomeshRA <somesh.acharya1@hmcts.net>
2024-11-07 17:21:09 +0000

    SSCSSI-247: Implemented SSCSCI-878 on the V2 flow

--- src/main/java/uk/gov/hmcts/reform/sscs/transform/deserialize/SubmitYourAppealToCcdCaseDataDeserializer.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/transform/deserialize/SubmitYourAppealToCcdCaseDataDeserializer.java
@@ -55,6 +55,7 @@ public final class SubmitYourAppealToCcdCaseDataDeserializer {
             .toBuilder()
             .region(region)
             .regionalProcessingCenter(rpc)
+            .schedulingAndListingFields(SchedulingAndListingFields.builder().hearingRoute(rpc.getHearingRoute()).build())
             .build();
     }
 
