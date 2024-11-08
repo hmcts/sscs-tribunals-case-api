be1842a7f65266bf609ad9470bfd160f6aee21ae
Pettedson John <pettedson.john1@hmcts.net>
2024-11-08 15:29:00 +0000

    test:testing the delay in case creation and appeal dormant.

--- definitions/test/e2e/provide-appointee-details.spec.ts
+++ definitions/test/e2e/provide-appointee-details.spec.ts
@@ -17,6 +17,7 @@ test.describe('Provide No Appointee Details on a Dormant Case',  {tag: '@nightly
     let caseId: string;
     test.beforeAll("Case has to be Created", async () => {
         caseId = await createCaseBasedOnCaseType('PIP');
+        await new Promise(f => setTimeout(f, 10000)); //Delay required for the Case to be ready
         await performAppealDormantOnCase(caseId);
     });
 
@@ -29,6 +30,7 @@ test.describe('Provide Appointee Details on a Dormant Case', {tag: ['@preview-re
     let caseId: string;
     test.beforeAll("Case has to be Created", async () => {
         caseId = await createCaseBasedOnCaseType('PIP');
+        await new Promise(f => setTimeout(f, 10000)); //Delay required for the Case to be ready
         await performAppealDormantOnCase(caseId);
     });
 
