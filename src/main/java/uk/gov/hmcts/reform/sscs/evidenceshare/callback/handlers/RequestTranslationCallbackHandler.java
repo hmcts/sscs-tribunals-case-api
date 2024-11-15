60991be3e5aa46cc17742beee60aecf62ed99b94
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2024-11-15 16:51:41 +0000

    SSCSSI-421: corrected translation reuested

--- src/main/java/uk/gov/hmcts/reform/sscs/evidenceshare/callback/handlers/RequestTranslationCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/evidenceshare/callback/handlers/RequestTranslationCallbackHandler.java
@@ -5,17 +5,22 @@ import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.CASE_UPDATED;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.REQUEST_TRANSLATION_FROM_WLU;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.ListUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.callback.CallbackHandler;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.DispatchPriority;
+import uk.gov.hmcts.reform.sscs.ccd.domain.AbstractDocument;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocumentTranslationStatus;
 import uk.gov.hmcts.reform.sscs.ccd.service.UpdateCcdCaseService;
 import uk.gov.hmcts.reform.sscs.evidenceshare.exception.WelshException;
 import uk.gov.hmcts.reform.sscs.evidenceshare.service.RequestTranslationService;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -57,15 +62,24 @@ public class RequestTranslationCallbackHandler implements CallbackHandler<SscsCa
         try {
             log.info("sending email for case  id {}", callback.getCaseDetails().getId());
             if (requestTranslationService.sendCaseToWlu(callback.getCaseDetails()) && callback.getEvent() == REQUEST_TRANSLATION_FROM_WLU) {
-                updateCcdCaseService.triggerCaseEventV2(Long.valueOf(callback.getCaseDetails().getCaseData().getCcdCaseId()),
+                updateCcdCaseService.updateCaseV2(Long.valueOf(callback.getCaseDetails().getCaseData().getCcdCaseId()),
                     CASE_UPDATED.getCcdType(), "Case translations sent to wlu", "Updated case with date sent to wlu",
-                    idamService.getIdamTokens());
+                    idamService.getIdamTokens(), sscsCaseDetails -> {
+                        updateDocumentStatus(sscsCaseDetails.getData().getSscsDocument());
+                        updateDocumentStatus(sscsCaseDetails.getData().getDwpDocuments());
+                        });
             }
         } catch (WelshException e) {
             log.error("Error when sending to request translation from wlu: {}", callback.getCaseDetails().getId(), e);
         }
     }
 
+    private void updateDocumentStatus(List<? extends AbstractDocument> docs) {
+    ListUtils.emptyIfNull(docs).stream().filter(doc -> SscsDocumentTranslationStatus.TRANSLATION_REQUIRED.equals(doc.getValue().getDocumentTranslationStatus()))
+            .forEach(doc -> {
+                doc.getValue().setDocumentTranslationStatus(SscsDocumentTranslationStatus.TRANSLATION_REQUESTED);
+            });
+    }
 
     @Override
     public DispatchPriority getPriority() {
