4e671908f187b49635436dda9a2c24e56cdb3392
nilay <nilay.patel@hmcts.net>
2024-11-21 12:55:52 +0000

    fixing potential npe

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createcase/CreateCaseMidEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/createcase/CreateCaseMidEventHandler.java
@@ -50,7 +50,7 @@ public class CreateCaseMidEventHandler implements PreSubmitCallbackHandler<SscsC
             caseData.getAppeal().getAppellant().getAddress().setPortOfEntry(selectedPortOfEntryLocationCode);
         }
 
-        if (callback.getEvent() == EventType.CASE_UPDATED && caseData.getRegionalProcessingCenter() != null && caseData.getRegionalProcessingCenter().getHearingRoute().equals(HearingRoute.GAPS)) {
+        if (callback.getEvent() == EventType.CASE_UPDATED && caseData.getRegionalProcessingCenter() != null && HearingRoute.GAPS.equals(caseData.getRegionalProcessingCenter().getHearingRoute())) {
             errorResponse.addError(HEARING_ROUTE_ERROR_MESSAGE);
         }
 
