292e35ed391e3a8cccd7a1997d3adb15898e0586
nitinprabhuhmcts <nitin.prabhu2@hmcts.net>
2024-12-03 16:26:17 +0000

    Added remove case outcome event for superusers

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/removecaseoutcome/RemoveCaseOutcomeAboutToSubmitHandlerTest.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.removecaseoutcome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.YES;
+
+@RunWith(SpringRunner.class)
+public class RemoveCaseOutcomeAboutToSubmitHandlerTest {
+
+    private static final String USER_AUTHORISATION = "Bearer token";
+
+    @InjectMocks
+    RemoveCaseOutcomeAboutToSubmitHandler handler;
+
+    @Mock
+    private Callback<SscsCaseData> callback;
+
+    @Mock
+    private CaseDetails<SscsCaseData> caseDetails;
+
+    @Test
+    public void givenRemoveOutcomeAboutToSubmitIsTriggered_thenOldCaseOutcomeAndDidPOAttendedFieldsAreRemoved() {
+        var  sscsCaseData = SscsCaseData.builder()
+                .ccdCaseId("ccdId")
+                .appeal(Appeal.builder().hearingOptions(HearingOptions.builder().build()).build())
+                .caseOutcome(CaseOutcome.builder().caseOutcome("in favour").didPoAttend(YES).build())
+                .build();
+
+        when(callback.getEvent()).thenReturn(EventType.REMOVE_CASE_OUTCOME);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+
+        PreSubmitCallbackResponse<SscsCaseData> response =
+                handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        assertThat(response.getData().getCaseOutcome()).hasAllNullFieldsOrProperties();
+
+    }
+}
