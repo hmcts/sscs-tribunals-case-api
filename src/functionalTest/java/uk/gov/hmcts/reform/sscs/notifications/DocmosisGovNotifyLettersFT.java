be6baed2b892f4bc1a4c68b976ec89798835d14d
taleb <tech@benouaer.com>
2024-12-05 11:11:29 +0000

    run as one test to manage timeout

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/notifications/DocmosisGovNotifyLettersFT.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/notifications/DocmosisGovNotifyLettersFT.java
@@ -2,15 +2,19 @@ package uk.gov.hmcts.reform.sscs.notifications;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.sscs.tyanotifications.config.NotificationEventTypeLists.DOCMOSIS_LETTERS;
+import static uk.gov.hmcts.reform.sscs.tyanotifications.domain.notify.NotificationEventType.ACTION_HEARING_RECORDING_REQUEST;
+import static uk.gov.hmcts.reform.sscs.tyanotifications.domain.notify.NotificationEventType.HEARING_BOOKED;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import junitparams.JUnitParamsRunner;
 import lombok.SneakyThrows;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 import uk.gov.hmcts.reform.sscs.tyanotifications.domain.notify.NotificationEventType;
 import uk.gov.service.notify.Notification;
@@ -18,6 +22,9 @@ import uk.gov.service.notify.NotificationClientException;
 
 @RunWith(JUnitParamsRunner.class)
 public class DocmosisGovNotifyLettersFT extends AbstractNotificationsFT {
+
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(30*60);
 
     public DocmosisGovNotifyLettersFT() {
         super(30);
@@ -29,23 +36,27 @@ public class DocmosisGovNotifyLettersFT extends AbstractNotificationsFT {
     }
 
     @SneakyThrows
-    @ParameterizedTest
-    @MethodSource("notificationEventTypesProvider")
-    public void shouldSendDocmosisLettersViaGovNotify(NotificationEventType notificationEventType) {
-        try {
-            simulateCcdCallbackToSendLetter(notificationEventType);
-            List<Notification> notifications = fetchLetters();
-            saveLetterPdfs(notifications);
-            assertThat(notifications)
-                    .extracting(Notification::getSubject)
-                    .allSatisfy(subject -> assertThat(subject).isPresent());
+    @Test
+    public void shouldSendDocmosisLettersViaGovNotify() {
+        getNotificationList().forEach(notificationEventType -> {
+            try {
+                simulateCcdCallbackToSendLetter(notificationEventType);
+                List<Notification> notifications = fetchLetters();
+                saveLetterPdfs(notifications);
+                assertThat(notifications)
+                        .extracting(Notification::getSubject)
+                        .allSatisfy(subject -> assertThat(subject).isPresent());
 
-        } catch (IOException | NotificationClientException e) {
-            logFailedEventNotification(notificationEventType, e);
-        }
+            } catch (IOException | NotificationClientException e) {
+                logFailedEventNotification(notificationEventType, e);
+            }
+        });
     }
 
-    private static Set<NotificationEventType> notificationEventTypesProvider() {
-        return DOCMOSIS_LETTERS;
+    private static Set<NotificationEventType> getNotificationList() {
+        return DOCMOSIS_LETTERS.stream()
+                .filter(notificationEventType ->
+                        !List.of(ACTION_HEARING_RECORDING_REQUEST, HEARING_BOOKED).contains(notificationEventType))
+                .collect(Collectors.toSet());
     }
 }
