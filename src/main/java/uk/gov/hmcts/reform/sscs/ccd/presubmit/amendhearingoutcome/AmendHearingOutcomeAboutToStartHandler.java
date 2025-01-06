94fd632a2d31b3e9520f3a604021f83f65b4be0b
NHAL22 <113360585+NHAL22@users.noreply.github.com>
2025-01-06 15:38:23 +0000

    SSCSCI-1202: Amend hearing outcome 3 (#4185)
    * amend hearing outcome changes
    
    * upgrade commons version
    
    * Update Jenkinsfile_CNP
    
    * Update Jenkinsfile_CNP
    
    * update commons version
    
    * update commons tag
    
    ---------
    
    Co-authored-by: Arunabha Chowdhury <90389022+ArunabhaChowdhury@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeAboutToStartHandler.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.amendhearingoutcome;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+
+@Component
+@Slf4j
+public class AmendHearingOutcomeAboutToStartHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        return callbackType.equals(CallbackType.ABOUT_TO_START)
+                && callback.getEvent() == EventType.AMEND_HEARING_OUTCOME;
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback, String userAuthorisation) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+        final CaseDetails<SscsCaseData> caseDetails = callback.getCaseDetails();
+        final SscsCaseData sscsCaseData = caseDetails.getCaseData();
+
+        PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
+
+        if (sscsCaseData.getHearingOutcomes() == null) {
+            preSubmitCallbackResponse.addError("There are no hearing outcomes recorded on the case. Please add a hearing outcome using 'Add a Hearing Outcome' event");
+        }
+
+        return preSubmitCallbackResponse;
+    }
+}
