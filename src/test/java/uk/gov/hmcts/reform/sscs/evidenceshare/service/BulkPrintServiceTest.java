e98c3e2aabe4ea598220f7ee4a388b867787a315
taleb <tech@benouaer.com>
2025-01-07 10:35:22 +0000

    SSCSCI-1498: fix checkstyle issues

--- src/test/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/BulkPrintServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/BulkPrintServiceTest.java
@@ -29,11 +29,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
@@ -67,14 +65,14 @@ public class BulkPrintServiceTest {
     private static final UUID LETTER_ID = UUID.randomUUID();
 
     private static final SscsCaseData SSCS_CASE_DATA = SscsCaseData.builder()
-        .ccdCaseId("234")
-        .appeal(Appeal.builder().appellant(
-            Appellant.builder()
-                .name(Name.builder().firstName("Appellant").lastName("LastName").build())
-                .address(Address.builder().line1("line1").build())
-                .build())
-            .build())
-        .build();
+            .ccdCaseId("234")
+            .appeal(Appeal.builder().appellant(
+                            Appellant.builder()
+                                    .name(Name.builder().firstName("Appellant").lastName("LastName").build())
+                                    .address(Address.builder().line1("line1").build())
+                                    .build())
+                    .build())
+            .build();
     private static final String AUTH_TOKEN = "Auth_Token";
 
     private BulkPrintService bulkPrintService;
@@ -94,14 +92,14 @@ public class BulkPrintServiceTest {
     @BeforeEach
     public void setUp() {
         this.bulkPrintService = new BulkPrintService(sendLetterApi, idamService, bulkPrintServiceHelper,
-            true, 1, ccdNotificationService);
-       lenient().when(idamService.generateServiceAuthorization()).thenReturn(AUTH_TOKEN);
+                true, 1, ccdNotificationService);
+        lenient().when(idamService.generateServiceAuthorization()).thenReturn(AUTH_TOKEN);
     }
 
     @Test
     public void willSendToBulkPrint() {
         when(sendLetterApi.sendLetter(eq(AUTH_TOKEN), captor.capture()))
-            .thenReturn(new SendLetterResponse(LETTER_ID));
+                .thenReturn(new SendLetterResponse(LETTER_ID));
         Optional<UUID> letterIdOptional = bulkPrintService.sendToBulkPrint(PDF_LIST, SSCS_CASE_DATA, null);
         assertEquals("letterIds must be equal", Optional.of(LETTER_ID), letterIdOptional);
         assertEquals("sscs-data-pack", captor.getValue().getAdditionalData().get("letterType"));
@@ -112,7 +110,7 @@ public class BulkPrintServiceTest {
     @Test
     public void willSendToBulkPrintWithAdditionalData() {
         when(sendLetterApi.sendLetter(eq(AUTH_TOKEN), any(LetterWithPdfsRequest.class)))
-            .thenReturn(new SendLetterResponse(LETTER_ID));
+                .thenReturn(new SendLetterResponse(LETTER_ID));
         Optional<UUID> letterIdOptional = bulkPrintService.sendToBulkPrint(PDF_LIST, SSCS_CASE_DATA, null);
         assertEquals("letterIds must be equal", Optional.of(LETTER_ID), letterIdOptional);
     }
@@ -123,9 +121,9 @@ public class BulkPrintServiceTest {
         Name name1 = Name.builder().firstName("Barry").lastName("Allen").build();
         Address address1 = Address.builder().line1("line1").build();
         Appointee appointee1 = Appointee.builder()
-            .name(name1)
-            .address(address1)
-            .build();
+                .name(name1)
+                .address(address1)
+                .build();
 
         Appellant appellant = SSCS_CASE_DATA.getAppeal().getAppellant();
         appellant.setAppointee(appointee1);
@@ -133,77 +131,77 @@ public class BulkPrintServiceTest {
 
         Name name2 = Name.builder().firstName("Jay").lastName("Garrick").build();
         JointParty jointParty = JointParty.builder()
-            .hasJointParty(YES)
-            .name(name2)
-            .build();
+                .hasJointParty(YES)
+                .name(name2)
+                .build();
         SSCS_CASE_DATA.setJointParty(jointParty);
 
         Name name3 = Name.builder().firstName("Wally").lastName("West").build();
         Representative representative = Representative.builder()
-            .hasRepresentative("YES")
-            .name(name3)
-            .build();
+                .hasRepresentative("YES")
+                .name(name3)
+                .build();
         SSCS_CASE_DATA.getAppeal().setRep(representative);
 
         Name name4 = Name.builder().firstName("Hunter").lastName("Zolomon").build();
         CcdValue<OtherParty> otherParty1 = CcdValue.<OtherParty>builder()
-            .value(OtherParty.builder()
-                .id("1")
-                .name(name4)
-                .isAppointee(NO.getValue())
-                .build())
-            .build();
+                .value(OtherParty.builder()
+                        .id("1")
+                        .name(name4)
+                        .isAppointee(NO.getValue())
+                        .build())
+                .build();
 
         Name name5 = Name.builder().firstName("Jessie").lastName("Quick").build();
         Name name6 = Name.builder().firstName("Max").lastName("Mercury").build();
         Representative representative1 = Representative.builder()
-            .hasRepresentative("YES")
-            .name(name6)
-            .build();
+                .hasRepresentative("YES")
+                .name(name6)
+                .build();
         CcdValue<OtherParty> otherParty2 = CcdValue.<OtherParty>builder()
-            .value(OtherParty.builder()
-                .id("2")
-                .name(name5)
-                .isAppointee(NO.getValue())
-                .rep(representative1)
-                .build())
-            .build();
+                .value(OtherParty.builder()
+                        .id("2")
+                        .name(name5)
+                        .isAppointee(NO.getValue())
+                        .rep(representative1)
+                        .build())
+                .build();
 
         Name name7 = Name.builder().firstName("Caitlin").lastName("Snow").build();
         Name name8 = Name.builder().firstName("Cisco").lastName("Ramone").build();
         Appointee appointee2 = Appointee.builder()
-            .name(name8)
-            .address(address1)
-            .build();
+                .name(name8)
+                .address(address1)
+                .build();
         CcdValue<OtherParty> otherParty3 = CcdValue.<OtherParty>builder()
-            .value(OtherParty.builder()
-                .id("3")
-                .name(name7)
-                .isAppointee(YES.getValue())
-                .appointee(appointee2)
-                .build())
-            .build();
+                .value(OtherParty.builder()
+                        .id("3")
+                        .name(name7)
+                        .isAppointee(YES.getValue())
+                        .appointee(appointee2)
+                        .build())
+                .build();
 
         Name name9 = Name.builder().firstName("Harrison").lastName("Wells").build();
         Name name10 = Name.builder().firstName("Eddie").lastName("Thawne").build();
         Name name11 = Name.builder().firstName("Eobard").lastName("Thawne").build();
         Representative representative2 = Representative.builder()
-            .hasRepresentative("YES")
-            .name(name10)
-            .build();
+                .hasRepresentative("YES")
+                .name(name10)
+                .build();
         Appointee appointee3 = Appointee.builder()
-            .name(name11)
-            .address(address1)
-            .build();
+                .name(name11)
+                .address(address1)
+                .build();
         CcdValue<OtherParty> otherParty4 = CcdValue.<OtherParty>builder()
-            .value(OtherParty.builder()
-                .id("4")
-                .name(name9)
-                .rep(representative2)
-                .isAppointee(YES.getValue())
-                .appointee(appointee3)
-                .build())
-            .build();
+                .value(OtherParty.builder()
+                        .id("4")
+                        .name(name9)
+                        .rep(representative2)
+                        .isAppointee(YES.getValue())
+                        .appointee(appointee3)
+                        .build())
+                .build();
 
         SSCS_CASE_DATA.setOtherParties(Arrays.asList(otherParty1, otherParty2, otherParty3, otherParty4));
         bulkPrintService.sendToBulkPrint(PDF_LIST, SSCS_CASE_DATA, "Appellant LastName");
@@ -216,7 +214,7 @@ public class BulkPrintServiceTest {
     @Test
     public void willThrowAnyExceptionsToBulkPrint() {
         when(sendLetterApi.sendLetter(eq(AUTH_TOKEN), any(LetterWithPdfsRequest.class)))
-            .thenThrow(new RuntimeException("error"));
+                .thenThrow(new RuntimeException("error"));
 
         assertThrows(BulkPrintException.class, () -> {
             bulkPrintService.sendToBulkPrint(PDF_LIST, SSCS_CASE_DATA, null);
@@ -226,7 +224,7 @@ public class BulkPrintServiceTest {
     @Test
     public void shouldThrowANonPdfBulkPrintExceptionOnHttpClientErrorExceptionFromBulkPrint() {
         when(sendLetterApi.sendLetter(eq(AUTH_TOKEN), any(LetterWithPdfsRequest.class)))
-            .thenThrow(new HttpClientErrorException(HttpStatus.valueOf(400)));
+                .thenThrow(new HttpClientErrorException(HttpStatus.valueOf(400)));
 
         assertThrows(NonPdfBulkPrintException.class, () -> {
             bulkPrintService.sendToBulkPrint(PDF_LIST, SSCS_CASE_DATA, null);
@@ -246,9 +244,9 @@ public class BulkPrintServiceTest {
         this.bulkPrintService = new BulkPrintService(sendLetterApi, idamService, bulkPrintServiceHelper, true, 1, ccdNotificationService);
 
         SSCS_CASE_DATA.setReasonableAdjustments(ReasonableAdjustments.builder()
-            .appellant(ReasonableAdjustmentDetails.builder()
-                .wantsReasonableAdjustment(YesNo.YES).reasonableAdjustmentRequirements("Big text")
-                .build()).build());
+                .appellant(ReasonableAdjustmentDetails.builder()
+                        .wantsReasonableAdjustment(YesNo.YES).reasonableAdjustmentRequirements("Big text")
+                        .build()).build());
 
         when(bulkPrintServiceHelper.sendForReasonableAdjustment(SSCS_CASE_DATA, APPELLANT_LETTER)).thenReturn(true);
         bulkPrintService.sendToBulkPrint(PDF_LIST, SSCS_CASE_DATA, APPELLANT_LETTER, ISSUE_FURTHER_EVIDENCE, null);
