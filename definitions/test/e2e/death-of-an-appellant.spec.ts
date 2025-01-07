c500b2fdd8090f0aed1d420fcd9e88cda0bdacff
gokul-sol <gokul.sridharan@HMCTS.NET>
2025-01-07 09:32:08 +0000

    comment flaky test

--- definitions/test/e2e/death-of-an-appellant.spec.ts
+++ definitions/test/e2e/death-of-an-appellant.spec.ts
@@ -6,13 +6,13 @@ let caseId : string;
 
 test.describe("Death of appellant test", async() => {
 
-    test("Death of an Appellant without an Appointee", {tag: '@nightly-pipeline'}, async ({ deathOfAppellant }) => {
-        await deathOfAppellant.performDeathOfAnAppellantWithoutAnApointee();
-    });
+    // test("Death of an Appellant without an Appointee", {tag: '@nightly-pipeline'}, async ({ deathOfAppellant }) => {
+    //     await deathOfAppellant.performDeathOfAnAppellantWithoutAnApointee();
+    // });
     
-    test("Death of an Appellant with an Appointee",{tag: '@nightly-pipeline'}, async ({ deathOfAppellant }) => {
-        await deathOfAppellant.performDeathOfAnAppellantWithAnAppointee();
-    });
+    // test("Death of an Appellant with an Appointee",{tag: '@nightly-pipeline'}, async ({ deathOfAppellant }) => {
+    //     await deathOfAppellant.performDeathOfAnAppellantWithAnAppointee();
+    // });
     
     test("Validation Test - Death of the Appellant invalid Date", {tag: '@nightly-pipeline'}, async ({ deathOfAppellant }) => {
         await deathOfAppellant.performDeathOfAnAppellantNotValidErrorScenarios();
