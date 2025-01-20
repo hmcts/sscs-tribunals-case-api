335fa6e75a81e9e331847f82dfaf5e306180858a
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2025-01-20 09:34:49 +0000

    SSCSSI-450: removed some unused classes3

--- src/test/java/uk/gov/hmcts/reform/sscs/controller/PostCodeControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/controller/PostCodeControllerTest.java
@@ -2,11 +2,11 @@ package uk.gov.hmcts.reform.sscs.controller;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.sscs.service.AirLookupService;
@@ -14,11 +14,11 @@ import uk.gov.hmcts.reform.sscs.service.AirLookupService;
 public class PostCodeControllerTest {
     PostCodeController postCodeController;
 
-    @Mock
     private AirLookupService airLookupServiceMock;
 
     @Before
     public void setUp() {
+        airLookupServiceMock = mock(AirLookupService.class);
         postCodeController = new PostCodeController(airLookupServiceMock);
     }
 
