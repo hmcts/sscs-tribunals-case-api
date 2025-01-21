c25535cb9d58124bf3d48e7becd6c42640b0b96b
Gemma Talbot <gemma.talbot@solirius.com>
2025-01-21 15:17:02 +0000

    save hearing details

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeAboutToSubmitHandler.java
@@ -0,0 +1,95 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.amendhearingoutcome;
+
+import static java.util.Objects.requireNonNull;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Hearing;
+import uk.gov.hmcts.reform.sscs.ccd.domain.HearingDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.HearingOutcome;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.sscs.service.HearingOutcomeService;
+import uk.gov.hmcts.reform.sscs.service.HmcHearingsApiService;
+
+import java.util.List;
+
+@Component
+@Slf4j
+public class AmendHearingOutcomeAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    private final HmcHearingsApiService hmcHearingsApiService;
+    private final HearingOutcomeService hearingOutcomeService;
+
+    public AmendHearingOutcomeAboutToSubmitHandler(HmcHearingsApiService hmcHearingsApiService, HearingOutcomeService hearingOutcomeService) {
+        this.hmcHearingsApiService = hmcHearingsApiService;
+        this.hearingOutcomeService = hearingOutcomeService;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        requireNonNull(callback, "callback must not be null");
+        requireNonNull(callbackType, "callbackType must not be null");
+
+        return callbackType.equals(CallbackType.ABOUT_TO_SUBMIT)
+                && callback.getEvent().equals(EventType.AMEND_HEARING_OUTCOME);
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback, String userAuthorisation) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        SscsCaseData sscsCaseData = callback.getCaseDetails().getCaseData();
+
+        log.info("Amending Hearing Outcome for case ID:{}", callback.getCaseDetails().getId());
+
+        PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
+
+        List<Hearing> hearingList = sscsCaseData.getCompletedHearingsList();
+
+        for (int i=0; i<hearingList.size(); i++) {
+            HearingOutcome hearingOutcome = sscsCaseData.getHearingOutcomes().get(i);
+
+            String selectedHearingId =  hearingOutcome.getValue().getCompletedHearings().getValue().getCode();
+
+            if (selectedHearingId != hearingOutcome.getValue().getCompletedHearingId()) {
+                HearingDetails selectedHearingDetails = sscsCaseData.getCompletedHearingsList().stream()
+                    .filter(hearing -> hearing.getValue().getHearingId().equalsIgnoreCase(selectedHearingId))
+                    .findFirst().orElse(Hearing.builder().build()).getValue();
+
+                hearingOutcome.getValue().setCompletedHearingId(selectedHearingDetails.getHearingId());
+                hearingOutcome.getValue().setHearingStartDateTime(selectedHearingDetails.getStart());
+                hearingOutcome.getValue().setHearingEndDateTime(selectedHearingDetails.getEnd());
+                hearingOutcome.getValue().setHearingChannelId(selectedHearingDetails.getHearingChannel());
+                hearingOutcome.getValue().setVenue(selectedHearingDetails.getVenue());
+                hearingOutcome.getValue().setEpimsId(selectedHearingDetails.getEpimsId());
+            }
+        }
+
+
+
+
+
+
+//        callback.getCaseDetails().getCaseData().getHearingOutcomes().get(0).getValue();
+
+
+
+//        get hearing outcome -> get hearing id -> overwrite hearing outcome
+
+//        loop through hearing outcome ->hearing 1: set did po attend to did po attend, set case outcome
+//        to case outcome, set hearing outcome to hearing outcome;
+
+
+
+        sscsCaseData.setCompletedHearingsList(null);
+
+        return preSubmitCallbackResponse;
+    }
+}
