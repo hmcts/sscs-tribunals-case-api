c5a5c5d52e0591cc2d956a007196b7045a34018b
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2025-01-21 17:00:10 +0000

    Create AmendHearingOutcomeAboutToSubmitHandlerTest

---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeAboutToSubmitHandlerTest.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.amendhearingoutcome;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+import junitparams.JUnitParamsRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+
+@RunWith(JUnitParamsRunner.class)
+public class AmendHearingOutcomeAboutToSubmitHandlerTest {
+
+    private AmendHearingOutcomeAboutToSubmitHandler handler;
+
+    @Mock
+    private Callback<SscsCaseData> callback;
+
+    @Mock
+    private CaseDetails<SscsCaseData> caseDetails;
+
+    private SscsCaseData sscsCaseData;
+
+    @Before
+    public void setUp() {
+        openMocks(this);
+        handler = new AmendHearingOutcomeAboutToSubmitHandler();
+
+        when(callback.getEvent()).thenReturn(EventType.AMEND_HEARING_OUTCOME);
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
+        sscsCaseData = SscsCaseData.builder().ccdCaseId("ccdDd").appeal(Appeal.builder().build()).build();
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+    }
+
+    @Test
+    public void givenAValidAboutToSubmitEvent_thenReturnTrue() {
+        assertThat(handler.canHandle(CallbackType.ABOUT_TO_SUBMIT, callback)).isTrue();
+    }
+
+}
