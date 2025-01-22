a35c279762085972196cfa2da9e5d9fe31f1d490
hashimalisolirius <HashimAli@HMCTS.NET>
2025-01-22 13:20:05 +0000

    test for AmendHearingOutcomeAboutToSubmitHandler

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeAboutToSubmitHandlerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
+import java.util.List;
 import junitparams.JUnitParamsRunner;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +12,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
@@ -43,6 +45,13 @@ public class AmendHearingOutcomeAboutToSubmitHandlerTest {
     @Test
     public void givenAValidAboutToSubmitEvent_thenReturnTrue() {
         assertThat(handler.canHandle(CallbackType.ABOUT_TO_SUBMIT, callback)).isTrue();
+    }
+
+    @Test
+    public void givenAnEmptyOutcomeList_thenSetHearingOutcomesToNull() {
+        sscsCaseData.setHearingOutcomes(List.of());
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(CallbackType.ABOUT_TO_SUBMIT, callback, "userAuthorisation");
+        assertThat(response.getData().getHearingOutcomes()).isEqualTo(null);
     }
 
 }
