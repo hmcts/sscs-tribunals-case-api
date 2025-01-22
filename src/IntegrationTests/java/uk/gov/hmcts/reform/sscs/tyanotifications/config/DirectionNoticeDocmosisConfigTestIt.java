d6b54fb3bf4219d050ebf25d73f52ef51f80c0f3
nilay <nilay.patel@solirius.com>
2025-01-22 15:39:54 +0000

    pointing to new templates and updating tests

--- src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/tyanotifications/config/DirectionNoticeDocmosisConfigTestIt.java
+++ src/IntegrationTests/java/uk/gov/hmcts/reform/sscs/tyanotifications/config/DirectionNoticeDocmosisConfigTestIt.java
@@ -27,7 +27,7 @@ public class DirectionNoticeDocmosisConfigTestIt extends AbstractNotificationCon
         "GRANT_EXTENSION, TB-SCS-GNO-ENG-00556.docx, TB-SCS-GNO-ENG-00556.docx, TB-SCS-GNO-ENG-00556.docx",
         "REFUSE_EXTENSION, TB-SCS-GNO-ENG-00557.docx, TB-SCS-GNO-ENG-00557.docx, TB-SCS-GNO-ENG-00557.docx",
         "GRANT_REINSTATEMENT, TB-SCS-GNO-ENG-00584.docx, TB-SCS-GNO-ENG-00584.docx, null",
-        "REFUSE_REINSTATEMENT, TB-SCS-GNO-ENG-00585.docx, TB-SCS-GNO-ENG-00585.docx, null",
+        "REFUSE_REINSTATEMENT, TB-SCS-GNO-ENG-00585-v2.docx, TB-SCS-GNO-ENG-00585-v2.docx, null",
         "REFUSE_HEARING_RECORDING_REQUEST, TB-SCS-GNO-ENG-00067.docx, TB-SCS-GNO-ENG-00089.docx, TB-SCS-GNO-ENG-00067.docx"
     })
     public void shouldGiveCorrectDocmosisIdForDirectionIssued(DirectionType directionType,
@@ -74,7 +74,7 @@ public class DirectionNoticeDocmosisConfigTestIt extends AbstractNotificationCon
         "APPEAL_TO_PROCEED, TB-SCS-GNO-WEL-00590.docx, TB-SCS-GNO-WEL-00590.docx, TB-SCS-GNO-WEL-00590.docx",
         "PROVIDE_INFORMATION, TB-SCS-GNO-WEL-00468.docx, TB-SCS-GNO-WEL-00472.docx, TB-SCS-GNO-WEL-00468.docx",
         "GRANT_REINSTATEMENT, TB-SCS-GNO-WEL-00586.docx, TB-SCS-GNO-WEL-00586.docx, null",
-        "REFUSE_REINSTATEMENT, TB-SCS-GNO-WEL-00587.docx, TB-SCS-GNO-WEL-00587.docx, null",
+        "REFUSE_REINSTATEMENT, TB-SCS-GNO-WEL-00587-v2.docx, TB-SCS-GNO-WEL-00587-v2.docx, null",
         "REFUSE_HEARING_RECORDING_REQUEST, TB-SCS-GNO-WEL-00468.docx, TB-SCS-GNO-WEL-00472.docx, TB-SCS-GNO-WEL-00468.docx"
     })
     public void shouldGiveCorrectDocmosisIdForDirectionIssuedWelsh(DirectionType directionType,
