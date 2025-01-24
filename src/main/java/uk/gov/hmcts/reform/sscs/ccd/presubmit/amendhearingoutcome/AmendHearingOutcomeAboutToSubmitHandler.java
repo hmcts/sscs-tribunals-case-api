1f2dc36715744892e03029362a763ecd9f7174c6
Gemma Talbot <gemma.talbot@solirius.com>
2025-01-24 15:01:35 +0000

    reorder null check

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeAboutToSubmitHandler.java
@@ -57,7 +57,7 @@ public class AmendHearingOutcomeAboutToSubmitHandler implements PreSubmitCallbac
             String checkHearingId =
                     checkHearingOutcomes.getValue().getCompletedHearings().getValue().getCode();
             log.info("Checking hearing ID for Amend Hearing Outcome Event: {}", checkHearingId);
-            if (hearingsSelected.contains(checkHearingId) && hearingsSelected != null) {
+            if (hearingsSelected != null && hearingsSelected.contains(checkHearingId)) {
                 preSubmitCallbackResponse.addError("This hearing already has an outcome recorded.");
                 return preSubmitCallbackResponse;
             } else {
