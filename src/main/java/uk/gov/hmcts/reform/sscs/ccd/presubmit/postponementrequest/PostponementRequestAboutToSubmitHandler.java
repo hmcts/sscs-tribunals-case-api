9462a0d2d890c0e0de6b60bd00ec4b45f47fccf6
nilay <nilay.patel@solirius.com>
2025-01-27 18:08:35 +0000

    hard set to FTA and using label in data rather than value

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postponementrequest/PostponementRequestAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/postponementrequest/PostponementRequestAboutToSubmitHandler.java
@@ -59,8 +59,8 @@ public class PostponementRequestAboutToSubmitHandler implements PreSubmitCallbac
             if (userDetails.hasRole(DWP)) {
                 uploadParty = Optional.of(UploadParty.DWP);
             }
-            UploadParty originalSender = sscsCaseData.isIbcCase() ? UploadParty.FTA : UploadParty.DWP;
-            postponementRequestService.processPostponementRequest(sscsCaseData, originalSender, uploadParty);
+
+            postponementRequestService.processPostponementRequest(sscsCaseData, UploadParty.FTA, uploadParty);
             List<SscsDocument> documents = sscsCaseData.getSscsDocument();
             documents.get(documents.size() - 1).getValue().setBundleAddition(footerService.getNextBundleAddition(documents));
         }
