a6d42ddb850252a5b7b5e17b76b5e46ed7afdc23
Gemma Talbot <gemma.talbot@solirius.com>
2025-01-29 12:01:40 +0000

    add tests

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeAboutToSubmitHandlerTest.java
@@ -7,7 +7,6 @@ import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import junitparams.JUnitParamsRunner;
 import org.junit.Before;
@@ -34,6 +33,36 @@ public class AmendHearingOutcomeAboutToSubmitHandlerTest {
 
     private AmendHearingOutcomeAboutToSubmitHandler handler;
     private static final String USER_AUTHORISATION = "Bearer token";
+
+    private static final DynamicList completedHearings = new DynamicList(new DynamicListItem("2", "test"),
+            List.of(new DynamicListItem("1", "test"), new DynamicListItem("2", "test2")));
+
+    private static final HearingOutcome hearingOutcome1 = HearingOutcome.builder()
+            .value(
+                    HearingOutcomeDetails.builder()
+                            .completedHearingId("1")
+                            .completedHearings(completedHearings)
+                            .venue(Venue.builder().name("venue 1 name").build())
+                            .hearingStartDateTime(LocalDateTime.of(2024,1,30,10,00))
+                            .hearingEndDateTime(LocalDateTime.of(2024,1,30,13,00))
+                            .epimsId("654321")
+                            .hearingChannelId(HearingChannel.FACE_TO_FACE)
+                            .build())
+            .build();
+
+    private static final HearingOutcome hearingOutcome2 = HearingOutcome.builder()
+            .value(
+                    HearingOutcomeDetails.builder()
+                            .completedHearingId("2")
+                            .completedHearings(completedHearings)
+                            .venue(Venue.builder().name("venue 2 name").build())
+                            .hearingStartDateTime(LocalDateTime.of(2024,6,30,10,00))
+                            .hearingEndDateTime(LocalDateTime.of(2024,6,30,13,00))
+                            .epimsId("123456")
+                            .hearingChannelId(HearingChannel.FACE_TO_FACE)
+                            .build()
+            ).build();
+
 
     @Mock
     private Callback<SscsCaseData> callback;
@@ -80,26 +109,34 @@ public class AmendHearingOutcomeAboutToSubmitHandlerTest {
                 .completedHearingsList(new ArrayList<>())
                 .build();
 
-        List completedHearingList = Collections.singletonList(Hearing.builder()
+        Hearing hearing1 = Hearing.builder()
+                .value(HearingDetails.builder()
+                        .hearingId("1")
+                        .venue(Venue.builder().name("venue 1 name").build())
+                        .start(LocalDateTime.of(2024,1,30,10,00))
+                        .end(LocalDateTime.of(2024,1,30,13,00))
+                        .epimsId("654321")
+                        .hearingChannel(HearingChannel.FACE_TO_FACE)
+                        .build())
+                .build();
+
+        Hearing hearing2 = Hearing.builder()
                 .value(HearingDetails.builder()
                         .hearingId("2")
-                        .venue(Venue.builder().name("venue 1 name").build())
+                        .venue(Venue.builder().name("venue 2 name").build())
                         .start(LocalDateTime.of(2024,6,30,10,00))
                         .end(LocalDateTime.of(2024,6,30,13,00))
                         .epimsId("123456")
                         .hearingChannel(HearingChannel.FACE_TO_FACE)
                         .build())
-                .build());
+                .build();
 
-        DynamicList dynamicList = new DynamicList(new DynamicListItem("2", "test"), List.of(new DynamicListItem("1", "test"), new DynamicListItem("2", "test2")));
+        List<Hearing> completedHearingList = new ArrayList<>();
+        completedHearingList.add(hearing1);
+        completedHearingList.add(hearing2);
+
         sscsCaseData.setCompletedHearingsList(completedHearingList);
-        sscsCaseData.getHearingOutcomes().add(HearingOutcome.builder()
-                .value(
-                        HearingOutcomeDetails.builder()
-                                .completedHearingId("1")
-                                .completedHearings(dynamicList)
-                                .build()
-                ).build());
+        sscsCaseData.getHearingOutcomes().add(hearingOutcome1);
 
         when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
 
@@ -108,10 +145,28 @@ public class AmendHearingOutcomeAboutToSubmitHandlerTest {
         assertThat(response.getData().getHearingOutcomes().get(0).getValue().getCompletedHearingId()).isEqualTo("2");
         assertThat(response.getData().getHearingOutcomes().get(0).getValue().getHearingStartDateTime()).isEqualTo(LocalDateTime.of(2024,6,30,10,00));
         assertThat(response.getData().getHearingOutcomes().get(0).getValue().getHearingEndDateTime()).isEqualTo(LocalDateTime.of(2024,6,30,13,00));
-        assertThat(response.getData().getHearingOutcomes().get(0).getValue().getVenue().getName()).isEqualTo("venue 1 name");
+        assertThat(response.getData().getHearingOutcomes().get(0).getValue().getVenue().getName()).isEqualTo("venue 2 name");
         assertThat(response.getData().getHearingOutcomes().get(0).getValue().getEpimsId()).isEqualTo("123456");
         assertThat(response.getData().getHearingOutcomes().get(0).getValue().getHearingChannelId()).isEqualTo(HearingChannel.FACE_TO_FACE);
 
     }
 
+    @Test
+    public void givenHearingOutcomeSelectedMoreThanOnce_thenThrowError() {
+
+        sscsCaseData = SscsCaseData.builder()
+                .hearingOutcomes(new ArrayList<>())
+                .build();
+
+        sscsCaseData.getHearingOutcomes().add(hearingOutcome1);
+
+        sscsCaseData.getHearingOutcomes().add(hearingOutcome2);
+
+        when(caseDetails.getCaseData()).thenReturn(sscsCaseData);
+
+        PreSubmitCallbackResponse<SscsCaseData> response = handler.handle(ABOUT_TO_SUBMIT,callback,USER_AUTHORISATION);
+
+        assertThat(response.getErrors())
+                .contains("This hearing already has an outcome recorded.");
+    }
 }
