04a89665b2f208c320b7cba5a05d2c3298215d77
Linus Norton <linusnorton@gmail.com>
2025-01-30 14:28:19 +0000

    Reconfigure the config to make it more like the other config

--- src/main/java/uk/gov/hmcts/reform/sscs/config/jms/HearingsJmsConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/jms/HearingsJmsConfig.java
@@ -1,18 +1,17 @@
 package uk.gov.hmcts.reform.sscs.config.jms;
 
 import jakarta.jms.ConnectionFactory;
-import jakarta.jms.Session;
+import javax.net.ssl.SSLContext;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.qpid.jms.JmsConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.connection.CachingConnectionFactory;
-import org.springframework.jms.listener.DefaultMessageListenerContainer;
+import org.springframework.jms.core.JmsTemplate;
 
 @Slf4j
 @Configuration
@@ -40,28 +39,41 @@ public class HearingsJmsConfig {
     @Value("${spring.application.name}")
     private String clientId;
 
-    private ConnectionFactory hmcHearingJmsConnectionFactory() {
-        String connection = String.format(AMQP_CONNECTION_STRING_TEMPLATE, namespace + connectionPostfix, idleTimeout);
-        JmsConnectionFactory jmsConnectionFactory = new JmsConnectionFactory(connection);
+    @Bean
+    public String jmsUrlString(@Value("${azure.service-bus.hmc-to-hearings-api.namespace}${azure.service-bus.connection-postfix}") final String host) {
+        return String.format(AMQP_CONNECTION_STRING_TEMPLATE, host, idleTimeout);
+    }
+
+    @Bean
+    public ConnectionFactory jmsConnectionFactory(@Autowired final String jmsUrlString,
+                                                  @Autowired(required = false) final SSLContext jmsSslContext
+    ) {
+        JmsConnectionFactory jmsConnectionFactory = new JmsConnectionFactory(jmsUrlString);
         jmsConnectionFactory.setUsername(username);
         jmsConnectionFactory.setPassword(password);
         jmsConnectionFactory.setClientID(clientId);
+        jmsConnectionFactory.setReceiveLocalOnly(true);
+        if (jmsSslContext != null) {
+            jmsConnectionFactory.setSslContext(jmsSslContext);
+        }
         return new CachingConnectionFactory(jmsConnectionFactory);
     }
 
     @Bean
-    @ConditionalOnProperty("flags.hmc-to-hearings-api.enabled")
-    public JmsListenerContainerFactory<DefaultMessageListenerContainer> hmcHearingsEventTopicContainerFactory(
-        DefaultJmsListenerContainerFactoryConfigurer configurer) {
-        ConnectionFactory hmcHearingJmsConnectionFactory = hmcHearingJmsConnectionFactory();
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(hmcHearingJmsConnectionFactory);
-        factory.setReceiveTimeout(receiveTimeout);
-        factory.setSubscriptionDurable(Boolean.TRUE);
-        factory.setSessionTransacted(Boolean.TRUE);
-        factory.setSessionAcknowledgeMode(Session.SESSION_TRANSACTED);
+    public JmsTemplate jmsTemplate(ConnectionFactory jmsConnectionFactory) {
+        JmsTemplate returnValue = new JmsTemplate();
+        returnValue.setConnectionFactory(jmsConnectionFactory);
+        return returnValue;
+    }
 
-        configurer.configure(factory, hmcHearingJmsConnectionFactory);
-        return factory;
+    @Bean
+    public JmsListenerContainerFactory hmcHearingsEventTopicContainerFactory(ConnectionFactory connectionFactory) {
+        log.info("Creating JMSListenerContainer bean for topics..");
+        DefaultJmsListenerContainerFactory returnValue = new DefaultJmsListenerContainerFactory();
+        returnValue.setConnectionFactory(connectionFactory);
+        returnValue.setSubscriptionDurable(Boolean.TRUE);
+        returnValue.setErrorHandler(t -> log.error("Error while processing JMS message", t));
+        returnValue.setExceptionListener(t -> log.error("Exception while processing JMS message", t));
+        return returnValue;
     }
 }
