0ef8b92f7c18c4bdc36510fc305d7983586d4422
hashimalisolirius <HashimAli@HMCTS.NET>
2025-01-30 12:49:23 +0000

    midevent for amend hearing

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeMidEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeMidEventHandler.java
@@ -1,9 +1,5 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.amendhearingoutcome;
 
-import static java.util.Objects.requireNonNull;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
@@ -11,31 +7,25 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 
-@Component
-@Slf4j
-public class AmendHearingOutcomeAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
-
+public class AmendHearingOutcomeMidEventHandler implements PreSubmitCallbackHandler<SscsCaseData> {
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
-        requireNonNull(callback, "callback must not be null");
-        requireNonNull(callbackType, "callbackType must not be null");
+        return callbackType.equals(CallbackType.MID_EVENT)
+                && callback.getEvent().equals(EventType.AMEND_HEARING_OUTCOME);
 
-        return callbackType.equals(CallbackType.ABOUT_TO_SUBMIT)
-            && callback.getEvent().equals(EventType.AMEND_HEARING_OUTCOME);
     }
 
     @Override
-    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback,
-                                                          String userAuthorisation) {
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback, String userAuthorisation) {
         if (!canHandle(callbackType, callback)) {
             throw new IllegalStateException("Cannot handle callback");
         }
-
         SscsCaseData sscsCaseData = callback.getCaseDetails().getCaseData();
         PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
-
-        if (sscsCaseData.getHearingOutcomes().isEmpty()) {
-            sscsCaseData.setHearingOutcomes(null);
+        boolean hasNewOutcomeBeenAdded = sscsCaseData.getHearingOutcomes().stream()
+                .anyMatch(hearingOutcome -> hearingOutcome.getValue().getCompletedHearingId() == null);
+        if (hasNewOutcomeBeenAdded) {
+            preSubmitCallbackResponse.addError("Hearing outcomes cannot be added in amend hearing.");
         }
 
         return preSubmitCallbackResponse;
