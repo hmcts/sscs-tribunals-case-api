224f356c1d40fef6f58aa1e7aa39d8c47f99e372
hashimalisolirius <HashimAli@HMCTS.NET>
2025-01-30 14:35:53 +0000

    update midevent check

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeMidEventHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/amendhearingoutcome/AmendHearingOutcomeMidEventHandler.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.amendhearingoutcome;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
@@ -7,6 +9,8 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 
+@Slf4j
+@Component
 public class AmendHearingOutcomeMidEventHandler implements PreSubmitCallbackHandler<SscsCaseData> {
     @Override
     public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
@@ -24,6 +28,7 @@ public class AmendHearingOutcomeMidEventHandler implements PreSubmitCallbackHand
         PreSubmitCallbackResponse<SscsCaseData> preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(sscsCaseData);
         boolean hasNewOutcomeBeenAdded = sscsCaseData.getHearingOutcomes().stream()
                 .anyMatch(hearingOutcome -> hearingOutcome.getValue().getCompletedHearingId() == null);
+        log.info("hasNewOutcomeBeenAdded: {}", hasNewOutcomeBeenAdded);
         if (hasNewOutcomeBeenAdded) {
             preSubmitCallbackResponse.addError("Hearing outcomes cannot be added in amend hearing.");
         }
