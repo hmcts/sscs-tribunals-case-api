51bc0d5af78fbee0092f922c245c57143f1a2d9e
Linus Norton <linusnorton@gmail.com>
2025-01-31 12:06:15 +0000

    Set up hearings url headers

--- src/main/java/uk/gov/hmcts/reform/sscs/service/HmcHearingApi.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/HmcHearingApi.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sscs.service;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,11 +24,17 @@ import uk.gov.hmcts.reform.sscs.model.single.hearing.HmcUpdateResponse;
 public interface HmcHearingApi {
 
     String SERVICE_AUTHORIZATION = "ServiceAuthorization";
+    String ROLE_ASSIGNMENT_HEADER = "Role-Assignment-Url: ${role-assignment.api.url}";
+    String DATA_STORE_HEADER = "Data-Store-Url: ${core_case_data.api.url}";
     String HEARING_ENDPOINT = "/hearing";
     String ID = "id";
     String HMCTS_DEPLOYMENT_ID = "hmctsDeploymentId";
 
     @PostMapping(value = HEARING_ENDPOINT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Headers({
+        ROLE_ASSIGNMENT_HEADER,
+        DATA_STORE_HEADER
+    })
     HmcUpdateResponse createHearingRequest(
             @RequestHeader(AUTHORIZATION) String authorisation,
             @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
@@ -36,6 +43,10 @@ public interface HmcHearingApi {
     );
 
     @PutMapping(value = HEARING_ENDPOINT + "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Headers({
+        ROLE_ASSIGNMENT_HEADER,
+        DATA_STORE_HEADER
+    })
     HmcUpdateResponse updateHearingRequest(
             @RequestHeader(AUTHORIZATION) String authorisation,
             @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
@@ -45,6 +56,10 @@ public interface HmcHearingApi {
     );
 
     @DeleteMapping(value = HEARING_ENDPOINT + "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Headers({
+        ROLE_ASSIGNMENT_HEADER,
+        DATA_STORE_HEADER
+    })
     HmcUpdateResponse cancelHearingRequest(
             @RequestHeader(AUTHORIZATION) String authorisation,
             @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
@@ -54,6 +69,10 @@ public interface HmcHearingApi {
     );
 
     @GetMapping(HEARING_ENDPOINT + "/{id}")
+    @Headers({
+        ROLE_ASSIGNMENT_HEADER,
+        DATA_STORE_HEADER
+    })
     HearingGetResponse getHearingRequest(
             @RequestHeader(AUTHORIZATION) String authorisation,
             @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
