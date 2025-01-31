8389ba60ae68cd8046c7eb1da53d12940df01577
Linus Norton <linusnorton@gmail.com>
2025-01-31 15:58:36 +0000

    Revert again (#4360)

--- src/main/java/uk/gov/hmcts/reform/sscs/config/MessagingConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/MessagingConfig.java
@@ -1,12 +1,11 @@
 package uk.gov.hmcts.reform.sscs.config;
 
-import jakarta.jms.ConnectionFactory;
+import javax.jms.ConnectionFactory;
 import javax.net.ssl.SSLContext;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.qpid.jms.JmsConnectionFactory;
 import org.apache.qpid.jms.policy.JmsDefaultPrefetchPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,25 +14,24 @@ import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 @Configuration
 @Slf4j
 @EnableJms
 public class MessagingConfig {
 
-    @Bean("evidenceShareJmsUrl")
+    @Bean
     public String jmsUrlString(@Value("${amqp.amqp-connection-string-template}") final String amqpConnectionStringTemplate,
                                @Value("${amqp.idleTimeout}") final Long idleTimeout,
                                @Value("${amqp.host}") final String host) {
         return String.format(amqpConnectionStringTemplate, host, idleTimeout);
     }
 
-    @Bean("evidenceShareJmsConnectionFactory")
+    @Bean
     public ConnectionFactory jmsConnectionFactory(@Value("${spring.application.name}") final String clientId,
                                                   @Value("${amqp.username}") final String username,
                                                   @Value("${amqp.password}") final String password,
-                                                  @Autowired @Qualifier("evidenceShareJmsUrl") final String jmsUrlString,
+                                                  @Autowired final String jmsUrlString,
                                                   @Autowired(required = false) final SSLContext jmsSslContext,
                                                   @Value("${amqp.prefetch.override}") final boolean prefetchOverride,
                                                   @Value("${amqp.prefetch.topicPrefetch}") final int topicPrefetch,
@@ -60,15 +58,15 @@ public class MessagingConfig {
         return cachingConnectionFactory;
     }
 
-    @Bean("evidenceShareJmsTemplate")
-    public JmsTemplate jmsTemplate(@Qualifier("evidenceShareJmsConnectionFactory") ConnectionFactory jmsConnectionFactory) {
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory jmsConnectionFactory) {
         JmsTemplate returnValue = new JmsTemplate();
         returnValue.setConnectionFactory(jmsConnectionFactory);
         return returnValue;
     }
 
-    @Bean("evidenceShareJmsListenerContainerFactory")
-    public JmsListenerContainerFactory<DefaultMessageListenerContainer> topicJmsListenerContainerFactory(@Qualifier("evidenceShareJmsConnectionFactory") ConnectionFactory connectionFactory) {
+    @Bean
+    public JmsListenerContainerFactory topicJmsListenerContainerFactory(ConnectionFactory connectionFactory) {
         log.info("Creating JMSListenerContainer bean for topics..");
         DefaultJmsListenerContainerFactory returnValue = new DefaultJmsListenerContainerFactory();
         returnValue.setConnectionFactory(connectionFactory);
--- src/main/java/uk/gov/hmcts/reform/sscs/config/jms/HearingsJmsConfig.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/jms/HearingsJmsConfig.java
@@ -1,19 +1,17 @@
 package uk.gov.hmcts.reform.sscs.config.jms;
 
-import jakarta.jms.ConnectionFactory;
-import javax.net.ssl.SSLContext;
+import javax.jms.ConnectionFactory;
+import javax.jms.Session;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.qpid.jms.JmsConnectionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.connection.CachingConnectionFactory;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 @Slf4j
@@ -42,42 +40,28 @@ public class HearingsJmsConfig {
     @Value("${spring.application.name}")
     private String clientId;
 
-    @Bean("hmcHearingsJmsUrl")
-    public String jmsUrlString(@Value("${azure.service-bus.hmc-to-hearings-api.namespace}${azure.service-bus.connection-postfix}") final String host) {
-        return String.format(AMQP_CONNECTION_STRING_TEMPLATE, host, idleTimeout);
-    }
-
-    @Bean("hmcHearingsJmsConnectionFactory")
-    public ConnectionFactory jmsConnectionFactory(@Autowired @Qualifier("hmcHearingsJmsUrl") final String jmsUrlString,
-                                                  @Autowired(required = false) final SSLContext jmsSslContext
-    ) {
-        JmsConnectionFactory jmsConnectionFactory = new JmsConnectionFactory(jmsUrlString);
+    private ConnectionFactory hmcHearingJmsConnectionFactory() {
+        String connection = String.format(AMQP_CONNECTION_STRING_TEMPLATE, namespace + connectionPostfix, idleTimeout);
+        JmsConnectionFactory jmsConnectionFactory = new JmsConnectionFactory(connection);
         jmsConnectionFactory.setUsername(username);
         jmsConnectionFactory.setPassword(password);
         jmsConnectionFactory.setClientID(clientId);
-        jmsConnectionFactory.setReceiveLocalOnly(true);
-        if (jmsSslContext != null) {
-            jmsConnectionFactory.setSslContext(jmsSslContext);
-        }
         return new CachingConnectionFactory(jmsConnectionFactory);
     }
 
-    @Bean("hmcHearingsJmsTemplate")
-    public JmsTemplate jmsTemplate(@Qualifier("hmcHearingsJmsConnectionFactory") ConnectionFactory jmsConnectionFactory) {
-        JmsTemplate returnValue = new JmsTemplate();
-        returnValue.setConnectionFactory(jmsConnectionFactory);
-        return returnValue;
-    }
-
-    @Bean("hmcHearingsEventTopicContainerFactory")
+    @Bean
     @ConditionalOnProperty("flags.hmc-to-hearings-api.enabled")
-    public JmsListenerContainerFactory<DefaultMessageListenerContainer> hmcHearingsEventTopicContainerFactory(@Qualifier("hmcHearingsJmsConnectionFactory") ConnectionFactory connectionFactory) {
-        log.info("Creating JMSListenerContainer bean for topics..");
-        DefaultJmsListenerContainerFactory returnValue = new DefaultJmsListenerContainerFactory();
-        returnValue.setConnectionFactory(connectionFactory);
-        returnValue.setSubscriptionDurable(Boolean.TRUE);
-        returnValue.setErrorHandler(t -> log.error("Error while processing JMS message", t));
-        returnValue.setExceptionListener(t -> log.error("Exception while processing JMS message", t));
-        return returnValue;
+    public JmsListenerContainerFactory<DefaultMessageListenerContainer> hmcHearingsEventTopicContainerFactory(
+        DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        ConnectionFactory hmcHearingJmsConnectionFactory = hmcHearingJmsConnectionFactory();
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(hmcHearingJmsConnectionFactory);
+        factory.setReceiveTimeout(receiveTimeout);
+        factory.setSubscriptionDurable(Boolean.TRUE);
+        factory.setSessionTransacted(Boolean.TRUE);
+        factory.setSessionAcknowledgeMode(Session.SESSION_TRANSACTED);
+
+        configurer.configure(factory, hmcHearingJmsConnectionFactory);
+        return factory;
     }
 }
