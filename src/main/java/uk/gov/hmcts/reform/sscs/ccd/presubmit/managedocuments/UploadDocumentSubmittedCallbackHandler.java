00abcb427a951a3147c9a38790dedb4e20b83ce2
nilay <nilay.patel@solirius.com>
2025-02-27 14:29:12 +0000

    handling trigger issue FE evidence in submitted if needed

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/managedocuments/UploadDocumentSubmittedCallbackHandler.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.managedocuments;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.DocumentTabChoice.REGULAR;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.YES;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.sscs.ccd.service.UpdateCcdCaseService;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class UploadDocumentSubmittedCallbackHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+    private final UpdateCcdCaseService updateCcdCaseService;
+
+    private final IdamService idamService;
+
+    @Value("${feature.tribunal-internal-documents.enabled}")
+    private final boolean isTribunalInternalDocumentsEnabled;
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        requireNonNull(callback, "callback must not be null");
+        requireNonNull(callbackType, "callbackType must not be null");
+        return callbackType.equals(CallbackType.SUBMITTED)
+            && callback.getEvent().equals(EventType.UPLOAD_DOCUMENT);
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback,
+                                                          String userAuthorisation) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+        SscsCaseData sscsCaseData = callback.getCaseDetails().getCaseData();
+        if (isTribunalInternalDocumentsEnabled
+            && REGULAR.equals(sscsCaseData.getInternalCaseDocumentData().getMoveDocumentTo())
+            && YES.equals(sscsCaseData.getInternalCaseDocumentData().getShouldBeIssued())) {
+            log.info("Updating case using triggerCaseEventV2 for event: {}, event description: {}, caseId: {}",
+                EventType.ISSUE_FURTHER_EVIDENCE.getCcdType(),
+                "Issue to all parties",
+                callback.getCaseDetails().getId());
+            SscsCaseDetails sscsCaseDetails = updateCcdCaseService.triggerCaseEventV2(callback.getCaseDetails().getId(),
+                EventType.ISSUE_FURTHER_EVIDENCE.getCcdType(), "Issue to all parties",
+                "Issue to all parties", idamService.getIdamTokens());
+            return new PreSubmitCallbackResponse<>(sscsCaseDetails.getData());
+        }
+        return new PreSubmitCallbackResponse<>(sscsCaseData);
+    }
+}
