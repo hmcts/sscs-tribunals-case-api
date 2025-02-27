b8de106791a89b5bf46baf3c2bc018350c520435
Linus Norton <linusnorton@gmail.com>
2025-02-27 16:41:11 +0000

    add another user

--- bin/setup-role-assignments.sh
+++ bin/setup-role-assignments.sh
@@ -13,7 +13,7 @@ S2S_TOKEN=$(${BASEDIR}/utils/s2s-token.sh "am_org_role_mapping_service")
 export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net
 
 IDAM_TOKEN=$(${BASEDIR}/utils/idam-lease-user-token.sh $IDAM_DATA_STORE_SYSTEM_USER_USERNAME $IDAM_DATA_STORE_SYSTEM_USER_PASSWORD)
-PAYLOAD='{"userIds": ["41033a79-b9c1-4a36-b0ff-113451f736ba", "a9ab7f4b-7e0c-49d4-8ed3-75b54d421cdc"]}'
+PAYLOAD='{"userIds": ["41033a79-b9c1-4a36-b0ff-113451f736ba", "a9ab7f4b-7e0c-49d4-8ed3-75b54d421cdc", "22cb52eb-9490-42ce-837b-58b81702855a"]}'
 URL="https://orm-sscs-tribunals-api-pr-${CHANGE_ID}.preview.platform.hmcts.net/am/testing-support/createOrgMapping?userType=CASEWORKER"
 
 curl --silent --show-error --fail "${URL}" \
