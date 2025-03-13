1a54e594d1f7f13cbc2507962e9f8f20a2e5f493
nilay <nilay.patel@solirius.com>
2025-03-13 09:33:28 +0000

    sonarcloud fix

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/managedocuments/UploadDocumentSubmittedCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/managedocuments/UploadDocumentSubmittedCallbackHandler.java
@@ -13,7 +13,6 @@ import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.DispatchPriority;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
-import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.service.UpdateCcdCaseService;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 
@@ -59,7 +58,7 @@ public class UploadDocumentSubmittedCallbackHandler implements CallbackHandler<S
             String issueToAllParties = "Issue to all parties";
             log.info("Updating case using triggerCaseEventV2 for event: {}, event description: {}, caseId: {}",
                 EventType.ISSUE_FURTHER_EVIDENCE.getCcdType(), issueToAllParties, callback.getCaseDetails().getId());
-            SscsCaseDetails sscsCaseDetails = updateCcdCaseService.triggerCaseEventV2(callback.getCaseDetails().getId(),
+            updateCcdCaseService.triggerCaseEventV2(callback.getCaseDetails().getId(),
                 EventType.ISSUE_FURTHER_EVIDENCE.getCcdType(), issueToAllParties,
                 issueToAllParties, idamService.getIdamTokens());
         }
