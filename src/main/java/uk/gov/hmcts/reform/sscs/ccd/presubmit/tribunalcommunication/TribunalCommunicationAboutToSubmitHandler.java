ac1914d7acac91300a6ac9a8cdbf919f02bb0651
erclaudio <claudio.andrei@icloud.com>
2025-03-21 14:38:42 +0000

    draft of event handler

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/tribunalcommunication/TribunalCommunicationAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/tribunalcommunication/TribunalCommunicationAboutToSubmitHandler.java
@@ -1,26 +1,71 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.tribunalcommunication;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertTrue;
-import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.APPEAL_RECEIVED;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
-import uk.gov.hmcts.reform.sscs.ccd.domain.TribunalCommunication;
-import uk.gov.hmcts.reform.sscs.ccd.domain.TribunalCommunicationFields;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.TribunalCommunication;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 import uk.gov.hmcts.reform.sscs.idam.UserDetails;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 
-public class TribunalCommunicationAboutToSubmitHandler {
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@Slf4j
+public class TribunalCommunicationAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    private final IdamService idamService;
+
+    @Autowired
+    public TribunalCommunicationAboutToSubmitHandler(IdamService idamService) {
+        this.idamService = idamService;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        Objects.requireNonNull(callback, "callback must not be null");
+        Objects.requireNonNull(callbackType, "callbackType must not be null");
+
+        return callbackType.equals(CallbackType.ABOUT_TO_SUBMIT)
+                && callback.getEvent() == EventType.TRIBUNAL_COMMUNICATION;
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback,
+                                                          String userAuthorisation) {
+
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        CaseDetails<SscsCaseData> caseDetails = callback.getCaseDetails();
+        SscsCaseData sscsCaseData = caseDetails.getCaseData();
+
+        List<TribunalCommunication> tribunalComms = sscsCaseData.getTribunalCommunicationFields().getTribunalCommunications();
+
+        String topic = sscsCaseData.getTribunalCommunicationFields().getTribunalRequestTopic();
+        String question = sscsCaseData.getTribunalCommunicationFields().getTribunalRequestMessage();
+        LocalDateTime now = LocalDateTime.now();
+        final UserDetails userDetails = idamService.getUserDetails(userAuthorisation);
+        tribunalComms.add(TribunalCommunication.builder()
+                .requestMessage(question)
+                .requestTopic(topic)
+                .requestDateTime(now)
+                .requestUserName(userDetails.getName())
+                .requestDueDate(now.plusDays(2))
+                .build());
+
+        tribunalComms.sort(Comparator.comparing(TribunalCommunication::getRequestDateTime).reversed());
+
+        return new PreSubmitCallbackResponse<>(sscsCaseData);
+    }
 }
