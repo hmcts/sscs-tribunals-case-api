d76b142c74c617a8c16de3b1bc1c95675a0981e6
erclaudio <claudio.andrei@icloud.com>
2025-03-27 13:41:07 +0000

    fixes + filters + due date function

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/tribunalcommunication/TribunalCommunicationAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/tribunalcommunication/TribunalCommunicationAboutToSubmitHandler.java
@@ -3,11 +3,10 @@ package uk.gov.hmcts.reform.sscs.ccd.presubmit.tribunalcommunication;
 import static java.util.Objects.requireNonNull;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
@@ -50,28 +49,47 @@ public class TribunalCommunicationAboutToSubmitHandler implements PreSubmitCallb
         SscsCaseData sscsCaseData = caseDetails.getCaseData();
 
         TribunalCommunicationDetails tribunalCommunicationDetails = Optional.ofNullable(sscsCaseData.getTribunalCommunicationsDetails()).orElse(TribunalCommunicationDetails.builder().build());
-        String topic = tribunalCommunicationDetails.getTribunalRequestTopic();
-        String question = tribunalCommunicationDetails.getTribunalRequestQuestion();
-        LocalDateTime dueDate = LocalDateTime.now().plusDays(2);
-        final UserDetails userDetails = idamService.getUserDetails(userAuthorisation);
+        if(tribunalCommunicationDetails.getTribunalRequestType()== TribunalRequestType.NEW_REQUEST) {
 
-        List<TribunalCommunication> tribunalComms = Optional.ofNullable(tribunalCommunicationDetails.getTribunalCommunications()).orElse(new ArrayList<>());
-        tribunalComms.add(TribunalCommunication.builder()
-                .value(TribunalCommunicationFields.builder()
-                    .requestMessage(question)
-                    .requestTopic(topic)
-                    .requestDateTime(LocalDateTime.now())
-                    .requestUserName(userDetails.getName())
-                    .requestResponseDue(dueDate)
-                    .build())
-                .build());
-        if (tribunalCommunicationDetails.getTribunalCommunications() != null) {
-            tribunalComms.addAll(tribunalCommunicationDetails.getTribunalCommunications());
+            String topic = tribunalCommunicationDetails.getTribunalRequestTopic();
+            String question = tribunalCommunicationDetails.getTribunalRequestQuestion();
+            LocalDateTime dueDate = dueDate(LocalDateTime.now());
+            final UserDetails userDetails = idamService.getUserDetails(userAuthorisation);
+
+            List<TribunalCommunication> tribunalComms = Optional.ofNullable(tribunalCommunicationDetails.getTribunalCommunications()).orElse(new ArrayList<>());
+            tribunalComms.add(TribunalCommunication.builder()
+                    .value(TribunalCommunicationFields.builder()
+                            .requestMessage(question)
+                            .requestTopic(topic)
+                            .requestDateTime(LocalDateTime.now())
+                            .requestUserName(userDetails.getName())
+                            .requestResponseDue(dueDate)
+                            .build())
+                    .build());
+            if (tribunalCommunicationDetails.getTribunalCommunications() != null) {
+                tribunalComms.addAll(tribunalCommunicationDetails.getTribunalCommunications());
+            }
+            tribunalCommunicationDetails.setTribunalCommunications(tribunalComms);
+            tribunalCommunicationDetails.setTribunalCommunicationFilter(TribunalCommunicationFilter.INFO_REQUEST_FROM_FTA);
+            tribunalCommunicationDetails.setFtaCommunicationFilter(FtaCommunicationFilter.AWAITING_INFO_FROM_TRIBUNAL);
+            tribunalComms.sort(Comparator.comparing(tribunalComm -> ((TribunalCommunication) tribunalComm).getValue().getRequestDateTime()).reversed());
+            tribunalCommunicationDetails.setTribunalCommunications(tribunalComms);
+            sscsCaseData.setTribunalCommunicationsDetails(tribunalCommunicationDetails);
         }
-
-        tribunalComms.sort(Comparator.comparing(tribunalComm -> ((TribunalCommunication) tribunalComm).getValue().getRequestDateTime()).reversed());
-        tribunalCommunicationDetails.setTribunalCommunications(tribunalComms);
-        sscsCaseData.setTribunalCommunicationsDetails(tribunalCommunicationDetails);
         return new PreSubmitCallbackResponse<>(sscsCaseData);
     }
+
+    private LocalDateTime dueDate(LocalDateTime now){
+        now = LocalDateTime.now();
+        LocalDateTime replyDueDate = now.plusDays(2);
+        if(replyDueDate.getDayOfWeek().getValue() == 6) {
+            replyDueDate = replyDueDate.plusDays(2);
+        }
+        else if(replyDueDate.getDayOfWeek().getValue()==7) {
+            replyDueDate = replyDueDate.plusDays(2);
+        }
+        //if friday dow = 5 due date should be tues therefore + 2
+        //if thur dow = 4  due date should be monday therefore  + 2
+    return replyDueDate;
+    };
 }
