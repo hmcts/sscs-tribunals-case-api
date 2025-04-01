860c616b72f5325fd2c5f089eb168638b54c82cf
nilay <nilay.patel@solirius.com>
2025-04-01 15:36:50 +0100

    checkstyle fix

--- src/test/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/placeholders/SorPlaceholderServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/placeholders/SorPlaceholderServiceTest.java
@@ -27,7 +27,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
