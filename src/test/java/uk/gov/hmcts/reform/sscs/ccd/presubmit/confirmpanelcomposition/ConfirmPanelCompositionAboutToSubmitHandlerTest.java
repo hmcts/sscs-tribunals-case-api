e3fa67b7c4252e9c7cc7c5990cd32c01b75344d8
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2025-05-14 13:22:06 +0100

    Added tests

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/confirmpanelcomposition/ConfirmPanelCompositionAboutToSubmitHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/confirmpanelcomposition/ConfirmPanelCompositionAboutToSubmitHandlerTest.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.confirmpanelcomposition;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.APPEAL_RECEIVED;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.NO;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.YES;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
@@ -15,12 +19,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReferralReason;
 import uk.gov.hmcts.reform.sscs.ccd.domain.InterlocReviewState;
+import uk.gov.hmcts.reform.sscs.ccd.domain.PanelMemberType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.YesNo;
 
@@ -118,4 +124,29 @@ public class ConfirmPanelCompositionAboutToSubmitHandlerTest {
         assertThat(response.getData().getInterlocReviewState(), is(InterlocReviewState.REVIEW_BY_JUDGE));
         assertThat(response.getData().getInterlocReferralReason(), is(InterlocReferralReason.NONE));
     }
+
+    @Test
+    public void givenFqpmRequiredYes_thenPanelMemberCompositionHasFqpm() {
+        ReflectionTestUtils.setField(handler, "isDefaultPanelCompEnabled", true);
+        sscsCaseData.setIsFqpmRequired(YES);
+
+        PreSubmitCallbackResponse<SscsCaseData> response =
+            handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        assertThat(response.getData().getPanelMemberComposition().getPanelCompDisabilityAndFqm(),
+            hasItem(PanelMemberType.TRIBUNAL_MEMBER_FINANCIALLY_QUALIFIED.getReference()));
+    }
+
+    @Test
+    public void givenFqpmRequiredNo_thenPanelMemberCompositionHasNoFqpm() {
+        ReflectionTestUtils.setField(handler, "isDefaultPanelCompEnabled", true);
+        sscsCaseData.setIsFqpmRequired(NO);
+
+        PreSubmitCallbackResponse<SscsCaseData> response =
+            handler.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        assertThat(response.getData().getPanelMemberComposition().getPanelCompDisabilityAndFqm(),
+            not(hasItem(PanelMemberType.TRIBUNAL_MEMBER_FINANCIALLY_QUALIFIED.getReference())));
+    }
+
 }
