140c44b37819294b9b8b1f90e12a112ac02a6cba
gokul-sol <gokul.sridharan@HMCTS.NET>
2025-06-18 10:03:32 +0100

    test code changes

--- definitions/test/e2e/update-not-listable.spec.ts
+++ definitions/test/e2e/update-not-listable.spec.ts
@@ -2,7 +2,7 @@ import { test } from '../lib/steps.factory';
 
 test.describe(
   'Update Not listable tests',
-  { tag: '@nightly-pipeline' },
+  { tag: '@fix-nightly-pipeline' },
   async () => {
     //Happy Path Test
     test('Not listable test - Set case to Not listable', async ({
