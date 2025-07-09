8461169c56f625b18e60e7c86ac8297c3a369cf2
hashimalisolirius <HashimAli@HMCTS.NET>
2025-07-09 09:51:34 +0100

    extra null check for override

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/updatelistingrequirements/UpdateListingRequirementsAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/updatelistingrequirements/UpdateListingRequirementsAboutToSubmitHandler.java
@@ -79,7 +79,7 @@ public class UpdateListingRequirementsAboutToSubmitHandler implements PreSubmitC
 
         OverrideFields overrideFields = caseDataSnlFields.getOverrideFields();
 
-        if (nonNull(overrideFields)) {
+        if (nonNull(overrideFields) && !overrideFields.isAllNull()) {
             HearingChannel hearingChannel = overrideFields.getAppellantHearingChannel();
             if (nonNull(hearingChannel)) {
                 SscsUtil.updateHearingChannel(sscsCaseData, hearingChannel);
