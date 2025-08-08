c2821f4136b03fa4d8e111fa6f289c8dedfd0cb6
taleb <tech@benouaer.com>
2025-08-08 15:28:11 +0100

    SSCSCI-1983: add panelComp and specialism nullChecks

--- src/test/java/uk/gov/hmcts/reform/sscs/helper/mapping/HearingsPanelMappingTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/helper/mapping/HearingsPanelMappingTest.java
@@ -210,6 +210,7 @@ class HearingsPanelMappingTest extends HearingsMappingBase {
     @CsvSource(value = {
         "cardiologist,eyeSurgeon,58|58,1|3",
         "carer,null,58,2",
+        "null,null,58,|",
     }, nullValues = {"null"})
     void testGetPanelSpecialisms(String doctorSpecialism, String secondSpecialism, String johTiers, String expected) {
         DefaultPanelComposition panelComposition = new DefaultPanelComposition();
