3682731a96afec8cd727dbd2481bdd0808d6b705
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2025-08-20 19:35:49 +0100

    Revert "fail the test to see logs"
    This reverts commit a870440d62be9b44fd175f69cdc4f2d722f2f5f2.

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/sya/SubmitAppealTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/sya/SubmitAppealTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.sscs.functional.sya;
 import static io.restassured.RestAssured.baseURI;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 import static uk.gov.hmcts.reform.sscs.util.SyaJsonMessageSerializer.ALL_DETAILS_NON_SAVE_AND_RETURN;
 import static uk.gov.hmcts.reform.sscs.util.SyaJsonMessageSerializer.ALL_DETAILS_NON_SAVE_AND_RETURN_CCD;
 import static uk.gov.hmcts.reform.sscs.util.SyaJsonMessageSerializer.ALL_DETAILS_NON_SAVE_AND_RETURN_CCD_CHILD_SUPPORT;
@@ -213,7 +212,5 @@ public class SubmitAppealTest {
         response.then().statusCode(HttpStatus.SC_CONFLICT);
 
         log.info("True duplicate was rejected");
-
-        fail("Remove this");
     }
 }
