372d9ac07f470c436f3046270228effbe0b35497
taleb <tech@benouaer.com>
2025-09-18 15:47:19 +0100

    SSCSCI-2039: update callback data in subsequent handle calls

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/PreSubmitCallbackDispatcher.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/PreSubmitCallbackDispatcher.java
@@ -10,6 +10,7 @@ import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.CaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
 
 @Slf4j
 @Component
@@ -22,38 +23,43 @@ public class PreSubmitCallbackDispatcher<T extends CaseData> {
         this.callbackHandlers = callbackHandlers;
     }
 
-    public PreSubmitCallbackResponse<T> handle(CallbackType callbackType, Callback<T> callback, String userAuthorisation) {
-
+    public PreSubmitCallbackResponse<T> handle(CallbackType callbackType, Callback<T> callback, String userAuth) {
         requireNonNull(callback, "callback must not be null");
-        T caseData = callback.getCaseDetails().getCaseData();
 
-        PreSubmitCallbackResponse<T> callbackResponse = new PreSubmitCallbackResponse<>(caseData);
-
-        dispatchToHandlers(callbackType, callback, callbackHandlers, callbackResponse, userAuthorisation);
-
-        return callbackResponse;
-    }
-
-    private void dispatchToHandlers(CallbackType callbackType, Callback<T> callback,
-                                    List<PreSubmitCallbackHandler<T>> callbackHandlers,
-                                    PreSubmitCallbackResponse<T> callbackResponse,
-                                    String userAuthorisation
-                                    ) {
         List<PreSubmitCallbackHandler<T>> eligibleHandlers = new ArrayList<>();
+        PreSubmitCallbackResponse<T> aggregateResponse =
+                new PreSubmitCallbackResponse<>(callback.getCaseDetails().getCaseData());
 
         for (PreSubmitCallbackHandler<T> callbackHandler : callbackHandlers) {
             if (callbackHandler.canHandle(callbackType, callback)) {
-
-                PreSubmitCallbackResponse<T> callbackResponseFromHandler = callbackHandler.handle(callbackType, callback, userAuthorisation);
-
-                callbackResponse.setData(callbackResponseFromHandler.getData());
-                callbackResponse.addErrors(callbackResponseFromHandler.getErrors());
-                callbackResponse.addWarnings(callbackResponseFromHandler.getWarnings());
                 eligibleHandlers.add(callbackHandler);
+
+                PreSubmitCallbackResponse<T> handlerResponse =
+                        callbackHandler.handle(callbackType, getUpdatedCallback(aggregateResponse, callback), userAuth);
+
+                aggregateResponse.setData(handlerResponse.getData());
+                aggregateResponse.addErrors(handlerResponse.getErrors());
+                aggregateResponse.addWarnings(handlerResponse.getWarnings());
             }
         }
         if (eligibleHandlers.size() > 1) {
             log.info("{} has more than one handler {}", callback.getEvent(), eligibleHandlers);
         }
+        return aggregateResponse;
+    }
+
+    private Callback<T> getUpdatedCallback(PreSubmitCallbackResponse<T> aggregateResponse,
+                                                     Callback<T> callback) {
+        var updatedCaseDetails = new CaseDetails<>(
+                callback.getCaseDetails().getId(),
+                callback.getCaseDetails().getJurisdiction(),
+                callback.getCaseDetails().getState(),
+                aggregateResponse.getData(),
+                callback.getCaseDetails().getCreatedDate(),
+                callback.getCaseDetails().getCaseTypeId()
+        );
+        return new Callback<>(
+                updatedCaseDetails, callback.getCaseDetailsBefore(), callback.getEvent(), callback.isIgnoreWarnings()
+        );
     }
 }
---
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/PreSubmitCallbackDispatcherTest.java
@@ -0,0 +1,127 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit;
+
+import static java.time.LocalDateTime.now;
+import static java.util.Optional.empty;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.DWP_UPLOAD_RESPONSE;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.State.READY_TO_LIST;
+import static uk.gov.hmcts.reform.sscs.ccd.util.CaseDataUtils.buildCaseData;
+
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseData;
+import uk.gov.hmcts.reform.sscs.ccd.domain.CaseDetails;
+
+@ExtendWith(MockitoExtension.class)
+public class PreSubmitCallbackDispatcherTest {
+    private static final String USER_AUTHORISATION = "Bearer token";
+
+    @Mock
+    private PreSubmitCallbackHandler<CaseData> handler1;
+    @Mock
+    private PreSubmitCallbackHandler<CaseData> handler2;
+    private Callback<CaseData> callback;
+    private CaseData caseData;
+    private CaseData caseDataMutation1;
+    private CaseData caseDataMutation2;
+    private PreSubmitCallbackResponse<CaseData> response1;
+    private PreSubmitCallbackResponse<CaseData> response2;
+
+    private PreSubmitCallbackDispatcher<CaseData> preSubmitCallbackDispatcher;
+
+    @BeforeEach
+    public void setUp() {
+        caseData = buildCaseData();
+        caseDataMutation1 = buildCaseData();
+        caseDataMutation2 = buildCaseData();
+        CaseDetails<CaseData> caseDetails =
+                new CaseDetails<>(1234L, "SSCS", READY_TO_LIST, caseData, now(), "Benefit");
+        callback = new Callback<>(caseDetails, empty(), DWP_UPLOAD_RESPONSE, false);
+        response1 = new PreSubmitCallbackResponse<>(caseDataMutation1);
+        response1.addErrors(List.of("error1"));
+        response2 = new PreSubmitCallbackResponse<>(caseDataMutation2);
+        response2.addErrors(List.of("error2", "error3"));
+        preSubmitCallbackDispatcher = new PreSubmitCallbackDispatcher<>(List.of(handler1, handler2));
+    }
+
+    @Test
+    public void should_dispatch_callback_to_handlers_collecting_any_error_messages() {
+        when(handler1.canHandle(eq(ABOUT_TO_SUBMIT), eq(callback))).thenReturn(true);
+        when(handler1.handle(eq(ABOUT_TO_SUBMIT), any(Callback.class), eq(USER_AUTHORISATION))).thenReturn(response1);
+
+        when(handler2.canHandle(eq(ABOUT_TO_SUBMIT), any(Callback.class))).thenReturn(true);
+        when(handler2.handle(eq(ABOUT_TO_SUBMIT), any(Callback.class), eq(USER_AUTHORISATION))).thenReturn(response2);
+
+        PreSubmitCallbackResponse<CaseData> callbackResponse =
+                preSubmitCallbackDispatcher.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        assertNotNull(callbackResponse);
+        assertEquals(caseDataMutation2, callbackResponse.getData());
+        assertThat(callbackResponse.getErrors()).containsAll(List.of("error1", "error2", "error3"));
+
+        ArgumentCaptor<Callback> callbackCaptor = ArgumentCaptor.forClass(Callback.class);
+        verify(handler1).handle(eq(ABOUT_TO_SUBMIT), callbackCaptor.capture(), eq(USER_AUTHORISATION));
+        assertEquals(caseData, callbackCaptor.getValue().getCaseDetails().getCaseData());
+        verify(handler2).handle(eq(ABOUT_TO_SUBMIT), callbackCaptor.capture(), eq(USER_AUTHORISATION));
+        assertEquals(caseDataMutation1, callbackCaptor.getValue().getCaseDetails().getCaseData());
+    }
+
+    @Test
+    public void should_only_dispatch_callback_to_handlers_that_can_handle_it() {
+        when(handler1.canHandle(eq(ABOUT_TO_SUBMIT), any(Callback.class))).thenReturn(false);
+        when(handler2.canHandle(eq(ABOUT_TO_SUBMIT), any(Callback.class))).thenReturn(false);
+
+        PreSubmitCallbackResponse<CaseData> callbackResponse =
+                preSubmitCallbackDispatcher.handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+
+        assertNotNull(callbackResponse);
+        assertEquals(caseData, callbackResponse.getData());
+        assertTrue(callbackResponse.getErrors().isEmpty());
+
+        verify(handler1, never()).handle(eq(ABOUT_TO_SUBMIT), any(Callback.class), eq(USER_AUTHORISATION));
+        verify(handler2, never()).handle(eq(ABOUT_TO_SUBMIT), any(Callback.class), eq(USER_AUTHORISATION));
+        ;
+    }
+
+    @Test
+    public void should_not_error_if_no_handlers_are_provided() {
+        PreSubmitCallbackDispatcher<CaseData> preSubmitCallbackDispatcher =
+                new PreSubmitCallbackDispatcher<>(Collections.emptyList());
+
+        assertDoesNotThrow(() -> {
+            PreSubmitCallbackResponse<CaseData> callbackResponse =
+                    preSubmitCallbackDispatcher
+                            .handle(ABOUT_TO_SUBMIT, callback, USER_AUTHORISATION);
+            assertNotNull(callbackResponse);
+            assertEquals(caseData, callbackResponse.getData());
+            assertTrue(callbackResponse.getErrors().isEmpty());
+        });
+    }
+
+    @Test
+    public void should_not_allow_null_arguments() {
+        assertThatThrownBy(() ->
+                preSubmitCallbackDispatcher.handle(ABOUT_TO_SUBMIT, null, USER_AUTHORISATION))
+                .hasMessage("callback must not be null")
+                .isExactlyInstanceOf(NullPointerException.class);
+    }
+}
