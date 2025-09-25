d09647e1259f971bf1ac602932515cbd8e133c29
taleb <tech@benouaer.com>
2025-09-25 10:45:42 +0100

    SSCSCI-2039: log callback state to check if always set

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/PreSubmitCallbackDispatcher.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/PreSubmitCallbackDispatcher.java
@@ -41,6 +41,9 @@ public class PreSubmitCallbackDispatcher<T extends CaseData> {
                                     ) {
         List<PreSubmitCallbackHandler<T>> eligibleHandlers = new ArrayList<>();
 
+        log.info("Dispatching callback for event={}, callbackType={}, state={}",
+                callback.getEvent(), callbackType, callback.getCaseDetails().getState());
+
         for (PreSubmitCallbackHandler<T> callbackHandler : callbackHandlers) {
             if (callbackHandler.canHandle(callbackType, callback)) {
 
