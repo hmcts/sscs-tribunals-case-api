862bf6657a3fd5d5df50df41c99052f7793d9883
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2025-10-03 13:52:40 +0100

    Revert "curl update for debug"
    This reverts commit 3bced24373ab24d48d83d4f00602a6c69f0939fd.

--- bin/setup-role-assignments.sh
+++ bin/setup-role-assignments.sh
@@ -26,7 +26,7 @@ function send_curl_request() {
   local payload=$(cat "${json_file}")
   local url="https://orm-sscs-tribunals-api-pr-${CHANGE_ID}.preview.platform.hmcts.net/am/testing-support/createOrgMapping?userType=${user_type}"
 
-  curl --silent --show-error -v "${url}" \
+  curl --silent --show-error --fail "${url}" \
   -H 'Content-Type: application/json' \
   -H "Authorization: Bearer ${IDAM_TOKEN}" \
   -H "ServiceAuthorization: ${S2S_TOKEN}" \
