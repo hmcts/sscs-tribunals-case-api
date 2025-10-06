6ed3d4aa2f27b5efd090a51505341199dade802c
tomparker-solirius <thomas.parker@hmcts.net>
2025-10-06 16:59:09 +0100

    add wa tag

--- definitions/test/e2e/work-allocation/review-incomplete-application.spec.ts
+++ definitions/test/e2e/work-allocation/review-incomplete-application.spec.ts
@@ -1,12 +1,10 @@
 import { test } from '../../lib/steps.factory';
 import createCaseBasedOnCaseType from '../../api/client/sscs/factory/appeal.type.factory';
-import { credentials } from '../../config/config';
 
 
-let caseId: string;
-test.describe('Work Allocation Feature', () => {
+test.describe('Work Allocation Feature', { tag: '@work-allocation' }, async () => {
   test('As a CTSC Team Leader I can assign a Review Incomplete Appeal task to myself and complete it', async ({ reviewIncompleteApplicationSteps }) => {
-    caseId = await createCaseBasedOnCaseType('PIPINCOMPLETE');
+    let caseId = await createCaseBasedOnCaseType('PIPINCOMPLETE');
     await reviewIncompleteApplicationSteps.assignTaskToSelf(caseId);
     await reviewIncompleteApplicationSteps.completeTask(caseId)
   });
