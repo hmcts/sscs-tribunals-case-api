a0a5fd8d7c4fb40b64533f60a14714a0e77d014f
hashimalisolirius <HashimAli@HMCTS.NET>
2025-11-03 10:08:34 +0000

    revert commit 9028f103

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/caseupdated/CaseUpdatedAboutToSubmitHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/caseupdated/CaseUpdatedAboutToSubmitHandler.java
@@ -63,6 +63,7 @@ import uk.gov.hmcts.reform.sscs.ccd.validation.address.PostcodeValidator;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 import uk.gov.hmcts.reform.sscs.idam.UserDetails;
 import uk.gov.hmcts.reform.sscs.model.CourtVenue;
+import uk.gov.hmcts.reform.sscs.model.VenueDetails;
 import uk.gov.hmcts.reform.sscs.model.dwp.OfficeMapping;
 import uk.gov.hmcts.reform.sscs.reference.data.service.HearingDurationsService;
 import uk.gov.hmcts.reform.sscs.reference.data.service.PanelCompositionService;
@@ -441,13 +442,23 @@ public class CaseUpdatedAboutToSubmitHandler extends ResponseEventsAboutToSubmit
         String venue = airLookupService.lookupAirVenueNameByPostCode(postCode, sscsCaseData.getAppeal().getBenefitType());
 
         if (venue != null && !venue.equalsIgnoreCase(sscsCaseData.getProcessingVenue())) {
-            log.info("Processing venue requires updating for case {}: setting venue name to {} from {}", caseDetails.getId(), venue,
-                sscsCaseData.getProcessingVenue());
+            String venueEpimsId = venueService.getEpimsIdForVenue(venue);
+            VenueDetails newVenue = venueService.getVenueDetailsForActiveVenueByEpimsId(venueEpimsId);
+            if (nonNull(newVenue)) {
+                if (isEmpty(newVenue.getLegacyVenue()) || !Objects.equals(newVenue.getLegacyVenue(), sscsCaseData.getProcessingVenue())) {
+                    log.info("Processing venue requires updating for case {}: setting venue name to {} from {}",
+                            caseDetails.getId(), venue, sscsCaseData.getProcessingVenue());
+                    sscsCaseData.setProcessingVenue(venue);
+                } else {
+                    venue = sscsCaseData.getProcessingVenue();
+                    venueEpimsId = venueService.getEpimsIdForVenue(venue);
+                    log.info("Legacy venue {} has not been updated for case {}",
+                            venue, caseDetails.getId());
+                }
 
-            sscsCaseData.setProcessingVenue(venue);
+            }
 
             if (isNotEmpty(venue)) {
-                String venueEpimsId = venueService.getEpimsIdForVenue(venue);
                 CourtVenue courtVenue = refDataService.getCourtVenueRefDataByEpimsId(venueEpimsId);
 
                 sscsCaseData.setCaseManagementLocation(CaseManagementLocation.builder()
