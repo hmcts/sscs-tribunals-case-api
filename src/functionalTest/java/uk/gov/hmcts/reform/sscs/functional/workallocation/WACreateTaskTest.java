48bdf4df1eac195b4a9e205570959a574759b8cf
Ravi Aghera <Ravi.Aghera@HMCTS.net>
2025-11-04 11:21:57 +0000

    Playing with UC Decision Notice case creation

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/workallocation/WACreateTaskTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/workallocation/WACreateTaskTest.java
@@ -12,9 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Benefit;
+import uk.gov.hmcts.reform.sscs.ccd.domain.BenefitType;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.State;
+import uk.gov.hmcts.reform.sscs.ccd.domain.YesNo;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
 import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
@@ -25,10 +28,13 @@ import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.Benefit.*;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.ADMIN_APPEAL_WITHDRAWN;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.CREATE_TEST_CASE;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.READY_TO_LIST;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.SYA_APPEAL_CREATED;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.UPDATE_CASE_ONLY;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.YesNo.*;
 import static uk.gov.hmcts.reform.sscs.transform.deserialize.SubmitYourAppealToCcdCaseDataDeserializer.convertSyaToCcdCaseDataV2;
 import static uk.gov.hmcts.reform.sscs.util.SyaJsonMessageSerializer.ALL_DETAILS;
 
@@ -110,6 +116,60 @@ public class WACreateTaskTest {
     }
 
     @Test
+    public void shouldCreateUCCaseWithCorrectData() {
+        //Get and update test data
+        SscsCaseData caseData =
+            convertSyaToCcdCaseDataV2(ALL_DETAILS.getDeserializeMessage(),
+                false,
+                new SscsCaseData());
+
+        // Create case in CCD and verify
+        SscsCaseDetails caseDetails =
+            ccdService.createCase(caseData,
+                CREATE_TEST_CASE.getCcdType(),
+                "SSCS: Creating a Test Case from FT",
+                "UC Test Case",
+                idamTokens
+            );
+
+        assertNotNull(caseDetails);
+        assertNotNull(caseDetails.getState());
+        assertEquals(State.VALID_APPEAL.getId(), caseDetails.getState());
+
+        BenefitType ucBenefitType = BenefitType.builder()
+            .code(UC.getShortName())
+            .description(UC.getDescription())
+            .build();
+
+        caseData.setWcaAppeal(YES);
+        caseData.setBenefitCode(UC.getBenefitCode());
+        caseData.getAppeal().setBenefitType(ucBenefitType);
+
+        // Run another event to update the Case data and change the State
+        SscsCaseData updatedCaseData =
+            ccdService.updateCase(caseData,
+                caseDetails.getId(),
+                UPDATE_CASE_ONLY.getCcdType(), //The user triggering the event needs to have permissions
+                "Update Case from functional test",
+                "Test case",
+                idamTokens).getData();
+
+        assertEquals(YES, updatedCaseData.getWcaAppeal());
+        assertEquals(ucBenefitType, updatedCaseData.getAppeal().getBenefitType());
+        assertEquals(UC.getBenefitCode(), updatedCaseData.getBenefitCode());
+
+        SscsCaseDetails fullUpdatedCase =
+            ccdService.getByCaseId(Long.valueOf(updatedCaseData.getCcdCaseId()), idamTokens);
+
+        assertEquals(State.VALID_APPEAL.getId(), fullUpdatedCase.getState());
+
+        //Run Write Final Appeal Event with populated data
+        //OR trigger the callbacks with populated fields
+
+
+    }
+
+    @Test
     public void shouldCreateCaseInCcdThenChangeTheCaseStateAndSomeAttributes_Alternative() {
         //Get and update test data
         SscsCaseData caseData =
@@ -169,29 +229,29 @@ public class WACreateTaskTest {
         //Wait for Task Management to do its thing
         //Fetch the Task record and verify it is correct
 
-        String taskId = "12345"; //placeholder
-        idamTokens = idamService.getIdamTokens();
-        log.info("idamTokens.getUserId()" + idamTokens.getUserId());
-        log.info("idamTokens.getServiceAuthorization()" + idamTokens.getServiceAuthorization());
-        log.info("idamTokens.getIdamOauth2Token()" + idamTokens.getIdamOauth2Token());
-
-        Headers headers = new Headers(
-            new Header("ServiceAuthorization", "" + idamTokens.getServiceAuthorization()),
-            new Header("Authorization", idamTokens.getIdamOauth2Token()),
-            new Header("Content-Type", "application/json")
-        );
-
-        Response result = tmClient.get(
-            TM_SEARCH_TASKS_PATH + TM_SEARCH_PARAMS,
-            ,
-            headers
-        );
-
-        result.then().assertThat()
-            .statusCode(HttpStatus.OK.value())
-            .body("tasks.size()", lessThanOrEqualTo(10))
-            .body("tasks.id", everyItem(notNullValue()));
-
+        //String taskId = "12345"; //placeholder
+        //idamTokens = idamService.getIdamTokens();
+        //log.info("idamTokens.getUserId()" + idamTokens.getUserId());
+        //log.info("idamTokens.getServiceAuthorization()" + idamTokens.getServiceAuthorization());
+        //log.info("idamTokens.getIdamOauth2Token()" + idamTokens.getIdamOauth2Token());
+        //
+        //Headers headers = new Headers(
+        //    new Header("ServiceAuthorization", "" + idamTokens.getServiceAuthorization()),
+        //    new Header("Authorization", idamTokens.getIdamOauth2Token()),
+        //    new Header("Content-Type", "application/json")
+        //);
+        //
+        //Response result = tmClient.get(
+        //    TM_SEARCH_TASKS_PATH + TM_SEARCH_PARAMS,
+        //    ,
+        //    headers
+        //);
+        //
+        //result.then().assertThat()
+        //    .statusCode(HttpStatus.OK.value())
+        //    .body("tasks.size()", lessThanOrEqualTo(10))
+        //    .body("tasks.id", everyItem(notNullValue()));
+        //
 
     }
 
