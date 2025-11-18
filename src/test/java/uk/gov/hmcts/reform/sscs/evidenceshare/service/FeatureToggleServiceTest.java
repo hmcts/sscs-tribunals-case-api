00f344795df136bc6c4d7114e66bc7fbacf3b603
Ravi Aghera <Ravi.Aghera@HMCTS.net>
2025-11-18 10:40:34 +0000

    update tests

--- src/test/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/FeatureToggleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/FeatureToggleServiceTest.java
@@ -1,8 +1,7 @@
 package uk.gov.hmcts.reform.sscs.evidenceshare.service;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyString;
@@ -13,6 +12,8 @@ import com.launchdarkly.sdk.server.LDClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.sscs.featureflag.FeatureFlag;
@@ -25,40 +26,24 @@ class FeatureToggleServiceTest {
     @Mock
     private LDClient ldClient;
 
-    private final String ldUserKey = "some-user-key";
-
     @BeforeEach
     void setUp() {
-        featureToggleService = new FeatureToggleService(ldClient, ldUserKey);
+        featureToggleService = new FeatureToggleService(ldClient, "some-user-key");
     }
 
-    @Test
-    void shouldGetTrueBooleanFeatureFlag() {
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void shouldGetTrueBooleanFeatureFlag(boolean expected) {
 
         when(ldClient.boolVariation(anyString(), any(LDContext.class), eq(Boolean.FALSE)))
-            .thenReturn(true);
+            .thenReturn(expected);
 
         boolean result = featureToggleService.getBooleanValue(
             FeatureFlag.SSCS_CHILD_MAINTENANCE_FT,
             "some-user-id",
             "example@email.com");
 
-        assertTrue(result);
-
-    }
-
-    @Test
-    void shouldGetFalseBooleanFeatureFlag() {
-
-        when(ldClient.boolVariation(anyString(), any(LDContext.class), eq(Boolean.FALSE)))
-            .thenReturn(false);
-
-        boolean result = featureToggleService.getBooleanValue(
-            FeatureFlag.SSCS_CHILD_MAINTENANCE_FT,
-            "some-user-id",
-            "example@email.com");
-
-        assertFalse(result);
+        assertEquals(expected, result);
 
     }
 
