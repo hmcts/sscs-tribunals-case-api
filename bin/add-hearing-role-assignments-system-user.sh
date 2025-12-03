e7f599939b7d1652b1f93be82f8e4cc4cbc6da41
nitinprabhuhmcts <nitin.prabhu2@hmcts.net>
2025-12-03 16:40:07 +0000

    Fix unbound variable

--- bin/add-hearing-role-assignments-system-user.sh
+++ bin/add-hearing-role-assignments-system-user.sh
@@ -24,7 +24,7 @@ curl --silent --show-error -X POST "https://am-role-assignment-service-sscs-trib
         },
         "requestedRoles": [
           {
-            "actorId": "'"${USER_ID}"'",
+            "actorId": "'"${SYSTEM_USER_ID}"'",
             "roleType": "ORGANISATION",
             "classification": "PUBLIC",
             "roleName": "hearing-manager",
@@ -37,7 +37,7 @@ curl --silent --show-error -X POST "https://am-role-assignment-service-sscs-trib
             "actorIdType": "IDAM"
           },
           {
-            "actorId": "'"${USER_ID}"'",
+            "actorId": "'"${SYSTEM_USER_ID}"'",
             "roleType": "ORGANISATION",
             "classification": "PUBLIC",
             "roleName": "hearing-viewer",
