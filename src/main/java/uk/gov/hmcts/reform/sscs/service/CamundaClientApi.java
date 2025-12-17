4ea5a52ffe05cda61ce7832a1a6c308cd201d079
Gemma Talbot <gemma.talbot@solirius.com>
2025-12-17 12:14:59 +0000

    url

--- src/main/java/uk/gov/hmcts/reform/sscs/service/CamundaClientApi.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/CamundaClientApi.java
@@ -14,9 +14,7 @@ import uk.gov.hmcts.reform.sscs.domain.CamundaTask;
 
 @FeignClient(
         name = "camunda",
-        url = "https://camunda-sscs-tribunals-api-pr-4961.preview.platform.hmcts.net/engine-rest"
-                +
-                "https://wa-task-management-api-sscs-tribunals-api-pr-4961.preview.platform.hmcts.net/task"
+        url = "https://wa-task-management-api-sscs-tribunals-api-pr-4961.preview.platform.hmcts.net"
 )
 @SuppressWarnings("PMD.UseObjectForClearerAPI")
 public interface CamundaClientApi {
