2e3f342798a16d1b6265c213d88ea63e55a87db3
Stuart Wilson <stuart.wilson@hmtcs.net>
2025-12-18 12:23:05 +0000

    Revert "SSCSFT-67 Add 'Confidentiality' to the Reasons Referred to Interloc when in send to interloc"
    This reverts commit cf81055d7a53e47ca24e2a4c62db3d29e8b9f570.

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/uc/scenarios/Scenario12Content.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/uc/scenarios/Scenario12Content.java
@@ -10,14 +10,11 @@ public class Scenario12Content extends UcTemplateContent {
 
     public Scenario12Content(WriteFinalDecisionTemplateBody writeFinalDecisionTemplateBody) {
         addComponent(new Paragraph(WriteFinalDecisionComponentId.ALLOWED_OR_REFUSED_PARAGRAPH.name(), getAllowedOrRefusedSentence(writeFinalDecisionTemplateBody.isAllowed())));
-        addComponent(new Paragraph(WriteFinalDecisionComponentId.CONFIRMED_OR_SET_ASIDE_PARAGRAPH.name(),
-            getConfirmedOrSetAsideSentence(writeFinalDecisionTemplateBody.isSetAside(), writeFinalDecisionTemplateBody.getDateOfDecision(), writeFinalDecisionTemplateBody.isHmrc(),
-                writeFinalDecisionTemplateBody.isIbca())));
-        addComponent(new Paragraph(UcTemplateComponentId.DOES_HAVE_LIMITED_CAPABILITY_FOR_WORK_PARAGRAPH.name(),
-            getDoesHaveLimitedCapabilityForWorkSentence(writeFinalDecisionTemplateBody.getAppellantName(), false, true, true, true,
-                writeFinalDecisionTemplateBody.getUcCapabilityAssessmentStartDate())));
-        addComponent(new Paragraph(UcTemplateComponentId.INSUFFICIENT_POINTS_PARAGRAPH.name(),
-            getSchedule6PointsSentence(writeFinalDecisionTemplateBody.getUcNumberOfPoints(), true, writeFinalDecisionTemplateBody.getUcSchedule6Descriptors())));
+        addComponent(new Paragraph(
+            WriteFinalDecisionComponentId.CONFIRMED_OR_SET_ASIDE_PARAGRAPH.name(), getConfirmedOrSetAsideSentence(writeFinalDecisionTemplateBody.isSetAside(), writeFinalDecisionTemplateBody.getDateOfDecision(), writeFinalDecisionTemplateBody.isHmrc(), writeFinalDecisionTemplateBody.isIbca())));
+        addComponent(new Paragraph(
+            UcTemplateComponentId.DOES_HAVE_LIMITED_CAPABILITY_FOR_WORK_PARAGRAPH.name(), getDoesHaveLimitedCapabilityForWorkSentence(writeFinalDecisionTemplateBody.getAppellantName(), false, true, true, true)));
+        addComponent(new Paragraph(UcTemplateComponentId.INSUFFICIENT_POINTS_PARAGRAPH.name(), getSchedule6PointsSentence(writeFinalDecisionTemplateBody.getUcNumberOfPoints(), true, writeFinalDecisionTemplateBody.getUcSchedule6Descriptors())));
         addDescriptorTableIfPopulated(new DescriptorTable(UcTemplateComponentId.SCHEDULE_6_DESCRIPTORS.name(), writeFinalDecisionTemplateBody.getUcSchedule6Descriptors(), false));
         addComponent(new Paragraph(UcTemplateComponentId.SCHEDULE_7_PARAGRAPH.name(), getNoSchedule7SentenceSchedule9Paragraph4Applies()));
         addComponent(new Paragraph(UcTemplateComponentId.DISEASE_OR_DISABLEMENT_PARAGRAPH.name(), getSchedule8Paragraph4AndSchedule9Paragraph4DiseaseOrDisablementSentence(false, true)));
--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/uc/scenarios/UcScenario7Test.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/uc/scenarios/UcScenario7Test.java
@@ -1,12 +1,9 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.scenarios;
 
-import static java.util.Collections.singletonList;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.uc.UcTemplateContent;
 import uk.gov.hmcts.reform.sscs.model.docassembly.Descriptor;
 import uk.gov.hmcts.reform.sscs.model.docassembly.WriteFinalDecisionTemplateBody;
@@ -16,110 +13,108 @@ public class UcScenario7Test {
     @Test
     public void testScenario7() {
 
-        List<Descriptor> schedule6Descriptors = singletonList(
-            Descriptor.builder().activityQuestionValue("Mobilising Unaided").activityAnswerValue("1").activityAnswerLetter("c").activityAnswerPoints(9).build());
+        List<Descriptor> schedule6Descriptors =
+            Arrays.asList(Descriptor.builder()
+                .activityQuestionValue("Mobilising Unaided")
+                .activityAnswerValue("1")
+                .activityAnswerLetter("c").activityAnswerPoints(9).build());
 
-        WriteFinalDecisionTemplateBody body = WriteFinalDecisionTemplateBody.builder()
-            .hearingType("faceToFace")
-            .attendedHearing(true)
-            .presentingOfficerAttended(true)
-            .isAllowed(true)
-            .isSetAside(true)
-            .dateOfDecision("2020-09-20")
-            .ucNumberOfPoints(9)
-            .pageNumber("A1")
-            .appellantName("Felix Sydney")
-            .reasonsForDecision(Arrays.asList("My first reasons", "My second reasons"))
-            .anythingElse("Something else")
-            .schedule8Paragraph4Applicable(true)
-            .ucCapabilityAssessmentStartDate(LocalDate.of(2025, 11, 18))
-            .ucSchedule6Descriptors(schedule6Descriptors)
-            .build();
+        WriteFinalDecisionTemplateBody body =
+            WriteFinalDecisionTemplateBody.builder()
+                .hearingType("faceToFace")
+                .attendedHearing(true)
+                .presentingOfficerAttended(true)
+                .isAllowed(true)
+                .isSetAside(true)
+                .dateOfDecision("2020-09-20")
+                .ucNumberOfPoints(9)
+                .pageNumber("A1")
+                .appellantName("Felix Sydney")
+                .reasonsForDecision(Arrays.asList("My first reasons", "My second reasons"))
+                .anythingElse("Something else")
+                .schedule8Paragraph4Applicable(true)
+                .ucSchedule6Descriptors(schedule6Descriptors).build();
 
         UcTemplateContent content = UcScenario.SCENARIO_7.getContent(body);
 
-        String expectedContent = """
-            The appeal is allowed.
-            
-            The decision made by the Secretary of State on 20/09/2020 is set aside.
-            
-            Felix Sydney is to be treated as having limited capability for work from 18/11/2025.
-            
-            This is because insufficient points were scored to meet the threshold for the Work Capability Assessment, but Schedule 8, paragraph 4 of the Universal Credit (UC) Regulations 2013 applied.
-            
-            Mobilising Unaided\tc.1\t9
-            
-            
-            The tribunal applied Schedule 8, paragraph 4 because there would be a substantial risk to the mental or physical health of any person if the appellant were found not to have limited capability for work.
-            
-            Felix Sydney does not have limited capability for work-related activity because no descriptor from Schedule 7 of the UC Regulations applied. Schedule 9, paragraph 4 did not apply.
-            
-            My first reasons
-            
-            My second reasons
-            
-            Something else
-            
-            This has been an oral (face to face) hearing. Felix Sydney the appellant attended the hearing today and the Tribunal considered the appeal bundle to page A1. First Tier Agency representative attended on behalf of the Respondent.
-            
-            """;
+        String expectedContent = "The appeal is allowed.\n"
+            + "\n"
+            + "The decision made by the Secretary of State on 20/09/2020 is set aside.\n"
+            + "\n"
+            + "Felix Sydney is to be treated as having limited capability for work. The matter is now remitted to the Secretary of State to make a final decision upon entitlement to Universal Credit (UC).\n"
+            + "\n"
+            + "This is because insufficient points were scored to meet the threshold for the Work Capability Assessment, but Schedule 8, paragraph 4 of the UC Regulations 2013 applied.\n"
+            + "\n"
+            + "Mobilising Unaided\tc.1\t9\n"
+            + "\n"
+            + "\n"
+            + "The tribunal applied Schedule 8, paragraph 4 because there would be a substantial risk to the mental or physical health of any person if the appellant were found not to have limited capability for work.\n"
+            + "\n"
+            + "Felix Sydney does not have limited capability for work-related activity because no descriptor from Schedule 7 of the UC Regulations applied. Schedule 9, paragraph 4 did not apply.\n"
+            + "\n"
+            + "My first reasons\n"
+            + "\n"
+            + "My second reasons\n"
+            + "\n"
+            + "Something else\n"
+            + "\n"
+            + "This has been an oral (face to face) hearing. Felix Sydney the appellant attended the hearing today and the Tribunal considered the appeal bundle to page A1. First Tier Agency representative attended on behalf of the Respondent.\n"
+            + "\n";
 
-        assertThat(content.getComponents()).hasSize(11);
+        Assert.assertEquals(11, content.getComponents().size());
 
-        assertThat(content.toString()).isEqualTo(expectedContent);
+        Assert.assertEquals(expectedContent, content.toString());
 
     }
 
     @Test
     public void testScenario7WhenNoSchedule6() {
 
-        List<Descriptor> schedule6Descriptors = List.of();
+        List<Descriptor> schedule6Descriptors =
+            Arrays.asList();
 
-        WriteFinalDecisionTemplateBody body = WriteFinalDecisionTemplateBody.builder()
-            .hearingType("faceToFace")
-            .attendedHearing(true)
-            .presentingOfficerAttended(true)
-            .isAllowed(true)
-            .isSetAside(true)
-            .dateOfDecision("2020-09-20")
-            .ucNumberOfPoints(0)
-            .pageNumber("A1")
-            .appellantName("Felix Sydney")
-            .reasonsForDecision(Arrays.asList("My first reasons", "My second reasons"))
-            .anythingElse("Something else")
-            .schedule8Paragraph4Applicable(true)
-            .ucCapabilityAssessmentStartDate(LocalDate.of(2025, 11, 18))
-            .ucSchedule6Descriptors(schedule6Descriptors)
-            .build();
+        WriteFinalDecisionTemplateBody body =
+            WriteFinalDecisionTemplateBody.builder()
+                .hearingType("faceToFace")
+                .attendedHearing(true)
+                .presentingOfficerAttended(true)
+                .isAllowed(true)
+                .isSetAside(true)
+                .dateOfDecision("2020-09-20")
+                .ucNumberOfPoints(0)
+                .pageNumber("A1")
+                .appellantName("Felix Sydney")
+                .reasonsForDecision(Arrays.asList("My first reasons", "My second reasons"))
+                .anythingElse("Something else")
+                .schedule8Paragraph4Applicable(true)
+                .ucSchedule6Descriptors(schedule6Descriptors).build();
 
         UcTemplateContent content = UcScenario.SCENARIO_7.getContent(body);
 
-        String expectedContent = """
-            The appeal is allowed.
-            
-            The decision made by the Secretary of State on 20/09/2020 is set aside.
-            
-            Felix Sydney is to be treated as having limited capability for work from 18/11/2025.
-            
-            This is because insufficient points were scored to meet the threshold for the Work Capability Assessment, but Schedule 8, paragraph 4 of the Universal Credit (UC) Regulations 2013 applied.
-            
-            The tribunal applied Schedule 8, paragraph 4 because there would be a substantial risk to the mental or physical health of any person if the appellant were found not to have limited capability for work.
-            
-            Felix Sydney does not have limited capability for work-related activity because no descriptor from Schedule 7 of the UC Regulations applied. Schedule 9, paragraph 4 did not apply.
-            
-            My first reasons
-            
-            My second reasons
-            
-            Something else
-            
-            This has been an oral (face to face) hearing. Felix Sydney the appellant attended the hearing today and the Tribunal considered the appeal bundle to page A1. First Tier Agency representative attended on behalf of the Respondent.
-            
-            """;
+        String expectedContent = "The appeal is allowed.\n"
+            + "\n"
+            + "The decision made by the Secretary of State on 20/09/2020 is set aside.\n"
+            + "\n"
+            + "Felix Sydney is to be treated as having limited capability for work. The matter is now remitted to the Secretary of State to make a final decision upon entitlement to Universal Credit (UC).\n"
+            + "\n"
+            + "This is because insufficient points were scored to meet the threshold for the Work Capability Assessment, but Schedule 8, paragraph 4 of the UC Regulations 2013 applied.\n"
+            + "\n"
+            + "The tribunal applied Schedule 8, paragraph 4 because there would be a substantial risk to the mental or physical health of any person if the appellant were found not to have limited capability for work.\n"
+            + "\n"
+            + "Felix Sydney does not have limited capability for work-related activity because no descriptor from Schedule 7 of the UC Regulations applied. Schedule 9, paragraph 4 did not apply.\n"
+            + "\n"
+            + "My first reasons\n"
+            + "\n"
+            + "My second reasons\n"
+            + "\n"
+            + "Something else\n"
+            + "\n"
+            + "This has been an oral (face to face) hearing. Felix Sydney the appellant attended the hearing today and the Tribunal considered the appeal bundle to page A1. First Tier Agency representative attended on behalf of the Respondent.\n"
+            + "\n";
 
-        assertThat(content.getComponents()).hasSize(10);
+        Assert.assertEquals(10, content.getComponents().size());
 
-        assertThat(content.toString()).isEqualTo(expectedContent);
+        Assert.assertEquals(expectedContent, content.toString());
 
     }
 }
