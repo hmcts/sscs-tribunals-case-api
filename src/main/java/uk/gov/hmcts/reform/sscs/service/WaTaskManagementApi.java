29a48750aa9a6519b3735a1908e988267be1a614
Gemma Talbot <gemma.talbot@solirius.com>
2026-01-06 16:45:20 +0000

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/sscs/service/WaTaskManagementApi.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/WaTaskManagementApi.java
@@ -4,7 +4,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
