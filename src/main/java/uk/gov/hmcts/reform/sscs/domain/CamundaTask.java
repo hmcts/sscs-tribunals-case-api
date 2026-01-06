49eb7ad5112b39ae54b4b450750b6a71588ae711
Gemma Talbot <gemma.talbot@solirius.com>
2026-01-06 11:19:13 +0000

    Task properties

--- src/main/java/uk/gov/hmcts/reform/sscs/domain/CamundaTask.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/domain/CamundaTask.java
@@ -1,23 +1,56 @@
 package uk.gov.hmcts.reform.sscs.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.time.ZonedDateTime;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @ToString
 @EqualsAndHashCode
+@Builder
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CamundaTask {
-    private final String id;
-    private final String name;
-    private final String processInstanceId;
 
-    public CamundaTask(@JsonProperty("id") String id,
-                       @JsonProperty("name") String name,
-                       @JsonProperty("processInstanceId") String processInstanceId) {
+    private String id;
+    private String name;
+    private String assignee;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private ZonedDateTime created;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private ZonedDateTime due;
+    private String description;
+    private String owner;
+    private String formKey;
+    private String processInstanceId;
+
+    private CamundaTask() {
+        //Hidden constructor
+        super();
+    }
+
+    public CamundaTask(String id,
+                       String name,
+                       String assignee,
+                       ZonedDateTime created,
+                       ZonedDateTime due,
+                       String description,
+                       String owner,
+                       String formKey,
+                       String processInstanceId
+    ) {
         this.id = id;
         this.name = name;
+        this.assignee = assignee;
+        this.created = created;
+        this.due = due;
+        this.description = description;
+        this.owner = owner;
+        this.formKey = formKey;
         this.processInstanceId = processInstanceId;
     }
+
 }
