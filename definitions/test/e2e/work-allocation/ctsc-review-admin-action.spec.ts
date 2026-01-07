431597cbc929c482babd3a911f225615f9922d8c
tomparker-solirius <thomas.parker@hmcts.net>
2026-01-07 15:53:07 +0000

    add tags

--- definitions/test/e2e/work-allocation/ctsc-review-admin-action.spec.ts
+++ definitions/test/e2e/work-allocation/ctsc-review-admin-action.spec.ts
@@ -2,9 +2,9 @@ import { test } from "../../lib/steps.factory";
 import createCaseBasedOnCaseType from "../../api/client/sscs/factory/appeal.type.factory";
 import performAppealDormantOnCase from "../../api/client/sscs/appeal.event";
 
-let caseId : string;
+let caseId: string;
 
-test.describe.serial('Work Allocation - CTSC - Review Admin action task tests', async () => {
+test.describe.serial('Work Allocation - CTSC - Review Admin action task tests', { tag: ['@work-allocation', '@preview-regression'] }, async () => {
 
     test("As a CSTC Admin without case allocator role, review admin action task", async ({ ctscReviewAdminActionSteps }) => {
         test.slow();
@@ -23,12 +23,12 @@ test.describe.serial('Work Allocation - CTSC - Review Admin action task tests', 
         await ctscReviewAdminActionSteps.verifyCtscAdminWithoutCaseAllocatorRoleCanCompleteReviewAdminActionTask(caseId);
     });
 
-    // test.afterAll("Case has to be set to Dormant",async () => {
-    //     await performAppealDormantOnCase(caseId);
-    // });
+    test.afterAll("Case has to be set to Dormant", async () => {
+        await performAppealDormantOnCase(caseId);
+    });
 });
 
-test("Review Admin Action task is cancelled automatically when case is void", async ({ ctscReviewAdminActionSteps }) => {
+test("Review Admin Action task is cancelled automatically when case is void", { tag: ['@work-allocation', '@preview-regression'] }, async ({ ctscReviewAdminActionSteps }) => {
     test.slow();
     caseId = await createCaseBasedOnCaseType('PIP');
     await ctscReviewAdminActionSteps.createReviewAdminActionTask(caseId);
