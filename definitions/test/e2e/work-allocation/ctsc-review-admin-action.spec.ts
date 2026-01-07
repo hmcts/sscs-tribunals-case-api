6a6742f2163c25440f5fa99a022f1490b91c37b7
tomparker-solirius <thomas.parker@hmcts.net>
2026-01-07 15:29:28 +0000

    add tests for review admin action

---
+++ definitions/test/e2e/work-allocation/ctsc-review-admin-action.spec.ts
@@ -0,0 +1,36 @@
+import { test } from "../../lib/steps.factory";
+import createCaseBasedOnCaseType from "../../api/client/sscs/factory/appeal.type.factory";
+import performAppealDormantOnCase from "../../api/client/sscs/appeal.event";
+
+let caseId : string;
+
+test.describe.serial('Work Allocation - CTSC - Review Admin action task tests', async () => {
+
+    test("As a CSTC Admin without case allocator role, review admin action task", async ({ ctscReviewAdminActionSteps }) => {
+        test.slow();
+        caseId = await createCaseBasedOnCaseType('PIP');
+        await ctscReviewAdminActionSteps.createReviewAdminActionTask(caseId);
+        await ctscReviewAdminActionSteps.verifyCtscAdminWithoutCaseAllocatorRoleCanViewReviewAdminActionTask(caseId);
+    });
+
+    test("As a CSTC Admin with case allocator role, review admin action task", async ({ ctscReviewAdminActionSteps }) => {
+        test.slow();
+        await ctscReviewAdminActionSteps.verifyCtscAdminWithCaseAllocatorRoleCanViewReviewAdminActionTask(caseId);
+    });
+
+    test("As a CSTC Administrator, complete review admin action task", async ({ ctscReviewAdminActionSteps }) => {
+        test.slow();
+        await ctscReviewAdminActionSteps.verifyCtscAdminWithoutCaseAllocatorRoleCanCompleteReviewAdminActionTask(caseId);
+    });
+
+    // test.afterAll("Case has to be set to Dormant",async () => {
+    //     await performAppealDormantOnCase(caseId);
+    // });
+});
+
+test("Review Admin Action task is cancelled automatically when case is void", async ({ ctscReviewAdminActionSteps }) => {
+    test.slow();
+    caseId = await createCaseBasedOnCaseType('PIP');
+    await ctscReviewAdminActionSteps.createReviewAdminActionTask(caseId);
+    await ctscReviewAdminActionSteps.verifyReviewAdminActionTaskIsCancelledAutomaticallyWhenTheCaseIsVoid(caseId);
+});
