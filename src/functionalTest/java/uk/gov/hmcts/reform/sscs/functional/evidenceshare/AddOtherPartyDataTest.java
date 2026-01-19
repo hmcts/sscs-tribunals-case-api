0325c23a231c175debf00ea22d85d145919e6881
alibase <alibase@gmail.com>
2026-01-19 18:03:38 +0000

    SSCSFT-102 Provide Other Party Details

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/evidenceshare/AddOtherPartyDataTest.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.sscs.functional.evidenceshare;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Benefit;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AddOtherPartyDataTest extends AbstractFunctionalTest {
+
+    @Nested
+    class CmToggleOn {
+        @ParameterizedTest
+        @CsvSource({"CHILD_SUPPORT,VALID_APPEAL_CREATED,awaitOtherPartyData"
+             })
+        @SneakyThrows
+        void shouldTransitionToCorrectStateForValidAppealEventBasedOnBenefit(Benefit benefit, EventType eventType,
+                                                                             String expectedState) {
+
+            final SscsCaseDetails caseWithState = createCaseFromEvent(benefit, eventType);
+
+            defaultAwait().untilAsserted(() -> {
+                SscsCaseDetails caseDetails = findCaseById(Long.toString(caseWithState.getId()));
+                assertThat(caseDetails.getState()).isEqualTo(expectedState);
+            });
+        }
+    }
+
+    @Nested
+    class CmToggleOff {
+        @Test
+        void shouldSendToDwpForValidAppealEvent() {
+
+        }
+
+    }
+
+}
