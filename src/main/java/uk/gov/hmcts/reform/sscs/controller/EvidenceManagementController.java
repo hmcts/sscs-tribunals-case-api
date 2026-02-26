e9eca56e53aa694c6f9fc573008fe31d7d3bb14f
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2026-02-26 18:18:39 +0000

    add service authorisation to evidence upload endpoint

--- src/main/java/uk/gov/hmcts/reform/sscs/controller/EvidenceManagementController.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/controller/EvidenceManagementController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +23,7 @@ import uk.gov.hmcts.reform.document.domain.UploadResponse;
 import uk.gov.hmcts.reform.sscs.exception.EvidenceDocumentsMissingException;
 import uk.gov.hmcts.reform.sscs.exception.FileToPdfConversionException;
 import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.service.AuthorisationService;
 import uk.gov.hmcts.reform.sscs.service.EvidenceManagementSecureDocStoreService;
 import uk.gov.hmcts.reform.sscs.service.EvidenceManagementService;
 import uk.gov.hmcts.reform.sscs.service.conversion.FileToPdfConversionService;
@@ -35,18 +37,23 @@ public class EvidenceManagementController {
     private final FileToPdfConversionService fileToPdfConversionService;
     private final boolean secureDocStoreEnabled;
     private final IdamService idamService;
+    private final AuthorisationService authorisationService;
+
+    private static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
 
     @Autowired
     public EvidenceManagementController(EvidenceManagementService evidenceManagementService,
                                         EvidenceManagementSecureDocStoreService evidenceManagementSecureDocStoreService,
                                         FileToPdfConversionService fileToPdfConversionService,
                                         @Value("${feature.secure-doc-store.enabled:false}") boolean secureDocStoreEnabled,
-                                        IdamService idamService) {
+                                        IdamService idamService,
+                                        AuthorisationService authorisationService) {
         this.evidenceManagementService = evidenceManagementService;
         this.evidenceManagementSecureDocStoreService = evidenceManagementSecureDocStoreService;
         this.fileToPdfConversionService = fileToPdfConversionService;
         this.secureDocStoreEnabled = secureDocStoreEnabled;
         this.idamService = idamService;
+        this.authorisationService = authorisationService;
     }
 
     @Operation(summary = "Upload additional evidence converted to PDF",
@@ -63,8 +70,12 @@ public class EvidenceManagementController {
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<String> upload(
-        @RequestParam("file") List<MultipartFile> files
+            @RequestHeader(value = SERVICE_AUTHORIZATION) String serviceAuthorization,
+            @RequestParam("file") List<MultipartFile> files
     ) throws JsonProcessingException {
+        String serviceName = authorisationService.authenticate(serviceAuthorization);
+        authorisationService.allowOnlySscs(serviceName);
+
         if (null == files || files.isEmpty()) {
             throw new EvidenceDocumentsMissingException();
         }
@@ -93,7 +104,7 @@ public class EvidenceManagementController {
 
 
         } catch (FileToPdfConversionException e) {
-            log.error("Error while converting files for evidence upload: " + e.getMessage());
+            log.error("Error while converting files for evidence upload: {}", e.getMessage());
             throw e;
         }
     }
