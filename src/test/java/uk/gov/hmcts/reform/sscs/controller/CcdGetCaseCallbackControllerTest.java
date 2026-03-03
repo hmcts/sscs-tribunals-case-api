1e18544cfad00e2dccd6fbd9ab36d39efc5ad760
Stuart Wilson <stuart.wilson@hmtcs.net>
2026-03-03 16:34:40 +0000

    SSCSFT-159 Remove unused Launch Darkly code and flag

--- src/test/java/uk/gov/hmcts/reform/sscs/controller/CcdGetCaseCallbackControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/controller/CcdGetCaseCallbackControllerTest.java
@@ -1,33 +1,23 @@
 package uk.gov.hmcts.reform.sscs.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.gov.hmcts.reform.sscs.service.AuthorisationService.SERVICE_AUTHORISATION_HEADER;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.ccd.client.model.GetCaseCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.metadatafields.CaseViewField;
 import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
@@ -38,39 +28,32 @@ import uk.gov.hmcts.reform.sscs.ccd.domain.State;
 import uk.gov.hmcts.reform.sscs.service.AuthorisationService;
 import uk.gov.hmcts.reform.sscs.service.GetCaseCallbackService;
 
-@SpringBootTest(classes = CcdGetCaseCallbackControllerTest.Config.class)
-@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
 class CcdGetCaseCallbackControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @MockitoBean
+    @Mock
     private AuthorisationService authorisationService;
-    @MockitoBean
+    @Mock
     private GetCaseCallbackService getCaseCallbackService;
+    @InjectMocks
+    private CcdGetCaseCallbackController controller;
 
     @Test
-    void shouldReturnGetCaseResponseWhenRequestIsValid() throws Exception {
+    void shouldReturnGetCaseResponseWhenRequestIsValid() {
         final GetCaseCallbackResponse response = new GetCaseCallbackResponse();
         final CaseViewField metadataField = new CaseViewField();
         metadataField.setId("[INJECTED_DATA.ENABLE_ADD_OTHER_PARTY_DATA]");
         metadataField.setValue("Yes");
         response.setMetadataFields(List.of(metadataField));
-        when(getCaseCallbackService.buildResponse(any())).thenReturn(response);
+        final Callback<SscsCaseData> callback = buildCallback();
+        when(getCaseCallbackService.buildResponse(callback)).thenReturn(response);
 
-        final MvcResult mvcResult = mockMvc.perform(post("/ccdGetCase")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header(SERVICE_AUTHORISATION_HEADER, "s2s-token")
-                .content(buildCallbackJson()))
-            .andExpect(status().isOk())
-            .andReturn();
+        final ResponseEntity<GetCaseCallbackResponse> result = controller.ccdGetCase("s2s-token", callback);
 
-        assertThat(mvcResult.getResponse().getContentAsString()).contains("\"metadataFields\"");
-        assertThat(mvcResult.getResponse().getContentAsString()).contains("\"value\":\"Yes\"");
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody()).isEqualTo(response);
         verify(authorisationService).authorise("s2s-token");
-        verify(getCaseCallbackService).buildResponse(any());
+        verify(getCaseCallbackService).buildResponse(callback);
     }
 
     @ParameterizedTest
@@ -79,33 +62,19 @@ class CcdGetCaseCallbackControllerTest {
         "true,false",
         "false,false"
     })
-    void shouldReturnBadRequestWhenRequiredInputsAreMissing(final boolean includeServiceAuthHeader,
-        final boolean includeBody) throws Exception {
-        final var requestBuilder = post("/ccdGetCase")
-            .contentType(MediaType.APPLICATION_JSON);
+    void shouldThrowWhenRequiredInputsAreMissing(final boolean includeServiceAuthHeader,
+                                                 final boolean includeBody) {
+        final String serviceAuthHeader = includeServiceAuthHeader ? "s2s-token" : null;
+        final Callback<SscsCaseData> callback = includeBody ? buildCallback() : null;
 
-        if (includeServiceAuthHeader) {
-            requestBuilder.header(SERVICE_AUTHORISATION_HEADER, "s2s-token");
-        }
-        if (includeBody) {
-            requestBuilder.content(buildCallbackJson());
-        }
-
-        mockMvc.perform(requestBuilder)
-            .andExpect(status().isBadRequest());
+        assertThatThrownBy(() -> controller.ccdGetCase(serviceAuthHeader, callback))
+            .isInstanceOf(NullPointerException.class);
 
         verifyNoInteractions(authorisationService);
         verifyNoInteractions(getCaseCallbackService);
     }
 
-    @SpringBootConfiguration
-    @ImportAutoConfiguration({JacksonAutoConfiguration.class, WebMvcAutoConfiguration.class})
-    @Import(CcdGetCaseCallbackController.class)
-    static class Config {
-
-    }
-
-    private String buildCallbackJson() throws Exception {
+    private Callback<SscsCaseData> buildCallback() {
         final SscsCaseData caseData = SscsCaseData.builder().build();
         final CaseDetails<SscsCaseData> caseDetails = new CaseDetails<>(
             1234L,
@@ -115,12 +84,6 @@ class CcdGetCaseCallbackControllerTest {
             LocalDateTime.now(),
             "Benefit"
         );
-        final Callback<SscsCaseData> callback = new Callback<>(
-            caseDetails,
-            Optional.empty(),
-            EventType.ACTION_FURTHER_EVIDENCE,
-            false
-        );
-        return objectMapper.writeValueAsString(callback);
+        return new Callback<>(caseDetails, Optional.empty(), EventType.ACTION_FURTHER_EVIDENCE, false);
     }
 }
