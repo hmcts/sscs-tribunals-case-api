2e382ad0c74199b7688360085066650678093b41
hemanta.sharma <hemanta.sharma1@hmcts.net>
2026-03-06 12:02:27 +0000

    SSCSFT-152 Add Confidentiality Confirmed Event flow

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/confidentialityconfirmed/ConfidentialityConfirmedAboutToSubmitHandler.java
@@ -0,0 +1,55 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.confidentialityconfirmed;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.Benefit.CHILD_SUPPORT;
+
+import java.util.Objects;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DwpState;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+
+@Service
+class ConfidentialityConfirmedAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    private final boolean cmOtherPartyConfidentialityEnabled;
+
+    public ConfidentialityConfirmedAboutToSubmitHandler(@Value("${feature.cm-other-party-confidentiality.enabled}") boolean cmOtherPartyConfidentialityEnabled) {
+        this.cmOtherPartyConfidentialityEnabled = cmOtherPartyConfidentialityEnabled;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        requireNonNull(callback, "callback must not be null");
+        requireNonNull(callbackType, "callbacktype must not be null");
+
+        if (!cmOtherPartyConfidentialityEnabled
+            || callbackType != CallbackType.ABOUT_TO_SUBMIT
+            || callback.getEvent() != EventType.CONFIDENTIALITY_CONFIRMED) {
+            return false;
+        }
+
+        return callback.getCaseDetails().getCaseData().isBenefitType(CHILD_SUPPORT);
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback, String userAuthorisation) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        SscsCaseData caseData = callback.getCaseDetails().getCaseData();
+        var preSubmitCallbackResponse = new PreSubmitCallbackResponse<>(caseData);
+
+        if (Objects.nonNull(caseData.getInterlocReviewState())) {
+            caseData.setInterlocReviewState(null);
+        }
+        caseData.setDwpState(DwpState.UNREGISTERED);
+        return preSubmitCallbackResponse;
+    }
+}
