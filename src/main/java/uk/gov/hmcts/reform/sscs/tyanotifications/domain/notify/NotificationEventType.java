9dbad15e08b07fae267031e1a6f6e7d767b64267
Stuart Wilson <stuart.wilson@hmtcs.net>
2026-03-24 16:11:58 +0000

    SSCSFT-96 Notification to Appellant

--- src/main/java/uk/gov/hmcts/reform/sscs/tyanotifications/domain/notify/NotificationEventType.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/tyanotifications/domain/notify/NotificationEventType.java
@@ -83,6 +83,12 @@ public enum NotificationEventType {
     // TODO put the delay back in
     VALID_APPEAL_CREATED(EventType.VALID_APPEAL_CREATED, true, true, false, true, false, 0),
 
+
+    ADMIN_SEND_TO_VALID_APPEAL(EventType.ADMIN_SEND_TO_VALID_APPEAL, false, false, false, true, false, 0),
+    VALID_APPEAL(EventType.VALID_APPEAL, false, false, false, true, false, 0),
+
+
+
     @JsonEnumDefaultValue
     DO_NOT_SEND(null);
 
