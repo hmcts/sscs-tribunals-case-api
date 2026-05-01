97a58aee84ee03893b5dcf92ef662f9badebdf41
stuart-hmcts <stuart.wilson@hmcts.net>
2026-05-01 14:50:12 +0100

    SSCSCI-2486 (#5241)
    * Removed unused dependencies
    
    * Enabled CM flag
    
    * Implemented CM flag status checks to conditionally execute tests and resolved a failure occurring when the flag is enabled.
    
    * Disabled CM flag
    
    * Enabled CM flag
    
    * Added tests for Add 'Confidentiality' to the Reasons Referred to Interloc when in send to interloc
    
    * Added tests for Child Maintenance features.
    
    * Removed the check to see if the super user is locked.
    
    * Added checks for flakiness
    
    * Disabled CM flag
    
    * Enabled CM flag
    
    * SSCSFT-167 DV option added to Other party details
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    Add state authorisation to allow testing of all user based scenarios
    
    * SSCSFT-179 Confidentiality Tab Refactor
    
    * SSCSFT-179 Confidentiality Tab Refactor
    
    * SSCSFT-179 Confidentiality Tab Refactor
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSFT-96 Notification to Appellant
    
    * SSCSFT-96 Notification to Appellant
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    * SSCSCI-2486 Merge CM UI Automation tests into Master
    
    Add concurrency retry
    
    * SSCSCI-2478 Apply Judges decision to Confidentiality tab
    
    * SSCSCI-2478 Apply Judges decision to Confidentiality tab
    
    * toggle CM Off
    
    * wip test
    
    * turn cm flag ON
    
    * refactored test
    
    * revert tag name
    
    ---------
    
    Co-authored-by: Ash Modi <ashish.modi1@hmcts.net>
    Co-authored-by: Stuart Wilson <stuart.wilson@hmtcs.net>
    Co-authored-by: gokul-sol <gokul.sridharan@HMCTS.NET>
    Co-authored-by: gokul <69240498+gokul-sol@users.noreply.github.com>

---
+++ definitions/test/e2e/send-to-interloc-cm-confidentiality-summary.spec.ts
@@ -0,0 +1,71 @@
+import { test } from '../lib/steps.factory';
+import createCaseBasedOnCaseType, {
+  createChildSupportCaseForCmConfidentiality
+} from '../api/client/sscs/factory/appeal.type.factory';
+import { credentials, featureFlags } from '../config/config';
+
+const appealTypes = ['UC', 'CHILDSUPPORT'] as const;
+
+test.describe('CM confidentiality send to interloc summary', () => {
+  test.skip(
+    !featureFlags.cmOtherPartyConfidentialityEnabled,
+    'CM confidentiality flag is disabled'
+  );
+
+  for (const appealType of appealTypes) {
+      test(
+        `Selected confidentiality reason is visible on summary for confidential ${appealType} appeal as Caseworker`,
+        { tag: ['@nightly-pipeline', '@confidentiality'] },
+        async ({
+          enhancedConfidentialitySteps,
+          sendToInterlocSteps,
+          updateOtherPartyDataSteps,
+          readyToListSteps,
+          uploadResponseSteps
+        }) => {
+          test.slow();
+
+          const caseId =
+            appealType === 'CHILDSUPPORT'
+              ? await createChildSupportCaseForCmConfidentiality()
+              : await createCaseBasedOnCaseType(appealType);
+
+          if (appealType === 'CHILDSUPPORT') {
+            await updateOtherPartyDataSteps.makeChildSupportCaseConfidential(
+              caseId
+            );
+          } else {
+            await uploadResponseSteps.performUploadResponseOnAUniversalCreditWithJP(
+              caseId,
+              false
+            );
+            await enhancedConfidentialitySteps.prepareConfidentialCase(caseId);
+          }
+
+          if (appealType === 'CHILDSUPPORT') {
+            await readyToListSteps.performReadyToListEvent(
+              caseId,
+              false,
+              credentials.amCaseWorker
+            );
+          } else {
+            await readyToListSteps.loginUserWithCaseId(
+              credentials.amCaseWorker,
+              true,
+              caseId
+            );
+            await readyToListSteps.performReadyToListEvent(
+              caseId,
+              false,
+              credentials.amCaseWorker
+            );
+          }
+          await readyToListSteps.signOut();
+          await sendToInterlocSteps.submitConfidentialityReferralAndVerifySummary(
+            caseId,
+            credentials.amCaseWorker
+          );
+        }
+      );
+    }
+});
--- definitions/test/pages/update.other.party.data.page.ts
+++ definitions/test/pages/update.other.party.data.page.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 import { WebAction } from '../common/web.action';
 import addUpdateOtherPartyData from './content/update.other.party.data_en.json';
 import addUpdateSubscriptionData from './content/update.subscription.data_en.json';
@@ -13,16 +13,30 @@ export class updateOtherPartyDataPage {
     webAction = new WebAction(this.page);
   }
 
-  async verifyPageContent() {
-    await webAction.verifyPageLabel(
-      '.govuk-caption-l',
-      addUpdateOtherPartyData.updateOtherPartyDataHeading
-    ); //Above heading Text
+  async verifyPageContent(
+    heading: string = addUpdateOtherPartyData.updateOtherPartyDataHeading
+  ) {
+    const captionCount = await this.page.locator('.govuk-caption-l').count();
+
+    if (captionCount > 0) {
+      await webAction.verifyPageLabel(
+        '.govuk-caption-l',
+        heading
+      );
+    } else {
+      await expect(
+        this.page.getByRole('heading', { name: heading, exact: true }).first()
+      ).toBeVisible();
+    }
+
     await webAction.isButtonClickable('Cancel');
   }
 
   // Applying other party data for the Mandatory fields only
-  async applyOtherPartyData( caseType: string = "NONIBC" ) {
+  async applyOtherPartyData(
+    caseType: string = "NONIBC",
+    confidentialityRequired: string = addUpdateOtherPartyData.updateOtherPartyDataConfidentiality
+  ) {
     await this.page.getByRole('button', { name: 'Add new' }).click(); //fields are expanded here
     await this.page.getByText('First Name');
     await this.page
@@ -46,7 +60,12 @@ export class updateOtherPartyDataPage {
       .locator('#otherParties_0_address_postcode')
       .fill(addUpdateOtherPartyData.updateOtherPartyDataAddressPostCode);      
     await this.page
-      .locator('#otherParties_0_confidentialityRequired_No')
+      .locator(
+        confidentialityRequired ===
+          addUpdateOtherPartyData.updateOtherPartyDataConfidentialityYes
+          ? '#otherParties_0_confidentialityRequired_Yes'
+          : '#otherParties_0_confidentialityRequired_No'
+      )
       .click();
     await this.page
       .locator('#otherParties_0_unacceptableCustomerBehaviour_No')
@@ -63,6 +82,89 @@ export class updateOtherPartyDataPage {
           .locator('#otherParties_0_address_inMainlandUk_Yes')
           .click(); //ToDo : Change to dynamic value
       }
+    await webAction.clickButton('Submit');
+  }
+
+  async applyChildSupportConfidentialOtherPartyData() {
+    await this.page.getByRole('button', { name: 'Add new' }).click();
+    await this.page.getByText('First Name');
+    await this.page
+      .locator('#otherParties_0_name_firstName')
+      .fill(addUpdateOtherPartyData.updateOtherPartyDataFirstName);
+    await this.page
+      .locator('#otherParties_0_name_lastName')
+      .fill(addUpdateOtherPartyData.updateOtherPartyDataLastName);
+    await this.page
+      .locator('#otherParties_0_address_line1')
+      .fill(addUpdateOtherPartyData.updateOtherPartyDataAddressLine);
+    await this.page
+      .locator('#otherParties_0_address_town')
+      .fill(addUpdateOtherPartyData.updateOtherPartyDataAddressTown);
+    await this.page
+      .locator('#otherParties_0_address_postcode')
+      .fill(addUpdateOtherPartyData.updateOtherPartyDataAddressPostCode);
+    await this.page.locator('#otherParties_0_confidentialityRequired_Yes').click();
+    await this.page
+      .locator('#otherParties_0_unacceptableCustomerBehaviour_No')
+      .click();
+    await this.page.locator('#otherParties_0_role_name').selectOption({ label: 'Paying parent' });
+    await this.page
+      .getByRole('group', { name: /Wants To Attend/i })
+      .getByLabel('Yes')
+      .check();
+    await this.page
+      .getByRole('group', { name: /Wants Support/i })
+      .getByLabel('No')
+      .check();
+    await this.page
+      .getByRole('group', { name: /Unavailable dates/i })
+      .getByLabel('No')
+      .check();
+    await this.page
+      .getByRole('group', { name: /Agree less notice/i })
+      .getByLabel('Yes')
+      .check();
+
+    await webAction.clickButton('Submit');
+  }
+
+  async applyChildSupportFtaAddOtherPartyData() {
+    await this.applyChildSupportFtaAddOtherPartyDataWithValues();
+  }
+
+  async applyChildSupportFtaAddOtherPartyDataWithValues(values = {
+    title: addUpdateOtherPartyData.updateOtherPartyDataTitle,
+    firstName: addUpdateOtherPartyData.updateOtherPartyDataFirstName,
+    lastName: addUpdateOtherPartyData.updateOtherPartyDataLastName,
+    addressLine1: addUpdateOtherPartyData.updateOtherPartyDataAddressLine,
+    town: addUpdateOtherPartyData.updateOtherPartyDataAddressTown,
+    postcode: addUpdateOtherPartyData.updateOtherPartyDataAddressPostCode
+  }) {
+    await this.page.getByRole('button', { name: 'Add new' }).click();
+    await this.page.locator('#otherParties_0_name_title').fill(values.title);
+    await this.page
+      .locator('#otherParties_0_name_firstName')
+      .fill(values.firstName);
+    await this.page
+      .locator('#otherParties_0_name_lastName')
+      .fill(values.lastName);
+    await this.page
+      .locator('#otherParties_0_address_line1')
+      .fill(values.addressLine1);
+    await this.page
+      .locator('#otherParties_0_address_town')
+      .fill(values.town);
+    await this.page
+      .locator('#otherParties_0_address_postcode')
+      .fill(values.postcode);
+    await this.page
+      .locator('#otherParties_0_unacceptableCustomerBehaviour_No')
+      .click();
+    await this.page
+      .locator('#otherParties_0_role_name')
+      .selectOption({ label: addUpdateOtherPartyData.updateOtherPartyDataDropdownValue });
+    await this.page.locator('#awareOfAnyAdditionalOtherParties_No').click();
+
     await webAction.clickButton('Submit');
   }
 
