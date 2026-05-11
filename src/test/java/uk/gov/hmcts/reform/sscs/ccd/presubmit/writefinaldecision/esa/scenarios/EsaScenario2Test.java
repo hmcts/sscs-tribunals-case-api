2ba0e49b10d15c88f4c95641df7c04dddcf69d75
Ali Dogan <alibaseit@users.noreply.github.com>
2026-05-11 15:20:34 +0100

    SSCSFT-88 Other party details not included in the body of the notice (#5083)
    * SSCSFT-88 Update decision notices
    
    * SSCSFT-88 Update decision notices
    
    * SSCSFT-88 Update final decision sentences
    
    * SSCSFT-88 Include other parties in the body of Notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * SSCSFT-88 Add other part details to body of the notice
    
    * Trigger Build
    
    * Trigger Build
    
    * Trigger Build
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Resolve conflict
    
    * SSCSFT-88 Update expected text not to have a changing date
    
    * cm flag on
    
    * turn off CM flag
    
    * SSCSFT-88 Update service to have latest param added
    
    * SSCSFT-88 Enable CM Flag for preview env
    
    * SSCSFT-88 Address comments
    
    * SSCSFT-88 Disable CM Flag, the change is not behind FF
    
    * SSCSFT-88 Roll back the change proposed by comment
    
    * SSCSFT-88 Roll back the change proposed by comment
    
    * toggle on
    
    * toggle off
    
    * add DV marker test fix
    
    ---------
    
    Co-authored-by: Elize Reyes <107991381+elizereyes123@users.noreply.github.com>
    Co-authored-by: Nitin Prabhu <142304341+nitinprabhuhmcts@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/esa/scenarios/EsaScenario2Test.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/writefinaldecision/esa/scenarios/EsaScenario2Test.java
@@ -1,16 +1,17 @@
 package uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.esa.scenarios;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Arrays;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.sscs.ccd.presubmit.writefinaldecision.esa.EsaTemplateContent;
 import uk.gov.hmcts.reform.sscs.model.docassembly.WriteFinalDecisionTemplateBody;
 
 
-public class EsaScenario2Test {
+class EsaScenario2Test {
 
     @Test
-    public void testScenario2() {
+    void testScenario2() {
         WriteFinalDecisionTemplateBody body =
                 WriteFinalDecisionTemplateBody.builder()
                         .hearingType("faceToFace")
@@ -28,47 +29,31 @@ public class EsaScenario2Test {
                         .anythingElse("Something else").build();
 
         EsaTemplateContent content = EsaScenario.SCENARIO_2.getContent(body);
-        /*
-        String expectedContent = "The appeal is refused.\n"
-                + "\n"
-                + "The decision made by the Secretary of State on 20/09/2020 is confirmed.\n"
-                + "\n"
-                + "Felix Sydney continues to have limited capability for work but does not have limited capability for work-related activity. This is because no descriptor from Schedule 3 of the Employment and Support Allowance (ESA) Regulations 2008 applied. Regulation 35 did not apply. The Secretary of State has accepted that Felix Sydney has limited capability for work. This was not in issue.\n"
-                + "\n"
-                + "My first reasons\n"
-                + "\n"
-                + "My second reasons\n"
-                + "\n"
-                + "Something else\n"
-                + "\n"
-                + "This has been an oral (face to face) hearing. Felix Sydney attended the hearing today and the Tribunal considered the appeal bundle to page A1. A Presenting Officer attended on behalf of the Respondent.\n"
-                + "\n";
 
-         */
+        String expectedContent = """
+            The appeal is refused.
 
+            The decision made by the Secretary of State on 20/09/2020 is confirmed.
 
-        String expectedContent = "The appeal is refused.\n"
-            + "\n"
-            + "The decision made by the Secretary of State on 20/09/2020 is confirmed.\n"
-            + "\n"
-            + "Felix Sydney continues to have limited capability for work but does not have limited capability for work-related activity and cannot be treated as having limited capability for work-related activity.\n"
-            + "\n"
-            + "This is because no descriptor from Schedule 3 of the Employment and Support Allowance (ESA) Regulations 2008 applied. Regulation 35 did not apply.\n"
-            + "\n"
-            + "The Secretary of State has accepted that Felix Sydney has limited capability for work. This was not in issue.\n"
-            + "\n"
-            + "My first reasons\n"
-            + "\n"
-            + "My second reasons\n"
-            + "\n"
-            + "Something else\n"
-            + "\n"
-            + "This has been an oral (face to face) hearing. Felix Sydney the appellant attended the hearing today and the Tribunal considered the appeal bundle to page A1. First Tier Agency representative attended on behalf of the Respondent.\n"
-            + "\n";
+            Felix Sydney continues to have limited capability for work but does not have limited capability for work-related activity and cannot be treated as having limited capability for work-related activity.
 
-        Assert.assertEquals(9, content.getComponents().size());
+            This is because no descriptor from Schedule 3 of the Employment and Support Allowance (ESA) Regulations 2008 applied. Regulation 35 did not apply.
 
-        Assert.assertEquals(expectedContent, content.toString());
+            The Secretary of State has accepted that Felix Sydney has limited capability for work. This was not in issue.
+
+            My first reasons
+
+            My second reasons
+
+            Something else
+
+            This has been an oral (face to face) hearing. The following people attended: Felix Sydney the appellant and a representative from the First Tier Agency. The Tribunal considered the appeal bundle to page A1.
+
+            """;
+
+        assertEquals(9, content.getComponents().size());
+
+        assertEquals(expectedContent, content.toString());
 
     }
 }
