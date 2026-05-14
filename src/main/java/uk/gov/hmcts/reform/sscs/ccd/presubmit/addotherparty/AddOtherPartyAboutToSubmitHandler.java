7be1de2de8e4dd072f3707eaec9567c91db65225
Stuart Wilson <stuart.wilson@hmtcs.net>
2026-05-14 10:42:18 +0100

    SSCSCI-2534 UC Removal of FTA state and response due date

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/addotherparty/AddOtherPartyAboutToSubmitHandler.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.addotherparty;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.Benefit.UC;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
+import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
+import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.sscs.ccd.domain.EventType;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
+
+@Service
+@Slf4j
+class AddOtherPartyAboutToSubmitHandler implements PreSubmitCallbackHandler<SscsCaseData> {
+
+    private final boolean cmOtherPartyConfidentialityEnabled;
+
+    public AddOtherPartyAboutToSubmitHandler(
+        @Value("${feature.cm-other-party-confidentiality.enabled}") final boolean cmOtherPartyConfidentialityEnabled) {
+        this.cmOtherPartyConfidentialityEnabled = cmOtherPartyConfidentialityEnabled;
+    }
+
+    @Override
+    public boolean canHandle(CallbackType callbackType, Callback<SscsCaseData> callback) {
+        requireNonNull(callbackType, "callbackType must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return cmOtherPartyConfidentialityEnabled
+            && callbackType == CallbackType.ABOUT_TO_SUBMIT
+            && callback.getEvent() == EventType.ADD_OTHER_PARTY_DATA
+            && callback.getCaseDetails().getCaseData().isBenefitType(UC);
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<SscsCaseData> handle(CallbackType callbackType, Callback<SscsCaseData> callback,
+        String userAuthorisation) {
+        if (!canHandle(callbackType, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        SscsCaseData caseData = callback.getCaseDetails().getCaseData();
+        caseData.setDwpDueDate(null);
+        caseData.setHmctsDwpState(null);
+
+        return new PreSubmitCallbackResponse<>(caseData);
+    }
+}
