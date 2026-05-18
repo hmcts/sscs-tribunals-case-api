b23bc738770e7dea8f72e25ffc2b5e721765f1ce
nitinprabhuhmcts <nitin.prabhu2@hmcts.net>
2026-05-18 15:07:55 +0100

    Remove disabling of autoscaling for role assignment

--- bin/setup-role-assignments.sh
+++ bin/setup-role-assignments.sh
@@ -30,7 +30,8 @@ function send_curl_request() {
   -H 'Content-Type: application/json' \
   -H "Authorization: Bearer ${IDAM_TOKEN}" \
   -H "ServiceAuthorization: ${S2S_TOKEN}" \
-  -d "${payload}"
+  -d "${payload}" \
+  -v --http1.1
 }
 
 send_curl_request "${BASEDIR}/staff-idam-ids.json" "CASEWORKER"
