69f37f64c92d40bc5e447d79ec74f270ad6c61eb
gokul-sol <gokul.sridharan@HMCTS.NET>
2026-05-21 17:24:17 +0100

    Add DV marker field step for UC case

--- definitions/test/e2e/update-other-party-data.spec.ts
+++ definitions/test/e2e/update-other-party-data.spec.ts
@@ -1,15 +1,31 @@
 import { test } from '../lib/steps.factory';
+import { createChildSupportCaseForCmConfidentiality, createUCCaseForConfidentiality } from '../api/client/sscs/factory/appeal.type.factory';
 
 let caseId: string;
 
-test(
-  'Adding Other party data + subscription in a ChildSupport case',
+const benefitTypes = [
+  {
+    label: 'Child Support',
+    createCase: createChildSupportCaseForCmConfidentiality
+  },
+  {
+    label: 'Universal Credit',
+    createCase: createUCCaseForConfidentiality
+  }
+]
+
+for (const benefit of benefitTypes) {
+  test(
+  `Adding Other party data + subscription in a ${benefit.label} case`,
   { tag: '@nightly-pipeline' },
   async ({ updateOtherPartyDataSteps }) => {
     test.slow();
-    await updateOtherPartyDataSteps.performUpdateOtherPartyData(caseId);
+    const caseId = await benefit.createCase();
+    await updateOtherPartyDataSteps.performUpdateOtherPartyData(caseId, benefit.label);
   }
 );
+}
+
 
 test(
   'Adding Other party data + subscription in a TaxCredit case',
