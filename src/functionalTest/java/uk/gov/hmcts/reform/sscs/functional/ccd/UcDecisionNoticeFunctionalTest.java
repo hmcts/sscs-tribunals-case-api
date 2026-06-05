6cd20f84aa2008296fb4dcb68f39f06e97e79d5e
Basit Siddiqui <Basit.Siddiqui@HMCTS.NET>
2026-06-05 11:29:23 +0100

    SSCSCI-2559 Update scenario13 functional test attendance assertions

--- src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/ccd/UcDecisionNoticeFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sscs/functional/ccd/UcDecisionNoticeFunctionalTest.java
@@ -295,7 +295,7 @@ public class UcDecisionNoticeFunctionalTest extends BaseFunctionTest {
             assertThat(pdfTextWithoutNewLines, containsString("4. Reasons for decision 1"));
             assertThat(pdfTextWithoutNewLines, containsString("5. Reasons for decision 2"));
             assertThat(pdfTextWithoutNewLines, containsString("6. Anything else"));
-            assertThat(pdfTextWithoutNewLines, containsString("7. This has been a remote hearing in the form of a telephone hearing. Joe Bloggs the appellant attended and the Tribunal considered the appeal bundle to page B7. First Tier Agency representative attended on behalf of the Respondent."));
+            assertThat(pdfTextWithoutNewLines, containsString("7. This has been a remote hearing in the form of a telephone hearing. The following people attended: Joe Bloggs the appellant and a representative from the First Tier Agency. The Tribunal considered the appeal bundle to page B7."));
             assertThat(pdfTextWithoutNewLines, not(containsString("8.")));
         }
     }
@@ -315,7 +315,7 @@ public class UcDecisionNoticeFunctionalTest extends BaseFunctionTest {
             assertThat(pdfTextWithoutNewLines, containsString("4. Reasons for decision 1"));
             assertThat(pdfTextWithoutNewLines, containsString("5. Reasons for decision 2"));
             assertThat(pdfTextWithoutNewLines, containsString("6. Anything else"));
-            assertThat(pdfTextWithoutNewLines, containsString("7. This has been a remote hearing in the form of a telephone hearing. Joe Bloggs the appellant attended and the Tribunal considered the appeal bundle to page B7. First Tier Agency representative attended on behalf of the Respondent."));
+            assertThat(pdfTextWithoutNewLines, containsString("7. This has been a remote hearing in the form of a telephone hearing. The following people attended: Joe Bloggs the appellant and a representative from the First Tier Agency. The Tribunal considered the appeal bundle to page B7."));
             assertThat(pdfTextWithoutNewLines, not(containsString("8.")));
         }
     }
