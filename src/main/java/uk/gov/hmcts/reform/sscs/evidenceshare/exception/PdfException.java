c78cd4f613f2d3d2cd868e66c942ad04d376c2ad
Stuart Wilson <stuart.wilson@hmtcs.net>
2026-06-18 11:39:49 +0100

    SSCSCI-2560 Issue generic letter event <=10 send via Gov.Notify, more than 10 pages send via Bulk print

--- src/main/java/uk/gov/hmcts/reform/sscs/evidenceshare/exception/PdfException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.sscs.evidenceshare.exception;
-
-public class PdfException extends Exception {
-
-    public PdfException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
