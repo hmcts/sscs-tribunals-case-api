8bbf9a12840e45640dda9f5325c22830961b6ace
nitinprabhuhmcts <nitin.prabhu2@hmcts.net>
2026-08-11 16:00:43 +0100

    Removed lauch darkly feature toggle

--- src/main/java/uk/gov/hmcts/reform/sscs/domain/email/RoboticsEmailTemplate.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/domain/email/RoboticsEmailTemplate.java
@@ -1,15 +1,11 @@
 package uk.gov.hmcts.reform.sscs.domain.email;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.sscs.evidenceshare.service.FeatureToggleService;
 
 @Component
 public class RoboticsEmailTemplate {
-    private final FeatureToggleService featureToggleService;
-    private final String from;
     private final String fromSendGrid;
     private final String to;
     private final String message;
@@ -17,16 +13,12 @@ public class RoboticsEmailTemplate {
     private final String pipAeTo;
 
     public RoboticsEmailTemplate(
-        @Autowired FeatureToggleService featureToggleService,
-        @Value("${robotics.email.from}") String from,
         @Value("${robotics.email.from-send-grid}") String fromSendGrid,
         @Value("${robotics.email.to}") String to,
         @Value("${robotics.email.scottishTo}") String scottishTo,
         @Value("${robotics.email.pipAeTo}") String pipAeTo,
         @Value("${robotics.email.message}") String message
     ) {
-        this.featureToggleService = featureToggleService;
-        this.from = from;
         this.fromSendGrid = fromSendGrid;
         this.to = to;
         this.scottishTo = scottishTo;
@@ -39,7 +31,7 @@ public class RoboticsEmailTemplate {
                                boolean isScottish,
                                boolean isPipAeTo) {
         return new Email(
-            featureToggleService.isSendGridEnabled() ? fromSendGrid : from,
+            fromSendGrid,
             isScottish ? scottishTo : isPipAeTo ? pipAeTo : to,
             subject,
             message,
--- src/main/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/EmailSenderProvider.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/evidenceshare/service/EmailSenderProvider.java
@@ -10,19 +10,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class EmailSenderProvider {
 
-    private final FeatureToggleService featureToggleService;
-
     @Autowired
     @Qualifier("sendGridMailSender")
     private final JavaMailSender sendGridMailSender;
 
-    @Autowired
-    @Qualifier("mtaMailSender")
-    private final JavaMailSender mtaMailSender;
-
-
     public JavaMailSender getMailSender() {
-        return featureToggleService.isSendGridEnabled() ? sendGridMailSender : mtaMailSender;
+        return sendGridMailSender;
     }
 
 }
