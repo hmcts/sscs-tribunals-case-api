549d8d30109a7233a506cab7ced1fe6ebbbde8fc
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2026-08-24 11:17:41 +0100

    Moving classes again

--- src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adminamendlocationdetails/AdminAmendLocationDetailsAboutToStart.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adminamendlocationdetails/AdminAmendLocationDetailsAboutToStart.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.sscs.ccd.presubmit;
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.adminamendlocationdetails;
 
 import static java.util.Objects.requireNonNull;
 
@@ -13,6 +13,7 @@ import uk.gov.hmcts.reform.sscs.ccd.callback.Callback;
 import uk.gov.hmcts.reform.sscs.ccd.callback.CallbackType;
 import uk.gov.hmcts.reform.sscs.ccd.callback.PreSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.presubmit.PreSubmitCallbackHandler;
 import uk.gov.hmcts.reform.sscs.service.RegionalProcessingCenterService;
 import uk.gov.hmcts.reform.sscs.service.VenueService;
 
--- src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adminamendlocationdetails/AdminAmendLocationDetailsAboutToStartTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/ccd/presubmit/adminamendlocationdetails/AdminAmendLocationDetailsAboutToStartTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.sscs.ccd.presubmit;
+package uk.gov.hmcts.reform.sscs.ccd.presubmit.adminamendlocationdetails;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
