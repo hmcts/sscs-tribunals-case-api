5ebc8ac36c5b82be245de3dab6f814dbe689f4d7
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2026-09-18 16:58:33 +0100

    Migrate away from deprecated IDAM endpoints

--- definitions/test/api/client/idam/idam.service.ts
+++ definitions/test/api/client/idam/idam.service.ts
@@ -105,7 +105,7 @@ export async function getSSCSServiceToken() {
 }
 
 export async function getIDAMUserID(idamToken) {
-  const idamDetailsPath = '/details';
+  const idamDetailsPath = '/o/userinfo';
 
   let apiContext = await request.newContext({
     // All requests we send go to this API Endpoint.
